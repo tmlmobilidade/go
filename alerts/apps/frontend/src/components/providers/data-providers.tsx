2430dadb7155deb173de980c4cdd394ae59124c2
Josue Monteiro <jusi.monteiro@protonmail.com>
2025-03-06 17:36:04 +0000

    AppWrapper rework

--- alerts/apps/frontend/src/components/providers/data-providers.tsx
+++ alerts/apps/frontend/src/components/providers/data-providers.tsx
@@ -4,18 +4,21 @@
 
 import { LinesContextProvider } from '@/contexts/Lines.context';
 import { LocationsContextProvider } from '@/contexts/Locations.context';
+import { MeContextProvider } from '@/contexts/Me.context';
 import { StopsContextProvider } from '@/contexts/Stops.context';
 
 /* * */
 
 export function DataProviders({ children }: { children: React.ReactNode }) {
 	return (
-		<LocationsContextProvider>
-			<StopsContextProvider>
-				<LinesContextProvider>
-					{children}
-				</LinesContextProvider>
-			</StopsContextProvider>
-		</LocationsContextProvider>
+		<MeContextProvider>
+			<LocationsContextProvider>
+				<StopsContextProvider>
+					<LinesContextProvider>
+						{children}
+					</LinesContextProvider>
+				</StopsContextProvider>
+			</LocationsContextProvider>
+		</MeContextProvider>
 	);
 }
---
+++ alerts/apps/frontend/src/contexts/Me.context.tsx
@@ -0,0 +1,96 @@
+'use client';
+
+import { swrFetcher } from '@/lib/http';
+import { Routes } from '@/lib/routes';
+import { User } from '@tmlmobilidade/core-types';
+import { AppWrapper, CMIcon, SidebarItemProps } from '@tmlmobilidade/ui';
+import { createContext, useContext, useEffect, useMemo, useState } from 'react';
+import useSWR from 'swr';
+
+interface MeContextState {
+	data: {
+		sidebar: SidebarItemProps[]
+		user: User
+	}
+	flags: {
+		error: null | string
+		loading: boolean
+	}
+}
+
+const MeContext = createContext<MeContextState | undefined>(undefined);
+
+export function useMeContext() {
+	const context = useContext(MeContext);
+	if (!context) {
+		throw new Error('useMeContext must be used within a MeContextProvider');
+	}
+	return context;
+}
+
+export const MeContextProvider = ({
+	children,
+	initialSidebar,
+	initialUser,
+}: {
+	children: React.ReactNode
+	initialSidebar?: MeContextState['data']['sidebar']
+	initialUser?: MeContextState['data']['user']
+}) => {
+	//
+	// A. Setup variables
+	const [userState, setUserState] = useState<MeContextState['data']['user']>(
+		initialUser ?? ({} as User),
+	);
+	const [sidebarState, setSidebarState] = useState<
+		MeContextState['data']['sidebar']
+	>(initialSidebar ?? []);
+
+	const { data, error, isLoading } = useSWR<{ sidebar: SidebarItemProps[], user: User }>(
+		Routes.AUTH_API + Routes.ME,
+		swrFetcher,
+	);
+
+	// Update user
+	useEffect(() => {
+		if (data?.user) setUserState(data.user);
+		if (data?.sidebar) setSidebarState(data.sidebar);
+	}, [data]);
+
+	//
+	// E. Define context value
+	const contextValue: MeContextState = useMemo(
+		() => ({
+			data: {
+				sidebar: sidebarState,
+				user: userState,
+			},
+			flags: {
+				error: error,
+				loading: isLoading,
+			},
+		}),
+		[userState, isLoading, error, sidebarState],
+	);
+
+	if (contextValue.flags.loading) {
+		return <div>loading...</div>;
+	}
+
+	//
+	// F. Render components
+	return (
+		<MeContext.Provider value={contextValue}>
+
+			<AppWrapper
+				icon={<CMIcon />}
+				sidebarItems={sidebarState}
+				headerProps={{
+					user_name: userState?.first_name,
+				}}
+			>
+				{children}
+			</AppWrapper>
+		</MeContext.Provider>
+	);
+};
