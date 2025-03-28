6b21f209aa75c67fc97d0f4045138a432785c95a
Samuel Santos <samuel.trabalho.96@gmail.com>
2025-03-28 22:59:52 +0000

    Update Shelter Component

--- stops/apps/frontend/src/components/Shelter/index.tsx
+++ stops/apps/frontend/src/components/Shelter/index.tsx
@@ -1,5 +1,43 @@
 export default function Shelter() {
     return <div>
-        Shelter
+        {/* Header */}
+        <div>
+            <div>Abrigo</div>
+            <div>Informações relacionadas com o abrigo.</div>
+        </div>
+
+        <div>
+            {/*  */}
+            <div>
+                <div>Existe Abrigo?</div>
+                <input type="text" />
+            </div>
+
+            {/*  */}
+            <div>
+                <div>Código do Abrigo</div>
+                <input type="text" />
+            </div>
+
+            {/*  */}
+            <div>
+                <div>Entidade Gestora do Abrigo</div>
+                <input type="text" />
+            </div>
+        </div>
+
+        <div>
+            {/*  */}
+            <div>
+                <div>Última verificação do estado do abrigo</div>
+                <input type="text" />
+            </div>
+
+            {/*  */}
+            <div>
+                <div>Data de Instalação do Abrigo</div>
+                <input type="text" />
+            </div>
+        </div>
     </div>;
 }
