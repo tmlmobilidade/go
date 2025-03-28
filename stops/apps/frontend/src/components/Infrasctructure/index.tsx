b3de487d0b4fb30ac177795a80b1b0ec60d5b791
Samuel Santos <samuel.trabalho.96@gmail.com>
2025-03-28 23:11:00 +0000

    Update Infrasctruture Component

--- stops/apps/frontend/src/components/Infrasctructure/index.tsx
+++ stops/apps/frontend/src/components/Infrasctructure/index.tsx
@@ -1,5 +1,72 @@
 export default function Infrasctructure() {
     return <div>
-        Infrasctructure
+        {/* Header */}
+        <div>
+            <div>Infraestrutura</div>
+            <div>Informações relacionadas com os equipamentos da paragem e envolvente.</div>
+        </div>
+
+        {/*  */}
+        <div>
+            <div>
+                <div>Existe Poste?</div>
+                <input type="text" />
+            </div>
+
+            <div>
+                <div>Existe Cobertura?</div>
+                <input type="text" />
+            </div>
+        </div>
+
+        {/*  */}
+        <div>
+            <div>
+                <div>Existe Mupi?</div>
+                <input type="text" />
+            </div>
+
+            <div>
+                <div>Existe Banco?</div>
+                <input type="text" />
+            </div>
+
+            <div>
+                <div>Existe Papeleira?</div>
+                <input type="text" />
+            </div>
+        </div>
+
+        {/*  */}
+        <div>
+            <div>
+                <div>Existe Iluminação?</div>
+                <input type="text" />
+            </div>
+
+            <div>
+                <div>Existe Ligação Elétrica?</div>
+                <input type="text" />
+            </div>
+        </div>
+
+        {/*  */}
+        <div>
+            <div>Tipo de Relação com a Via</div>
+            <input type="text" />
+        </div>
+
+        {/*  */}
+        <div>
+            <div>
+                <div>Última Manutenção da Infraestrutura</div>
+                <input type="text" />
+            </div>
+
+            <div>
+                <div>Última Verificação da Infraestrutura</div>
+                <input type="text" />
+            </div>
+        </div>
     </div>;
 }
