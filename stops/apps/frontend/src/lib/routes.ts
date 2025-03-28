f39d003a9d103d89ceb31eaeb568fbe8ad3044d4
Samuel Santos <samuel.trabalho.96@gmail.com>
2025-03-28 17:47:21 +0000

    Update Front-End Routes

--- stops/apps/frontend/src/lib/routes.ts
+++ stops/apps/frontend/src/lib/routes.ts
@@ -1,8 +1,8 @@
 export const PageRoutes = Object.freeze({
 	HOME: '/',
 
-	// Alerts
-	// Example: ALERT_DETAIL: (id: string) => `/alerts/${id}`,
+	// Stops
+	// Example: STOP_DETAIL: (id: string) => `/stops/${id}`,
 });
 
 export const ApiRoutes = Object.freeze({
@@ -11,13 +11,13 @@ export const ApiRoutes = Object.freeze({
 	CMET_API: process.env.NEXT_PUBLIC_CMET_API_URL ?? 'https://api.carrismetropolitana.pt/v2',
 
 	// Page routes
-	// Example: ALERT_DETAIL_API: (id: string) => `/api/alerts/${id}`,
+	// Example: STOP_DETAIL_API: (id: string) => `/api/stops/${id}`,
 });
 
 export const Routes = Object.freeze({
 	// Common
 	AUTH_URL: process.env.NEXT_PUBLIC_AUTH_URL ?? 'https://auth.sae.carrismetropolitana.pt',
-	URL: process.env.NEXT_PUBLIC_URL ?? 'https://alerts.sae.carrismetropolitana.pt',
+	URL: process.env.NEXT_PUBLIC_URL ?? 'https://stops.sae.carrismetropolitana.pt',
 
 	// Other
 	...PageRoutes,
