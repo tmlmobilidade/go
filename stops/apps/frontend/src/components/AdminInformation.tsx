a2e3b8bad650a10a13e488529f63eca2108bb6e9
Samuel Santos <samuel.trabalho.96@gmail.com>
2025-03-28 22:30:37 +0000

    Update AdminInformation Component

--- stops/apps/frontend/src/components/AdminInformation.tsx
+++ stops/apps/frontend/src/components/AdminInformation.tsx
@@ -1,5 +1,32 @@
 export default function AdminInformation() {
     return <div>
-        AdminInformation
+        {/* Header */}
+        <div>
+            <div>Informação Administrativa</div>
+            <div>Informações sobre a localização administrativa e responsabilidade de gestão desta paragem</div>
+        </div>
+
+        <div>
+            <div>
+                <div>Município</div>
+                <input type="text" />
+            </div>
+
+            <div>
+                <div>Freguesia</div>
+                <input type="text" />
+            </div>
+
+            <div>
+                <div>Localidade</div>
+                <input type="text" />
+            </div>
+        </div>
+
+        {/* jurisdiction */}
+        <div>
+            <div>Jurisdição</div>
+            <input type="text" />
+        </div>
     </div>;
 }
