353d7d70b983d3c0d55e8cf3bf8a4c3ae6ab38ff
jusimen <jusi.monteiro@protonmail.com>
2025-03-28 16:20:58 +0000

    Update package version to 1.0.1 in package.json and configure environment variables for production URLs in next.config.ts to enhance deployment consistency.

--- auth/apps/frontend/next.config.ts
+++ auth/apps/frontend/next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
 	/* config options here */
+	env: {
+		NEXT_PUBLIC_AUTH_URL: process.env.NODE_ENV === 'development' ? process.env.NEXT_PUBLIC_AUTH_URL : 'https://auth.sae.carrismetropolitana.pt',
+		NEXT_PUBLIC_URL: process.env.NODE_ENV === 'development' ? process.env.NEXT_PUBLIC_URL : 'https://auth.sae.carrismetropolitana.pt',
+	},
 	experimental: {
 		optimizePackageImports: ['@tmlmobilidade/ui'],
 	},
