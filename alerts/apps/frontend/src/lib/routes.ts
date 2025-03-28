0e107485ba14351ec3e11d7d626e375cd5a4a91c
jusimen <jusi.monteiro@protonmail.com>
2025-03-28 11:10:55 +0000

    Add ALERTS_API route to ApiRoutes in routes.ts

--- alerts/apps/frontend/src/lib/routes.ts
+++ alerts/apps/frontend/src/lib/routes.ts
@@ -6,6 +6,7 @@ export const PageRoutes = Object.freeze({
 });
 
 export const ApiRoutes = Object.freeze({
+	ALERTS_API: '/api',
 	AUTH_API: process.env.NEXT_PUBLIC_AUTH_URL ?? 'https://auth.sae.carrismetropolitana.pt',
 	CMET_API: process.env.NEXT_PUBLIC_CMET_API_URL ?? 'https://api.carrismetropolitana.pt/v2',
 });
