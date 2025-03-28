97a248ada79c83014721f812d5c5d5c258beeb75
jusimen <jusi.monteiro@protonmail.com>
2025-03-28 13:59:15 +0000

    Refactor Dockerfile to remove ARG definitions for public URLs and set them in next.config.ts for environment management

--- alerts/apps/frontend/next.config.ts
+++ alerts/apps/frontend/next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from 'next';
 import { Routes } from '@/lib/routes';
 
 const nextConfig: NextConfig = {
+	env: {
+		NEXT_PUBLIC_AUTH_URL: process.env.NEXT_PUBLIC_AUTH_URL,
+		NEXT_PUBLIC_URL: process.env.NEXT_PUBLIC_URL,
+	},
 	images: {
 		remotePatterns: [
 			{
