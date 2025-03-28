5d9b5d477fc17b5322489de6f71d4a4f67cc09ce
Samuel Santos <samuel.trabalho.96@gmail.com>
2025-03-28 17:37:51 +0000

    Setup Stops App

---
+++ stops/apps/frontend/src/app/layout.tsx
@@ -0,0 +1,37 @@
+/* * */
+
+import '@tmlmobilidade/ui/styles';
+import { Routes } from '@/lib/routes';
+import { AppProvider, AppWrapper } from '@tmlmobilidade/ui';
+import { cookies as nextCookies } from 'next/headers';
+import { redirect, RedirectType } from 'next/navigation';
+
+/* * */
+
+export default async function Layout({
+	children,
+}: {
+	children: React.ReactNode
+}) {
+	const cookies = await nextCookies();
+	const sessionToken = cookies.get('session_token')?.value;
+
+	if (!sessionToken) {
+		redirect(
+			`${Routes.AUTH_URL}/login?redirect=${encodeURI(Routes.URL)}`,
+			RedirectType.replace,
+		);
+	}
+
+	return (
+		<html lang="en" suppressHydrationWarning>
+			<body>
+				<AppProvider>
+					<AppWrapper>
+						{children}
+					</AppWrapper>
+				</AppProvider>
+			</body>
+		</html>
+	);
+}
