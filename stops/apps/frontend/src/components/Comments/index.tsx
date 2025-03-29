76f2cfec8fd88b4fc91dd586380dc70b101bd1a6
Samuel Santos <samuel.trabalho.96@gmail.com>
2025-03-29 11:45:56 +0000

    Update Comments Component

--- stops/apps/frontend/src/components/Comments/index.tsx
+++ stops/apps/frontend/src/components/Comments/index.tsx
@@ -1,5 +1,12 @@
 export default function Comments() {
     return <div>
-        Comments
+        <div>
+            <div>Notas e Comentários</div>
+            <div>Texto livre para informações adicionais.</div>
+        </div>
+
+        <div>
+            <input type="text" placeholder="Construção planeada a..." />
+        </div>
     </div>;
 }
