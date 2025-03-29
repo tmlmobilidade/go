3af43298e412e5bd376315e13bcc9d7fa8ec97d2
Samuel Santos <samuel.trabalho.96@gmail.com>
2025-03-29 11:41:23 +0000

    Update Connections Component

--- stops/apps/frontend/src/components/Connections/index.tsx
+++ stops/apps/frontend/src/components/Connections/index.tsx
@@ -1,5 +1,54 @@
 export default function Connections() {
     return <div>
-        Connections
+        {/*  */}
+        <div>
+            <div>
+                <div>Ligações Intermodais</div>
+                <div>Quais são os outros modos de transporte, para além do autocarro, que esteja paragem serve.</div>
+            </div>
+        </div>
+
+        {/*  */}
+        <div>
+            <div>
+                <input type="checkbox" name="" id="" />
+                <div>Metro</div>
+            </div>
+            
+            <div>
+                <input type="checkbox" name="" id="" />
+                <div>Metro de Superfície</div>
+            </div>
+
+            <div>
+                <input type="checkbox" name="" id="" />
+                <div>Comboio</div>
+            </div>
+
+            <div>
+                <input type="checkbox" name="" id="" />
+                <div>Barco</div>
+            </div>
+
+            <div>
+                <input type="checkbox" name="" id="" />
+                <div>Aeroporto</div>
+            </div>
+
+            <div>
+                <input type="checkbox" name="" id="" />
+                <div>Partilha de Bicicletas</div>
+            </div>
+
+            <div>
+                <input type="checkbox" name="" id="" />
+                <div>Estacionamento de Bicicletas</div>
+            </div>
+
+            <div>
+                <input type="checkbox" name="" id="" />
+                <div>Estacionamento Automóvel</div>
+            </div>
+        </div>
     </div>;
 }
