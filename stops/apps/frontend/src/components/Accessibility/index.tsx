ef5f072117299bde83f329b8b471412417e9361f
Samuel Santos <samuel.trabalho.96@gmail.com>
2025-03-29 11:30:29 +0000

    Update PublicInformation Component

--- stops/apps/frontend/src/components/Accessibility/index.tsx
+++ stops/apps/frontend/src/components/Accessibility/index.tsx
@@ -1,5 +1,5 @@
 export default function Accessibility() {
     return <div>
-        Accessibility
+        
     </div>;
 }
--- stops/apps/frontend/src/components/PublicInformation/index.tsx
+++ stops/apps/frontend/src/components/PublicInformation/index.tsx
@@ -1,5 +1,105 @@
 export default function PublicInformation() {
     return <div>
-        PublicInformation
+        {/* Header */}
+        <div>
+            <div>Informação ao Público</div>
+            <div>Informações relacionadas com os suportes de informação ao público.</div>
+        </div>
+
+        {/*  */}
+        <div>
+            <div>
+                <div>Tem Postalete</div>
+                <input type="text" />
+            </div>
+
+            <div>
+                <div>Entidade Gestora do Postalete</div>
+                <input type="text" />
+            </div>
+        </div>
+
+        {/*  */}
+        <div>
+            <div>Tem Moldura?</div>
+            <input type="text" />
+        </div>
+
+        {/*  */}
+        <div>
+            <div>
+                <div>Tem PIP Áudio?</div>
+                <input type="text" />
+            </div>
+
+            <div>
+                <div>Código do PIP Áudio</div>
+                <input type="text" />
+            </div>
+        </div>
+
+        {/*  */}
+        <div>
+            <div>
+                <div>Tem PIP Realtime?</div>
+                <input type="text" />
+            </div>
+
+            <div>
+                <div>Código do PIP Realtime</div>
+                <input type="text" />
+            </div>
+        </div>
+
+        {/*  */}
+        <div>
+            <div>Tem Sinalização H2OA?</div>
+            <input type="text" />
+        </div>
+
+        {/*  */}
+        <div>
+            <div>
+                <div>Tem Horários?</div>
+                <input type="text" />
+            </div>
+
+            <div>
+                <div>Tem Horários Táteis?</div>
+                <input type="text" />
+            </div>
+        </div>
+
+        {/*  */}
+        <div>
+            <div>Tem Mapa da Rede?</div>
+            <input type="text" />
+        </div>
+
+        {/*  */}
+        <div>
+            <div>
+                <div>Última Manutenção dos Horários</div>
+                <input type="text" />
+            </div>
+
+            <div>
+                <div>Última Verificação dos Horários</div>
+                <input type="text" />
+            </div>
+        </div>
+
+        {/*  */}
+        <div>
+            <div>
+                <div>Última Manutenção do Postalete</div>
+                <input type="text" />
+            </div>
+
+            <div>
+                <div>Última Verificação dos Postalete</div>
+                <input type="text" />
+            </div>
+        </div>
     </div>;
 }
