6447a603967f2a41fcc11a5f40b19e34585f1085
Samuel Santos <samuel.trabalho.96@gmail.com>
2025-03-29 11:33:38 +0000

    Update Accessibility Component

--- stops/apps/frontend/src/components/Accessibility/index.tsx
+++ stops/apps/frontend/src/components/Accessibility/index.tsx
@@ -1,5 +1,66 @@
 export default function Accessibility() {
     return <div>
-        
+        {/*  */}
+        <div>
+            <div>
+                <div>Tem Passeio?</div>
+                <input type="text" />
+            </div>
+
+            <div>
+                <div>Tipo de Passeio</div>
+                <input type="text" />
+            </div>
+        </div>
+
+
+        {/*  */}
+        <div>
+            <div>
+                <div>Tem Passadeira?</div>
+                <input type="text" />
+            </div>
+
+            <div>
+                <div>Tem Acesso Rebaixado/Contínuo?</div>
+                <input type="text" />
+            </div>
+
+            <div>
+                <div>Tem Acesso Largo?</div>
+                <input type="text" />
+            </div>
+
+            <div>
+                <div>Tem Pavimento Tátil?</div>
+                <input type="text" />
+            </div>
+        </div>
+
+        {/*  */}
+        <div>
+            <div>
+                <div>Tem Estacionamento Abusivo?</div>
+                <input type="text" />
+            </div>
+
+            <div>
+                <div>Permite Embarque de PMR?</div>
+                <input type="text" />
+            </div>
+        </div>
+
+        {/*  */}
+        <div>
+            <div>
+                <div>Última Manutenção da Acessibilidade</div>
+                <input type="text" />
+            </div>
+
+            <div>
+                <div>Última Verificação da Acessibilidade</div>
+                <input type="text" />
+            </div>
+        </div>
     </div>;
 }
