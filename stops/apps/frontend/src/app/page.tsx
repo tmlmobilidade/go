fb0932a91e16c679c7a6034c0a3bac5c5d654d5b
Samuel Santos <samuel.trabalho.96@gmail.com>
2025-03-29 17:49:36 +0000

    Refactor Front-End

--- stops/apps/frontend/src/app/page.tsx
+++ stops/apps/frontend/src/app/page.tsx
@@ -1,14 +1,11 @@
 import Navigation from "@/components/Navigation";
-import Accessibility from "@/components/StopContainer/Accessibility";
+import Stop from "@/components/Stop";
 
 import styles from './styles.module.css';
 
 export default function Page() {
 	return <div className={styles.container}>
 		<Navigation stops={["A", "B", "C"]} />
-
-		<div>
-			<Accessibility />
-		</div>
+		<Stop />
 	</div>;
 }
