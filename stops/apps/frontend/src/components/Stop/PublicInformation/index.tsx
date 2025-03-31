f3e0f224be45eb1d6fa948c0c43135235379e237
Samuel Santos <samuel.trabalho.96@gmail.com>
2025-03-31 10:24:27 +0100

    Update PublicInformation Component

--- stops/apps/frontend/src/components/Stop/PublicInformation/index.tsx
+++ stops/apps/frontend/src/components/Stop/PublicInformation/index.tsx
@@ -1,105 +1,56 @@
+import Header from '@/components/common/Header';
+import Item from '@/components/common/Row/Item';
+import Row from '@/components/common/Row';
+
+import styles from '../styles.module.css';
+
 export default function PublicInformation() {
-    return <div>
-        {/* Header */}
-        <div>
-            <div>Informação ao Público</div>
-            <div>Informações relacionadas com os suportes de informação ao público.</div>
-        </div>
+    return <div className={styles.section}>
+        <Header
+            title={"Informação ao Público"}
+            description={"Informações relacionadas com os suportes de informação ao público."}
+        />
 
-        {/*  */}
-        <div>
-            <div>
-                <div>Tem Postalete</div>
-                <input type="text" />
-            </div>
+        <Row>
+            <Item label={"Tem Postalete"} type={"text"} value={"Sim"} />
+            <Item label={"Entidade Gestora do Postalete"} type={"text"} value={"Sim"} />
+        </Row>
 
-            <div>
-                <div>Entidade Gestora do Postalete</div>
-                <input type="text" />
-            </div>
-        </div>
+        <Row>
+            <Item label={"Tem Moldura?"} type={"text"} value={"Sim"} />
+        </Row>
 
-        {/*  */}
-        <div>
-            <div>Tem Moldura?</div>
-            <input type="text" />
-        </div>
+        <Row>
+            <Item label={"Tem PIP Áudio?"} type={"text"} value={"Sim"} />
+            <Item label={"Código do PIP Áudio"} type={"text"} value={"Sim"} />
+        </Row>
 
-        {/*  */}
-        <div>
-            <div>
-                <div>Tem PIP Áudio?</div>
-                <input type="text" />
-            </div>
+        <Row>
+            <Item label={"Tem PIP Realtime?"} type={"text"} value={"Sim"} />
+            <Item label={"Código do PIP Realtime"} type={"text"} value={"Sim"} />
+        </Row>
 
-            <div>
-                <div>Código do PIP Áudio</div>
-                <input type="text" />
-            </div>
-        </div>
+        <Row>
+            <Item label={"Tem Sinalização H2OA?"} type={"text"} value={"Sim"} />
+        </Row>
 
-        {/*  */}
-        <div>
-            <div>
-                <div>Tem PIP Realtime?</div>
-                <input type="text" />
-            </div>
+        <Row>
+            <Item label={"Tem Horários?"} type={"text"} value={"Sim"} />
+            <Item label={"Tem Horários Táteis?"} type={"text"} value={"Sim"} />
+        </Row>
 
-            <div>
-                <div>Código do PIP Realtime</div>
-                <input type="text" />
-            </div>
-        </div>
+        <Row>
+            <Item label={"Tem Mapa da Rede?"} type={"text"} value={"Sim"} />
+        </Row>
 
-        {/*  */}
-        <div>
-            <div>Tem Sinalização H2OA?</div>
-            <input type="text" />
-        </div>
+        <Row>
+            <Item label={"Última Manutenção dos Horários"} type={"text"} value={"Sim"} />
+            <Item label={"Última Verificação dos Horários"} type={"text"} value={"Sim"} />
+        </Row>
 
-        {/*  */}
-        <div>
-            <div>
-                <div>Tem Horários?</div>
-                <input type="text" />
-            </div>
-
-            <div>
-                <div>Tem Horários Táteis?</div>
-                <input type="text" />
-            </div>
-        </div>
-
-        {/*  */}
-        <div>
-            <div>Tem Mapa da Rede?</div>
-            <input type="text" />
-        </div>
-
-        {/*  */}
-        <div>
-            <div>
-                <div>Última Manutenção dos Horários</div>
-                <input type="text" />
-            </div>
-
-            <div>
-                <div>Última Verificação dos Horários</div>
-                <input type="text" />
-            </div>
-        </div>
-
-        {/*  */}
-        <div>
-            <div>
-                <div>Última Manutenção do Postalete</div>
-                <input type="text" />
-            </div>
-
-            <div>
-                <div>Última Verificação dos Postalete</div>
-                <input type="text" />
-            </div>
-        </div>
+        <Row>
+            <Item label={"Última Manutenção do Postalete"} type={"text"} value={"Sim"} />
+            <Item label={"Última Verificação dos Postalete"} type={"text"} value={"Sim"} />
+        </Row>
     </div>;
 }
