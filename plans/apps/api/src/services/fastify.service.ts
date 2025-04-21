99933e872e7d7b97e224d7741b54eb7b3abb775f
jusimen <jusi.monteiro@protonmail.com>
2025-04-21 11:16:47 +0100

    Add initial project structure with Docker configurations, cleanup script, and package management for API and frontend applications. Includes setup for Nginx and Let's Encrypt for SSL management.

---
+++ plans/apps/api/src/services/fastify.service.ts
@@ -0,0 +1,63 @@
+/* * */
+
+import fastify, {
+	FastifyInstance,
+	FastifyListenOptions,
+	FastifyServerOptions,
+} from 'fastify';
+
+/* * */
+
+class FastifyService {
+	private static _instance: FastifyService;
+	public readonly server: FastifyInstance;
+
+	private constructor(options: FastifyServerOptions) {
+		this.server = fastify(options);
+		this._setupDefaultRoutes();
+	}
+
+	public static getInstance(options?: FastifyServerOptions) {
+		if (!FastifyService._instance) {
+			FastifyService._instance = new FastifyService(options || {});
+		}
+
+		return FastifyService._instance;
+	}
+
+	async start() {
+		const options: FastifyListenOptions = {
+			host: process.env.HOST || '0.0.0.0',
+			port: Number(process.env.API_PORT) || 5050,
+		};
+
+		try {
+			await this.server.listen(options);
+		}
+		catch (error) {
+			this.server.log.error({
+				error,
+				message: 'Error starting server',
+			});
+			process.exit(1);
+		}
+	}
+
+	async stop() {
+		try {
+			await this.server.close();
+		}
+		catch (error) {
+			this.server.log.error(error);
+			process.exit(1);
+		}
+	}
+
+	private _setupDefaultRoutes() {
+		this.server.get('/', (req, res) => {
+			res.send('Jusi was here!');
+		});
+	}
+}
+
+export default FastifyService;
