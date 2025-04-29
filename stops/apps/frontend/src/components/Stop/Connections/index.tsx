3e937b9b1bda5db301bcc6fe7ef75737079bf958
Samuel Santos <samuel.trabalho.96@gmail.com>
2025-04-29 16:58:02 +0100

    Refactor Connections Component

--- stops/apps/frontend/src/components/Stop/Connections/index.tsx
+++ stops/apps/frontend/src/components/Stop/Connections/index.tsx
@@ -1,17 +1,9 @@
 'use client';
 
-/* * */
-
-import Header from '@/components/common/Header';
-import Item from '@/components/common/Row/Item';
-import { Grid } from '@tmlmobilidade/ui';
-
-/* * */
-
 import { useStopDetailContext } from '@/contexts/StopDetail.context';
-import { Connections as ConnectionsType } from '@tmlmobilidade/types';
+import { Checkbox, Collapsible, Grid, Section } from '@tmlmobilidade/ui';
 
-import styles from '../styles.module.css';
+import styles from './styles.module.css';
 
 /* * */
 
@@ -22,8 +14,6 @@ export default function Connections() {
 	// A. Setup variables
 
 	const stopDetailContext = useStopDetailContext();
-
-	const connections = stopDetailContext.data.form.getValues().connections || [''];
 
 	const AIRPORT = 'airport';
 	const BIKE_PARKING = 'bike_parking';
@@ -46,31 +36,103 @@ export default function Connections() {
 		subway = 'Metro',
 		train = 'Comboio',
 	}
-	//
 
 	//
-	// A. Render components
+	// B. Render components
 
 	return (
-		<div className={styles.section}>
-			<Header
-				description="Quais são os outros modos de transporte, para além do autocarro, que esteja paragem serve."
-				title="Ligações Intermodais"
-			/>
+		<Collapsible
+			description="Quais são os outros modos de transporte, para além do autocarro, que esteja paragem serve."
+			title="Ligações Intermodais"
+		>
+			<Section gap="md">
+				<Grid className={styles.grid} columns="abcd">
+					<div className={styles.input_checkbox_container}>
+						<Checkbox
+							checked={stopDetailContext.data.form.getInputProps('connections').value.includes(SUBWAY)}
+							className={styles.input_checkbox}
+							label={ConnectionsValues[SUBWAY]}
+							{...stopDetailContext.data.form.getInputProps('connections')}
+						/>
+					</div>
 
-			<Grid className={styles.grid} columns="abcd">
-				<Item label={ConnectionsValues[SUBWAY]} value={connections.includes(SUBWAY)} />
-				<Item label={ConnectionsValues[LIGHT_RAIL]} value={connections.includes(LIGHT_RAIL)} />
-				<Item label={ConnectionsValues[TRAIN]} value={connections.includes(TRAIN)} />
-				<Item label={ConnectionsValues[BOAT]} value={connections.includes(BOAT)} />
+					<div className={styles.input_checkbox_container}>
+						<Checkbox
+							checked={stopDetailContext.data.form.getInputProps('connections').value.includes(LIGHT_RAIL)}
+							className={styles.input_checkbox}
+							label={ConnectionsValues[LIGHT_RAIL]}
+							{...stopDetailContext.data.form.getInputProps('connections')}
+						/>
+					</div>
 
-				<Item label={ConnectionsValues[BIKE_SHARING]} value={connections.includes(BIKE_SHARING)} />
-				<Item label={ConnectionsValues[AIRPORT]} value={connections.includes(AIRPORT)} />
-				<Item label={ConnectionsValues[BIKE_PARKING]} value={connections.includes(BIKE_PARKING)} />
-				<Item label={ConnectionsValues[CAR_PARKING]} value={connections.includes(CAR_PARKING)} />
+					<div className={styles.input_checkbox_container}>
+						<Checkbox
+							checked={stopDetailContext.data.form.getInputProps('connections').value.includes(TRAIN)}
+							className={styles.input_checkbox}
+							label={ConnectionsValues[TRAIN]}
+							{...stopDetailContext.data.form.getInputProps('connections')}
+						/>
+					</div>
 
-				<Item label={ConnectionsValues[FERRY]} value={connections.includes(FERRY)} />
-			</Grid>
-		</div>
+					<div className={styles.input_checkbox_container}>
+						<Checkbox
+							checked={stopDetailContext.data.form.getInputProps('connections').value.includes(BOAT)}
+							className={styles.input_checkbox}
+							label={ConnectionsValues[BOAT]}
+							{...stopDetailContext.data.form.getInputProps('connections')}
+						/>
+					</div>
+				</Grid>
+
+				<Grid className={styles.grid} columns="abcd">
+					<div className={styles.input_checkbox_container}>
+						<Checkbox
+							checked={stopDetailContext.data.form.getInputProps('connections').value.includes(BIKE_SHARING)}
+							className={styles.input_checkbox}
+							label={ConnectionsValues[BIKE_SHARING]}
+							{...stopDetailContext.data.form.getInputProps('connections')}
+						/>
+					</div>
+
+					<div className={styles.input_checkbox_container}>
+						<Checkbox
+							checked={stopDetailContext.data.form.getInputProps('connections').value.includes(AIRPORT)}
+							className={styles.input_checkbox}
+							label={ConnectionsValues[AIRPORT]}
+							{...stopDetailContext.data.form.getInputProps('connections')}
+						/>
+					</div>
+
+					<div className={styles.input_checkbox_container}>
+						<Checkbox
+							checked={stopDetailContext.data.form.getInputProps('connections').value.includes(BIKE_PARKING)}
+							className={styles.input_checkbox}
+							label={ConnectionsValues[BIKE_PARKING]}
+							{...stopDetailContext.data.form.getInputProps('connections')}
+						/>
+					</div>
+
+					<div className={styles.input_checkbox_container}>
+						<Checkbox
+							checked={stopDetailContext.data.form.getInputProps('connections').value.includes(CAR_PARKING)}
+							className={styles.input_checkbox}
+							label={ConnectionsValues[CAR_PARKING]}
+							{...stopDetailContext.data.form.getInputProps('connections')}
+						/>
+					</div>
+				</Grid>
+
+				<Grid className={styles.grid} columns="abcd">
+					<div className={styles.input_checkbox_container}>
+						<Checkbox
+							checked={stopDetailContext.data.form.getInputProps('connections').value.includes(FERRY)}
+							className={styles.input_checkbox}
+							label={ConnectionsValues[FERRY]}
+							{...stopDetailContext.data.form.getInputProps('connections')}
+						/>
+					</div>
+				</Grid>
+			</Section>
+		</Collapsible>
 	);
 }
