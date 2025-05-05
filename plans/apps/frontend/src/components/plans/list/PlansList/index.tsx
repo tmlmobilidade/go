30045affc8baeb7311a94feea78e743233268a04
jusimen <jusi.monteiro@protonmail.com>
2025-05-05 12:21:47 +0100

    chore: update @tmlmobilidade/lib and @tmlmobilidade/ui dependencies in package.json files
    - Updated the version of @tmlmobilidade/lib to 20250505.1118.29 in both API and frontend package.json files.
    - Updated the version of @tmlmobilidade/ui to 20250505.1118.49 in the frontend package.json file.
    - Removed unused imports and commented-out code in the PlansList component for improved readability and maintainability.

--- plans/apps/frontend/src/components/plans/list/PlansList/index.tsx
+++ plans/apps/frontend/src/components/plans/list/PlansList/index.tsx
@@ -6,13 +6,9 @@ import { PlansListFilters } from '@/components/plans/list/PlansListFilters';
 import { PlansListHeader } from '@/components/plans/list/PlansListHeader';
 import { usePlanListContext } from '@/contexts/PlanList.context';
 import { Routes } from '@/lib/routes';
-import { IconCheck, IconLock, IconLockOff, IconX } from '@tabler/icons-react';
 import { type Plan } from '@tmlmobilidade/types';
 import { DataTable, DataTableColumn, Pane, Tag } from '@tmlmobilidade/ui';
 import { useRouter } from 'next/navigation';
-
-import AgencyCell from '../AgencyCell';
-import DateCell from '../DateCell';
 /* * */
 
 export function PlanList() {
@@ -28,45 +24,7 @@ export function PlanList() {
 		{
 			accessor: '_id',
 			render: ({ _id }) => <Tag label={_id} variant="muted" />,
-			title: 'ID',
 			width: 150,
-		},
-		{
-			accessor: 'agency_id',
-			render: ({ agency_id }) => {
-				return <AgencyCell agencyId={agency_id} />;
-			},
-			title: 'Operador',
-			width: 300,
-		},
-		{
-			accessor: 'is_locked',
-			render: ({ is_locked }) => {
-				return is_locked ? <Tag icon={<IconLock />} variant="danger" /> : <Tag icon={<IconLockOff />} variant="success" />;
-			},
-			title: 'Bloqueado',
-			width: 100,
-		},
-		{
-			accessor: 'is_approved',
-			center: true,
-			render: ({ is_approved }) => {
-				return is_approved ? <Tag icon={<IconCheck />} variant="success" /> : <Tag icon={<IconX />} variant="danger" />;
-			},
-			title: 'Aprovado',
-			width: 200,
-		},
-		{
-			accessor: 'valid_from',
-			render: ({ valid_from }) => <DateCell date={valid_from} endDate={valid_from} />,
-			title: 'Data de início',
-			width: 300,
-		},
-		{
-			accessor: 'valid_until',
-			render: ({ valid_until }) => <DateCell date={valid_until} endDate={valid_until} />,
-			title: 'Data de fim',
-			width: 300,
 		},
 	];
 
