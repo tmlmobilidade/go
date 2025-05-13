afe0ba4bee55eef62d2610ec39964ecb744d4cc5
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-05-13 19:30:37 +0100

    Simplify implementation

--- replicator/apps/enrich/src/index.ts
+++ replicator/apps/enrich/src/index.ts
@@ -1,6 +1,8 @@
 /* * */
 
-import { syncApexOnBoardSales } from '@/tasks/sync-apex-on-board-sales.js';
+import LOGGER from '@helperkits/logger';
+import TIMETRACKER from '@helperkits/timer';
+import { apexOnBoardRefunds, apexOnBoardSales, apexValidations } from '@tmlmobilidade/interfaces';
 
 /* * */
 
@@ -8,19 +10,57 @@ const RUN_INTERVAL = 1800000; // 30 minutes
 
 /* * */
 
+export async function enrichApexTransactions() {
+	try {
+		//
+
+		LOGGER.init();
+
+		const globalTimer = new TIMETRACKER();
+
+		//
+		// Connect to databases
+
+		const apexOnBoardSalesCollection = await apexOnBoardSales.getCollection();
+		const apexOnBoardRefundsCollection = await apexOnBoardRefunds.getCollection();
+		const apexValidationsCollection = await apexValidations.getCollection();
+
+		//
+		// The goal is to enrich APEX OnBoard Sales with APEX Validations and APEX OnBoard Refunds data.
+		// To accomplish this, we need to start by fetching all validations and refunds data that do not yet
+		// have a corresponding OnBoard Sale ID. By doing it this way, we reduce the number of documents
+		// we need to process, as we are sure that there is a corresponding OnBoard Sale ID for each
+		// validation (of an on-board ticket) and refund document.
+
+		const unlinkedOnBoardRefunds = apexOnBoardRefundsCollection
+			.find({ on_board_sale_transaction_id: { $exists: false } })
+			.limit(1000)
+			.toArray();
+
+		//
+
+		LOGGER.terminate(`Run took ${globalTimer.get()}.`);
+
+		//
+	}
+	catch (err) {
+		console.log('An error occurred. Halting execution.', err);
+		console.log('Retrying in 10 seconds...');
+		setTimeout(() => {
+			process.exit(0); // End process
+		},
+		10000); // after 10 seconds
+	}
+
+	//
+};
+
+/* * */
+
 (async function init() {
-	//
-
-	//
-	// Ensure that all documents are synced between collections
-	// by checking the presence of each individual document ID.
-
 	const runOnInterval = async () => {
-		await syncApexOnBoardSales();
+		await enrichApexTransactions();
 		setTimeout(runOnInterval, RUN_INTERVAL);
 	};
-
 	runOnInterval();
-
-	//
 })();
