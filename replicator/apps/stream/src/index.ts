c65ce7377a1fdf40d81e3cc7166a4b5e17a87fda
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-06-01 02:48:28 +0100

    Stream sales and refunds

--- replicator/apps/stream/src/index.ts
+++ replicator/apps/stream/src/index.ts
@@ -1,6 +1,8 @@
 /* * */
 
 import { processApexLocation } from '@/tasks/process-apex-location.js';
+import { processApexOnBoardRefund } from '@/tasks/process-apex-on-board-refund.js';
+import { processApexOnBoardSale } from '@/tasks/process-apex-on-board-sale.js';
 import { processApexValidation } from '@/tasks/process-apex-validation.js';
 import { processVehicleEvent } from '@/tasks/process-vehicle-event.js';
 import { PCGIDB } from '@tmlmobilidade/sae-replicator-pckg-utils';
@@ -16,9 +18,13 @@ import { PCGIDB } from '@tmlmobilidade/sae-replicator-pckg-utils';
 	// Watch for changes to the MongoDB collections
 	// and integrate those documents immediately.
 
-	PCGIDB.ValidationEntity.watch().on('change', processApexValidation);
-
 	PCGIDB.LocationEntity.watch().on('change', processApexLocation);
+
+	PCGIDB.SalesEntity.watch().on('change', processApexOnBoardRefund);
+
+	PCGIDB.SalesEntity.watch().on('change', processApexOnBoardSale);
+
+	PCGIDB.ValidationEntity.watch().on('change', processApexValidation);
 
 	PCGIDB.VehicleEvents.watch().on('change', processVehicleEvent);
 
