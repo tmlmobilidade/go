960ad4264fecb8c574b908b475dfd3b4c252f6c3
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-06-01 04:17:52 +0100

    Handle error logs

--- replicator/packages/parse/src/parse-apex-on-board-refund.ts
+++ replicator/packages/parse/src/parse-apex-on-board-refund.ts
@@ -7,34 +7,36 @@ import { Dates } from '@tmlmobilidade/utils';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function parseSimplifiedApexOnBoardRefund(pcgiDoc: any): SimplifiedApexOnBoardRefund {
-	//
-
-	return {
-		_id: pcgiDoc.transaction.transactionId,
-		agency_id: pcgiDoc.transaction.operatorLongID,
-		apex_version: pcgiDoc.transaction.apexVersion,
-		block_id: null,
-		card_physical_type: pcgiDoc.transaction.cardPhysicalType,
-		card_serial_number: pcgiDoc.transaction.cardSerialNumber,
-		created_at: Dates.fromISO(pcgiDoc.transaction.transactionDate).unix_timestamp,
-		device_id: pcgiDoc.transaction.deviceID,
-		duty_id: null,
-		line_id: null,
-		mac_ase_counter_value: pcgiDoc.transaction.macDataFields.aseCounterValue,
-		mac_sam_serial_number: pcgiDoc.transaction.macDataFields.samSerialNumber,
-		on_board_sale_id: pcgiDoc.transaction.corrTransactionId,
-		pattern_id: null,
-		payment_method: pcgiDoc.transaction.paymentMethod,
-		price: pcgiDoc.transaction.price,
-		product_long_id: pcgiDoc.transaction.productLongID,
-		product_quantity: pcgiDoc.transaction.productQuantity,
-		received_at: Dates.fromISO(pcgiDoc.createdAt).unix_timestamp,
-		stop_id: null,
-		trip_id: null,
-		updated_at: Dates.fromISO(pcgiDoc.createdAt).unix_timestamp,
-		validation_id: null,
-		vehicle_id: null,
-	};
-
-	//
+	try {
+		return {
+			_id: pcgiDoc.transaction.transactionId,
+			agency_id: pcgiDoc.transaction.operatorLongID,
+			apex_version: pcgiDoc.transaction.apexVersion,
+			block_id: null,
+			card_physical_type: pcgiDoc.transaction.cardPhysicalType,
+			card_serial_number: pcgiDoc.transaction.cardSerialNumber,
+			created_at: Dates.fromISO(pcgiDoc.transaction.transactionDate).unix_timestamp,
+			device_id: pcgiDoc.transaction.deviceID,
+			duty_id: null,
+			line_id: null,
+			mac_ase_counter_value: pcgiDoc.transaction.macDataFields.aseCounterValue,
+			mac_sam_serial_number: pcgiDoc.transaction.macDataFields.samSerialNumber,
+			on_board_sale_id: pcgiDoc.transaction.corrTransactionId,
+			pattern_id: null,
+			payment_method: pcgiDoc.transaction.paymentMethod,
+			price: pcgiDoc.transaction.price,
+			product_long_id: pcgiDoc.transaction.productLongID,
+			product_quantity: pcgiDoc.transaction.productQuantity,
+			received_at: Dates.fromISO(pcgiDoc.createdAt).unix_timestamp,
+			stop_id: null,
+			trip_id: null,
+			updated_at: Dates.fromISO(pcgiDoc.createdAt).unix_timestamp,
+			validation_id: null,
+			vehicle_id: null,
+		};
+	}
+	catch (error) {
+		console.log(`Error parsing simplified APEX OnBoardRefund. Transaction ID: "${pcgiDoc.transaction.transactionId}"`);
+		throw new Error(error);
+	}
 }
