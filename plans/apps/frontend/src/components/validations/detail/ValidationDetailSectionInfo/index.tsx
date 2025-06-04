4538f10c999753e6d2db6656c23127ea57ec89aa
jusimen <jusi.monteiro@protonmail.com>
2025-06-04 15:33:56 +0100

    Enhance ValidationDetailSectionInfo to display GTFS feed information

--- plans/apps/frontend/src/components/validations/detail/ValidationDetailSectionInfo/index.tsx
+++ plans/apps/frontend/src/components/validations/detail/ValidationDetailSectionInfo/index.tsx
@@ -1,7 +1,8 @@
 /* * */
 
 import { useValidationDetailContext } from '@/contexts/ValidationDetail.context';
-import { Collapsible, Combobox, Section } from '@tmlmobilidade/ui';
+import { Collapsible, Divider, Grid, Label, Section, Text } from '@tmlmobilidade/ui';
+import { Dates } from '@tmlmobilidade/utils';
 
 /* * */
 
@@ -10,25 +11,88 @@ export function ValidationDetailSectionInfo() {
 
 	//
 	// A. Setup variables
-	const validationDetailContext = useValidationDetailContext();
+	const { data: { form } } = useValidationDetailContext();
 
 	//
 	// C. Render components
 
+	if (!form.values.gtfs_feed_info) return null;
+
+	const renderAgencyInfo = () => {
+		return (
+			<Section gap="sm">
+				<Label caps>Agência</Label>
+				<Grid columns="abc" gap="sm">
+					<Section padding="none">
+						<Label size="sm" caps>Agência</Label>
+						<Text size="base">{form.values.gtfs_feed_info.feed_publisher_name ?? 'N/A'}</Text>
+					</Section>
+					<Section padding="none">
+						<Label size="sm" caps>URL da agência</Label>
+						<Text size="base">{form.values.gtfs_feed_info.feed_publisher_url ?? 'N/A'}</Text>
+					</Section>
+					<Section padding="none">
+						<Label size="sm" caps>Email de contacto</Label>
+						<Text size="base">{form.values.gtfs_feed_info.feed_contact_email ?? 'N/A'}</Text>
+					</Section>
+					<Section padding="none">
+						<Label size="sm" caps>URL de contacto</Label>
+						<Text size="base">{form.values.gtfs_feed_info.feed_contact_url ?? 'N/A'}</Text>
+					</Section>
+				</Grid>
+			</Section>
+		);
+	};
+
+	const renderDatesInfo = () => {
+		return (
+			<Section gap="sm">
+				<Label caps>Data de vigência</Label>
+				<Grid columns="abc" gap="sm">
+					<Section padding="none">
+						<Label size="sm" caps>Data de início</Label>
+						<Text size="base">{form.values.gtfs_feed_info.feed_start_date ? Dates.fromUnixTimestamp(form.values.gtfs_feed_info.feed_start_date).toFormat('dd/MM/yyyy') : 'N/A'}</Text>
+					</Section>
+					<Section padding="none">
+						<Label size="sm" caps>Data de fim</Label>
+						<Text size="base">{form.values.gtfs_feed_info.feed_end_date ? Dates.fromUnixTimestamp(form.values.gtfs_feed_info.feed_end_date).toFormat('dd/MM/yyyy') : 'N/A'}</Text>
+					</Section>
+				</Grid>
+			</Section>
+		);
+	};
+	const renderMiscInfo = () => {
+		return (
+			<Section gap="sm">
+				<Label caps>Outras informações</Label>
+				<Grid columns="abc" gap="sm">
+					<Section padding="none">
+						<Label size="sm" caps>Versão</Label>
+						<Text size="base">{form.values.gtfs_feed_info.feed_version ?? 'N/A'}</Text>
+					</Section>
+					<Section padding="none">
+						<Label size="sm" caps>Idioma padrão</Label>
+						<Text size="base">{form.values.gtfs_feed_info.default_lang?.toUpperCase() ?? 'N/A'}</Text>
+					</Section>
+					<Section padding="none">
+						<Label size="sm" caps>Idioma do feed</Label>
+						<Text size="base">{form.values.gtfs_feed_info.feed_lang?.toUpperCase() ?? 'N/A'}</Text>
+					</Section>
+				</Grid>
+			</Section>
+		);
+	};
+
 	return (
 		<Collapsible
-			description="Informações gerais sobre o validação, como operador, data de vigência, etc."
-			title="Informação do validação"
+			description="Informações gerais sobre o ficheiro GTFS, estas informações são extraídas do ficheiro feed_info.txt"
+			title="Informação do ficheiro GTFS"
 		>
-			<Section gap="md">
-				<Combobox
-					aria-label="Agência"
-					data={validationDetailContext.data.agencies}
-					label="Agência"
-					fullWidth
-					{...validationDetailContext.data.form.getInputProps('agency_id')}
-				/>
-			</Section>
+			{renderAgencyInfo()}
+			<Divider />
+			{renderDatesInfo()}
+			<Divider />
+			{renderMiscInfo()}
 		</Collapsible>
 	);
 }
