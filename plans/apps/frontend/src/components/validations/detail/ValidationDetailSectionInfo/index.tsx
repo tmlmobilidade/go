c23a4d9e514311bff6f761652f03adda65246220
jusimen <jusi.monteiro@protonmail.com>
2025-06-04 11:55:40 +0100

    Refactor ValidationDetailSectionInfo to remove unused date handling logic
    - Eliminated DatePicker components and associated date transformation logic for 'valid_from' and 'valid_until'.
    - Streamlined imports by removing unused dependencies, enhancing component clarity.

--- plans/apps/frontend/src/components/validations/detail/ValidationDetailSectionInfo/index.tsx
+++ plans/apps/frontend/src/components/validations/detail/ValidationDetailSectionInfo/index.tsx
@@ -1,9 +1,7 @@
 /* * */
 
 import { useValidationDetailContext } from '@/contexts/ValidationDetail.context';
-import { Collapsible, Combobox, DatePicker, Grid, Section } from '@tmlmobilidade/ui';
-import { Dates } from '@tmlmobilidade/utils';
-import { useMemo } from 'react';
+import { Collapsible, Combobox, Section } from '@tmlmobilidade/ui';
 
 /* * */
 
@@ -12,20 +10,7 @@ export function ValidationDetailSectionInfo() {
 
 	//
 	// A. Setup variables
-
 	const validationDetailContext = useValidationDetailContext();
-
-	//
-	// B. Transform data
-	const validFrom = useMemo(() => {
-		if (!validationDetailContext.data.form.values.valid_from) return null;
-		return Dates.fromOperationalDate(validationDetailContext.data.form.values.valid_from, 'local').js_date;
-	}, [validationDetailContext.data.form.values.valid_from]);
-
-	const validUntil = useMemo(() => {
-		if (!validationDetailContext.data.form.values.valid_until) return null;
-		return Dates.fromOperationalDate(validationDetailContext.data.form.values.valid_until, 'local').js_date;
-	}, [validationDetailContext.data.form.values.valid_until]);
 
 	//
 	// C. Render components
@@ -43,34 +28,6 @@ export function ValidationDetailSectionInfo() {
 					fullWidth
 					{...validationDetailContext.data.form.getInputProps('agency_id')}
 				/>
-				<Grid columns="ab" gap="md">
-					<DatePicker
-						description="Data de início da vigência do validação"
-						flex={1}
-						label="Data de início"
-						{...validationDetailContext.data.form.getInputProps('valid_from')}
-						value={validFrom}
-						onChange={(date) => {
-							validationDetailContext.data.form.setValues({
-								valid_from: Dates.fromFormat(date, 'yyyy-MM-dd', 'Europe/Lisbon').operational_date,
-							});
-						}}
-						withAsterisk
-					/>
-					<DatePicker
-						description="Data de fim da vigência do validação"
-						label="Data de fim"
-						clearable
-						{...validationDetailContext.data.form.getInputProps('valid_until')}
-						value={validUntil}
-						onChange={(date) => {
-							console.log('date', date);
-							validationDetailContext.data.form.setValues({
-								valid_until: Dates.fromFormat(date, 'yyyy-MM-dd', 'Europe/Lisbon').operational_date,
-							});
-						}}
-					/>
-				</Grid>
 			</Section>
 		</Collapsible>
 	);
