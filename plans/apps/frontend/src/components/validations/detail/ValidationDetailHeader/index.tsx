58a9f776e62abbdd222bf5c955fbfb671a6da672
jusimen <jusi.monteiro@protonmail.com>
2025-06-19 02:05:20 +0100

    Integrate permission checks for converting validations to plans in ValidationDetailHeader component

--- plans/apps/frontend/src/components/validations/detail/ValidationDetailHeader/index.tsx
+++ plans/apps/frontend/src/components/validations/detail/ValidationDetailHeader/index.tsx
@@ -7,8 +7,10 @@ import { StatusTag } from '@/components/common/StatusTag';
 import { OpenCreatePlanModal } from '@/components/plans/detail/CreatePlanModal';
 import { useValidationDetailContext } from '@/contexts/ValidationDetail.context';
 import { IconTransform } from '@tabler/icons-react';
-import { Button, Label, Spacer } from '@tmlmobilidade/ui';
+import { Permissions } from '@tmlmobilidade/lib';
+import { Button, HasPermission, Label, Spacer } from '@tmlmobilidade/ui';
 import { useMemo } from 'react';
+
 /* * */
 
 export function ValidationDetailHeader() {
@@ -30,7 +32,14 @@ export function ValidationDetailHeader() {
 			<Label size="lg" caps>{validationDetailContext.data.id}</Label>
 			<Spacer />
 			{canConvertToPlan && (
-				<Button icon={<IconTransform size={24} />} label="Converter para plano" onClick={() => OpenCreatePlanModal(validationDetailContext.data.validation._id)} size="lg" variant="primary" />
+				<HasPermission
+					action={Permissions.validations.actions.create_plan}
+					resource_key="agency_ids"
+					scope={Permissions.validations.scope}
+					value={validationDetailContext.data.validation.gtfs_agency.agency_id}
+				>
+					<Button icon={<IconTransform size={24} />} label="Converter para plano" onClick={() => OpenCreatePlanModal(validationDetailContext.data.validation._id)} size="lg" variant="primary" />
+				</HasPermission>
 			)}
 		</>
 	);
