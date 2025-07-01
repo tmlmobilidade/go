b44cfb2164176ccc2a371c64cd587a8b48e028eb
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-07-01 15:24:51 +0100

    Update UI components

--- alerts/apps/frontend/src/app/layout.tsx
+++ alerts/apps/frontend/src/app/layout.tsx
@@ -6,10 +6,11 @@ import '@tmlmobilidade/ui/styles';
 
 import { DataProviders } from '@/components/providers/data-providers';
 import { Routes } from '@/lib/routes';
+import { getAppConfig } from '@tmlmobilidade/lib';
 import { AppProvider, AppWrapper } from '@tmlmobilidade/ui';
 import { cookies as nextCookies } from 'next/headers';
 import { redirect, RedirectType } from 'next/navigation';
-import { PropsWithChildren } from 'react';
+import { type PropsWithChildren } from 'react';
 
 /* * */
 
@@ -26,7 +27,7 @@ export default async function RootLayout({ children }: PropsWithChildren) {
 	// B. Handle actions
 
 	if (!sessionToken) {
-		redirect(`${Routes.AUTH_API}/login?redirectTo=${encodeURI(Routes.URL)}`, RedirectType.replace);
+		redirect(`${getAppConfig('auth', 'frontend_url')}/login?redirectTo=${encodeURI(Routes.URL)}`, RedirectType.replace);
 	}
 
 	//
