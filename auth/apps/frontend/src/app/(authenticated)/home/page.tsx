ae86b4043fd27d0190a9b3e8764973502510c61d
Classic-German <175220123+Classic-German@users.noreply.github.com>
2025-07-10 14:52:01 +0100

    Show links in home page

--- auth/apps/frontend/src/app/(authenticated)/home/page.tsx
+++ auth/apps/frontend/src/app/(authenticated)/home/page.tsx
@@ -1,7 +1,26 @@
 'use client';
-
 import MarkdownPage from '@/components/markdown/files/home.mdx';
+import { quickLinks } from '@/components/QuickLink/data';
+import { Grid, Pane, Section, Tag, Text } from '@tmlmobilidade/ui';
 
 export default function Page() {
-	return <MarkdownPage />;
+	return (
+		<Pane header={[<MarkdownPage />]}>
+			<Grid columns="abcd">
+				{quickLinks.map(item => (
+					<div key={item.icon}>
+						<Section alignItems="center" flexDirection="column" flexWrap="nowrap" gap="sm">
+							<Tag label={item.icon} />
+							<Text size="lg">{item.title}</Text>
+							<Text size="base">{item.link}</Text>
+						</Section>
+
+						{/* <div>{item.icon}</div>
+					<h3>{item.title}</h3>
+					<a href={item.link}>Acessar</a> */}
+					</div>
+				))}
+			</Grid>
+		</Pane>
+	);
 }
---
+++ auth/apps/frontend/src/components/QuickLink/data.ts
@@ -0,0 +1,6 @@
+export const quickLinks = [
+	{ icon: '👤', link: 'http://localhost:51000', title: 'Utilizadores' },
+	{ icon: '⚠️', link: 'http://localhost:51001', title: 'Alertas' },
+	{ icon: '📊', link: 'http://localhost:51002', title: 'Monitorização' },
+	{ icon: '🚌', link: 'http://localhost:51003', title: 'Paragens' },
+];
