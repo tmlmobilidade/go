c9c07e9e56c972dd026efdde91c6ee2345bddf16
Tomás Martins <tomassalgadomartins@gmail.com>
2025-07-11 16:51:23 +0100

    Change links

--- auth/apps/frontend/src/components/home/homePage/data.ts
+++ auth/apps/frontend/src/components/home/homePage/data.ts
@@ -8,8 +8,8 @@ export interface QuickLink {
 
 
 export const quickLinks: QuickLink[] = [
-	{ icon: '🌐', href: 'https://carrismetropolitana.pt/', title: 'Site' },
-	{ icon: '🐈‍⬛', href: 'https://github.com/carrismetropolitana/', title: 'GitHub' },
+	{ icon: '🌐', href: 'https://go.carrismetropolitana.pt', title: 'GO v1' },
+	{ icon: '📀', href: 'https://eu.remix.com', title: 'Remix' },
 	{ icon: '📖', href: 'https://pt.wikipedia.org/wiki/Carris_Metropolitana', title: 'Wiki' },
 	{ icon: '🐤', href: 'https://x.com/CMetropolitana_?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor', title: 'Twiitter' },
 ];
