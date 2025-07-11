a910d77b18419fd9fa96c724f8ad85f35c3f4363
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-07-11 01:33:14 +0100

    Tweaks

--- plans/apps/frontend/src/components/plans/detail/PlanDetailHeader/index.tsx
+++ plans/apps/frontend/src/components/plans/detail/PlanDetailHeader/index.tsx
@@ -3,11 +3,11 @@
 /* * */
 
 import { usePlanDetailContext } from '@/contexts/PlanDetail.context';
-import { IconLock, IconLockOpen, IconUpload } from '@tabler/icons-react';
+import { IconUpload } from '@tabler/icons-react';
 import { Permissions } from '@tmlmobilidade/lib';
-import { ActionIcon, BackButton, Button, HasPermission, Label, Spacer, Tag } from '@tmlmobilidade/ui';
+import { BackButton, Button, HasPermission, LockButton, Spacer, Tag } from '@tmlmobilidade/ui';
 import { useRouter } from 'next/navigation';
-import { useMemo } from 'react';
+
 /* * */
 
 export function PlanDetailHeader() {
@@ -29,34 +29,18 @@ export function PlanDetailHeader() {
 	//
 	// C. Render components
 
-	const lockButton = useMemo(() => {
-		const is_locked = planDetailContext.data.plan.is_locked;
-		return (
-			<ActionIcon
-				variant={is_locked ? 'danger' : 'success'}
-				onClick={() => {
-					planDetailContext.actions.toggleLock();
-				}}
-			>
-				{is_locked ? <IconLock size={28} /> : <IconLockOpen size={28} />}
-			</ActionIcon>
-		);
-	}, [planDetailContext.data.plan.is_locked]);
-
 	return (
 		<>
 			<BackButton onClick={handleClose} type="close" />
-			<Tag label={planDetailContext.data.plan._id} variant="muted" />
-			<Label size="lg" caps>{planDetailContext.data.id}</Label>
+			<Tag label={planDetailContext.data.plan._id} variant="secondary" />
 			<Spacer />
-			{lockButton}
+			<LockButton isLocked={planDetailContext.data.plan.is_locked} onClick={planDetailContext.actions.toggleLock} />
 			<HasPermission
 				action={Permissions.plans.actions.update}
 				resource_key="agency_ids"
 				scope={Permissions.plans.scope}
 				value={planDetailContext.data.plan.gtfs_agency.agency_id}
 			>
-
 				<Button
 					disabled={planDetailContext.flags.isSaving || !planDetailContext.data.form.isDirty()}
 					icon={<IconUpload size={28} />}
