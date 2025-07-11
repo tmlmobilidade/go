c65bfa56cdd3f9080424fd45b4c72054300010cb
Tomás Martins <tomassalgadomartins@gmail.com>
2025-07-11 17:09:01 +0100

    Change style and add transition

--- auth/apps/frontend/src/components/home/homePage/data.ts
+++ auth/apps/frontend/src/components/home/homePage/data.ts
@@ -11,5 +11,5 @@ export const quickLinks: QuickLink[] = [
 	{ icon: '🌐', href: 'https://go.carrismetropolitana.pt', title: 'GO v1' },
 	{ icon: '📀', href: 'https://eu.remix.com', title: 'Remix' },
 	{ icon: '📖', href: 'https://pt.wikipedia.org/wiki/Carris_Metropolitana', title: 'Wiki' },
-	{ icon: '🐤', href: 'https://x.com/CMetropolitana_?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor', title: 'Twiitter' },
+	{ icon: '🐤', href: 'https://x.com/CMetropolitana_?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor', title: 'Twitter' },
 ];
