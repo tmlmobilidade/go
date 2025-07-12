2860e8a6c30071b71ca5bd27b61a4a198356a618
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-07-12 01:31:58 +0100

    Remove markdown references

--- auth/apps/frontend/next.config.ts
+++ auth/apps/frontend/next.config.ts
@@ -1,6 +1,5 @@
 /* * */
 
-import createMDX from '@next/mdx';
 import { type NextConfig } from 'next';
 
 /* * */
@@ -10,7 +9,6 @@ const nextConfig: NextConfig = {
 		optimizePackageImports: ['@tmlmobilidade/ui'],
 	},
 	output: 'standalone',
-	pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
 	reactStrictMode: true,
 	async rewrites() {
 		return [
@@ -22,11 +20,6 @@ const nextConfig: NextConfig = {
 	},
 };
 
-const withMDX = createMDX({
-	extension: /\.(md|mdx)$/,
-});
-
 /* * */
 
-// Merge MDX config with Next.js config
-export default withMDX(nextConfig);
+export default nextConfig;
