24ba8e3b282537c3f246d22f8e2998d5362c4162
Classic-German <175220123+Classic-German@users.noreply.github.com>
2025-07-15 11:18:12 +0100

    Update index.tsx
    Update index.tsx

--- auth/apps/frontend/src/components/home/HomePage/index.tsx
+++ auth/apps/frontend/src/components/home/HomePage/index.tsx
@@ -1,33 +1,26 @@
-/* * */
-
-import { QuickLinkButton } from '@/components/home/QuickLink';
-import { QuickLink } from '@/types/quick-links';
-import { IconBrandGithub, IconChartBubble, IconEaseOutControlPointFilled, IconFileTypeSql, IconFlag, IconUmbrella } from '@tabler/icons-react';
+import { quickLinks } from '@/components/home/homePage/data';
 import { Grid, Pane, Section } from '@tmlmobilidade/ui';
-
+import { QuickLinkButton } from '../QuickLink';
+import { WikiFiles } from '@/components/markdown/wiki-generator';
 /* * */
 
-const QUICK_LINKS: QuickLink[] = [
-	{ href: 'https://go.carrismetropolitana.pt', icon: <IconFlag size={50} />, title: 'GO v1' },
-	{ href: 'https://eu.remix.com', icon: <IconEaseOutControlPointFilled size={50} />, title: 'Remix' },
-	{ href: 'https://dados.tmlmobilidade.pt', icon: <IconFileTypeSql size={50} />, title: 'BDH' },
-	{ href: 'https://consola.tmlmobilidade.pt/managementconsole/offer-manager/planned-offer', icon: <IconUmbrella size={50} />, title: 'PCGI' },
-	{ href: 'https://app.eu.amplitude.com/analytics/tmlmobilidade/home', icon: <IconChartBubble size={50} />, title: 'Amplitude' },
-	{ href: 'https://github.com/carrismetropolitana', icon: <IconBrandGithub size={50} />, title: 'Github' },
-];
-
-/* * */
 
 export function HomePage() {
-	return (
-		<Pane>
-			<Section padding="lg">
-				<Grid columns="abcd" gap="md">
-					{QUICK_LINKS.map(item => (
-						<QuickLinkButton key={item.href} item={item} />
-					))}
-				</Grid>
-			</Section>
-		</Pane>
-	);
+    return(  
+       
+        // <Pane>
+        //     <Section padding='lg'>
+        //         <Grid columns="abcd" gap='md'>
+        //             {quickLinks.map(item => (
+        //                 <QuickLinkButton key={item.href} item={item} />
+        //             ))}
+        //         </Grid>
+
+        //         <WikiFiles/>
+        //     </Section>
+		// </Pane>
+        <>
+        <WikiFiles/>
+        </>
+    );
 }
