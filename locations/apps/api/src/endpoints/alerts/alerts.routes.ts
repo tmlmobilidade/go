68fcaebef03c618463bfa3a9d7ee68c9d8d7f0d0
jusimen <jusi.monteiro@protonmail.com>
2025-07-16 17:17:20 +0100

    Init Locatinons Project

---
+++ locations/apps/api/src/endpoints/alerts/alerts.routes.ts
@@ -0,0 +1,82 @@
+/* * */
+
+import { authorizationMiddleware, FastifyService } from '@tmlmobilidade/connectors';
+import { Permissions } from '@tmlmobilidade/lib';
+import { Alert } from '@tmlmobilidade/types';
+import { FastifyInstance } from 'fastify';
+
+import { AlertsController } from './alerts.controller.js';
+
+/* * */
+
+const server: FastifyInstance = FastifyService.getInstance().server;
+const namespace = '/alerts';
+
+/* * */
+
+server.register(
+	(instance, opts, next) => {
+		// GET /alerts
+		instance.get(
+			'/',
+			{
+				preHandler: authorizationMiddleware<Alert>(
+					Permissions.alerts.scope,
+					Permissions.alerts.actions.list,
+				),
+			},
+			AlertsController.getAll,
+		);
+
+		// GET /alerts/:id
+		instance.get(
+			'/:id',
+			{
+				preHandler: authorizationMiddleware<Alert>(
+					Permissions.alerts.scope,
+					Permissions.alerts.actions.read,
+				),
+			},
+			AlertsController.getById,
+		);
+
+		// POST /alerts
+		instance.post(
+			'/',
+			{
+				preHandler: authorizationMiddleware<Alert>(
+					Permissions.alerts.scope,
+					Permissions.alerts.actions.create,
+				),
+			},
+			AlertsController.create,
+		);
+
+		// PUT /alerts/:id
+		instance.put(
+			'/:id',
+			{
+				preHandler: authorizationMiddleware<Alert>(
+					Permissions.alerts.scope,
+					Permissions.alerts.actions.update,
+				),
+			},
+			AlertsController.update,
+		);
+
+		// DELETE /alerts/:id
+		instance.delete(
+			'/:id',
+			{
+				preHandler: authorizationMiddleware<Alert>(
+					Permissions.alerts.scope,
+					Permissions.alerts.actions.delete,
+				),
+			},
+			AlertsController.delete,
+		);
+
+		next();
+	},
+	{ prefix: namespace },
+);
