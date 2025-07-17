eb5ec5fae54601fc84e64f5804d2270ee435a0f1
jusimen <jusi.monteiro@protonmail.com>
2025-07-17 15:29:02 +0100

    Implement session check and redirect in reset password page; update login form routing to use constants for improved maintainability.

--- auth/apps/frontend/src/app/reset-password/page.tsx
+++ auth/apps/frontend/src/app/reset-password/page.tsx
@@ -3,6 +3,8 @@
 import { Background4 } from '@/components/login/Background4';
 import { ResetPasswordForm } from '@/components/reset-password/ResetPasswordForm';
 import SendResetEmailForm from '@/components/reset-password/SendResetEmailForm';
+import { cookies as nextCookies } from 'next/headers';
+import { redirect } from 'next/navigation';
 
 import styles from './styles.module.css';
 
@@ -17,7 +19,21 @@ interface Props {
 export default async function Page({ searchParams }: Props) {
 	//
 
+	//
+	// A. Setup variables
+
+	const cookies = await nextCookies();
+	const session = cookies.get('session_token');
 	const { token } = await searchParams;
+
+	//
+	// B. Handle actions
+
+	if (session) {
+		// Redirect to the main page
+		// if the user is already logged in.
+		redirect('/');
+	}
 
 	//
 	// C. Render components
