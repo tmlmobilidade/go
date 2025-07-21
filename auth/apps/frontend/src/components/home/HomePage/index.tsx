16a232614fca6e7467acc21e77bfbd705c0851de
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-07-21 18:52:41 +0100

    Add filedoc URL

--- auth/apps/frontend/src/components/home/HomePage/index.tsx
+++ auth/apps/frontend/src/components/home/HomePage/index.tsx
@@ -2,7 +2,7 @@
 
 import { QuickLinkButton } from '@/components/home/QuickLink';
 import { QuickLink } from '@/types/quick-links';
-import { IconBrandGithub, IconChartBubble, IconEaseOutControlPointFilled, IconFileTypeSql, IconFlag, IconUmbrella } from '@tabler/icons-react';
+import { IconBrandGithub, IconChartBubble, IconEaseOutControlPointFilled, IconFileCertificate, IconFileTypeSql, IconFlag, IconUmbrella } from '@tabler/icons-react';
 import { Grid, Pane, Section } from '@tmlmobilidade/ui';
 
 /* * */
@@ -11,6 +11,7 @@ const QUICK_LINKS: QuickLink[] = [
 	{ href: 'https://go.carrismetropolitana.pt', icon: <IconFlag size={50} />, title: 'GO v1' },
 	{ href: 'https://eu.remix.com', icon: <IconEaseOutControlPointFilled size={50} />, title: 'Remix' },
 	{ href: 'https://dados.tmlmobilidade.pt', icon: <IconFileTypeSql size={50} />, title: 'BDH' },
+	{ href: 'http://filedoc.tmlmobilidade.pt', icon: <IconFileCertificate size={50} />, title: 'Filedoc' },
 	{ href: 'https://consola.tmlmobilidade.pt/managementconsole/offer-manager/planned-offer', icon: <IconUmbrella size={50} />, title: 'PCGI' },
 	{ href: 'https://app.eu.amplitude.com/analytics/tmlmobilidade/home', icon: <IconChartBubble size={50} />, title: 'Amplitude' },
 	{ href: 'https://github.com/carrismetropolitana', icon: <IconBrandGithub size={50} />, title: 'Github' },
