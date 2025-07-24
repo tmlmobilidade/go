695ed14886bb9a46991af935ded5d4f647f302b2
jusimen <jusi.monteiro@protonmail.com>
2025-07-24 17:09:00 +0100

    Enhance Locations API by adding new endpoints for districts, localities, municipalities, and parishes; improve response structure for findByCoordinates method to include status and error handling.

--- locations/apps/api/src/endpoints/locations/locations.routes.ts
+++ locations/apps/api/src/endpoints/locations/locations.routes.ts
@@ -7,7 +7,7 @@ import { LocationsController } from './locations.controller.js';
 /* * */
 
 const server = FastifyService.getInstance().server;
-const namespace = '/';
+const namespace = '/locations';
 
 /* * */
 
@@ -15,6 +15,18 @@ server.register(
 	(instance, opts, next) => {
 		// GET /
 		instance.get('/coordinates', LocationsController.findByCoordinates);
+
+		// GET /districts
+		instance.get('/districts', LocationsController.getDistricts);
+
+		// GET /localities
+		instance.get('/localities', LocationsController.getLocalities);
+
+		// GET /municipalities
+		instance.get('/municipalities', LocationsController.getMunicipalities);
+
+		// GET /parishes
+		instance.get('/parishes', LocationsController.getParishes);
 
 		next();
 	},
