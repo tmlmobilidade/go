1df48dbd365b7a537a4745c9a703430ac0d5c423
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-07-25 16:45:24 +0100

    Update URL

--- auth/apps/frontend/src/components/home/QuickLinks/index.tsx
+++ auth/apps/frontend/src/components/home/QuickLinks/index.tsx
@@ -13,7 +13,7 @@ const QUICK_LINKS: QuickLink[] = [
 	{ href: 'https://go.carrismetropolitana.pt', icon: <IconFlag size={40} />, title: 'GO v1' },
 	{ href: 'https://eu.remix.com', icon: <IconEaseOutControlPointFilled size={40} />, title: 'Remix' },
 	{ href: 'https://dados.tmlmobilidade.pt', icon: <IconFileTypeSql size={40} />, title: 'BDH' },
-	{ href: '#', icon: <IconPlant2 size={40} />, title: 'PBI Reclamações' },
+	{ href: 'https://app.powerbi.com/groups/a314fe73-6989-4d83-b8c9-81812e4eab7d/reports/8f1e0f02-cf87-45ce-9b12-9b0515be2e19?experience=power-bi', icon: <IconPlant2 size={40} />, title: 'PBI Reclamações' },
 	{ href: 'https://github.com/carrismetropolitana/datasets', icon: <IconListNumbers size={40} />, title: 'Datasets' },
 	{ href: 'https://app.eu.amplitude.com/analytics/tmlmobilidade/home', icon: <IconChartBubble size={40} />, title: 'Amplitude' },
 	{ href: '#', icon: <IconFileInfo size={40} />, title: 'Loop' },
