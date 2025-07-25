085bb9cb6cbc1648566365564bb871dcbe5ae992
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-07-25 02:57:27 +0100

    Added more links

--- auth/apps/frontend/src/components/home/QuickLinks/index.tsx
+++ auth/apps/frontend/src/components/home/QuickLinks/index.tsx
@@ -4,19 +4,23 @@
 
 import { QuickLinkButton } from '@/components/home/QuickLinkButton';
 import { QuickLink } from '@/types/quick-links';
-import { IconBrandGithub, IconChartBubble, IconEaseOutControlPointFilled, IconFileCertificate, IconFileTypeSql, IconFlag, IconUmbrella } from '@tabler/icons-react';
+import { IconChartBubble, IconEaseOutControlPointFilled, IconFileCertificate, IconFileInfo, IconFileTypeSql, IconFlag, IconListNumbers, IconPlant2, IconTicket, IconTransferIn, IconUmbrella } from '@tabler/icons-react';
 import { Grid, Section } from '@tmlmobilidade/ui';
 
 /* * */
 
 const QUICK_LINKS: QuickLink[] = [
-	{ href: 'https://go.carrismetropolitana.pt', icon: <IconFlag size={50} />, title: 'GO v1' },
-	{ href: 'https://eu.remix.com', icon: <IconEaseOutControlPointFilled size={50} />, title: 'Remix' },
-	{ href: 'https://dados.tmlmobilidade.pt', icon: <IconFileTypeSql size={50} />, title: 'BDH' },
-	{ href: 'http://filedoc.tmlmobilidade.pt', icon: <IconFileCertificate size={50} />, title: 'Filedoc' },
-	{ href: 'https://consola.tmlmobilidade.pt/managementconsole/offer-manager/planned-offer', icon: <IconUmbrella size={50} />, title: 'PCGI' },
-	{ href: 'https://app.eu.amplitude.com/analytics/tmlmobilidade/home', icon: <IconChartBubble size={50} />, title: 'Amplitude' },
-	{ href: 'https://github.com/carrismetropolitana', icon: <IconBrandGithub size={50} />, title: 'Github' },
+	{ href: 'https://go.carrismetropolitana.pt', icon: <IconFlag size={40} />, title: 'GO v1' },
+	{ href: 'https://eu.remix.com', icon: <IconEaseOutControlPointFilled size={40} />, title: 'Remix' },
+	{ href: 'https://dados.tmlmobilidade.pt', icon: <IconFileTypeSql size={40} />, title: 'BDH' },
+	{ href: '#', icon: <IconPlant2 size={40} />, title: 'PBI Reclamações' },
+	{ href: 'https://github.com/carrismetropolitana/datasets', icon: <IconListNumbers size={40} />, title: 'Datasets' },
+	{ href: 'https://app.eu.amplitude.com/analytics/tmlmobilidade/home', icon: <IconChartBubble size={40} />, title: 'Amplitude' },
+	{ href: '#', icon: <IconFileInfo size={40} />, title: 'Loop' },
+	{ href: 'http://filedoc.tmlmobilidade.pt', icon: <IconFileCertificate size={40} />, title: 'Filedoc' },
+	{ href: 'https://consola.tmlmobilidade.pt/managementconsole/offer-manager/planned-offer', icon: <IconUmbrella size={40} />, title: 'PCGI' },
+	{ href: '#', icon: <IconTransferIn size={40} />, title: 'Intranet' },
+	{ href: 'https://helpdesk.tmlmobilidade.pt', icon: <IconTicket size={40} />, title: 'Helpdesk ITS' },
 ];
 
 /* * */
@@ -24,9 +28,9 @@ const QUICK_LINKS: QuickLink[] = [
 export function QuickLinks() {
 	return (
 		<Section padding="lg">
-			<Grid columns="abcd" gap="md">
+			<Grid columns="abcde" gap="md">
 				{QUICK_LINKS.map(item => (
-					<QuickLinkButton key={item.href} item={item} />
+					<QuickLinkButton key={`${item.title}-${item.href}`} item={item} />
 				))}
 			</Grid>
 		</Section>
