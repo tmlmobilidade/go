afae78b6a4097772f28ad4482c6c5aabd2ce7637
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-07-30 17:33:15 +0100

    Validate plan

--- controller/apps/rides-feeder/src/validate-plan.ts
+++ controller/apps/rides-feeder/src/validate-plan.ts
@@ -9,6 +9,14 @@ export function validatePlan(planData: Plan): boolean {
 	//
 
 	//
+	// Return false if the agency is not for the given operators
+
+	if (!['41', '42', '43', '44'].includes(planData.gtfs_agency?.agency_id)) {
+		LOGGER.error(`Skip processing: gtfs_agency is '${planData.gtfs_agency?.agency_id}'. Only '41', '42', '43', or '44' are allowed.`);
+		return false;
+	}
+
+	//
 	// Return false if its status is other than 'waiting' or 'processing'
 
 	if (planData.status_controller !== ProcessingStatus.Waiting && planData.status_controller !== ProcessingStatus.Processing) {
