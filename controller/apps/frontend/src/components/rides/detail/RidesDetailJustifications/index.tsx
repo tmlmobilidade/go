ad05a268886382b0ba573cd399f7ba2ce18c20dc
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-08-29 09:48:25 +0100

    Add trip_id override

--- controller/apps/frontend/src/components/rides/detail/RidesDetailJustifications/index.tsx
+++ controller/apps/frontend/src/components/rides/detail/RidesDetailJustifications/index.tsx
@@ -2,7 +2,7 @@
 
 /* * */
 
-import { Button, Collapsible, Section, Textarea } from '@tmlmobilidade/ui';
+import { Button, Collapsible, Section, Textarea, TextInput } from '@tmlmobilidade/ui';
 
 /* * */
 
@@ -10,7 +10,8 @@ export function RidesDetailJustifications() {
 	return (
 		<Collapsible description="O que aconteceu com esta circulação." title="Justificações">
 			<Section gap="lg">
-				<Textarea resize="vertical" size="lg" w="100%" autosize />
+				<Textarea label="Mensagem" minRows={5} resize="vertical" size="lg" w="100%" autosize />
+				<TextInput label="Trip ID manual (override)" size="lg" w="100%" />
 				<Button label="Submeter Justificação" />
 			</Section>
 		</Collapsible>
