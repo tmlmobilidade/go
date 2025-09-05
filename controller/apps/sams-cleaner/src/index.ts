fa9789569220722b410ca730c562e1db42be7990
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-09-05 17:46:34 +0100

    Add new sams-cleaner package

---
+++ controller/apps/sams-cleaner/src/index.ts
@@ -0,0 +1,45 @@
+/* * */
+
+import LOGGER from '@helperkits/logger';
+import TIMETRACKER from '@helperkits/timer';
+import { sams } from '@tmlmobilidade/interfaces';
+
+/* * */
+
+async function reprocessallSams() {
+	try {
+		//
+
+		LOGGER.init();
+
+		//
+		// Reset all SAMs to 'waiting' so they can be reprocessed
+
+		const timer = new TIMETRACKER();
+
+		const result = await sams.updateMany({}, { system_status: 'waiting' }, { returnResults: false });
+
+		LOGGER.terminate(`Updated ${result.modifiedCount} SAMs. (${timer.get()})`);
+
+		//
+	}
+	catch (err) {
+		LOGGER.error('An error occurred. Halting execution.', err);
+		LOGGER.error('Retrying in 10 seconds...');
+		setTimeout(() => {
+			process.exit(0); // End process
+		}, 10000); // after 10 seconds
+	}
+
+	//
+};
+
+/* * */
+
+(async function init() {
+	const runOnInterval = async () => {
+		await reprocessallSams();
+		setTimeout(runOnInterval, 43_200_000); // 12 hours
+	};
+	runOnInterval();
+})();
