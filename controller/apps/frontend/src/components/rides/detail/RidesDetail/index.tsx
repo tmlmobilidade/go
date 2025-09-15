78e99a1783b1e87b31b41d6094792c64d22e4ae8
jusimen <jusi.monteiro@protonmail.com>
2025-09-15 10:39:40 +0100

    Add RidesDetailViewNavigation component and integrate view selection in RidesDetail context
    - Introduced RidesDetailViewNavigation component for view selection.
    - Updated RidesDetail context to manage selected view state.
    - Modified RidesDetail component to include the new navigation header.
    - Enhanced context with view options for better user interaction.

--- controller/apps/frontend/src/components/rides/detail/RidesDetail/index.tsx
+++ controller/apps/frontend/src/components/rides/detail/RidesDetail/index.tsx
@@ -8,10 +8,10 @@ import { RidesDetailApexOnBoardRefunds } from '@/components/rides/detail/RidesDe
 import { RidesDetailApexOnBoardSales } from '@/components/rides/detail/RidesDetailApexOnBoardSales';
 import { RidesDetailApexValidations } from '@/components/rides/detail/RidesDetailApexValidations';
 import { RidesDetailHeader } from '@/components/rides/detail/RidesDetailHeader';
-import { RidesDetailJustifications } from '@/components/rides/detail/RidesDetailJustifications';
 import { RidesDetailMap } from '@/components/rides/detail/RidesDetailMap';
 import { RidesDetailMetadata } from '@/components/rides/detail/RidesDetailMetadata';
 import { RidesDetailVehicleEvents } from '@/components/rides/detail/RidesDetailVehicleEvents';
+import { RidesDetailViewNavigation } from '@/components/rides/detail/RidesDetailViewNavigation';
 import { useRidesDetailContext } from '@/contexts/RidesDetail.context';
 import { ErrorDisplay, LoadingOverlay, Pane } from '@tmlmobilidade/ui';
 
@@ -37,11 +37,10 @@ export function RidesDetail() {
 	}
 
 	return (
-		<Pane header={[<RidesDetailHeader />]}>
+		<Pane header={[<RidesDetailHeader />, <RidesDetailViewNavigation />]}>
 			<RidesDetailMap />
 			<RidesDetailMetadata />
 			<RidesDetailAnalysisResult />
-			<RidesDetailJustifications />
 			<RidesDetailVehicleEvents />
 			<RidesDetailApexValidations />
 			<RidesDetailApexOnBoardSales />
