43732a25f2424912d1621cc25ec0265a2ad94eca
jusimen <jusi.monteiro@protonmail.com>
2025-09-24 16:54:14 +0100

    feat: add real-time updates for ride acceptance based on analysis changes, including new monitoring logic and improved test handling

--- controller/apps/rides-acceptor/src/index.ts
+++ controller/apps/rides-acceptor/src/index.ts
@@ -2,7 +2,7 @@
 
 import LOGGER from '@helperkits/logger';
 import TIMETRACKER from '@helperkits/timer';
-import { AggregationPipeline, rideAcceptances, rides } from '@tmlmobilidade/interfaces';
+import { AggregationPipeline, ChangeStreamDocument, rideAcceptances, rides } from '@tmlmobilidade/interfaces';
 import { normalizeRide } from '@tmlmobilidade/sae-controller-pckg-ride-normalized';
 import { Ride } from '@tmlmobilidade/types';
 import { Dates } from '@tmlmobilidade/utils';
@@ -17,12 +17,12 @@ async function main() {
 	try {
 		//
 
-		LOGGER.init();
 		const globalTimer = new TIMETRACKER();
+		LOGGER.title(`Running Acceptor [${Dates.now('Europe/Lisbon').toLocaleString(Dates.FORMATS.DATETIME_FULL_WITH_SECONDS)}]`);
 
 		//
 		//
-		const startTimeScheduledMaxLimit = Dates.now('Europe/Lisbon').minus({ minutes: 10 }).unix_timestamp;
+		const startTimeScheduledMaxLimit = Dates.now('Europe/Lisbon').plus({ days: 3 }).unix_timestamp;
 		const startTimeScheduledMinLimit = Dates.now('Europe/Lisbon').minus({ days: 3, minutes: 10 }).unix_timestamp;
 
 		/**
@@ -56,7 +56,7 @@ async function main() {
 
 			//
 			// Test the ride against the required tests.
-			const [allRequiredTestsArePass, requiredTestsSummary] = testRide(normalizedRide);
+			const [allRequiredTestsArePass, requiredTestsSummary] = testRide(normalizedRide.analysis);
 
 			//
 			// Create the acceptance.
@@ -90,10 +90,99 @@ async function main() {
 	//
 }
 
+async function updateAcceptanceOnAnalysis() {
+	try {
+		//
+		LOGGER.title(`Watching for changes in the rides collection...`);
+
+		//
+		//
+		const rideAcceptancesFound = await rideAcceptances.findMany({ created_at: { $gte: Dates.now('Europe/Lisbon').minus({ days: 3 }).unix_timestamp } });
+		LOGGER.info(`Watching for changes in the rides collection for ${rideAcceptancesFound.length} ride acceptances...`);
+
+		//
+		// Pipeline to listen to changes in the rides collection.
+		const pipeline: AggregationPipeline<Ride> = [
+			{ $match: { 'fullDocument._id': { $in: rideAcceptancesFound.map(rideAcceptance => rideAcceptance.ride_id) } } },
+			{ $match: { operationType: 'update' } },
+			{
+				$match: {
+					$expr: {
+						$gt: [
+							{
+								$size: {
+									$filter: {
+										as: 'field',
+										cond: { $regexMatch: { input: '$$field.k', regex: /^analysis\./ } },
+										input: { $objectToArray: '$updateDescription.updatedFields' },
+									},
+								},
+							},
+							0,
+						],
+					},
+				},
+			},
+		];
+
+		const ridesCollection = await rides.getCollection();
+
+		ridesCollection
+			.watch(pipeline, { fullDocument: 'updateLookup' })
+			.on('change', async (operation: ChangeStreamDocument<Ride>) => {
+				//
+				console.log('HERE =======> ', JSON.stringify(operation, null, 2));
+
+				//
+				// If the operation is not an update, return.
+				if (operation.operationType !== 'update') {
+					return;
+				}
+
+				//
+				// If the updated fields do not contain any keys starting with 'analysis.', return.
+				if (!Object.keys(operation.updateDescription.updatedFields).some(key => key.startsWith('analysis.'))) {
+					return;
+				}
+
+				//
+				// Test the ride against the required tests.
+				const [allRequiredTestsArePass, requiredTestsSummary] = testRide(operation.fullDocument.analysis);
+
+				//
+				// Update the acceptance.
+				await rideAcceptances.updateByRideId(operation.fullDocument._id, {
+					acceptance_status: allRequiredTestsArePass ? 'accepted' : 'justification_required',
+					analysis_summary: requiredTestsSummary,
+				});
+
+				//
+				// Log the ride acceptance.
+				LOGGER.info(`Updated acceptance for ride ${operation.fullDocument._id} with status ${allRequiredTestsArePass ? 'accepted' : 'justification_required'}.`);
+			});
+	}
+	catch (error) {
+		LOGGER.error('An error occurred. Halting execution.', error);
+		LOGGER.info('Retrying in 10 seconds...');
+		setTimeout(() => {
+			process.exit(0); // End process
+		}, 10000); // after 10 seconds
+	}
+
+	//
+}
+
 //
 
 (async function init() {
+	//
+
+	LOGGER.init();
+
+	//
+	// Run the main function on interval.
 	const runOnInterval = async () => {
+		updateAcceptanceOnAnalysis(); // Watch for changes in the rides collection.
 		await main();
 		setTimeout(runOnInterval, RUN_INTERVAL);
 	};
--- controller/apps/rides-acceptor/src/utils.ts
+++ controller/apps/rides-acceptor/src/utils.ts
@@ -1,5 +1,4 @@
-import { RideNormalized } from '@tmlmobilidade/sae-controller-pckg-ride-normalized';
-import { RideAnalysis, RideAnalysisSummary } from '@tmlmobilidade/types';
+import { Ride, RideAnalysis, RideAnalysisSummary } from '@tmlmobilidade/types';
 
 /* * */
 
@@ -9,22 +8,22 @@ export const REQUIRED_TESTS = ['AT_LEAST_ONE_VEHICLE_EVENT_ON_FIRST_STOP', 'SIMP
 
 /**
  * This function tests a ride by running a set of required tests and returning a summary of the results.
- * @param ride The ride to test.
+ * @param ride_analysis The ride analysis to test.
  * @param requiredTests The tests to run.
  * @returns A tuple containing a boolean indicating if all required tests passed and a summary of the results to be used for the ride acceptance.
  */
-export function testRide(ride: RideNormalized): [boolean, RideAnalysisSummary] {
+export function testRide(ride_analysis: Ride['analysis']): [boolean, RideAnalysisSummary] {
 	const summaryEntries: [string, RideAnalysis][] = [];
 
 	for (const test of REQUIRED_TESTS) {
-		const analysis = ride.analysis[test] ?? { grade: 'fail', message: 'Test was not found for this ride', reason: test };
+		const analysis = ride_analysis[test] ?? { grade: 'fail', message: 'Test was not found for this ride', reason: test };
 		summaryEntries.push([test, analysis]);
 	}
 
 	const requiredTestsSummary: RideAnalysisSummary = Object.fromEntries(summaryEntries);
 
 	const allRequiredTestsArePass = REQUIRED_TESTS.every((test) => {
-		const analysis = ride.analysis[test];
+		const analysis = ride_analysis[test];
 		return analysis?.grade === 'pass';
 	});
 
