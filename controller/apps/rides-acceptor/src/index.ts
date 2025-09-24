8fe7100269fa3a1804c1ab3f17b1210e599f7978
jusimen <jusi.monteiro@protonmail.com>
2025-09-24 15:07:44 +0100

    refactor: implement ride acceptance logic for ended and missed rides, including aggregation pipelines and testing mechanisms

--- controller/apps/rides-acceptor/src/index.ts
+++ controller/apps/rides-acceptor/src/index.ts
@@ -1,60 +1,101 @@
-import { type ChangeStreamDocument, rides } from '@tmlmobilidade/interfaces';
-import { normalizeRide, type RideNormalized } from '@tmlmobilidade/sae-controller-pckg-ride-normalized';
-import { Ride, RideAnalysis, RideAnalysisSummary } from '@tmlmobilidade/types';
+/* * */
+
+import LOGGER from '@helperkits/logger';
+import TIMETRACKER from '@helperkits/timer';
+import { AggregationPipeline, rideAcceptances, rides } from '@tmlmobilidade/interfaces';
+import { normalizeRide } from '@tmlmobilidade/sae-controller-pckg-ride-normalized';
+import { Ride } from '@tmlmobilidade/types';
+import { Dates } from '@tmlmobilidade/utils';
+
+import { testRide } from './utils.js';
+
+/* * */
+
+const RUN_INTERVAL = 600_000; // 10 minutes in milliseconds
 
 async function main() {
+	try {
+		//
+
+		LOGGER.init();
+		const globalTimer = new TIMETRACKER();
+
+		//
+		//
+		const startTimeScheduledMaxLimit = Dates.now('Europe/Lisbon').minus({ minutes: 10 }).unix_timestamp;
+		const startTimeScheduledMinLimit = Dates.now('Europe/Lisbon').minus({ days: 3, minutes: 10 }).unix_timestamp;
+
+		/**
+		 * Aggregation pipeline to filter rides that are scheduled to start within a specific time range
+		 * and do not have any associated acceptances.
+		 */
+		const pipeline: AggregationPipeline<Ride> = [
+			{ $match: { start_time_scheduled: { $gte: startTimeScheduledMinLimit, $lte: startTimeScheduledMaxLimit } } },
+			{ $lookup: { as: 'acceptances', foreignField: 'ride_id', from: 'ride_acceptances', localField: '_id' } },
+			{ $match: { $expr: { $eq: [{ $size: '$acceptances' }, 0] } } },
+		];
+
+		//
+		// Fetch the rides.
+
+		LOGGER.info(`Fetching rides between ${Dates.fromUnixTimestamp(startTimeScheduledMinLimit).setZone('Europe/Lisbon', 'offset_only').toLocaleString(Dates.FORMATS.DATETIME_FULL_WITH_SECONDS)} (${startTimeScheduledMinLimit}) and ${Dates.fromUnixTimestamp(startTimeScheduledMaxLimit).setZone('Europe/Lisbon', 'offset_only').toLocaleString(Dates.FORMATS.DATETIME_FULL_WITH_SECONDS)} (${startTimeScheduledMaxLimit})...`);
+
+		const fetchTimer = new TIMETRACKER();
+		const ridesBatch = await rides.aggregate(pipeline);
+
+		LOGGER.info(`Found ${ridesBatch.length} rides. (${fetchTimer.get()})`);
+
+		for (const ride of ridesBatch) {
+			//
+			// Normalize the ride.
+			const normalizedRide = normalizeRide(ride);
+
+			//
+			// If the ride is not ended or missed, skip it.
+			if (normalizedRide.operational_status !== 'ended' && normalizedRide.operational_status !== 'missed') continue;
+
+			//
+			// Test the ride against the required tests.
+			const [allRequiredTestsArePass, requiredTestsSummary] = testRide(normalizedRide);
+
+			//
+			// Create the acceptance.
+			await rideAcceptances.createByRideId(ride._id, {
+				acceptance_status: allRequiredTestsArePass ? 'accepted' : 'justification_required',
+				analysis_summary: requiredTestsSummary,
+				comments: [],
+				is_locked: false,
+				justification: null,
+				ride_id: ride._id,
+			});
+
+			//
+			// Log the ride acceptance.
+			LOGGER.info(`Created acceptance for ride ${ride._id} with status ${allRequiredTestsArePass ? 'accepted' : 'justification_required'}.`);
+		}
+
+		//
+		// Log the number of rides found.
+
+		LOGGER.success(`Processed ${ridesBatch.length} rides. (${globalTimer.get()})`);
+	}
+	catch (error) {
+		LOGGER.error('An error occurred. Halting execution.', error);
+		LOGGER.info('Retrying in 10 seconds...');
+		setTimeout(() => {
+			process.exit(0); // End process
+		}, 10000); // after 10 seconds
+	}
+
 	//
-	console.log('Starting Rides Acceptor...');
-
-	// 1. Check if ride as ended
-
-	const ridesCollection = await rides.getCollection();
-
-	ridesCollection
-		.watch([], { fullDocument: 'updateLookup' })
-		.on('change', (operation: ChangeStreamDocument<Ride>) => {
-			if (operation.operationType !== 'update') {
-				return;
-			}
-
-			console.log('Ride Updated: ', operation.fullDocument._id);
-
-			const normalizedRide: RideNormalized = normalizeRide(operation.fullDocument);
-
-			if (normalizedRide.operational_status === 'scheduled' || normalizedRide.operational_status === 'running') {
-				return;
-			}
-
-			console.log('Ride Ended: ', normalizedRide._id);
-
-			// Test
-			function testRide(ride: RideNormalized, requiredTests: string[]): [boolean, RideAnalysisSummary] {
-				const summaryEntries: [string, RideAnalysis][] = [];
-
-				for (const test of requiredTests) {
-					const analysis = ride.analysis[test] ?? { grade: 'fail', message: 'Test was not found for this ride', reason: test };
-					summaryEntries.push([test, analysis]);
-				}
-
-				const requiredTestsSummary: RideAnalysisSummary = Object.fromEntries(summaryEntries);
-
-				const allRequiredTestsArePass = requiredTests.every((test) => {
-					const analysis = ride.analysis[test];
-					return analysis?.grade === 'pass';
-				});
-
-				return [allRequiredTestsArePass, requiredTestsSummary];
-			}
-
-			const [allRequiredTestsArePass, requiredTestsSummary] = testRide(normalizedRide, ['AT_LEAST_ONE_VEHICLE_EVENT_ON_FIRST_STOP', 'SIMPLE_THREE_VEHICLE_EVENTS', 'SIMPLE_ONE_APEX_VALIDATION']);
-			console.log('Ride Acceptance: ', normalizedRide._id, ' - ', allRequiredTestsArePass);
-			console.log('Ride Acceptance Summary: ', requiredTestsSummary);
-		});
 }
 
 //
 
-main().catch((err) => {
-	console.error(err);
-	process.exit(1);
-});
+(async function init() {
+	const runOnInterval = async () => {
+		await main();
+		setTimeout(runOnInterval, RUN_INTERVAL);
+	};
+	runOnInterval();
+})();
---
+++ controller/apps/rides-acceptor/src/utils.ts
@@ -0,0 +1,32 @@
+import { RideNormalized } from '@tmlmobilidade/sae-controller-pckg-ride-normalized';
+import { RideAnalysis, RideAnalysisSummary } from '@tmlmobilidade/types';
+
+/* * */
+
+export const REQUIRED_TESTS = ['AT_LEAST_ONE_VEHICLE_EVENT_ON_FIRST_STOP', 'SIMPLE_THREE_VEHICLE_EVENTS', 'SIMPLE_ONE_APEX_VALIDATION'];
+
+/* * */
+
+/**
+ * This function tests a ride by running a set of required tests and returning a summary of the results.
+ * @param ride The ride to test.
+ * @param requiredTests The tests to run.
+ * @returns A tuple containing a boolean indicating if all required tests passed and a summary of the results to be used for the ride acceptance.
+ */
+export function testRide(ride: RideNormalized): [boolean, RideAnalysisSummary] {
+	const summaryEntries: [string, RideAnalysis][] = [];
+
+	for (const test of REQUIRED_TESTS) {
+		const analysis = ride.analysis[test] ?? { grade: 'fail', message: 'Test was not found for this ride', reason: test };
+		summaryEntries.push([test, analysis]);
+	}
+
+	const requiredTestsSummary: RideAnalysisSummary = Object.fromEntries(summaryEntries);
+
+	const allRequiredTestsArePass = REQUIRED_TESTS.every((test) => {
+		const analysis = ride.analysis[test];
+		return analysis?.grade === 'pass';
+	});
+
+	return [allRequiredTestsArePass, requiredTestsSummary];
+}
