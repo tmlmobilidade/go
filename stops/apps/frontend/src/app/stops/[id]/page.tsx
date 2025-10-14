46d42cec85e8afda765380f18545e8dfff2e04d4
Bruno Castelo <brunoalexcastelo@gmail.com>
2025-10-14 17:56:50 +0100

    fix: update ProposedChangesContextProvider to use relatedId and set scope to 'stop'

--- stops/apps/frontend/src/app/stops/[id]/page.tsx
+++ stops/apps/frontend/src/app/stops/[id]/page.tsx
@@ -10,7 +10,7 @@ export default async function Page({ params }: { params: Promise<{ id: string }>
 	const { id } = await params;
 	return (
 		<StopDetailContextProvider stopId={id}>
-			<ProposedChangesContextProvider stopId={id}>
+			<ProposedChangesContextProvider relatedId={id} scope="stop">
 				<StopDetail />
 			</ProposedChangesContextProvider>
 		</StopDetailContextProvider>
