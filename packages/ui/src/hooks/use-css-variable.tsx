f84ca6ace72a586b06eec46d72a23584c000eb46
Jusi Monteiro <jusi.monteiro@protonmail.com>
2025-11-06 04:21:56 +0000

    Update import path for getCssVariableValue utility

--- packages/ui/src/hooks/use-css-variable.tsx
+++ packages/ui/src/hooks/use-css-variable.tsx
@@ -2,7 +2,7 @@
 
 /* * */
 
-import { getCssVariableValue } from '@tmlmobilidade/utils';
+import { getCssVariableValue } from '@/utils/get-variable-value';
 import { useEffect, useState } from 'react';
 
 /**
