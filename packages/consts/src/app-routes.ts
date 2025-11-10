d47defc18f88f3b74d16c1ebc3cb25b4ea42e864
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-11-10 23:27:12 +0000

    Fix stops URL

--- packages/consts/src/app-routes.ts
+++ packages/consts/src/app-routes.ts
@@ -99,8 +99,8 @@ export const PAGE_ROUTES = Object.freeze({
 	/* STOPS */
 	stops: {
 		// STOPS
-		STOPS_DETAIL: (id: string) => `/${id}`,
-		STOPS_LIST: '/',
+		STOPS_DETAIL: (id: string) => `${getAppConfig('stops', 'frontend_url')}/stops/${id}`,
+		STOPS_LIST: `${getAppConfig('stops', 'frontend_url')}/stops`,
 	},
 } as const);
 
