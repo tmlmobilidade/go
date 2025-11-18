9716c5d2cce1747647f78d21ef5f5007b8d5c30e
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-11-18 18:07:49 +0000

    Add set symlinks script

---
+++ scripts/set-public-symlinks.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+## Detect directories with the following structure:
+## modules/{MODULE}/apps/frontend/
+
+find modules -type d -path "*/apps/frontend" | while read -r dir; do
+
+    public_dir="$dir/public"
+    echo "Setting up symlink at $public_dir"
+
+    # Remove existing directory or symlink
+    rm -rf "$public_dir"
+
+    # Create public as a symlink pointing to root-level assets
+    ln -s "$(pwd)/assets" "$public_dir"
+
+done
