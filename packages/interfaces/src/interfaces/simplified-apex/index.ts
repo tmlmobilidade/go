1c2507a03df0bc36b0a56cba3df9ea5d2b6d3064
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-11-21 15:50:36 +0000

    Renamed directory

---
+++ packages/interfaces/src/interfaces/simplified-apex/index.ts
@@ -0,0 +1,6 @@
+export * from '@/interfaces/simplified-apex/simplified-apex-inspection-decisions.js';
+export * from '@/interfaces/simplified-apex/simplified-apex-inspections.js';
+export * from '@/interfaces/simplified-apex/simplified-apex-locations.js';
+export * from '@/interfaces/simplified-apex/simplified-apex-on-board-refunds.js';
+export * from '@/interfaces/simplified-apex/simplified-apex-on-board-sales.js';
+export * from '@/interfaces/simplified-apex/simplified-apex-validations.js';
