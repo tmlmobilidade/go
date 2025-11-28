f99a64706aa58f797d5693f84754c89e9a2f2f48
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-11-28 11:50:47 +0000

    Updated URL for GTFS

--- modules/exporter/apps/api/src/endpoints/gtfs-merged/gtfs-merged.controller.ts
+++ modules/exporter/apps/api/src/endpoints/gtfs-merged/gtfs-merged.controller.ts
@@ -2,7 +2,7 @@
 
 import { HttpException, HttpStatus } from '@tmlmobilidade/consts';
 import { type FastifyReply, type FastifyRequest } from '@tmlmobilidade/fastify';
-import { fileExports, files } from '@tmlmobilidade/interfaces';
+import { files } from '@tmlmobilidade/interfaces';
 
 /* * */
 
@@ -15,11 +15,8 @@ export class GtfsMergedController {
 	 * @param reply The reply object
 	 */
 	static async download(request: FastifyRequest, reply: FastifyReply<string>) {
-		// Retrieve file export record from database
-		const foundFileExports = await fileExports.findById('gtfs-merged-latest');
-		if (!foundFileExports) throw new HttpException(HttpStatus.NOT_FOUND, 'File Export not found');
 		// Retrieve file data from database
-		const foundFileData = await files.findById(foundFileExports.file_id);
+		const foundFileData = await files.findById('gtfs-merged-latest');
 		if (!foundFileData) throw new HttpException(HttpStatus.NOT_FOUND, 'File not found');
 		// Send file URL as response to client
 		reply.send({ data: foundFileData.url, error: null, statusCode: HttpStatus.OK });
