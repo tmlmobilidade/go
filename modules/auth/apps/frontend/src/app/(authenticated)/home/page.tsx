d5ba1539d62619f69abf28e7e633f8f6c1e67bb1
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-12-04 00:44:37 +0000

    Cleanup styles

--- modules/auth/apps/frontend/src/app/(authenticated)/home/page.tsx
+++ modules/auth/apps/frontend/src/app/(authenticated)/home/page.tsx
@@ -1,25 +1,9 @@
-'use client';
-
 /* * */
 
 import { HomePage } from '@/components/home/HomePage';
-import { OrganizationsDetailContextProvider } from '@/contexts/OrganizationDetail.context';
-import { useMeContext } from '@tmlmobilidade/ui';
 
 /* * */
 
-export default function Page() {
-	//
-
-	//
-	// A. Setup variables
-	const me = useMeContext();
-
-	//
-	// B. Render Components
-	return (
-		<OrganizationsDetailContextProvider organization_id={me.data.user.organization_id}>
-			<HomePage />
-		</OrganizationsDetailContextProvider>
-	);
+export default async function Page() {
+	return <HomePage />;
 }
