d4cc564fc6c60f3348547abf4bf56e3c95fafffb
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-12-12 11:02:01 +0000

    Make select dropdown more prominent

--- packages/ui/src/components/inputs/Select/index.tsx
+++ packages/ui/src/components/inputs/Select/index.tsx
@@ -7,11 +7,13 @@ import { Select as MantineSelect, type SelectProps as MantineSelectProps } from 
 /* * */
 
 interface SelectProps extends Omit<MantineSelectProps, 'allowDeselect'> {
+
 	/**
 	 * The `key` prop is required to ensure correct re-mounting behavior.
 	 * Use the `form.key('fieldName')` method to generate a unique key based on the form state.
 	 */
 	key?: string
+
 };
 
 /**
