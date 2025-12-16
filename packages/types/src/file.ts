81560753e21d03dba8fccad3c3cca92c48f96cf1
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-12-16 19:09:28 +0000

    Remove is_locked from files

--- packages/types/src/file.ts
+++ packages/types/src/file.ts
@@ -5,18 +5,20 @@ import { z } from 'zod';
 
 /* * */
 
-export const FileSchema = DocumentSchema.extend({
-	created_by: z.string(),
-	description: z.string().nullish(),
-	metadata: z.record(z.unknown()).nullish(),
-	name: z.string(),
-	resource_id: z.string(),
-	scope: z.string(),
-	size: z.number().describe('size in bytes'),
-	type: z.string().describe('mime type'),
-	updated_by: z.string(),
-	url: z.string().nullish(),
-});
+export const FileSchema = DocumentSchema
+	.omit({ is_locked: true })
+	.extend({
+		created_by: z.string(),
+		description: z.string().nullish(),
+		metadata: z.record(z.unknown()).nullish(),
+		name: z.string(),
+		resource_id: z.string(),
+		scope: z.string(),
+		size: z.number().describe('size in bytes'),
+		type: z.string().describe('mime type'),
+		updated_by: z.string(),
+		url: z.string().nullish(),
+	});
 
 export const CreateFileSchema = FileSchema.omit({ _id: true, created_at: true, updated_at: true });
 export const UpdateFileSchema = FileSchema.omit({ _id: true, created_at: true, created_by: true, updated_at: true }).partial();
