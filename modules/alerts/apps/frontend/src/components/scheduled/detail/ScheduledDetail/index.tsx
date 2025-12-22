2e591f374197cdcaba2912be9862427d76eb5c81
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-12-22 22:11:25 +0000

    Refactor ScheduledDetail component for improved readability and error handling

--- modules/alerts/apps/frontend/src/components/scheduled/detail/ScheduledDetail/index.tsx
+++ modules/alerts/apps/frontend/src/components/scheduled/detail/ScheduledDetail/index.tsx
@@ -2,17 +2,36 @@
 
 /* * */
 
+import { useScheduledDetailContext } from '@/components/scheduled/detail/ScheduledDetail.context';
 import { ScheduledDetailHeader } from '@/components/scheduled/detail/ScheduledDetailHeader';
 import { ScheduledDetailSectionCauseEffect } from '@/components/scheduled/detail/ScheduledDetailSectionCauseEffect';
 import { ScheduledDetailSectionReferences } from '@/components/scheduled/detail/ScheduledDetailSectionReferences';
 import { ScheduledDetailSectionTitle } from '@/components/scheduled/detail/ScheduledDetailSectionTitle';
 import { ScheduledDetailSectionValidity } from '@/components/scheduled/detail/ScheduledDetailSectionValidity';
 import { ScheduledDetailSectionVisibility } from '@/components/scheduled/detail/ScheduledDetailSectionVisibility';
-import { Pane } from '@tmlmobilidade/ui';
+import { ErrorDisplay, LoadingOverlay, Pane } from '@tmlmobilidade/ui';
 
 /* * */
 
 export function ScheduledDetail() {
+	//
+
+	//
+	// A. Setup variables
+
+	const scheduledDetailContext = useScheduledDetailContext();
+
+	//
+	// B. Render components
+
+	if (scheduledDetailContext.flags.isLoading) {
+		return <LoadingOverlay />;
+	}
+
+	if (scheduledDetailContext.flags.error) {
+		return <ErrorDisplay message={scheduledDetailContext.flags.error.message} />;
+	}
+
 	return (
 		<Pane header={[<ScheduledDetailHeader />]}>
 			<ScheduledDetailSectionTitle />
