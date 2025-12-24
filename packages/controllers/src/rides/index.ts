26f4a7360be2a48fa6bd3b7cf7a42169e3913409
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-12-24 07:19:10 +0000

    Add shared crontrollers package

---
+++ packages/controllers/src/rides/index.ts
@@ -0,0 +1,2 @@
+export * from '@/rides/rides.js';
+export * from '@/rides/schema.js';
---
+++ packages/controllers/src/rides/schema.ts
@@ -0,0 +1,39 @@
+/* * */
+
+import { DelayStatusSchema, OperationalStatusSchema, RideAcceptanceStatusSchema, RideAnalysisGradeSchema, SeenStatusSchema, validateUnixTimestamp } from '@tmlmobilidade/types';
+import { z } from 'zod';
+
+const RideAnalysisGradeWithNoneSchema = RideAnalysisGradeSchema.or(z.literal('none'));
+
+export const GetRidesBatchQuerySchema = z.object({
+	agency_ids: z.preprocess((val: string) => val ? val.split(',').map(id => id.trim()) : [], z.array(z.string())).optional(),
+	search: z.string().optional(),
+
+	/* * */
+
+	analysis_ended_at_last_stop_grade: z.preprocess((val: string) => val ? val.split(',').map(grade => grade.trim()) : [], z.array(RideAnalysisGradeWithNoneSchema)).optional(),
+	analysis_expected_apex_validation_interval: z.preprocess((val: string) => val ? val.split(',').map(grade => grade.trim()) : [], z.array(RideAnalysisGradeWithNoneSchema)).optional(),
+	analysis_simple_three_vehicle_events_grade: z.preprocess((val: string) => val ? val.split(',').map(grade => grade.trim()) : [], z.array(RideAnalysisGradeWithNoneSchema)).optional(),
+	analysis_transaction_sequentiality: z.preprocess((val: string) => val ? val.split(',').map(grade => grade.trim()) : [], z.array(RideAnalysisGradeWithNoneSchema)).optional(),
+
+	/* * */
+
+	date_end: z.coerce.number().transform(validateUnixTimestamp).brand('UnixTimestamp'),
+	date_start: z.coerce.number().transform(validateUnixTimestamp).brand('UnixTimestamp'),
+
+	/* * */
+
+	delay_statuses: z.preprocess((val: string) => val ? val.split(',').map(status => status.trim()) : [], z.array(DelayStatusSchema)).optional(),
+	operational_statuses: z.preprocess((val: string) => val ? val.split(',').map(status => status.trim()) : [], z.array(OperationalStatusSchema)).optional(),
+	seen_statuses: z.preprocess((val: string) => val ? val.split(',').map(status => status.trim()) : [], z.array(SeenStatusSchema)).optional(),
+
+	/* * */
+
+	line_ids: z.preprocess((val: string) => val ? val.split(',').map(id => id.trim()) : [], z.array(z.string())).optional(),
+	stop_ids: z.preprocess((val: string) => val ? val.split(',').map(id => id.trim()) : [], z.array(z.string())).optional(),
+
+	/* * */
+	acceptance_status: z.preprocess((val: string) => val ? val.split(',').map(status => status.trim()) : [], z.array(z.enum([...RideAcceptanceStatusSchema.options, 'none']))).optional(),
+});
+
+export type GetRidesBatchQuery = z.infer<typeof GetRidesBatchQuerySchema>;
