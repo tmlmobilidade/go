e8e3fd97b01684ac7355d394062225a84ae00a6d
Bruno Castelo <brunoalexcastelo@gmail.com>
2026-01-06 10:08:26 +0000

    Agencies i18n fixes

--- modules/auth/apps/frontend/src/components/agencies/detail/AgencySectionContacts/index.tsx
+++ modules/auth/apps/frontend/src/components/agencies/detail/AgencySectionContacts/index.tsx
@@ -15,29 +15,29 @@ export function AgencySectionContacts() {
 	// A. Setup variables
 
 	const agencyDetailContext = useAgencyDetailContext();
-	const { t } = useTranslation('auth', { keyPrefix: 'agencies.detail.Contacts' });
+	const { t } = useTranslation('auth');
 
 	//
 	// B. Render components
 
 	return (
 		<Collapsible
-			description={t('description')}
-			title={t('title')}
+			description={t('agencies.detail.SectionContacts.description')}
+			title={t('agencies.detail.SectionContacts.title')}
 		>
 			<Section gap="lg">
 				<Grid>
 					<TagsInput
 						key={agencyDetailContext.data.form.key('contact_emails_pto')}
-						description={t('fields.contactEmailsPto')}
-						label={t('fields.contactEmailsPtoLabel')}
+						description={t('agencies.detail.SectionContacts.fields.contact_emails_pto.label')}
+						label={t('agencies.detail.SectionContacts.fields.contact_emails_pto.title')}
 						readOnly={agencyDetailContext.flags.isReadOnly}
 						{...agencyDetailContext.data.form.getInputProps('contact_emails_pto')}
 					/>
 					<TagsInput
 						key={agencyDetailContext.data.form.key('contact_emails_pta')}
-						description={t('fields.contactEmailPta')}
-						label={t('fields.contactEmailPtaLabel')}
+						description={t('agencies.detail.SectionContacts.fields.contact_emails_pta.label')}
+						label={t('agencies.detail.SectionContacts.fields.contact_emails_pta.title')}
 						readOnly={agencyDetailContext.flags.isReadOnly}
 						w="100%"
 						{...agencyDetailContext.data.form.getInputProps('contact_emails_pta')}
