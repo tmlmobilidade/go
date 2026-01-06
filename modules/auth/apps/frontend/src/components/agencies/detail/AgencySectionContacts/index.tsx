e797636e4bf85b87befe924a9331758ab1c50162
Bruno Castelo <brunoalexcastelo@gmail.com>
2026-01-06 09:45:18 +0000

    .

--- modules/auth/apps/frontend/src/components/agencies/detail/AgencySectionContacts/index.tsx
+++ modules/auth/apps/frontend/src/components/agencies/detail/AgencySectionContacts/index.tsx
@@ -4,6 +4,7 @@
 
 import { useAgencyDetailContext } from '@/components/agencies/detail/AgencyDetail.context';
 import { Collapsible, Grid, Section, TagsInput } from '@tmlmobilidade/ui';
+import { useTranslation } from 'react-i18next';
 
 /* * */
 
@@ -14,28 +15,29 @@ export function AgencySectionContacts() {
 	// A. Setup variables
 
 	const agencyDetailContext = useAgencyDetailContext();
+	const { t } = useTranslation('auth', { keyPrefix: 'agencies.detail.Contacts' });
 
 	//
 	// B. Render components
 
 	return (
 		<Collapsible
-			description="Informação de contatos da agência"
-			title="Informação de contatos"
+			description={t('description')}
+			title={t('title')}
 		>
 			<Section gap="lg">
 				<Grid>
 					<TagsInput
 						key={agencyDetailContext.data.form.key('contact_emails_pto')}
-						description="Notificações serão enviadas para os emails de contacto da agência."
-						label="Emails de contacto do Operador"
+						description={t('fields.contactEmailsPto')}
+						label={t('fields.contactEmailsPtoLabel')}
 						readOnly={agencyDetailContext.flags.isReadOnly}
 						{...agencyDetailContext.data.form.getInputProps('contact_emails_pto')}
 					/>
 					<TagsInput
 						key={agencyDetailContext.data.form.key('contact_emails_pta')}
-						description="Notificações serão enviadas para os emails de contacto da TML."
-						label="Emails de contacto da Autoridade"
+						description={t('fields.contactEmailPta')}
+						label={t('fields.contactEmailPtaLabel')}
 						readOnly={agencyDetailContext.flags.isReadOnly}
 						w="100%"
 						{...agencyDetailContext.data.form.getInputProps('contact_emails_pta')}
