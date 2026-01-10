1ae293fbb8675478857b54089f41f7efb9eca7e4
João de Vasconcelos <johnyvasconcelos@icloud.com>
2026-01-10 12:29:35 +0000

    Setup auth config

--- modules/auth/apps/frontend/src/app/(authenticated)/layout.tsx
+++ modules/auth/apps/frontend/src/app/(authenticated)/layout.tsx
@@ -1,7 +1,7 @@
 /* * */
 
+import { i18nNamespaces } from '@/i18n/resources';
 import { DataProviders } from '@/providers/data-providers';
-import ptTranslation from '@/translations/pt.json';
 import { AppProvider, AppWrapper } from '@tmlmobilidade/ui';
 import { type PropsWithChildren } from 'react';
 
@@ -9,7 +9,7 @@ import { type PropsWithChildren } from 'react';
 
 export default async function Layout({ children }: PropsWithChildren) {
 	return (
-		<AppProvider i18n={[{ namespace: 'auth', pt: ptTranslation }]}>
+		<AppProvider i18n={i18nNamespaces}>
 			<AppWrapper>
 				<DataProviders>
 					{children}
