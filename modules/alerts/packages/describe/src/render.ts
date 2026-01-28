16abbc2177d28b9f73a7173e93e5b2be12d8b2f7
João de Vasconcelos <johnyvasconcelos@icloud.com>
2026-01-28 18:38:02 +0000

    Fix

--- modules/alerts/packages/describe/src/render.ts
+++ modules/alerts/packages/describe/src/render.ts
@@ -69,7 +69,7 @@ export async function describeAlert(props: DescribeAlertProps): Promise<Describe
 			// Each translated string has several articles that need to be replaced with actual values.
 			for (const articleKey of Object.keys(templateArticlesReplacements)) {
 				// Use the templates param builders to get the actual value for each article
-				const replacementValue = templateArticlesReplacements[articleKey];
+				const replacementValue = templateArticlesReplacements[articleKey][i18nCode];
 				// Replace all occurrences of the article in the string with the actual value
 				result[resultStringKey][i18nCode] = result[resultStringKey][i18nCode].replaceAll(articleKey, replacementValue);
 			}
