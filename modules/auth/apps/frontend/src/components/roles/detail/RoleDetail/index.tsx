5d770c27dd532ad26b0b275e40f5d4020a0375be
João de Vasconcelos <johnyvasconcelos@icloud.com>
2026-01-28 23:20:02 +0000

    Disable i18n for now

--- modules/auth/apps/frontend/src/components/roles/detail/RoleDetail/index.tsx
+++ modules/auth/apps/frontend/src/components/roles/detail/RoleDetail/index.tsx
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-
 'use client';
 
 /* * */
@@ -10,7 +8,6 @@ import { RoleDetailBasicInfo } from '@/components/roles/detail/RoleDetailBasicIn
 import { RoleDetailHeader } from '@/components/roles/detail/RoleDetailHeader';
 import { permissionsConfig } from '@/lib/permissions';
 import { Pane } from '@tmlmobilidade/ui';
-import { useTranslation } from 'react-i18next';
 
 /* * */
 
@@ -21,7 +18,6 @@ export function RoleDetail() {
 	// A. Setup variables
 
 	const rolesDetailContext = useRoleDetailContext();
-	const { t } = useTranslation();
 
 	//
 	// B. Render components
@@ -33,12 +29,12 @@ export function RoleDetail() {
 				<PermissionSection
 					key={item.scope}
 					configActions={item.actions}
-					description={t(`default:permissions.${item.description}` as any)}
+					description={item.description}
 					enabledPermissions={rolesDetailContext.data.form.values.permissions}
 					onResourceToggle={rolesDetailContext.actions.handlePermissionResourceToggle}
 					onToggle={rolesDetailContext.actions.handlePermissionToggle}
 					scope={item.scope}
-					title={t(`default:permissions.${item.title}` as any)}
+					title={item.title}
 				/>
 			))}
 		</Pane>
