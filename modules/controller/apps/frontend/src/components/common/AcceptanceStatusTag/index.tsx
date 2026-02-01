5af079a60b444d295ebea3f251c8e4e7248e3087
João de Vasconcelos <johnyvasconcelos@icloud.com>
2026-02-01 12:29:20 +0000

    Fix i18n error

--- modules/controller/apps/frontend/src/components/common/AcceptanceStatusTag/index.tsx
+++ modules/controller/apps/frontend/src/components/common/AcceptanceStatusTag/index.tsx
@@ -4,7 +4,7 @@
 
 import { IconAlertCircle, IconCheck, IconClock, IconX } from '@tabler/icons-react';
 import { RideAcceptanceStatus } from '@tmlmobilidade/types';
-import { Tag, TagProps } from '@tmlmobilidade/ui';
+import { Tag } from '@tmlmobilidade/ui';
 import { useTranslation } from 'react-i18next';
 
 /* * */
@@ -14,29 +14,6 @@ interface AcceptanceStatusTagProps {
 }
 
 /* * */
-
-export const AcceptanceStatusProps = Object.freeze({
-	accepted: {
-		icon: <IconCheck />,
-		label: 'default:rides.detail.RidesDetailAcceptanceStatusTag.accepted',
-		variant: 'success',
-	},
-	justification_required: {
-		icon: <IconAlertCircle />,
-		label: 'default:rides.detail.RidesDetailAcceptanceStatusTag.justification_required',
-		variant: 'warning',
-	},
-	rejected: {
-		icon: <IconX />,
-		label: 'default:rides.detail.RidesDetailAcceptanceStatusTag.rejected',
-		variant: 'danger',
-	},
-	under_review: {
-		icon: <IconClock />,
-		label: 'default:rides.detail.RidesDetailAcceptanceStatusTag.under_review',
-		variant: 'secondary',
-	},
-} as const);
 
 export function AcceptanceStatusTag({ grade }: AcceptanceStatusTagProps) {
 	//
@@ -54,24 +31,19 @@ export function AcceptanceStatusTag({ grade }: AcceptanceStatusTagProps) {
 	}
 
 	if (grade === 'accepted') {
-		return <Tag icon={AcceptanceStatusProps.accepted.icon} label={t(AcceptanceStatusProps.accepted.label)} variant={AcceptanceStatusProps.accepted.variant as TagProps['variant']} />;
+		return <Tag icon={<IconCheck />} label={t('default:rides.detail.RidesDetailAcceptanceStatusTag.accepted')} variant="success" />;
 	}
 
 	if (grade === 'under_review') {
-		return <Tag icon={AcceptanceStatusProps.under_review.icon} label={t('default:rides.detail.RidesDetailAcceptanceStatusTag.under_review')} variant={AcceptanceStatusProps.under_review.variant as TagProps['variant']} />;
+		return <Tag icon={<IconClock />} label={t('default:rides.detail.RidesDetailAcceptanceStatusTag.under_review')} variant="secondary" />;
 	}
 
 	if (grade === 'justification_required') {
-		return (
-			<div style={{ color: 'red', display: 'flex', flexDirection: 'column', gap: '4px' }}>
-				<Tag icon={AcceptanceStatusProps.justification_required.icon} label={t(AcceptanceStatusProps.justification_required.label)} variant={AcceptanceStatusProps.justification_required.variant as TagProps['variant']} />
-				<div>{JSON.stringify(AcceptanceStatusProps.justification_required)}</div>
-			</div>
-		);
+		return <Tag icon={<IconAlertCircle />} label={t('default:rides.detail.RidesDetailAcceptanceStatusTag.justification_required')} variant="warning" />;
 	}
 
 	if (grade === 'rejected') {
-		return <Tag icon={AcceptanceStatusProps.rejected.icon} label={t('default:rides.detail.RidesDetailAcceptanceStatusTag.rejected')} variant={AcceptanceStatusProps.rejected.variant as TagProps['variant']} />;
+		return <Tag icon={<IconX />} label={t('default:rides.detail.RidesDetailAcceptanceStatusTag.rejected')} variant="danger" />;
 	}
 
 	//
