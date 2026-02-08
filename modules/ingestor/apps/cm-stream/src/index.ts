0ff6ffa911f58fe179fca6a6bdeadbcc894a409d
João de Vasconcelos <johnyvasconcelos@icloud.com>
2026-02-08 02:29:58 +0000

    Cleanup

--- modules/ingestor/apps/cm-stream/src/index.ts
+++ modules/ingestor/apps/cm-stream/src/index.ts
@@ -1,9 +1,5 @@
 /* * */
 
-import { processApexLocation } from '@/tasks/process-apex-location.js';
-import { processApexOnBoardRefund } from '@/tasks/process-apex-on-board-refund.js';
-import { processApexOnBoardSale } from '@/tasks/process-apex-on-board-sale.js';
-import { processApexValidation } from '@/tasks/process-apex-validation.js';
 import { processVehicleEvent } from '@/tasks/process-vehicle-event.js';
 import { pcgidbLegacy, pcgidbTicketing, pcgidbValidations } from '@tmlmobilidade/interfaces';
 
@@ -21,12 +17,6 @@ import { pcgidbLegacy, pcgidbTicketing, pcgidbValidations } from '@tmlmobilidade
 	// and integrate those documents immediately.
 
 	pcgidbLegacy.VehicleEvents.watch().on('change', processVehicleEvent);
-
-	pcgidbTicketing.SalesEntity.watch().on('change', processApexOnBoardRefund);
-	pcgidbTicketing.SalesEntity.watch().on('change', processApexOnBoardSale);
-
-	pcgidbValidations.LocationEntity.watch().on('change', processApexLocation);
-	pcgidbValidations.ValidationEntity.watch().on('change', processApexValidation);
 
 	//
 })();
