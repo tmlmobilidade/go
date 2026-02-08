fbb70bda5baa01430e9e164a32a216b9f55219f6
João de Vasconcelos <johnyvasconcelos@icloud.com>
2026-02-08 01:09:53 +0000

    Cleanup

--- modules/apex/apps/stream/src/index.ts
+++ modules/apex/apps/stream/src/index.ts
@@ -4,7 +4,6 @@ import { processApexLocation } from '@/tasks/process-apex-location.js';
 import { processApexOnBoardRefund } from '@/tasks/process-apex-on-board-refund.js';
 import { processApexOnBoardSale } from '@/tasks/process-apex-on-board-sale.js';
 import { processApexValidation } from '@/tasks/process-apex-validation.js';
-import { processVehicleEvent } from '@/tasks/process-vehicle-event.js';
 import { pcgidbLegacy, pcgidbTicketing, pcgidbValidations } from '@tmlmobilidade/interfaces';
 
 /* * */
@@ -20,11 +19,8 @@ import { pcgidbLegacy, pcgidbTicketing, pcgidbValidations } from '@tmlmobilidade
 	// Watch for changes to the MongoDB collections
 	// and integrate those documents immediately.
 
-	pcgidbLegacy.VehicleEvents.watch().on('change', processVehicleEvent);
-
 	pcgidbTicketing.SalesEntity.watch().on('change', processApexOnBoardRefund);
 	pcgidbTicketing.SalesEntity.watch().on('change', processApexOnBoardSale);
-
 	pcgidbValidations.LocationEntity.watch().on('change', processApexLocation);
 	pcgidbValidations.ValidationEntity.watch().on('change', processApexValidation);
 
