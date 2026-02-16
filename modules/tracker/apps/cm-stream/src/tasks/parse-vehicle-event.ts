118acd1acfbca8d492b79cb580e8f4c26d33b363
João de Vasconcelos <johnyvasconcelos@icloud.com>
2026-02-16 20:02:20 +0000

    Add initial implementation for CM Stream module with vehicle event processing and synchronization

---
+++ modules/tracker/apps/cm-stream/src/tasks/parse-vehicle-event.ts
@@ -0,0 +1,43 @@
+/* * */
+
+import { Dates } from '@tmlmobilidade/dates';
+import { type SimplifiedVehicleEvent } from '@tmlmobilidade/types';
+import crypto from 'node:crypto';
+
+/* * */
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function parseVehicleEvent(pcgiDoc: any): null | SimplifiedVehicleEvent {
+	try {
+		const parsedVehicleEvent: Omit<SimplifiedVehicleEvent, '_id' | 'received_at'> = {
+			agency_id: pcgiDoc.content.entity[0].vehicle.agencyId,
+			created_at: Dates.fromSeconds(pcgiDoc.content.entity[0].vehicle.timestamp).unix_timestamp,
+			current_status: pcgiDoc.content.entity[0].vehicle.currentStatus,
+			door: pcgiDoc.content.entity[0].vehicle.trigger.door,
+			driver_id: pcgiDoc.content.entity[0].vehicle.vehicle.driverId,
+			extra_trip_id: pcgiDoc.content.entity[0].vehicle.trip?.extraTripId,
+			latitude: pcgiDoc.content.entity[0].vehicle.position.latitude,
+			longitude: pcgiDoc.content.entity[0].vehicle.position.longitude,
+			odometer: pcgiDoc.content.entity[0].vehicle.position.odometer,
+			pattern_id: pcgiDoc.content.entity[0].vehicle.trip?.patternId,
+			stop_id: pcgiDoc.content.entity[0].vehicle.stopId,
+			trip_id: pcgiDoc.content.entity[0].vehicle.trip?.tripId,
+			vehicle_id: pcgiDoc.content.entity[0].vehicle.vehicle._id,
+		};
+		// Hash the contents of the simplified event to create a unique event ID
+		const hashableSimplifiedEvent = JSON.stringify(parsedVehicleEvent);
+		const uniqueIdValueForSimplifiedEvent = crypto
+			.createHash('sha256')
+			.update(hashableSimplifiedEvent)
+			.digest('hex');
+		return {
+			_id: uniqueIdValueForSimplifiedEvent,
+			received_at: Dates.fromUnixTimestamp(pcgiDoc.millis).unix_timestamp,
+			...parsedVehicleEvent,
+		};
+	}
+	catch (error) {
+		console.error(`Error parsing Vehicle Event. Transaction ID: "${pcgiDoc._id}"`, error.message);
+		return null;
+	}
+}
