249c1fb980526735ced3e9317a561f495467f2f5
João de Vasconcelos <johnyvasconcelos@icloud.com>
2026-02-16 22:25:02 +0000

    Refactor vehicle event processing: replace process-vehicle-event module with process-pcgi-vehicle-event, implement database operation handling, and update MongoDB watchers.

--- modules/tracker/apps/cm-stream/src/index.ts
+++ modules/tracker/apps/cm-stream/src/index.ts
@@ -1,7 +1,8 @@
 /* * */
 
-import { processPcgiVehicleEvent } from '@/process-vehicle-event.js';
-import { pcgidbLegacy } from '@tmlmobilidade/interfaces';
+import { processPcgiVehicleEvent } from '@/process-pcgi-vehicle-event.js';
+import { type ChangeStreamDocument, pcgidbLegacy } from '@tmlmobilidade/interfaces';
+import { type PcgiVehicleEvent } from '@tmlmobilidade/types';
 
 /* * */
 
@@ -11,11 +12,20 @@ import { pcgidbLegacy } from '@tmlmobilidade/interfaces';
 	await pcgidbLegacy.connect();
 
 	//
+	// Handle the database operation. Ensure it is an insert operation.
+	// Only insert operations are expected to occur in this PCGIDB collection.
+
+	async function handleDatabaseOperation(databaseOperation: ChangeStreamDocument<PcgiVehicleEvent>) {
+		if (databaseOperation.operationType !== 'insert') return;
+		await processPcgiVehicleEvent(databaseOperation.fullDocument);
+	}
+
+	//
 	// Watch for changes to the MongoDB collections
 	// and integrate those documents immediately.
 
-	pcgidbLegacy.VehicleEventsCore.watch().on('change', processPcgiVehicleEvent);
-	pcgidbLegacy.VehicleEventsLog.watch().on('change', processPcgiVehicleEvent);
+	pcgidbLegacy.VehicleEventsCore.watch().on('change', handleDatabaseOperation);
+	pcgidbLegacy.VehicleEventsLog.watch().on('change', handleDatabaseOperation);
 
 	//
 })();
