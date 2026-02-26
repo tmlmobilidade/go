8b494ed2c80b37bd06635028f166d7452cc37e29
jusimen <jusi.monteiro@protonmail.com>
2026-02-26 15:04:57 +0000

    Refactor ClickHouseType definition for improved readability and maintainability by aligning type declarations in a more structured format.

--- packages/writers/src/clickhouse.ts
+++ packages/writers/src/clickhouse.ts
@@ -11,21 +11,21 @@ import { Timer } from '@tmlmobilidade/timer';
 /**
  * Supported ClickHouse data types
  */
-export type ClickHouseType
-	= | 'Bool'
-	  | 'Boolean' | 'Date32' | 'Date' | 'DateTime' | 'Decimal' | 'Float32'
-	  | 'Float64' | 'Int8' | 'Int16' | 'Int32' | 'Int64' | 'Int128'
-	  | 'Int256' | 'String'
-	  | 'UInt8' | 'UInt16'
-	  | 'UInt32' | 'UInt64'
-	  | 'UInt128' | 'UInt256' | 'UUID' | `Array(${string})`
-	  | `DateTime64(${number})`
-	  | `Decimal(${number}, ${number})`
-	  | `Enum8(${string})`
-	  | `Enum16(${string})`
-	  | `FixedString(${number})`
-	  | `LowCardinality(${string})` | `Map(${string}, ${string})`
-	  | `Nullable(${string})`;
+export type ClickHouseType =
+  | 'Bool'
+  | 'Boolean' | 'Date32' | 'Date' | 'DateTime' | 'Decimal' | 'Float32'
+  | 'Float64' | 'Int8' | 'Int16' | 'Int32' | 'Int64' | 'Int128'
+  | 'Int256' | 'String'
+  | 'UInt8' | 'UInt16'
+  | 'UInt32' | 'UInt64'
+  | 'UInt128' | 'UInt256' | 'UUID' | `Array(${string})`
+  | `DateTime64(${number})`
+  | `Decimal(${number}, ${number})`
+  | `Enum8(${string})`
+  | `Enum16(${string})`
+  | `FixedString(${number})`
+  | `LowCardinality(${string})` | `Map(${string}, ${string})`
+  | `Nullable(${string})`;
 
 export interface ClickHouseColumn<T> {
 	/** Alias expression (computed on read) */
