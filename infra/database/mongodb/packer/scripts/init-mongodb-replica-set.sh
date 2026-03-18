8556ddceabc19a7595a9f3844577924908f30dec
João de Vasconcelos <johnyvasconcelos@icloud.com>
2026-03-18 03:54:26 +0000

    Refactor MongoDB initialization script to remove unused argument; update Docker command to use dynamic port; adjust cloud-init configuration to reflect changes in argument structure; modify base image OCID in Terraform variables.

--- infra/database/mongodb/packer/scripts/init-mongodb-replica-set.sh
+++ infra/database/mongodb/packer/scripts/init-mongodb-replica-set.sh
@@ -14,7 +14,6 @@ PORT=""
 USERNAME=""
 PASSWORD=""
 REPLICA_SET_NAME=""
-ALL_IPS=""
 
 while [[ $# -gt 0 ]]; do
   case "$1" in
@@ -23,7 +22,6 @@ while [[ $# -gt 0 ]]; do
     --username) USERNAME="$2"; shift 2 ;;
     --password) PASSWORD="$2"; shift 2 ;;
     --replica-set-name) REPLICA_SET_NAME="$2"; shift 2 ;;
-    --all-private-ips) ALL_IPS="$2"; shift 2 ;;
     *)
       echo "Unknown argument: $1"
       exit 1
@@ -37,7 +35,6 @@ done
 : "${USERNAME:?Missing --username}"
 : "${PASSWORD:?Missing --password}"
 : "${REPLICA_SET_NAME:?Missing --replica-set-name}"
-: "${ALL_IPS:?Missing --all-private-ips}"
 
 # Only primary initializes
 if [[ "$NODE_INDEX" -ne 0 ]]; then
@@ -50,19 +47,6 @@ fi
 # ----------------------------
 echo "Node 0: initiating replica set '$REPLICA_SET_NAME'..."
 
-cat <<EOF
-	use admin;
-	rs.initiate({
-		_id: "$REPLICA_SET_NAME",
-		members: [$MEMBERS_JS]
-	})
-
-	// Wait until primary is elected
-	while (!rs.isMaster().ismaster) {
-		sleep(1000);
-	}
-EOF
-
 docker compose exec -T mongodb mongosh \
   -u "$MONGO_INITDB_ROOT_USERNAME" \
   -p "$MONGO_INITDB_ROOT_PASSWORD" \
@@ -71,9 +55,9 @@ docker compose exec -T mongodb mongosh \
 	rs.initiate({
 		_id: "$REPLICA_SET_NAME",
 		members: [
-			{ _id: 0, host: "go-mongodb-1.tmlmobilidade.pt:27017", priority: 1 },
-			{ _id: 1, host: "go-mongodb-2.tmlmobilidade.pt:27017", priority: 0.5 },
-			{ _id: 2, host: "go-mongodb-3.tmlmobilidade.pt:27017", priority: 0.5 }
+			{ _id: 0, host: "go-mongodb-1.tmlmobilidade.pt:$PORT", priority: 1 },
+			{ _id: 1, host: "go-mongodb-2.tmlmobilidade.pt:$PORT", priority: 0.5 },
+			{ _id: 2, host: "go-mongodb-3.tmlmobilidade.pt:$PORT", priority: 0.5 }
 		]
 	})
 
--- infra/database/mongodb/terraform/variables.tf
+++ infra/database/mongodb/terraform/variables.tf
@@ -102,7 +102,7 @@ variable "private_ips" {
 variable "base_image_ocid" {
 	type = string
 	description = "OCID of the Packer-built image."
-	default = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaayhhmu7pq5c43ixnunzqrhiuhkupxghdrctwmzkvb4wblfofnfwha"
+	default = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaa5pas4spa6yu6uogii45rr7vshihg3kny3ke72r43p5zh2eltc6oq"
 }
 
 variable "vm_shape" {
