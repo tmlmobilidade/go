29e9eb7481ab6604b50df87bcc738e1b89fe70c6
João de Vasconcelos <johnyvasconcelos@icloud.com>
2026-03-18 22:20:42 +0000

    Update base image OCID in Terraform variables for MongoDB configuration

--- infra/database/mongodb/terraform/variables.tf
+++ infra/database/mongodb/terraform/variables.tf
@@ -102,7 +102,7 @@ variable "private_ips" {
 variable "base_image_ocid" {
 	type = string
 	description = "OCID of the Packer-built image."
-	default = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaavj7j3pvck7ykkguc4c4skv6jkgfqk3syxe6cqvkczff2oqbp3idq"
+	default = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaa7rcorfa7zo7gj3x3c3eaxv6dafegp2uuapg446xzigzukabpp7ba"
 }
 
 variable "vm_shape" {
