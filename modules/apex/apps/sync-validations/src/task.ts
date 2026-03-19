77c42d922d572fa0243bde72a21dde2eaafecf5b
João de Vasconcelos <johnyvasconcelos@icloud.com>
2026-03-19 10:59:09 +0000

    Initialize ClickHouseWriter before syncing APEX validations

--- modules/apex/apps/sync-validations/src/task.ts
+++ modules/apex/apps/sync-validations/src/task.ts
@@ -26,6 +26,8 @@ const writer = new ClickHouseWriter<SimplifiedApexValidation>({
 export async function syncApexValidations(timeChunk: PerformInTimeChunksItem) {
 	//
 
+	await writer.init();
+
 	const chunkStartDate = Dates
 		.fromUnixTimestamp(timeChunk.start)
 		.setZone('Europe/Lisbon', 'offset_only');
