a0438bfb41066ee75542f8c16404fe717c5152b2
João de Vasconcelos <johnyvasconcelos@icloud.com>
2026-03-19 23:59:10 +0000

    Update base image OCID for ClickHouse and MongoDB variables

--- infra/mongodb/terraform/variables.tf
+++ infra/mongodb/terraform/variables.tf
@@ -108,7 +108,7 @@ variable "private_ips" {
 variable "base_image_ocid" {
 	type = string
 	description = "OCID of the Packer-built image."
-	default = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaa7rcorfa7zo7gj3x3c3eaxv6dafegp2uuapg446xzigzukabpp7ba"
+	default = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaagogydlicjs6vgrazm7h43kg7vsgf64q32nordafy5faeshbfzkca"
 }
 
 variable "vm_shape" {
