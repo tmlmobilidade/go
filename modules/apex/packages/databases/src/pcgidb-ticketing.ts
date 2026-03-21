d45d2e590d8aa53c3078cc97509492288a8124e4
João de Vasconcelos <johnyvasconcelos@icloud.com>
2026-03-21 00:47:23 +0000

    Add SSH key handling for PCGIDB tunnel connections and expose PostgreSQL port in compose.yml

--- modules/apex/packages/databases/src/pcgidb-ticketing.ts
+++ modules/apex/packages/databases/src/pcgidb-ticketing.ts
@@ -3,6 +3,7 @@
 import { Logger } from '@tmlmobilidade/logger';
 import { MongoConnector } from '@tmlmobilidade/mongo';
 import { type SshConfig, SshTunnelService, type SshTunnelServiceOptions } from '@tmlmobilidade/ssh';
+import { readFileSync } from 'fs';
 import { type Collection, type MongoClientOptions } from 'mongodb';
 
 /* * */
@@ -112,11 +113,12 @@ class PCGIDBTicketingClass {
 				 * Ensure that your SSH key is added to the SSH agent beforehand.
 				 * @see https://developer.1password.com/docs/ssh/agent/compatibility/#ssh-auth-sock
 				 */
-				agent: process.env.SSH_AUTH_SOCK,
+				agent: process.env.PCGIDB_TUNNEL_SSH_KEY_PATH ? undefined : process.env.SSH_AUTH_SOCK,
 				host: process.env.PCGIDB_TUNNEL_SSH_HOST,
 				keepaliveCountMax: 3, // Retry 3 times before closing the connection
 				keepaliveInterval: 10_000, // Send keep-alive every 10 seconds
 				port: 22,
+				privateKey: process.env.PCGIDB_TUNNEL_SSH_KEY_PATH ? readFileSync(process.env.PCGIDB_TUNNEL_SSH_KEY_PATH) : undefined,
 				username: process.env.PCGIDB_TUNNEL_SSH_USERNAME,
 			},
 			tunnelOptions: {
