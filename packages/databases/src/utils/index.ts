575fb5e832af0e4d03aa37df5e6ac32c3774f426
jusimen <jusi.monteiro@protonmail.com>
2026-03-23 17:27:10 +0000

    feat(utils): add exports for query-from-file and query-from-string utilities

--- packages/databases/src/utils/index.ts
+++ packages/databases/src/utils/index.ts
@@ -1,3 +1,5 @@
 export * from '@/utils/prepare-named-query-params.js';
 export * from '@/utils/prepare-positional-query-params.js';
+export * from '@/utils/query-from-file.js';
+export * from '@/utils/query-from-string.js';
 export * from '@/utils/validate-sql-param.js';
