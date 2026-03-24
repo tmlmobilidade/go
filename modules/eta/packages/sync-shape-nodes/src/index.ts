77ccb8523821cfb022d5fab1e4597242731c5241
jusimen <jusi.monteiro@protonmail.com>
2026-03-24 10:48:50 +0000

    refactor(sync-shape-nodes): replace ClickHouse with databases package, update data processing logic, and remove unused types

--- modules/eta/packages/sync-shape-nodes/src/index.ts
+++ modules/eta/packages/sync-shape-nodes/src/index.ts
@@ -1,34 +1,31 @@
 /* * */
 
-import { ClickHouseClient } from '@tmlmobilidade/clickhouse';
+import { etaShapeNodes } from '@tmlmobilidade/databases';
 import { chunkLineByDistanceV2, hashedShapesToFeatureCollection } from '@tmlmobilidade/geo';
 import { Filter, hashedShapes, rides } from '@tmlmobilidade/interfaces';
 import { Logger } from '@tmlmobilidade/logger';
 import { Ride } from '@tmlmobilidade/types';
-import { ClickHouseWriter } from '@tmlmobilidade/writers';
-
-import { ShapeNode, shapeNodeTableSchema } from './types.js';
+import { BatchWriter } from '@tmlmobilidade/writers';
 
 /* * */
 
+const BATCH_SIZE = 100_000;
+
 interface SyncShapeNodesOptions {
-	batchSize: number
 	chunkLength: number // in meters
-	client: ClickHouseClient
 	ridesQuery: Filter<Ride>
 }
 
-export async function syncShapeNodes({ batchSize = 100_000, chunkLength = 25, client, ridesQuery }: SyncShapeNodesOptions): Promise<{ shapeNodesProcessed: number }> {
+export async function syncShapeNodes({ chunkLength = 25, ridesQuery }: SyncShapeNodesOptions): Promise<{ shapeNodesProcessed: number }> {
 	//
 
-	const writer = new ClickHouseWriter<ShapeNode>({
-		batch_size: batchSize,
-		client,
-		table: 'shape_nodes',
-		tableSchema: shapeNodeTableSchema,
+	const writer = new BatchWriter({
+		batch_size: BATCH_SIZE,
+		insertFn: async (data) => {
+			await etaShapeNodes.insert('JSONEachRow', data);
+		},
+		title: 'shape_nodes',
 	});
-	await client.command({ query: 'DROP TABLE IF EXISTS shape_nodes' });
-	await writer.ensureTable();
 
 	Logger.info(`Getting distinct hashed shape ids from rides`);
 	const distinctHashedShapeIds = await rides.distinct('hashed_shape_id', ridesQuery);
@@ -37,7 +34,7 @@ export async function syncShapeNodes({ batchSize = 100_000, chunkLength = 25, cl
 	const hashedShapesCursor = hashedShapesCollection.find(
 		{ _id: { $in: distinctHashedShapeIds } },
 		{ projection: { _id: 1, points: { shape_pt_lat: 1, shape_pt_lon: 1 } } },
-	).batchSize(batchSize).stream();
+	).batchSize(BATCH_SIZE).stream();
 
 	Logger.info(`Creating shape nodes for ${distinctHashedShapeIds.length} hashed shape ids`);
 
