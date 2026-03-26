b9b49b7308f80a38779c737bc4739d180270e1d8
João de Vasconcelos <johnyvasconcelos@icloud.com>
2026-03-26 02:19:14 +0000

    Add console logging for CCFL vehicle events and update development scripts for tracker modules

--- modules/tracker/apps/ccfl-fetch/src/index.ts
+++ modules/tracker/apps/ccfl-fetch/src/index.ts
@@ -43,6 +43,8 @@ const main = async () => {
 	for (const entity of decodedMessage.entity ?? []) {
 		//
 
+		console.log(entity);
+
 		//
 		// Skip entities that do not have a vehicle field,
 		// as they are not relevant for our use case.
