e809b519cfc6bf0b116905c08beee2d9d72cb01c
João de Vasconcelos <johnyvasconcelos@icloud.com>
2026-03-27 22:25:01 +0000

    Refactor APEX parsers; add location, validation, and sale parsing functions for v30, and update existing validation logic

--- modules/apex/packages/parsers/src/index.ts
+++ modules/apex/packages/parsers/src/index.ts
@@ -3,6 +3,9 @@
 import { parseRawApexTransactionBankingTapV40 } from '@/banking-taps/banking-tap-v40.js';
 import { parseRawApexTransactionInspectionDecisionV20 } from '@/inspections/inspection-decision-v20.js';
 import { parseRawApexTransactionInspectionV20 } from '@/inspections/inspection-v20.js';
+import { parseRawApexTransactionLocationV30 } from '@/locations/location-v30.js';
+import { parseRawApexTransactionValidationV20 } from '@/validations/validation-v20.js';
+import { parseRawApexTransactionValidationV30 } from '@/validations/validation-v30.js';
 import { type AnySimplifiedApex, type RawApexTransaction } from '@tmlmobilidade/types';
 
 /* * */
@@ -11,9 +14,9 @@ export const PARSER_MAP: Record<RawApexTransaction['version'], (apexTransaction:
 	'apex-banking-tap-4.0': parseRawApexTransactionBankingTapV40,
 	'apex-inspection-2.0': parseRawApexTransactionInspectionV20,
 	'apex-inspection-decision-2.0': parseRawApexTransactionInspectionDecisionV20,
-	'apex-location-3.0': () => null,
+	'apex-location-3.0': parseRawApexTransactionLocationV30,
 	'apex-refund-3.0': () => null,
 	'apex-sale-3.0': () => null,
-	'apex-validation-2.0': () => null,
-	'apex-validation-3.0': () => null,
+	'apex-validation-2.0': parseRawApexTransactionValidationV20,
+	'apex-validation-3.0': parseRawApexTransactionValidationV30,
 };
