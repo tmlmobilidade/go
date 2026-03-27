968c3485c2e673ebdd3913fb4a15e3c2d0ff0c1f
João de Vasconcelos <johnyvasconcelos@icloud.com>
2026-03-27 14:41:21 +0000

    Move files

---
+++ packages/types/src/apex/simplified/index.ts
@@ -0,0 +1,7 @@
+export * from '@/apex/simplified/simplified-apex-inspection-decision.js';
+export * from '@/apex/simplified/simplified-apex-inspection.js';
+export * from '@/apex/simplified/simplified-apex-location.js';
+export * from '@/apex/simplified/simplified-apex-on-board-refund.js';
+export * from '@/apex/simplified/simplified-apex-on-board-sale.js';
+export * from '@/apex/simplified/simplified-apex-type.js';
+export * from '@/apex/simplified/simplified-apex-validation.js';
