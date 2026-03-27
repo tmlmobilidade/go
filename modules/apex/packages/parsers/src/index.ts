d65b453d2281777f3cf526570131cd653925a6b9
João de Vasconcelos <johnyvasconcelos@icloud.com>
2026-03-27 22:30:00 +0000

    Refactor APEX modules; remove deprecated locations, refunds, sales, and validations files, and introduce new callback functionality for invalidating rides based on SimplifiedApex data

--- modules/apex/packages/parsers/src/index.ts
+++ modules/apex/packages/parsers/src/index.ts
@@ -4,6 +4,8 @@ import { parseRawApexTransactionBankingTapV40 } from '@/banking-taps/banking-tap
 import { parseRawApexTransactionInspectionDecisionV20 } from '@/inspections/inspection-decision-v20.js';
 import { parseRawApexTransactionInspectionV20 } from '@/inspections/inspection-v20.js';
 import { parseRawApexTransactionLocationV30 } from '@/locations/location-v30.js';
+import { parseRawApexTransactionRefundV30 } from '@/sales/refund-v30.js';
+import { parseRawApexTransactionSaleV30 } from '@/sales/sale-v30.js';
 import { parseRawApexTransactionValidationV20 } from '@/validations/validation-v20.js';
 import { parseRawApexTransactionValidationV30 } from '@/validations/validation-v30.js';
 import { type AnySimplifiedApex, type RawApexTransaction } from '@tmlmobilidade/types';
@@ -15,8 +17,8 @@ export const PARSER_MAP: Record<RawApexTransaction['version'], (apexTransaction:
 	'apex-inspection-2.0': parseRawApexTransactionInspectionV20,
 	'apex-inspection-decision-2.0': parseRawApexTransactionInspectionDecisionV20,
 	'apex-location-3.0': parseRawApexTransactionLocationV30,
-	'apex-refund-3.0': () => null,
-	'apex-sale-3.0': () => null,
+	'apex-refund-3.0': parseRawApexTransactionRefundV30,
+	'apex-sale-3.0': parseRawApexTransactionSaleV30,
 	'apex-validation-2.0': parseRawApexTransactionValidationV20,
 	'apex-validation-3.0': parseRawApexTransactionValidationV30,
 };
--- modules/apex/packages/shared/src/apex-on-board-sales/index.ts
+++
@@ -1,2 +0,0 @@
-export * from '@/apex-on-board-sales/parser.js';
-export * from '@/apex-on-board-sales/settings.js';
