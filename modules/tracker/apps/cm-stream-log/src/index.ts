4a54701057d5dd9203c4dd78098720d530215432
João de Vasconcelos <johnyvasconcelos@icloud.com>
2026-03-28 01:09:07 +0000

    feat: create cm-stream-core and cm-stream-log modules with initial setup

---
+++ modules/tracker/apps/cm-stream-log/src/index.ts
@@ -0,0 +1,22 @@
+/* * */
+
+import { processPcgiVehicleEventCore } from '@/process-pcgi-vehicle-event-core.js';
+import { processPcgiVehicleEventLog } from '@/process-pcgi-vehicle-event-log.js';
+import { pcgidbLegacy } from '@tmlmobilidade/go-tracker-pckg-databases';
+
+/* * */
+
+(async function init() {
+	//
+
+	await pcgidbLegacy.connect();
+
+	//
+	// Watch for changes to the MongoDB collections
+	// and integrate those documents immediately.
+
+	pcgidbLegacy.VehicleEventsCore.watch().on('change', processPcgiVehicleEventCore);
+	pcgidbLegacy.VehicleEventsLog.watch().on('change', processPcgiVehicleEventLog);
+
+	//
+})();
