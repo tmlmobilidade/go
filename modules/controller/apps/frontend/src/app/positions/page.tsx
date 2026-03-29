73963f51ddfc418ea21ba80e88560e19a3441922
jusimen <jusi.monteiro@protonmail.com>
2026-03-29 03:35:25 +0100

    refactor: simplify vehicle positions page by removing unnecessary context providers

--- modules/controller/apps/frontend/src/app/positions/page.tsx
+++ modules/controller/apps/frontend/src/app/positions/page.tsx
@@ -4,18 +4,13 @@
 
 import { VehiclesPositionsPage } from '@/components/positions/VehiclePositionPage';
 import { VehiclePositionContextProvider } from '@/contexts/VehiclePosition.context';
-import { MapContextProvider, MeContextProvider } from '@tmlmobilidade/ui';
 
 /* * */
 
 export default function Page() {
 	return (
-		<MeContextProvider> {/* ! Temporary | needs to be removed */}
-			<MapContextProvider>
-				<VehiclePositionContextProvider>
-					<VehiclesPositionsPage />
-				</VehiclePositionContextProvider>
-			</MapContextProvider>
-		</MeContextProvider>
+		<VehiclePositionContextProvider>
+			<VehiclesPositionsPage />
+		</VehiclePositionContextProvider>
 	);
 }
