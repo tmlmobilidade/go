31538fa3e909b701d1c92c694b25d2d1375d72c0
LoudNeo <meliodasgamer077@gmail.com>
2026-04-02 10:55:49 +0100

    Refactor SamsDetailBasicInfos component to replace NumberInput fields with ValueDisplay for transactions and add new fields for seen dates, system status, and latest APEX version. Update Portuguese translations for the new fields to enhance user experience.

--- modules/controller/apps/frontend/src/components/sams/detail/SamsDetailBasicInfos/index.tsx
+++ modules/controller/apps/frontend/src/components/sams/detail/SamsDetailBasicInfos/index.tsx
@@ -1,7 +1,8 @@
 /* * */
 
 import { useSamsDetailContext } from '@/contexts/SamsDetail.context';
-import { Collapsible, ErrorDisplay, Grid, LoadingOverlay, NumberInput, Section, ValueDisplay } from '@tmlmobilidade/ui';
+import { formatUnixTimestampToDateString } from '@/lib/utils';
+import { Collapsible, Divider, ErrorDisplay, Grid, LoadingOverlay, NumberInput, Section, Spacer, ValueDisplay } from '@tmlmobilidade/ui';
 import { useTranslation } from 'react-i18next';
 
 /* * */
@@ -33,27 +34,21 @@ export function SamsDetailBasicInfos() {
 			defaultOpen
 		>
 			<Section>
+				<Grid columns="abc" gap="md">
+					<ValueDisplay label={t('default:sams.detail.SamsDetailBasicInfos.fields.transactions_expected.label')} value={samDetailContext.data.sam?.transactions_expected?.toString()} raised />	
+					<ValueDisplay label={t('default:sams.detail.SamsDetailBasicInfos.fields.transactions_found.label')} value={samDetailContext.data.sam?.transactions_found?.toString()} raised />
+					<ValueDisplay label={t('default:sams.detail.SamsDetailBasicInfos.fields.transactions_missing.label')} value={samDetailContext.data.sam?.transactions_missing?.toString()} raised />
+				</Grid>
 				<Grid columns="ab" gap="md">
-					<NumberInput
-						key={samDetailContext.data.sam._id.toString() + 'transactions_expected'}
-						label={t('default:sams.detail.SamsDetailBasicInfos.fields.transactions_expected.label')}
-						value={samDetailContext.data.sam?.transactions_expected}
-						readOnly
-					/>
-					<NumberInput
-						key={samDetailContext.data.sam._id.toString() + 'transactions_found'}
-						label={t('default:sams.detail.SamsDetailBasicInfos.fields.transactions_found.label')}
-						value={samDetailContext.data.sam?.transactions_found}
-						readOnly
-					/>
-					<NumberInput
-						key={samDetailContext.data.sam._id.toString() + 'transactions_missing'}
-						label={t('default:sams.detail.SamsDetailBasicInfos.fields.transactions_missing.label')}
-						value={samDetailContext.data.sam?.transactions_missing}
-						readOnly
-					/>
+					<ValueDisplay label={t('default:sams.detail.SamsDetailBasicInfos.fields.seen_first_at.label')} value={formatUnixTimestampToDateString(samDetailContext.data.sam?.seen_first_at)} raised />
+					<ValueDisplay label={t('default:sams.detail.SamsDetailBasicInfos.fields.seen_last_at.label')} value={formatUnixTimestampToDateString(samDetailContext.data.sam?.seen_last_at)} raised />
+				</Grid>
+				<Grid columns="ab" gap="md">
+					<ValueDisplay label={t('default:sams.detail.SamsDetailBasicInfos.fields.system_status.label')} value={samDetailContext.data.sam?.system_status} raised />
+					<ValueDisplay label={t('default:sams.detail.SamsDetailBasicInfos.fields.latest_apex_version.label')} value={samDetailContext.data.sam?.latest_apex_version} raised />
 				</Grid>
 			</Section>
+				
 		</Collapsible>
 	);
 }
