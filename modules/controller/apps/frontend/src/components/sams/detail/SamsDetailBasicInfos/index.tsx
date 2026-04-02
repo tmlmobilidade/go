4db68e48e56b8f5d787464e0fe2349bb35dad155
LoudNeo <meliodasgamer077@gmail.com>
2026-04-02 10:27:51 +0100

    Enhance SamsDetailBasicInfos component by adding new fields for expected, found, and missing transactions. Update Portuguese translations for these fields to improve user clarity and context.

--- modules/controller/apps/frontend/src/components/sams/detail/SamsDetailBasicInfos/index.tsx
+++ modules/controller/apps/frontend/src/components/sams/detail/SamsDetailBasicInfos/index.tsx
@@ -1,7 +1,7 @@
 /* * */
 
 import { useSamsDetailContext } from '@/contexts/SamsDetail.context';
-import { Collapsible, ErrorDisplay, Grid, LoadingOverlay, Section, ValueDisplay } from '@tmlmobilidade/ui';
+import { Collapsible, ErrorDisplay, Grid, LoadingOverlay, NumberInput, Section, ValueDisplay } from '@tmlmobilidade/ui';
 import { useTranslation } from 'react-i18next';
 
 /* * */
@@ -34,7 +34,24 @@ export function SamsDetailBasicInfos() {
 		>
 			<Section>
 				<Grid columns="ab" gap="md">
-					<ValueDisplay label={t('default:sams.detail.SamsDetailBasicInfos.fields.latest_apex_version.label')} value={samDetailContext.data.sam?.latest_apex_version} />
+					<NumberInput
+						key={samDetailContext.data.sam._id.toString() + 'transactions_expected'}
+						label={t('default:sams.detail.SamsDetailBasicInfos.fields.transactions_expected.label')}
+						value={samDetailContext.data.sam?.transactions_expected}
+						readOnly
+					/>
+					<NumberInput
+						key={samDetailContext.data.sam._id.toString() + 'transactions_found'}
+						label={t('default:sams.detail.SamsDetailBasicInfos.fields.transactions_found.label')}
+						value={samDetailContext.data.sam?.transactions_found}
+						readOnly
+					/>
+					<NumberInput
+						key={samDetailContext.data.sam._id.toString() + 'transactions_missing'}
+						label={t('default:sams.detail.SamsDetailBasicInfos.fields.transactions_missing.label')}
+						value={samDetailContext.data.sam?.transactions_missing}
+						readOnly
+					/>
 				</Grid>
 			</Section>
 		</Collapsible>
