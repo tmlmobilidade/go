5e3f6eb3002183a34d91301fede7357a17dc1f81
João de Vasconcelos <johnyvasconcelos@icloud.com>
2026-04-10 19:03:28 +0100

    fix: update tag label to use line name instead of short name

--- modules/alerts/apps/frontend/src/components/list/AlertsListCellLines/index.tsx
+++ modules/alerts/apps/frontend/src/components/list/AlertsListCellLines/index.tsx
@@ -28,7 +28,7 @@ export function AlertsListCellLines({ values }: AlertsListCellLinesProps) {
 		.map((item): TagProps => {
 			const lineData = linesContext.actions.getLineDataById(item);
 			if (!lineData) return null;
-			return { label: lineData.short_name, variant: 'muted' };
+			return { label: lineData.name, variant: 'muted' };
 		})
 		.filter(Boolean);
 
