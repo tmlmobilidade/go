8998952c5df54eb284b12ffc68c21e62b8906e2a
LoudNeo <meliodasgamer077@gmail.com>
2026-04-13 15:11:02 +0100

    Refactor SamsDetaisListItems component to replace Tag components with Label for date fields and streamline rendering logic. Update render methods for transactions to remove unnecessary Tag usage, enhancing code clarity and consistency.

--- modules/controller/apps/frontend/src/components/sams/detail/SamsDetaisListItems/index.tsx
+++ modules/controller/apps/frontend/src/components/sams/detail/SamsDetaisListItems/index.tsx
@@ -3,7 +3,7 @@
 import { useSamsDetailContext } from '@/contexts/SamsDetail.context';
 import { formatUnixTimestampToDateString } from '@/lib/utils';
 import { SamAnalysis } from '@tmlmobilidade/types';
-import { DataTable, DataTableColumn, IdTag, Tag } from '@tmlmobilidade/ui';
+import { DataTable, DataTableColumn, Label } from '@tmlmobilidade/ui';
 import { useMemo } from 'react';
 
 /* * */
@@ -46,85 +46,74 @@ export function SamsDetaisListItems() {
 		return [
 			{
 				accessor: 'start_time',
-				render: item => <Tag label={formatUnixTimestampToDateString(item.start_time)} />,
+				render: item => <Label>{formatUnixTimestampToDateString(item.start_time)}</Label>,
 				title: 'Data de início',
 				width: 200,
 			},
 			{
 				accessor: 'end_time',
-				render: item => <Tag label={formatUnixTimestampToDateString(item.end_time)} />,
+				render: item => <Label>{formatUnixTimestampToDateString(item.end_time)}</Label>,
 				title: 'Data de fim',
 				width: 200,
 			},
 			{
 				accessor: 'device_id',
-				render: item => <IdTag id={item.device_id ?? '-'} copyOnClick />,
 				title: 'Device ID',
 				width: 250,
 			},
 			{
 				accessor: 'vehicle_id',
-				render: item => <IdTag id={item.vehicle_id ?? '-'} copyOnClick />,
 				title: 'Vehicle ID',
 				width: 100,
 			},
 			{
 				accessor: 'apex_version',
-				render: item => <Tag label={item.apex_version ?? '-'} />,
 				title: 'Versão APEX',
 				width: 100,
 			},
 			{
 				accessor: 'transactions_expected',
-				render: item => <Tag label={item.transactions_expected?.toString() ?? '-'} />,
+
 				title: 'Transações esperadas',
 				width: 200,
 			},
 			{
 				accessor: 'transactions_found',
-				render: item => <Tag label={item.transactions_found?.toString() ?? '-'} />,
 				title: 'Transações encontradas',
 				width: 200,
 			},
 			{
 				accessor: 'transactions_missing',
-				render: item => <Tag label={item.transactions_missing?.toString() ?? '-'} />,
 				title: 'Transações em falta',
 				width: 200,
 			},
 			{
 				accessor: 'first_transaction_id',
-				render: item => <IdTag id={item.first_transaction_id ?? '-'} copyOnClick />,
 				title: 'ID da primeira transação',
 				width: 400,
 			},
 			{
 				accessor: 'last_transaction_id',
-				render: item => <IdTag id={item.last_transaction_id ?? '-'} copyOnClick />,
 				title: 'ID da última transação',
 				width: 400,
 			},
 			{
 				accessor: 'first_transaction_ase_counter_value',
-				render: item => <Tag label={item.first_transaction_ase_counter_value?.toString() ?? '-'} />,
 				title: 'Valor do ASE Counter da primeira transação',
 				width: 350,
 			},
 			{
 				accessor: 'last_transaction_ase_counter_value',
-				render: item => <Tag label={item.last_transaction_ase_counter_value?.toString() ?? '-'} />,
 				title: 'Valor do ASE Counter da última transação',
 				width: 350,
 			},
 			{
 				accessor: 'first_transaction_type',
-				render: item => <Tag label={item.first_transaction_type ?? '-'} />,
 				title: 'Tipo da primeira transação',
 				width: 250,
 			},
 			{
 				accessor: 'last_transaction_type',
-				render: item => <Tag label={item.last_transaction_type ?? '-'} />,
 				title: 'Tipo da última transação',
 				width: 250,
 			},
