d8a54ac19735a679d518de32467614e2b65e1c55
LoudNeo <meliodasgamer077@gmail.com>
2026-04-14 16:53:56 +0100

    Add active state to SamsFiltersDate based on date filters

--- modules/controller/apps/frontend/src/components/sams/list/SamsFiltersDate/index.tsx
+++ modules/controller/apps/frontend/src/components/sams/list/SamsFiltersDate/index.tsx
@@ -19,6 +19,7 @@ export function SamsFiltersDate() {
 
 	return (
 		<FilterTypeDateRange
+			active={samsListContext.filters.seen_last_at != null || samsListContext.filters.seen_first_at != null}
 			endDate={samsListContext.filters.seen_last_at as UnixTimestamp}
 			label={t('default:sams.list.SamsFiltersDate.label')}
 			onEndDateChange={samsListContext.actions.setFilterSeenLastAt}
