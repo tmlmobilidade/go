c4bcf6c22d1d4c8aefb03704622170513955edd8
Bruno Castelo <brunoalexcastelo@gmail.com>
2026-04-20 11:36:47 +0100

    Enhance Header component with mobile menu functionality and update styles
    - Added mobile menu toggle functionality to the Header component for improved navigation on smaller screens.
    - Updated styles for Header and HeaderLinks to support mobile layout and responsiveness.
    - Refactored HomeHero styles for better alignment and spacing.

--- modules/base/apps/frontend/src/components/common/layout/Header/index.tsx
+++ modules/base/apps/frontend/src/components/common/layout/Header/index.tsx
@@ -1,8 +1,14 @@
 /* * */
 
+'use client';
+
 import { HeaderActions } from '@/components/common/layout/Header/HeaderActions';
 import { HeaderLinks } from '@/components/common/layout/Header/HeaderLinks';
 import { HeaderLogo } from '@/components/common/layout/Header/HeaderLogo';
+import { IconMenu } from '@tabler/icons-react';
+import { IconButton } from '@tmlmobilidade/ui';
+import Link from 'next/link';
+import { useState } from 'react';
 
 import styles from './styles.module.css';
 
@@ -10,16 +16,25 @@ import styles from './styles.module.css';
 
 export function Header() {
 	//
+	const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
 	//
 	// A. Render components
 
 	return (
-		<div className={styles.headerWrapper}>
+		<div className={`${styles.headerWrapper} ${isMobileMenuOpen ? styles.mobileOpen : ''}`}>
 			<div className={styles.headerInner}>
 				<HeaderLogo />
 				<HeaderLinks />
 				<HeaderActions />
+				<div className={styles.mobileMenuToggle}>
+					<IconButton icon={<IconMenu />} onClick={() => setIsMobileMenuOpen(prev => !prev)} />
+				</div>
+			</div>
+			<div className={styles.mobilePanel}>
+				<Link className={styles.mobilePanelLink} href="https://go.tmlmobilidade.pt/reference" onClick={() => setIsMobileMenuOpen(false)}>
+					Documentação
+				</Link>
 			</div>
 		</div>
 	);
