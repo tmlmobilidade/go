71c1a8cc735d6944550fed962ae9fdad88c5215c
João de Vasconcelos <johnyvasconcelos@icloud.com>
2026-04-23 19:20:17 +0100

    refactor: simplify UsersListFilterOrganization and UsersListFilterRole components by removing unused contexts and memoization fix: update UsersListHeader to correctly set search filter value

--- modules/auth/apps/frontend/src/components/users/list/UsersListFilterOrganization/index.tsx
+++ modules/auth/apps/frontend/src/components/users/list/UsersListFilterOrganization/index.tsx
@@ -1,9 +1,7 @@
 /* * */
 
 import { useUsersListContext } from '@/components/users/list/UsersList.context';
-import { useOrganizationsContext } from '@/contexts/Organizations.context';
 import { FilterTypeList } from '@tmlmobilidade/ui';
-import { useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 
 /* * */
@@ -14,45 +12,19 @@ export function UsersListFilterOrganization() {
 	//
 	// A. Setup variables
 
-	const organizationsContext = useOrganizationsContext();
 	const usersListContext = useUsersListContext();
 
 	const { t } = useTranslation();
 
 	//
-	// B. Transform data
-
-	const isActive = useMemo(() => {
-		// The default for this filter is to show all statuses
-		const defaultValues = organizationsContext.data.raw.map(item => item._id);
-		const enabledValues = usersListContext.filters.organization_ids;
-		// Check if the arrays are equal by quickly comparing their lengths
-		if (defaultValues.length !== enabledValues.length) return true;
-		// If the length is the same ensure they're equal by also
-		// checking if every item in one array is included in the other.
-		return !defaultValues.every(item => enabledValues.includes(item));
-	}, [usersListContext.filters.organization_ids, organizationsContext.data.raw]);
-
-	const parsedOptions = useMemo(() => {
-		// Skip if options are not provided or are empty.
-		if (!organizationsContext.data.raw?.length) return [];
-		// Parse options to the expected format.
-		return organizationsContext.data.raw.map(item => ({
-			checked: usersListContext.filters.organization_ids.includes(item._id),
-			label: item.long_name,
-			value: item._id,
-		}));
-	}, [usersListContext.filters.organization_ids, organizationsContext.data.raw]);
-
-	//
-	// C. Render components
+	// B. Render components
 
 	return (
 		<FilterTypeList
-			active={isActive}
+			active={usersListContext.filters.organization_ids.isActive}
 			label={t('default:users.list.FilterBar.organization.label')}
-			onChange={usersListContext.actions.setFilterOrganizationIds}
-			options={parsedOptions}
+			onChange={usersListContext.filters.organization_ids.set}
+			options={usersListContext.filters.organization_ids.options}
 			withToggleAll
 		/>
 	);
--- modules/auth/apps/frontend/src/components/users/list/UsersListFilterRole/index.tsx
+++ modules/auth/apps/frontend/src/components/users/list/UsersListFilterRole/index.tsx
@@ -1,9 +1,7 @@
 /* * */
 
 import { useUsersListContext } from '@/components/users/list/UsersList.context';
-import { useRolesContext } from '@/contexts/Roles.context';
 import { FilterTypeList } from '@tmlmobilidade/ui';
-import { useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 
 /* * */
@@ -14,45 +12,19 @@ export function UsersListFilterRole() {
 	//
 	// A. Setup variables
 
-	const rolesContext = useRolesContext();
 	const usersListContext = useUsersListContext();
 
 	const { t } = useTranslation();
 
 	//
-	// B. Transform data
-
-	const isActive = useMemo(() => {
-		// The default for this filter is to show all roles
-		const defaultValues = rolesContext.data.raw.map(item => item._id);
-		const enabledValues = usersListContext.filters.role_ids;
-		// Check if the arrays are equal by quickly comparing their lengths
-		if (defaultValues.length !== enabledValues.length) return true;
-		// If the length is the same ensure they're equal by also
-		// checking if every item in one array is included in the other.
-		return !defaultValues.every(item => enabledValues.includes(item));
-	}, [usersListContext.filters.role_ids, rolesContext.data.raw]);
-
-	const parsedOptions = useMemo(() => {
-		// Skip if options are not provided or are empty.
-		if (!rolesContext.data.raw?.length) return [];
-		// Parse options to the expected format.
-		return rolesContext.data.raw.map(item => ({
-			checked: usersListContext.filters.role_ids.includes(item._id),
-			label: item.name,
-			value: item._id,
-		}));
-	}, [usersListContext.filters.role_ids, rolesContext.data.raw]);
-
-	//
-	// C. Render components
+	// B. Render components
 
 	return (
 		<FilterTypeList
-			active={isActive}
+			active={usersListContext.filters.role_ids.isActive}
 			label={t('default:users.list.FilterBar.role.label')}
-			onChange={usersListContext.actions.setFilterRoleIds}
-			options={parsedOptions}
+			onChange={usersListContext.filters.role_ids.set}
+			options={usersListContext.filters.role_ids.options}
 			withToggleAll
 		/>
 	);
