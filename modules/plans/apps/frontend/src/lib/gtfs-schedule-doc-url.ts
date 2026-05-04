b097b4dffb1dcb646b17b2f39ed62491ac1a41f7
LoudNeo <meliodasgamer077@gmail.com>
2026-05-04 16:34:02 +0100

    refactor: update getGtfsScheduleDocUrl to use PAGE_ROUTES instead of API_ROUTES for constructing schedule document URLs

--- modules/plans/apps/frontend/src/lib/gtfs-schedule-doc-url.ts
+++ modules/plans/apps/frontend/src/lib/gtfs-schedule-doc-url.ts
@@ -1,13 +1,13 @@
 /* * */
 
-import { API_ROUTES } from '@tmlmobilidade/consts';
+import { PAGE_ROUTES } from '@tmlmobilidade/consts';
 
 /* * */
 
 export function getGtfsScheduleDocUrl(ruleId: string): null | string {
 	//
 
-	const getRulesDocUrl = `${API_ROUTES.auth.BASE}/reference/gtfs/schedule/rules/${ruleId}`;
+	const getRulesDocUrl = `${PAGE_ROUTES.auth.BASE}/reference/gtfs/schedule/rules/${ruleId}`;
 
 	return getRulesDocUrl;
 }
