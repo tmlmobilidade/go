8b2516ecba815e91f70cfd48f530fd84b1204986
jusimen <jusi.monteiro@protonmail.com>
2026-05-07 15:04:01 +0100

    refactor: simplify environment-based configuration for development settings in AppConfig

--- modules/eta/apps/loader/src/lib/config.ts
+++ modules/eta/apps/loader/src/lib/config.ts
@@ -1,10 +1,12 @@
 import type { TimeSlot } from '@tmlmobilidade/dates';
 
+const isDevelopment = process.env.ENVIRONMENT === 'development';
+
 export const AppConfig = Object.freeze({
 	// Agency and line configurations
 	agencyIds: ['41', '42', '43', '44'],
 	devLineIds: [2652, 2708, 2711, 2713, 2722, 2725, 2728, 2729, 2730, 2731, 2734],
-	useDevLineIds: process.env.ENVIRONMENT === 'development' ? true : false,
+	useDevLineIds: isDevelopment ? true : false,
 
 	// Data and time settings
 	historicalDataDaysBack: 30,
@@ -22,6 +24,6 @@ export const AppConfig = Object.freeze({
 		insertHistoricalVehicleEvents: true,
 		runDdl: true,
 		runTransformationAndAggregationQueries: true,
-		truncatePipelineTables: true,
+		truncatePipelineTables: isDevelopment ? true : false,
 	},
 });
