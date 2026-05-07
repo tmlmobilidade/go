f1cdbca1a70f7e8f42923031ed817a891b637927
João de Vasconcelos <10943992+joao-vasconcelos@users.noreply.github.com>
2026-05-07 22:32:59 +0100

    Add separate publisher for CM

---
+++ modules/plans/apps/publish-gtfs-cm/src/index.ts
@@ -0,0 +1,8 @@
+/* * */
+
+import { main } from '@/main.js';
+import { runOnInterval } from '@tmlmobilidade/utils';
+
+/* * */
+
+await runOnInterval(main, { intervalMs: '1m', throwOnError: true });
