46f15cbcccb58932835b264707f29816f6f87d4a
João de Vasconcelos <10943992+joao-vasconcelos@users.noreply.github.com>
2026-05-07 14:40:11 +0100

    Cleanup

--- modules/hub/apps/api/src/endpoints/index.ts
+++ modules/hub/apps/api/src/endpoints/index.ts
@@ -1,3 +1,1 @@
-import '@/endpoints/alerts/alerts.routes.js';
-import '@/endpoints/gtfs/gtfs.routes.js';
-import '@/endpoints/operation/operation.routes.js';
+import '@/endpoints/v1/index.js';
--- modules/hub/apps/api/src/main.ts
+++ modules/hub/apps/api/src/main.ts
@@ -1,6 +1,5 @@
 /* * */
 
-import { fastifyMultipart } from '@fastify/multipart';
 import { getModuleConfig } from '@tmlmobilidade/consts';
 import { FastifyService } from '@tmlmobilidade/fastify';
 
@@ -12,10 +11,6 @@ import { FastifyService } from '@tmlmobilidade/fastify';
 	const fastifyService = FastifyService.getInstance({
 		origin: getModuleConfig('alerts', 'cors_origin'),
 		port: getModuleConfig('alerts', 'api_port'),
-	});
-
-	await fastifyService.server.register(fastifyMultipart, {
-		limits: { fileSize: 10 * 1024 * 1024 }, // 10MB
 	});
 
 	await fastifyService.start();
