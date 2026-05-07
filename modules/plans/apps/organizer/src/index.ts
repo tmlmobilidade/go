a48d594e0eaad5c86aadcb7a0502f329fa3c7079
João de Vasconcelos <10943992+joao-vasconcelos@users.noreply.github.com>
2026-05-07 19:37:25 +0100

    Renamed plans cleaner to organizer

--- modules/plans/apps/organizer/src/index.ts
+++ modules/plans/apps/organizer/src/index.ts
@@ -2,11 +2,13 @@
 
 import { cleanOldValidations } from '@/tasks/clean-old-validations.js';
 import { ensureGtfsFiles } from '@/tasks/ensure-gtfs-files.js';
+import { publishApprovedPlansFeed } from '@/tasks/publish-approved-plans.js';
 import { runOnInterval } from '@tmlmobilidade/utils';
 
 /* * */
 
 async function main() {
+	await publishApprovedPlansFeed();
 	await cleanOldValidations();
 	await ensureGtfsFiles();
 }
