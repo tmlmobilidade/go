8b47692a5c99d1349c686a0e664a46eac0d9f3d9
jusimen <jusi.monteiro@protonmail.com>
2026-05-13 12:37:19 +0100

    refactor: update SAMS index to include aggregations and remove pipelines file

--- packages/interfaces/src/interfaces/sams/index.ts
+++ packages/interfaces/src/interfaces/sams/index.ts
@@ -1,4 +1,4 @@
+export * from '@/interfaces/sams/aggregations/index.js';
 export * from '@/interfaces/sams/build-sams-match.js';
-export * from '@/interfaces/sams/pipelines.js';
 export * from '@/interfaces/sams/regex-search.js';
 export * from '@/interfaces/sams/sams.js';
--- packages/interfaces/src/interfaces/sams/pipelines.ts
+++
@@ -1,1 +0,0 @@
-export * from '@/interfaces/sams/aggregations/index.js';
