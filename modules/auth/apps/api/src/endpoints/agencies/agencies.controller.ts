2e2a965676f64588b45ffc95dfc30186b4eb09e5
LoudNeo <meliodasgamer077@gmail.com>
2026-05-19 11:28:00 +0100

    Refactor error handling and logging in AgenciesController: Enhance error logging by including structured context for agency retrieval, locking, and updating actions. This update improves clarity and consistency in error tracking and informational logging across agency-related operations.

--- modules/auth/apps/api/src/endpoints/agencies/agencies.controller.ts
+++ modules/auth/apps/api/src/endpoints/agencies/agencies.controller.ts
@@ -3,6 +3,7 @@
 import { HTTP_STATUS, HttpException } from '@tmlmobilidade/consts';
 import { type FastifyReply, type FastifyRequest } from '@tmlmobilidade/fastify';
 import { agencies } from '@tmlmobilidade/interfaces';
+import { Logger } from '@tmlmobilidade/logger';
 import { type Agency, type UpdateAgencyDto, UpdateAgencySchema } from '@tmlmobilidade/types';
 
 /* * */
@@ -17,6 +18,16 @@ export class AgenciesController {
 	 */
 	static async getAll(request: FastifyRequest, reply: FastifyReply<Agency[]>) {
 		const allAgencies = await agencies.findMany({}, { projection: { validation_rules: 0 }, sort: { _id: 1 } });
+		if (!allAgencies) {
+			const error = new HttpException(HTTP_STATUS.INTERNAL_SERVER_ERROR, 'Can not get agencies from database');
+			Logger.error(error, {
+				action: 'getAll',
+				feature: 'agencies',
+				message: error.message,
+				request,
+			});
+			throw error;
+		}
 		reply.send({ data: allAgencies, error: null, statusCode: HTTP_STATUS.OK });
 	}
 
@@ -27,7 +38,17 @@ export class AgenciesController {
 	 */
 	static async getById(request: FastifyRequest<{ Params: { id: string } }>, reply: FastifyReply<Agency>) {
 		const agencyData = await agencies.findById(request.params.id);
-		if (!agencyData) throw new HttpException(HTTP_STATUS.NOT_FOUND, 'Agency not found');
+		if (!agencyData) {
+			const error = new HttpException(HTTP_STATUS.NOT_FOUND, `Can not find agency with ID ${request.params.id}`);
+			Logger.error(error, {
+				action: 'getById',
+				feature: 'agencies',
+				message: error.message,
+				request,
+				value: request.params.id,
+			});
+			throw error;
+		}
 		reply.send({ data: agencyData, error: null, statusCode: HTTP_STATUS.OK });
 	}
 
@@ -39,8 +60,26 @@ export class AgenciesController {
 	static async lock(request: FastifyRequest<{ Params: { id: string } }>, reply: FastifyReply<Agency>) {
 		await agencies.toggleLockById(request.params.id);
 		const foundAgency = await agencies.findById(request.params.id);
-		if (!foundAgency) throw new HttpException(HTTP_STATUS.NOT_FOUND, 'Agency not found');
+		if (!foundAgency) {
+			const error = new HttpException(HTTP_STATUS.NOT_FOUND, `Can not find agency with ID ${request.params.id}`);
+			Logger.error(error, {
+				action: 'lock',
+				feature: 'agencies',
+				message: error.message,
+				request,
+				value: request.params.id,
+			});
+			throw error;
+		}
 		reply.send({ data: foundAgency, error: null, statusCode: HTTP_STATUS.OK });
+
+		Logger.info([], {
+			action: 'lock',
+			feature: 'agencies',
+			message: `Agency locked - ${request.params.id}`,
+			request,
+			value: request.params.id,
+		});
 	}
 
 	/**
@@ -51,12 +90,30 @@ export class AgenciesController {
 	static async update(request: FastifyRequest<{ Body: UpdateAgencyDto, Params: { id: string } }>, reply: FastifyReply<Agency>) {
 		// Validate the request body
 		const validatedAgency = UpdateAgencySchema.safeParse(request.body);
-		if (!validatedAgency.success) throw new HttpException(HTTP_STATUS.BAD_REQUEST, 'Dados inválidos', validatedAgency.error);
+		if (!validatedAgency.success) {
+			const error = new HttpException(HTTP_STATUS.BAD_REQUEST, validatedAgency.error.message);
+			Logger.error(error, {
+				action: 'update',
+				feature: 'agencies',
+				message: error.message,
+				request,
+				value: request.params.id,
+			});
+			throw error;
+		}
 		// Set the updated_by field to the current user's id
 		validatedAgency.data.updated_by = request.me._id;
 		// Update the agency in the database
 		const updatedAgencyData = await agencies.updateById(request.params.id, validatedAgency.data);
 		reply.send({ data: updatedAgencyData, error: null, statusCode: HTTP_STATUS.OK });
+
+		Logger.info([], {
+			action: 'update',
+			feature: 'agencies',
+			message: `Agency updated - ${request.params.id}`,
+			request,
+			value: request.params.id,
+		});
 	}
 
 	//
