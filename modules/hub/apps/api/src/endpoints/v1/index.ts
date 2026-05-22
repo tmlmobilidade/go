12d18e02580070fcfaf84fdb91042c87dc300fd5
João de Vasconcelos <10943992+joao-vasconcelos@users.noreply.github.com>
2026-05-22 18:52:33 +0100

    chore(deps): update protobufjs and other package versions in package.json files

--- modules/hub/apps/api/src/endpoints/v1/index.ts
+++ modules/hub/apps/api/src/endpoints/v1/index.ts
@@ -2,15 +2,3 @@ import '@/endpoints/v1/alerts/alerts.routes.js';
 import '@/endpoints/v1/plans/plans.routes.js';
 import '@/endpoints/v1/debug/debug.routes.js';
 import '@/endpoints/v1/realtime/realtime.routes.js';
-
-/* * */
-
-import '@/endpoints/v1/facilities/facilities.routes.js';
-import '@/endpoints/v1/locations/municipalities/municipalities.routes.js';
-import '@/endpoints/v1/network/feeds/feeds.routes.js';
-import '@/endpoints/v1/locations/districts/districts.routes.js';
-import '@/endpoints/v1/locations/localities/localities.routes.js';
-import '@/endpoints/v1/locations/parishes/parishes.routes.js';
-import '@/endpoints/v1/network/patterns/patterns.routes.js';
-import '@/endpoints/v1/network/shapes/shapes.routes.js';
-import '@/endpoints/v1/network/arrivals/arrivals.routes.js';
