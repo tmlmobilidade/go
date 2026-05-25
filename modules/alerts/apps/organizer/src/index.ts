21d5e6ee5cb77111f584de9da2c54b0bcc4fa14d
João de Vasconcelos <10943992+joao-vasconcelos@users.noreply.github.com>
2026-05-25 14:29:39 +0100

    refactor(organizer): streamline main task execution and update interval to 1 hour

--- modules/alerts/apps/organizer/src/index.ts
+++ modules/alerts/apps/organizer/src/index.ts
@@ -1,16 +1,11 @@
 /* * */
 
 import { ensureStructure } from '@/tasks/ensure-structure.js';
-import { publishGtfsRtFeed } from '@/tasks/publish-gtfs-rt-feed.js';
-import { publishJsonFeed } from '@/tasks/publish-json-feed.js';
-import { publishRssFeed } from '@/tasks/publish-rss-feed.js';
 import { Logger } from '@tmlmobilidade/logger';
 import { Timer } from '@tmlmobilidade/timer';
 import { runOnInterval } from '@tmlmobilidade/utils';
 
 /* * */
-
-let ITERATIONS_COUNTER = 0;
 
 const main = async () => {
 	//
@@ -22,18 +17,10 @@ const main = async () => {
 	//
 	// Run all tasks sequentially
 
-	if (ITERATIONS_COUNTER % 10 === 0) await ensureStructure();
-
-	await publishGtfsRtFeed();
-
-	await publishJsonFeed();
-
-	await publishRssFeed();
+	await ensureStructure();
 
 	//
 	// Log the total time taken for all tasks
-
-	ITERATIONS_COUNTER++;
 
 	Logger.terminate(`Organization completed in ${globalTimer.get()}`);
 
@@ -42,4 +29,4 @@ const main = async () => {
 
 /* * */
 
-await runOnInterval(main, { intervalMs: '30s' });
+await runOnInterval(main, { intervalMs: '1h' });
