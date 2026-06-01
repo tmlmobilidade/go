6fcda5062e278372a6f236777b3546d4effde31a
Bruno Castelo <brunoalexcastelo@gmail.com>
2026-06-01 11:29:32 +0100

    feat(stops): enhance StopDetailNamesModal with TTS playback and action buttons

--- modules/stops/apps/frontend/src/components/stops/detail/StopDetailCoordinates/StopDetailNamesModal/index.tsx
+++ modules/stops/apps/frontend/src/components/stops/detail/StopDetailCoordinates/StopDetailNamesModal/index.tsx
@@ -3,7 +3,10 @@
 /* * */
 
 import { useStopDetailContext } from '@/contexts/StopDetailCoordinates.modal';
-import { Grid, Label, Pane, Section, Spacer, TextInput, Toolbar } from '@tmlmobilidade/ui';
+import { audioTtsUrl } from '@/settings/urls.settings';
+import { Button, Grid, Label, Pane, Section, Spacer, TextInput, Toolbar } from '@tmlmobilidade/ui';
+
+import { StopDetailPlayTTS } from '../../StopDetailPlayTTS';
 
 /* * */
 
@@ -16,31 +19,43 @@ export function StopDetailNamesModalBody() {
 	const stopDetailContext = useStopDetailContext();
 
 	//
-	// B. Render components
+	// . Render components
 
 	return (
-		<Section>
-			<Grid columns="a" gap="md">
-				<TextInput
-					label="Nome Único da Paragem"
-					value={stopDetailContext.data.form.getValues()?.name ?? 'N/A'}
-					variant="bordered"
-				/>
-			</Grid>
-			<Grid columns="ab" gap="md">
-				<TextInput
-					label="Nome Curto"
-					value={stopDetailContext.data.form.getValues()?.short_name ?? 'N/A'}
-					variant="bordered"
-				/>
+		<>
+			<Section>
 
-				<TextInput
-					label="Nome TTS"
-					value={stopDetailContext.data.form.getValues()?.tts_name ?? 'N/A'}
-					variant="bordered"
-				/>
-			</Grid>
-		</Section>
+				<Grid columns="a" gap="md">
+					<TextInput
+						label="Nome Único da Paragem"
+						value={stopDetailContext.data.form.getValues()?.name ?? 'N/A'}
+						variant="bordered"
+					/>
+				</Grid>
+				<Grid columns="ab" gap="md">
+					<TextInput
+						label="Nome Curto"
+						value={stopDetailContext.data.form.getValues()?.short_name ?? 'N/A'}
+						variant="bordered"
+					/>
+
+					<TextInput
+						label="Nome TTS"
+						value={stopDetailContext.data.form.getValues()?.tts_name ?? 'N/A'}
+						variant="bordered"
+					/>
+				</Grid>
+
+			</Section>
+
+			<Section>
+				<Grid columns="abc" gap="md">
+					<Button label="Cancelar" onClick={stopDetailContext.actions.closeNameEditor} />
+					<Button label="Salvar" onClick={stopDetailContext.actions.closeNameEditor} />
+					<StopDetailPlayTTS />
+				</Grid>
+			</Section>
+		</>
 	);
 
 	//
---
+++ modules/stops/apps/frontend/src/components/stops/detail/StopDetailPlayTTS/index.tsx
@@ -0,0 +1,32 @@
+import { useStopDetailContext } from '@/contexts/StopDetailCoordinates.modal';
+import { audioTtsUrl } from '@/settings/urls.settings';
+import { IconPlayerPlay } from '@tabler/icons-react';
+import { Button } from '@tmlmobilidade/ui';
+
+export function StopDetailPlayTTS() {
+	//
+	//
+
+	//
+	// A. Setup variables
+
+	const stopDetailContext = useStopDetailContext();
+
+	//
+	// B. Handle actions
+
+	const handlePlayTTS = () => {
+		const ttsName = stopDetailContext.data.form.getValues()?.tts_name ?? '';
+		const audio = new Audio(`${audioTtsUrl}/stops/${ttsName}.mp3`);
+		audio.play();
+	};
+
+	//
+	// C. Render components
+
+	return (
+		<Button label="Reproduzir TTS" onClick={handlePlayTTS} rightSection={<IconPlayerPlay size={16} />} />
+	);
+
+	//
+}
