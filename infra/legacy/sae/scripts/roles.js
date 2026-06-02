cc4ed5ef06f0734b07c40f661934bf7f34315fd5
jusimen <jusi.monteiro@protonmail.com>
2026-06-02 14:48:38 +0100

    chore(roles): add 'find' action for vehicles collection in 'hub' role permissions

--- infra/legacy/sae/scripts/roles.js
+++ infra/legacy/sae/scripts/roles.js
@@ -74,6 +74,7 @@ db.createRole({
 		{ actions: ['find'], resource: { collection: 'localities', db: 'production' } },
 		{ actions: ['find'], resource: { collection: 'municipalities', db: 'production' } },
 		{ actions: ['find'], resource: { collection: 'parishes', db: 'production' } },
+		{ actions: ['find'], resource: { collection: 'vehicles', db: 'production' } },
 		{ actions: ['find', 'update'], resource: { collection: 'plans', db: 'production' } },
 		{ actions: ['find', 'changeStream'], resource: { collection: 'rides', db: 'production' } },
 		{ actions: ['find', 'insert', 'update', 'remove'], resource: { collection: 'files', db: 'production' } },
