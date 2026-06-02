46cc39bf614bb52e65f3352759e86315d8ef90d6
jusimen <jusi.monteiro@protonmail.com>
2026-06-02 11:53:54 +0100

    feat(agency): add agency logos and names mapping for improved asset management

---
+++ modules/hub/apps/frontend-navegante-app/src/lib/agency-logos-map.ts
@@ -0,0 +1,16 @@
+/* eslint-disable perfectionist/sort-objects */
+export const AGENCY_LOGO_MAP = Object.freeze({
+	1: `${process.env.NEXT_PUBLIC_BASE_PATH}/images/logos/carris.png`,
+	2: `${process.env.NEXT_PUBLIC_BASE_PATH}/images/logos/ml.png`,
+	3: `${process.env.NEXT_PUBLIC_BASE_PATH}/images/logos/cp.png`,
+	4: `${process.env.NEXT_PUBLIC_BASE_PATH}/images/logos/ttsl.png`,
+	8: `${process.env.NEXT_PUBLIC_BASE_PATH}/images/logos/tcb.png`,
+	15: `${process.env.NEXT_PUBLIC_BASE_PATH}/images/logos/fertagus.png`,
+	16: `${process.env.NEXT_PUBLIC_BASE_PATH}/images/logos/mts.png`,
+	21: `${process.env.NEXT_PUBLIC_BASE_PATH}/images/logos/mobicascais.png`,
+	41: `${process.env.NEXT_PUBLIC_BASE_PATH}/images/logos/va.png`,
+	42: `${process.env.NEXT_PUBLIC_BASE_PATH}/images/logos/rl.png`,
+	43: `${process.env.NEXT_PUBLIC_BASE_PATH}/images/logos/tst.png`,
+	44: `${process.env.NEXT_PUBLIC_BASE_PATH}/images/logos/alsa.png`,
+	CM: `${process.env.NEXT_PUBLIC_BASE_PATH}/images/logos/cmet.png`,
+});
