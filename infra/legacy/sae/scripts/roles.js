f26f93639869a1dfaa64a4f47e4b8f3ec4733927
jusimen <jusi.monteiro@protonmail.com>
2026-06-02 09:50:42 +0100

    feat(roles): add 'find' action for vehicles collection in role permissions

--- infra/legacy/sae/scripts/roles.js
+++ infra/legacy/sae/scripts/roles.js
@@ -27,6 +27,7 @@ db.createRole({
 		{ actions: ['find'], resource: { collection: 'hashed_patterns', db: 'production' } },
 		{ actions: ['find'], resource: { collection: 'hashed_shapes', db: 'production' } },
 		{ actions: ['find'], resource: { collection: 'hashed_trips', db: 'production' } },
+		{ actions: ['find'], resource: { collection: 'vehicles', db: 'production' } },
 		{ actions: ['find'], resource: { collection: 'stops', db: 'production' } },
 		{ actions: ['find', 'update'], resource: { collection: 'plans', db: 'production' } },
 		{ actions: ['find', 'insert', 'update', 'remove'], resource: { collection: 'files', db: 'production' } },
