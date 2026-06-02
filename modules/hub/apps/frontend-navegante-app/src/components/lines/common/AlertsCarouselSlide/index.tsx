4982a84918e59f903c07938d151c9f247918d8b1
Bruno Castelo <brunoalexcastelo@gmail.com>
2026-06-02 22:36:50 +0100

    feat(alerts): add AlertDetailModal to AlertsCarouselSlide

--- modules/hub/apps/frontend-navegante-app/src/components/lines/common/AlertsCarouselSlide/index.tsx
+++ modules/hub/apps/frontend-navegante-app/src/components/lines/common/AlertsCarouselSlide/index.tsx
@@ -3,7 +3,9 @@
 import type { HubAlert } from '@tmlmobilidade/types';
 
 import { AlertActivePeriodStart } from '@/components/alerts/common/AlertActivePeriod';
+import { AlertDetailModal } from '@/components/lines/common/AlertDetailModal';
 import { IconCircleArrowRightFilled } from '@tabler/icons-react';
+import { useState } from 'react';
 
 import styles from './styles.module.css';
 
@@ -19,15 +21,21 @@ export function AlertsCarouselSlide({ alert }: Props) {
 	//
 
 	//
-	// A. Render components
+	// A. Setup variables
+
+	const [isOpen, setIsOpen] = useState<boolean>(false);
+
+	//
+	// B. Render components
 
 	return (
 		<div className={styles.container}>
 			<AlertActivePeriodStart date={alert.active_period_start_date} size="sm" />
-			<p className={styles.title}>
+			<p className={styles.title} onClick={() => setIsOpen(true)}>
 				{alert.title}
 				<IconCircleArrowRightFilled className={styles.icon} size={16} />
 			</p>
+			<AlertDetailModal alert={alert} isOpen={isOpen} onClose={() => setIsOpen(false)} />
 		</div>
 	);
 
