1d4f989ab6527c76eb04cac9a19a451f9ebd6b3f
jusimen <jusi.monteiro@protonmail.com>
2026-06-05 16:34:08 +0100

    feat(tracker): enhance Fertagus event processing with aggregated ride data
    - Introduced a new interface, FoundRideDocument, to structure ride data retrieved from the database.
    - Updated the ride fetching logic to utilize MongoDB aggregation for improved data retrieval, including a lookup for hashed patterns.
    - Enhanced error logging to provide clearer context on ride fetching issues, specifically regarding event start times and stop IDs.
    - Replaced the ridesMap type to utilize the new FoundRideDocument interface for better type safety and clarity.

--- modules/tracker/apps/fertagus-fetch/src/index.ts
+++ modules/tracker/apps/fertagus-fetch/src/index.ts
@@ -7,13 +7,32 @@ import { TrainsResponse } from '@tmlmobilidade/external/dist/clients/fertagus/ty
 import { rides } from '@tmlmobilidade/interfaces';
 import { Logger } from '@tmlmobilidade/logger';
 import { Timer } from '@tmlmobilidade/timer';
-import { type HashableRawVehicleEvent, RawVehicleEventFertagusV1, Ride } from '@tmlmobilidade/types';
+import { type HashableRawVehicleEvent, HashedPattern, RawVehicleEventFertagusV1, Ride } from '@tmlmobilidade/types';
 import { runOnInterval } from '@tmlmobilidade/utils';
 import crypto from 'node:crypto';
 
 /* * */
 
 let ITERATION = 0;
+
+interface FoundRideDocument {
+	_id: Ride['_id']
+	agency_id: Ride['agency_id']
+	first_stop_id: string
+	last_stop_id: string
+	line_id: HashedPattern['line_id']
+	line_long_name: HashedPattern['line_long_name']
+	line_short_name: HashedPattern['line_short_name']
+	pattern_id: HashedPattern['pattern_id']
+	route_color: HashedPattern['route_color']
+	route_id: HashedPattern['route_id']
+	route_long_name: HashedPattern['route_long_name']
+	route_short_name: HashedPattern['route_short_name']
+	route_text_color: HashedPattern['route_text_color']
+	trip_headsign: HashedPattern['trip_headsign']
+	trip_id: Ride['trip_id']
+
+}
 
 /* * */
 
@@ -38,7 +57,7 @@ const main = async () => {
 
 	Logger.info(`[${ITERATION}] Found ${response.length ?? 0} Vehicle Events in the Fertagus data.`);
 
-	const ridesMap = new Map<string, Ride>();
+	const ridesMap = new Map<string, FoundRideDocument>();
 
 	for (const event of response ?? []) {
 		//
@@ -53,7 +72,57 @@ const main = async () => {
 
 		if (!ridesMap.has(rideKey)) {
 			try {
-				const foundRides = await rides.findMany({ agency_id: '15', start_time_scheduled: Dates.fromISO(event.startsAt).unix_timestamp });
+				const ridesCollection = await rides.getCollection();
+				const foundRides = await ridesCollection.aggregate<FoundRideDocument>([
+					{
+						$match: {
+							agency_id: '15',
+							start_time_scheduled: Dates.fromISO(event.startsAt).unix_timestamp,
+						},
+					},
+					{
+						$lookup: {
+							as: 'hashed_pattern',
+							foreignField: '_id',
+							from: 'hashed_patterns',
+							localField: 'hashed_pattern_id',
+						},
+					},
+					{
+						$unwind: '$hashed_pattern',
+					},
+					{
+						$project: {
+							_id: 1,
+							agency_id: 1,
+							first_stop_id: {
+								$first: '$hashed_pattern.path.stop_id',
+							},
+
+							last_stop_id: {
+								$last: '$hashed_pattern.path.stop_id',
+							},
+							line_id: '$hashed_pattern.line_id',
+							line_long_name: '$hashed_pattern.line_long_name',
+							line_short_name: '$hashed_pattern.line_short_name',
+							pattern_id: '$hashed_pattern.pattern_id',
+							route_color: '$hashed_pattern.route_color',
+							route_id: '$hashed_pattern.route_id',
+							route_long_name: '$hashed_pattern.route_long_name',
+							route_short_name: '$hashed_pattern.route_short_name',
+							route_text_color: '$hashed_pattern.route_text_color',
+
+							trip_headsign: '$hashed_pattern.trip_headsign',
+							trip_id: 1,
+						},
+					},
+					{
+						$match: {
+							first_stop_id: event.stop_id_start,
+							last_stop_id: event.stop_id_end,
+						},
+					},
+				]).toArray();
 
 				//
 				// If no ride is found, skip this event.
@@ -65,13 +134,13 @@ const main = async () => {
 				// If multiple rides are found, skip this event.
 				// This can be improved by checking start and end stop IDs.
 				if (foundRides.length > 1) {
-					Logger.error(`[${ITERATION}] Multiple rides found for event ${event.date} ${event.train_id} ${event.stop_id_start} ${event.stop_id_end} ${event.startsAt}.`);
+					Logger.error(`[${ITERATION}] Multiple rides found for event start time scheduled: ${Dates.fromISO(event.startsAt).unix_timestamp} - ${event.stop_id_start} -> ${event.stop_id_end}.`);
 					continue;
 				}
 
 				ridesMap.set(rideKey, foundRides[0]);
 			} catch (error) {
-				Logger.error(`[${ITERATION}] Error finding rides for event ${event.date} ${event.train_id} ${event.stop_id_start} ${event.stop_id_end} ${event.startsAt}:`, error);
+				Logger.error(`[${ITERATION}] Error finding rides for event start time scheduled: ${Dates.fromISO(event.startsAt).unix_timestamp} - ${event.stop_id_start} -> ${event.stop_id_end}:`, error);
 				continue;
 			}
 		}
