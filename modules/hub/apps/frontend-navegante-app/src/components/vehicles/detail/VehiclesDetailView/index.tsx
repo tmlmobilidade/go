1974c775c7356ff6740aa37ebe7000a2fb954f97
Bruno Castelo <brunoalexcastelo@gmail.com>
2026-06-05 18:31:18 +0100

    refactor(vehicles): remove LicensePlate component

--- modules/hub/apps/frontend-navegante-app/src/components/vehicles/detail/VehiclesDetailView/index.tsx
+++ modules/hub/apps/frontend-navegante-app/src/components/vehicles/detail/VehiclesDetailView/index.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useVehiclesDetailContext } from '@/components/vehicles/detail/VehiclesDetail.context';
+import { VehiclesDetailsContent } from '@/components/vehicles/detail/VehiclesDetailsContent';
 import { Section } from '@tmlmobilidade/ui';
 
 /* * */
@@ -9,24 +9,11 @@ export function VehiclesDetailView() {
 	//
 
 	//
-	// A. Setup variables
-
-	const vehiclesDetailContext = useVehiclesDetailContext();
-
-	//
-	// B. Render componentss
+	// A. Render componentss
 
 	return (
 		<Section flexDirection="column" padding="none">
-			<p>vehicle_id: {vehiclesDetailContext.data.vehicle?.vehicle_id}</p>
-			<p>agency_id: {vehiclesDetailContext.data.vehicle?.agency_id}</p>
-			<p>trip_id: {vehiclesDetailContext.data.vehicle?.trip_id}</p>
-			<p>stop_id: {vehiclesDetailContext.data.vehicle?.stop_id}</p>
-			<p>received_at: {vehiclesDetailContext.data.vehicle?.received_at}</p>
-			<p>created_at: {vehiclesDetailContext.data.vehicle?.created_at}</p>
-			<p>line_id: {vehiclesDetailContext.data.vehicle?.line_id}</p>
-			<p>pattern_id: {vehiclesDetailContext.data.vehicle?.pattern_id}</p>
-			<p>ride_id: {vehiclesDetailContext.data.vehicle?.ride_id}</p>
+			<VehiclesDetailsContent />
 		</Section>
 	);
 }
--- modules/hub/apps/frontend-navegante-app/src/components/vehicles/detail/VehiclesDetailsContent/index.tsx
+++ modules/hub/apps/frontend-navegante-app/src/components/vehicles/detail/VehiclesDetailsContent/index.tsx
@@ -3,13 +3,8 @@
 /* * */
 
 import { useLinesContext } from '@/components/lines/Lines.context';
-import { LicensePlate } from '@/components/vehicles/common/LicensePlate';
 import { useVehiclesDetailContext } from '@/components/vehicles/detail/VehiclesDetail.context';
-import { LineDisplay, Section, Table } from '@tmlmobilidade/ui';
-import { useMemo } from 'react';
-import { useTranslation } from 'react-i18next';
-
-import styles from './styles.module.css';
+import { LineDisplay, Section } from '@tmlmobilidade/ui'; ;
 
 /* * */
 
@@ -19,32 +14,17 @@ export function VehiclesDetailsContent() {
 	//
 	// A. Setup variables
 
-	const { t } = useTranslation();
-
-	const optionLabels = useTranslation();
-
 	const linesContext = useLinesContext();
 	const vehiclesDetailContext = useVehiclesDetailContext();
 
 	//
 	// B. Fetch data
 
-	const activeLineData = useMemo(() => {
-		return linesContext.data.lines.find(line => line._id === vehiclesDetailContext.data.vehicle?.line_id);
-	}, [vehiclesDetailContext.data.vehicle?.line_id, linesContext.data.lines]);
+	const activeLineData = linesContext.data.lines.find(line => line._id === vehiclesDetailContext.data.selected?.line_id?.trim());
 
-	const rows = [
-		{ label: 'ID', value: vehiclesDetailContext.data.vehicle?.vehicle_id },
-		{ label: 'Lugares Sentados', value: vehiclesDetailContext.data.vehicle?.available_seats },
-		{ label: 'Lugares em pé', value: vehiclesDetailContext.data.vehicle?.available_standing },
-		{ label: 'Capacidade Total', value: vehiclesDetailContext.data.vehicle?.available_seats + vehiclesDetailContext.data.vehicle?.available_standing },
-		{ label: 'Marca', value: vehiclesDetailContext.data.vehicle?.make },
-		{ label: 'Modelo', value: vehiclesDetailContext.data.vehicle?.model },
-		{ label: 'Propulsão', value: vehiclesDetailContext.data.vehicle?.propulsion ? optionLabels(`VehiclePropulsion.${vehiclesDetailContext.data.vehicle.propulsion}`) : t('unknown') },
-		{ label: 'Emission Class', value: vehiclesDetailContext.data.vehicle?.emission ? optionLabels(`VehicleEmissionClass.${vehiclesDetailContext.data.vehicle.emission}`) : t('unknown') },
-		{ label: 'Estado Atual', value: vehiclesDetailContext.data.vehicle?.current_status },
-		{ label: 'Trip ID', value: vehiclesDetailContext.data.vehicle?.trip_id || t('unknown') },
-	];
+	console.log(vehiclesDetailContext.data.vehicle?.line_id);
+	console.log('linesContext.data.lines', linesContext.data.lines);
+	console.log(activeLineData);
 
 	//
 	// C. Render components
@@ -52,34 +32,17 @@ export function VehiclesDetailsContent() {
 	return (
 		<Section padding="md">
 
-			{vehiclesDetailContext.data.vehicle ? (
-				<>
-					<div className={styles.dataWrapper}>
-						<LineDisplay lineData={activeLineData} />
+			<LineDisplay color={activeLineData?.color} longName={activeLineData?.long_name} shortName={activeLineData?.short_name} textColor={activeLineData?.text_color} />
 
-						<div className={styles.iconList}>
-							<TooltipIcon icon={vehiclesListContext.data.selected?.bikes_allowed ? <IconBike /> : <IconBikeOff />} label={vehiclesListContext.data.selected?.bikes_allowed ? t('bikes_allowed') : t('no_bikes_allowed')} position="bottom" />
-							<TooltipIcon icon={vehiclesListContext.data.selected?.wheelchair_accessible ? <IconDisabled2 /> : <IconDisabledOff />} label={vehiclesListContext.data.selected?.wheelchair_accessible ? t('wheelchair_accessible') : t('no_wheelchair_accessible')} position="bottom" />
-							<TooltipIcon icon={vehiclesListContext.data.selected.contactless ? <IconCreditCard /> : <IconCreditCardOff />} label={vehiclesListContext.data.selected.contactless ? t('contactless') : t('no_contactless')} position="bottom" />
-							{vehiclesListContext.data.selected.license_plate && <LicensePlate value={vehiclesListContext.data.selected.license_plate} />}
-						</div>
-
-						<Table withRowBorders>
-							<Table.Tbody>
-								{rows.map(row => (
-									<Table.Tr key={row.label}>
-										<Table.Td className={styles.rowLabel}>{row.label}</Table.Td>
-										<Table.Td className={styles.rowValue}>{row.value}</Table.Td>
-									</Table.Tr>
-								))}
-							</Table.Tbody>
-						</Table>
-
-					</div>
-				</>
-			) : (
-				<NoDataLabel text={t('no_data')} />
-			)}
+			<p>vehicle_id: {vehiclesDetailContext.data.vehicle?.vehicle_id}</p>
+			<p>agency_id: {vehiclesDetailContext.data.vehicle?.agency_id}</p>
+			<p>trip_id: {vehiclesDetailContext.data.vehicle?.trip_id}</p>
+			<p>stop_id: {vehiclesDetailContext.data.vehicle?.stop_id}</p>
+			<p>received_at: {vehiclesDetailContext.data.vehicle?.received_at}</p>
+			<p>created_at: {vehiclesDetailContext.data.vehicle?.created_at}</p>
+			<p>line_id: {vehiclesDetailContext.data.vehicle?.line_id}</p>
+			<p>pattern_id: {vehiclesDetailContext.data.vehicle?.pattern_id}</p>
+			<p>ride_id: {vehiclesDetailContext.data.vehicle?.ride_id}</p>
 		</Section>
 	);
 
