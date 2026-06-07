ecf3ad97018c1b090cf3b711c5bd25b6bc79229b
João de Vasconcelos <10943992+joao-vasconcelos@users.noreply.github.com>
2026-06-07 22:48:40 +0100

    feat(action-bar-button): add label prop and improve accessibility
    - Introduced a `label` prop to the ActionBarButton component for better accessibility.
    - Added `aria-label` and `role` attributes to the button for enhanced screen reader support.

--- modules/hub/apps/frontend-navegante-app/src/components/common/action-bar/ActionBarButton/index.tsx
+++ modules/hub/apps/frontend-navegante-app/src/components/common/action-bar/ActionBarButton/index.tsx
@@ -7,15 +7,22 @@ import styles from './styles.module.css';
 interface ActionBarButtonProps {
 	badgeCount?: number
 	icon: React.ReactNode
+	label: string
 	onClick?: () => void
 	variant?: 'active' | 'default' | 'disabled'
 }
 
 /* * */
 
-export function ActionBarButton({ badgeCount, icon, onClick, variant = 'default' }: ActionBarButtonProps) {
+export function ActionBarButton({ badgeCount, icon, label, onClick, variant = 'default' }: ActionBarButtonProps) {
 	return (
-		<div className={styles.button} data-variant={variant} onClick={onClick}>
+		<div
+			aria-label={label}
+			className={styles.button}
+			data-variant={variant}
+			onClick={onClick}
+			role="button"
+		>
 			{badgeCount && <div className={styles.badge}>{badgeCount}</div>}
 			{icon}
 		</div>
