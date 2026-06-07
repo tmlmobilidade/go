1ae33aeb96acd54528c264e6fd43648dc46c0796
jusimen <jusi.monteiro@protonmail.com>
2026-06-07 18:49:59 +0100

    feat(hub): wrap children with EtaContextProvider for real-time trip updates
    - Integrated `EtaContextProvider` into the Providers component to enable real-time trip updates within the application.
    - This addition enhances the context management for trip-related data, ensuring that child components can access the latest updates.

--- modules/hub/apps/frontend-navegante-app/src/app/providers.tsx
+++ modules/hub/apps/frontend-navegante-app/src/app/providers.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { AlertsContextProvider } from '@/components/alerts/Alerts.context';
+import { EtaContextProvider } from '@/components/eta/eta.context';
 import { LinesContextProvider } from '@/components/lines/Lines.context';
 import { StopsContextProvider } from '@/components/stops/Stops.context';
 import { VehiclesContextProvider } from '@/components/vehicles/Vehicles.context';
@@ -16,7 +17,9 @@ export function Providers({ children }: PropsWithChildren) {
 				<StopsContextProvider>
 					<LinesContextProvider>
 						<VehiclesContextProvider>
-							{children}
+							<EtaContextProvider>
+								{children}
+							</EtaContextProvider>
 						</VehiclesContextProvider>
 					</LinesContextProvider>
 				</StopsContextProvider>
