91596712e2b4a1d75c347c01aff00b56fd4b428b
João de Vasconcelos <10943992+joao-vasconcelos@users.noreply.github.com>
2026-06-08 13:20:50 +0100

    feat(hub): integrate SWR for app status and update styles
    - Added SWR to fetch app status from an external JSON endpoint, allowing dynamic updates.
    - Updated the ComingSoon component to conditionally render based on the app status.
    - Enhanced CSS to hide the container when the app is enabled, improving user experience.

--- modules/hub/apps/frontend-navegante-app/src/components/ComingSoon/index.tsx
+++ modules/hub/apps/frontend-navegante-app/src/components/ComingSoon/index.tsx
@@ -1,14 +1,23 @@
 'use client';
 
+import useSWR from 'swr';
+
 import styles from './styles.module.css';
 
 /* * */
 
 export function ComingSoon() {
+	//
+
+	const { data: appEnabled } = useSWR<{ app_enabled: boolean }>({ credentials: 'omit', url: 'https://storage.carrismetropolitana.pt/static/navegante/flag.json', useProperApiResponse: false }, { refreshInterval: 5_000 });
+
+	//
+
 	return (
-		<div className={styles.container}>
+		<div className={styles.container} data-app-enabled={appEnabled?.app_enabled ?? false}>
 			<h1>Disponível em breve</h1>
 			<p>Fique atento às novidades nas nossas redes!</p>
+			<p className={styles.dismiss}>dimsiss</p>
 		</div>
 	);
 }
