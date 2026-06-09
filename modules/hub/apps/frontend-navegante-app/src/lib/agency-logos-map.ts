78772e24d069c00086b77ae3aaa8cc4f875bd596
João de Vasconcelos <10943992+joao-vasconcelos@users.noreply.github.com>
2026-06-09 14:29:18 +0100

    fix(agency-logos-map): update agency logo filename for '21' to correct naming convention

--- modules/hub/apps/frontend-navegante-app/src/lib/agency-logos-map.ts
+++ modules/hub/apps/frontend-navegante-app/src/lib/agency-logos-map.ts
@@ -7,7 +7,7 @@ export const AGENCY_LOGO_MAP = Object.freeze({
 	8: `${process.env.NEXT_PUBLIC_BASE_PATH}/assets/navegante/agency-logos/navegante-agency-logo-tcb-120x80-light.png`,
 	15: `${process.env.NEXT_PUBLIC_BASE_PATH}/assets/navegante/agency-logos/navegante-agency-logo-fertagus-120x80-light.png`,
 	16: `${process.env.NEXT_PUBLIC_BASE_PATH}/assets/navegante/agency-logos/navegante-agency-logo-mts-120x80-light.png`,
-	21: `${process.env.NEXT_PUBLIC_BASE_PATH}/assets/navegante/agency-logos/navegante-agency-logo-mobicascais-120x80-light.png`,
+	21: `${process.env.NEXT_PUBLIC_BASE_PATH}/assets/navegante/agency-logos/navegante-agency-logo-mobi-120x80-light.png`,
 	41: `${process.env.NEXT_PUBLIC_BASE_PATH}/assets/navegante/agency-logos/navegante-agency-logo-cmet-120x80-light.png`,
 	42: `${process.env.NEXT_PUBLIC_BASE_PATH}/assets/navegante/agency-logos/navegante-agency-logo-cmet-120x80-light.png`,
 	43: `${process.env.NEXT_PUBLIC_BASE_PATH}/assets/navegante/agency-logos/navegante-agency-logo-cmet-120x80-light.png`,
