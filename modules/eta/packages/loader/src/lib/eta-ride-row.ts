0a0fe9d93544c5d72278c98c5d2f04ff87bc5c71
jusimen <jusi.monteiro@protonmail.com>
2026-06-11 19:04:27 +0100

    feat(eta): add agency_id and plan_id to ride data structure
    - Updated the toEtaRideRow function to include agency_id and plan_id in the returned ride object.
    - Modified the SQL schema to add agency_id and plan_id columns to the hist_rides table, ensuring alignment with the updated data structure.

--- modules/eta/packages/loader/src/lib/eta-ride-row.ts
+++ modules/eta/packages/loader/src/lib/eta-ride-row.ts
@@ -3,9 +3,11 @@ import type { Ride } from '@tmlmobilidade/types';
 export function toEtaRideRow(ride: Ride) {
 	return {
 		_id: ride._id,
+		agency_id: ride.agency_id,
 		end_time_observed: ride.end_time_observed,
 		hashed_shape_id: ride.hashed_shape_id,
 		hashed_trip_id: ride.hashed_trip_id,
+		plan_id: ride.plan_id,
 		start_time_observed: ride.start_time_observed,
 		start_time_scheduled: ride.start_time_scheduled,
 		trip_id: ride.trip_id,
