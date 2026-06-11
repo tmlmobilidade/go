b5f2b4c7ec852b40ac3d4866b80794f7d44fca25
João de Vasconcelos <10943992+joao-vasconcelos@users.noreply.github.com>
2026-06-11 19:20:19 +0100

    refactor(clickhouse-stream): improve changeStream validation logic

--- modules/tracker/apps/clickhouse-stream/src/index.ts
+++ modules/tracker/apps/clickhouse-stream/src/index.ts
@@ -21,29 +21,29 @@ import { Logger } from '@tmlmobilidade/logger';
 		.on('change', async (change) => {
 			//
 
-		//
-		// Validate that the operation is an insert or update. Otherwise, send an email to the emergency contact.
-		// Only insert operations are expected to occur in this PCGIDB collection.
+			//
+			// Validate that the operation is an insert or update. Otherwise, send an email to the emergency contact.
+			// Only insert operations are expected to occur in this PCGIDB collection.
 
-		if (change.operationType !== 'insert') {
-			Logger.error(`[clickhouse-stream] WARNING: changeStream document with operationType != "insert": operationType="${change.operationType}" _id="${change._id}"`);
-			continue;
-		}
+			if (change.operationType !== 'insert') {
+				Logger.error(`[clickhouse-stream] WARNING: changeStream document with operationType != "insert": operationType="${change.operationType}" _id="${change._id}"`);
+				return;
+			}
 
-		if (!change.fullDocument) {
-			Logger.error(`[clickhouse-stream] WARNING: changeStream document with missing fullDocument: operationType="${change.operationType}" _id="${change._id}"`);
-			continue;
-		}
+			if (!change.fullDocument) {
+				Logger.error(`[clickhouse-stream] WARNING: changeStream document with missing fullDocument: operationType="${change.operationType}" _id="${change._id}"`);
+				return;
+			}
 
-		const nowMinus5Minutes = Dates.now('Europe/Lisbon').minus({ minutes: 5 }).unix_timestamp;
+			const nowMinus5Minutes = Dates.now('Europe/Lisbon').minus({ minutes: 5 }).unix_timestamp;
 
-		if (!change.fullDocument.created_at || change.fullDocument.created_at < nowMinus5Minutes) {
-			Logger.error(`[clickhouse-stream] WARNING: changeStream document with missing or outdated created_at field: operationType="${change.operationType}" _id="${change._id}"`);
-			continue;
-		}
+			if (!change.fullDocument.created_at || change.fullDocument.created_at < nowMinus5Minutes) {
+				Logger.error(`[clickhouse-stream] WARNING: changeStream document with missing or outdated created_at field: operationType="${change.operationType}" _id="${change._id}"`);
+				return;
+			}
 
-		await processVehicleEvent(change);
-	}
+			await processVehicleEvent(change);
+		});
 
 	//
 })();
