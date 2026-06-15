5765c1ac434fec01a32d3dd8dd5dff0126c753f8
João de Vasconcelos <10943992+joao-vasconcelos@users.noreply.github.com>
2026-06-15 11:56:24 +0100

    refactor(types): remove AnySimplifiedApex type and update exports
    - Deleted the AnySimplifiedApex type definition to streamline the type structure.
    - Updated the index file to remove the export of AnySimplifiedApex, enhancing clarity and maintainability of the type exports.

---
+++ modules/apex/apps/raw-sync-banking-taps/src/index.ts
@@ -0,0 +1,45 @@
+/* * */
+
+import { syncApexLocations } from '@/task.js';
+import { getEarliestDate } from '@tmlmobilidade/consts';
+import { Logger } from '@tmlmobilidade/logger';
+import { Timer } from '@tmlmobilidade/timer';
+import { performInTimeChunks, runOnInterval } from '@tmlmobilidade/utils';
+
+/* * */
+
+async function main() {
+	try {
+		//
+
+		Logger.init();
+
+		const globalTimer = new Timer();
+
+		//
+		// Get the earliest date from which we have data to sync,
+		// and perform the sync in time chunks until we reach the current date.
+
+		const earliestDate = getEarliestDate();
+
+		//
+		// Divide the time range into chunks
+		// and sync each one sequentially.
+
+		await performInTimeChunks({
+			onChunk: syncApexLocations,
+			splitBy: { hours: 2 },
+			startDate: earliestDate.unix_timestamp,
+		});
+
+		Logger.terminate(`Run took ${globalTimer.get()}.`);
+
+		//
+	} catch (err) {
+		console.log('An error occurred. Halting execution.', err);
+	}
+}
+
+/* * */
+
+await runOnInterval(main, { intervalMs: '30m' });
---
+++ modules/apex/apps/raw-sync-refunds/src/task.ts
@@ -0,0 +1,115 @@
+/* * */
+
+import { rawApexTransactions, simplifiedApexLocationsNew } from '@tmlmobilidade/databases';
+import { Dates } from '@tmlmobilidade/dates';
+import { parseRawApexTransactionLocationV30 } from '@tmlmobilidade/go-apex-pckg-parsers';
+import { type RawApexTransaction, type SimplifiedApexLocation } from '@tmlmobilidade/go-types-apex';
+import { Logger } from '@tmlmobilidade/logger';
+import { type PerformInTimeChunksItem, replicate } from '@tmlmobilidade/utils';
+import { BatchWriter } from '@tmlmobilidade/utils';
+import { type Filter } from 'mongodb';
+
+/* * */
+
+const writer = new BatchWriter<SimplifiedApexLocation>({
+	batch_size: 50_000,
+	insertFn: async (data) => {
+		await simplifiedApexLocationsNew.insert('JSONEachRow', data);
+	},
+	title: await simplifiedApexLocationsNew.getTableName(),
+});
+
+/**
+ * Syncs APEX Locations from the PCGI database
+ * to the ClickHouse database for a given time chunk.
+ * @param timeChunk The time chunk to sync the data for.
+ */
+export async function syncApexLocations(timeChunk: PerformInTimeChunksItem) {
+	//
+
+	const chunkStartDate = Dates
+		.fromUnixTimestamp(timeChunk.start)
+		.setZone('Europe/Lisbon', 'offset_only');
+
+	const chunkEndDate = Dates
+		.fromUnixTimestamp(timeChunk.end)
+		.setZone('Europe/Lisbon', 'offset_only');
+
+	Logger.spacer(1);
+	Logger.divider(`[${timeChunk.total - timeChunk.index}/${timeChunk.total}] - ${chunkEndDate.iso}[${chunkEndDate.unix_timestamp}] › ${chunkStartDate.iso}[${chunkStartDate.unix_timestamp}]`, 150);
+
+	//
+	// Prepare the PCGIDB query to retrieve documents
+	// for the current timestamp chunk.
+
+	const rawdbQuery: Filter<RawApexTransaction> = {
+		created_at: {
+			$gte: chunkStartDate.unix_timestamp,
+			$lte: chunkEndDate.unix_timestamp,
+		},
+		version: { $in: ['location-3.0'] },
+	};
+
+	//
+	// Implement the replication process using the generic replicate function from the utils package.
+	// This function will handle the logic of counting, comparing, syncing and deleting documents
+	// between the source and destination databases based on the provided functions.
+
+	const rawApexTransactionsCollection = await rawApexTransactions.getCollection();
+
+	await replicate<RawApexTransaction>({
+
+		countDestinationDbFn: async () => {
+			return await simplifiedApexLocationsNew.count(
+				'*',
+				'created_at >= $1 AND created_at <= $2',
+				{ 1: chunkStartDate.unix_timestamp, 2: chunkEndDate.unix_timestamp },
+			);
+		},
+
+		countSourceDbFn: async () => {
+			const result = await rawApexTransactions.count(rawdbQuery);
+			return result;
+		},
+
+		deleteDestinationDbFn: async (ids: string[]) => {
+			await simplifiedApexLocationsNew.delete(
+				'_id IN ($1)',
+				{ 1: ids.map(id => `'${id}'`).join(', ') },
+			);
+		},
+
+		distinctDestinationDbFn: async () => {
+			return await simplifiedApexLocationsNew.distinct(
+				'_id',
+				'created_at >= $1 AND created_at <= $2',
+				{ 1: chunkStartDate.unix_timestamp, 2: chunkEndDate.unix_timestamp },
+			);
+		},
+
+		distinctSourceDbFn: async () => {
+			const result = await rawApexTransactions.distinct('_id', rawdbQuery);
+			return result.map(String);
+		},
+
+		missingDocumentsSourceDbAsyncIterator: (missingDocumentIds) => {
+			return rawApexTransactionsCollection
+				.find({ _id: { $in: missingDocumentIds } })
+				.stream();
+		},
+
+		onCompleteCallbackFn: async () => {
+			await writer.flush();
+		},
+
+		writeSourceDocumentToDestinationDbFn: async (sourceDbDocument) => {
+			let parseResult: null | SimplifiedApexLocation = null;
+			if (sourceDbDocument.version === 'location-3.0') parseResult = parseRawApexTransactionLocationV30(sourceDbDocument);
+			if (!parseResult) return;
+			await writer.write(parseResult);
+		},
+
+	});
+
+	//
+}
