8fbde7af774ddc7230304b71608c2ecefab6e9ce
LoudNeo <meliodasgamer077@gmail.com>
2026-06-16 12:54:12 +0100

    refactor(logger): update Sentry client retrieval and package dependencies
    - Replaced the `GoSentryClient` class with an async function `getSentryNodeClient` for improved simplicity in retrieving the Sentry client.
    - Adjusted the package.json to move `@tmlmobilidade/consts` dependency to the correct location, ensuring proper dependency management.

--- packages/logger/src/sentry/client/go-sentry-node.ts
+++ packages/logger/src/sentry/client/go-sentry-node.ts
@@ -1,57 +1,14 @@
 import * as Sentry from '@sentry/node';
 
-export class GoSentryClient {
+export async function getSentryNodeClient() {
 	//
 
-	private static _instance: null | Promise<GoSentryClient> = null;
+	//
+	// Validate required environment variables
 
-	/**
-	 * Disallow direct instantiation of the service.
-	 * Use getClient() instead to ensure singleton behavior.
-	 */
-	private constructor() {}
-
-	public static async getClient() {
-		// If no instance exists, create one and store the promise.
-		// This ensures that if multiple calls to getClient() happen concurrently,
-		// they will all await the same initialization process.
-		if (!this._instance) {
-			this._instance = (async () => {
-				const instance = new GoSentryClient();
-				await instance.connect();
-				return instance;
-			})();
-		}
-		// Await the instance if it's still initializing,
-		// or return it immediately if ready.
-		const instance = await this._instance;
-		return instance;
+	if (!process.env.SENTRY_DSN) {
+		throw new Error('Missing SENTRY_DSN, please set the SENTRY_DSN environment variable for this work my friend!');
 	}
 
-	private async connect() {
-		//
-
-		//
-		// Initialize Sentry
-
-		const connectionString = await this.getConnectionString();
-		Sentry.init({ dsn: connectionString });
-	}
-
-	private async getConnectionString(): Promise<string> {
-		//
-
-		//
-		// Validate required environment variables
-
-		if (!process.env.SENTRY_DSN) {
-			throw new Error('Missing SENTRY_DSN, please set the SENTRY_DSN environment variable for this work my friend!');
-		}
-
-		return process.env.SENTRY_DSN;
-	}
-}
-
-export async function getSentryNodeClient() {
-	return await GoSentryClient.getClient();
+	return Sentry.init({ dsn: process.env.SENTRY_DSN });
 }
