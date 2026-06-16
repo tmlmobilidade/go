7163ccf73339da109c32bf837e40f79663034df1
João de Vasconcelos <10943992+joao-vasconcelos@users.noreply.github.com>
2026-06-16 20:30:45 +0100

    refactor(locations): rename parsing function and enhance date handling
    - Renamed the parsing function to `parseRawApexTransactionLocationV30IntoSimplifiedApexLocation` for clarity.
    - Integrated the `calendar_date` property into the simplified location structure.
    - Streamlined date handling by preparing and validating transaction dates consistently across the parsing logic.

--- modules/apex/packages/parsers/src/from-raw-to-simplified/locations/location-v30.ts
+++ modules/apex/packages/parsers/src/from-raw-to-simplified/locations/location-v30.ts
@@ -1,54 +1,39 @@
 /* * */
 
-import { getEarliestDate } from '@tmlmobilidade/consts';
 import { Dates } from '@tmlmobilidade/dates';
-import { type RawApexTransactionLocationV30, type SimplifiedApexLocation } from '@tmlmobilidade/go-types-apex';
+import { type RawApexTransactionLocationV30, type SimplifiedApexLocation, SimplifiedApexLocationSchema } from '@tmlmobilidade/go-types-apex';
 
 /* * */
 
-export function parseRawApexTransactionLocationV30(doc: RawApexTransactionLocationV30): null | SimplifiedApexLocation {
-	try {
-		//
+export function parseRawApexTransactionLocationV30IntoSimplifiedApexLocation(doc: RawApexTransactionLocationV30): null | SimplifiedApexLocation {
+	//
 
-		//
-		// Validate the document structure and content
+	//
+	// Prepare the date field values
 
-		if (!doc.payload.operatorInfo.operatorLongID) throw new Error('Missing operatorLongID in transaction.');
+	const transactionDateValue = Dates
+		.fromISO(doc.payload.transactionInfo.transactionDate)
+		.setZone('Europe/Lisbon', 'rebase_utc');
 
-		//
-		// Evaluate the transaction date and ensure it is not before the set earliest date
+	//
+	// Validate the document structure and content
 
-		if (!doc.payload.transactionInfo.transactionDate) throw new Error('Missing transactionDate in transaction.');
+	const result: SimplifiedApexLocation = {
+		_id: doc.payload.transactionInfo.transactionId,
+		agency_id: doc.payload.operatorInfo.operatorLongID,
+		apex_version: doc.payload.versionInfo.apexVersion,
+		calendar_date: transactionDateValue.calendar_date,
+		created_at: transactionDateValue.unix_timestamp,
+		device_id: doc.payload.operatorInfo.deviceID,
+		line_id: doc.payload.validationServiceInfo.lineLongID,
+		mac_ase_counter_value: doc.payload.mac.aseCounterValue,
+		mac_sam_serial_number: doc.payload.mac.samSerialNumber,
+		pattern_id: doc.payload.validationServiceInfo.patternLongID,
+		received_at: doc.received_at,
+		stop_id: doc.payload.validationServiceInfo.stopLongID,
+		trip_id: doc.payload.validationServiceInfo.journeyID,
+		vehicle_id: doc.payload.validationServiceInfo.vehicleID,
+	};
 
-		const earliestTransactionDate = getEarliestDate();
-
-		const transactionDate = Dates
-			.fromISO(doc.payload.transactionInfo.transactionDate)
-			.setZone('Europe/Lisbon', 'rebase_utc')
-			.unix_timestamp;
-
-		if (transactionDate < earliestTransactionDate.unix_timestamp) throw new Error(`Transaction date "${doc.payload.transactionInfo.transactionDate}" is before the earliest allowed date "${earliestTransactionDate.operational_date}".`);
-
-		//
-		// Parse the document and return the simplified APEX object
-
-		return {
-			_id: doc.payload.transactionInfo.transactionId,
-			agency_id: doc.payload.operatorInfo.operatorLongID,
-			apex_version: doc.payload.versionInfo.apexVersion,
-			created_at: transactionDate,
-			device_id: doc.payload.operatorInfo.deviceID,
-			line_id: doc.payload.validationServiceInfo.lineLongID,
-			mac_ase_counter_value: doc.payload.mac.aseCounterValue,
-			mac_sam_serial_number: doc.payload.mac.samSerialNumber,
-			pattern_id: doc.payload.validationServiceInfo.patternLongID,
-			received_at: doc.created_at,
-			stop_id: doc.payload.validationServiceInfo.stopLongID,
-			trip_id: doc.payload.validationServiceInfo.journeyID,
-			vehicle_id: doc.payload.validationServiceInfo.vehicleID,
-		};
-	} catch (error) {
-		console.error(`Error parsing simplified APEX Validation. Transaction ID: "${doc.payload.transactionInfo.transactionId}"`, error.message);
-		return null;
-	}
+	return SimplifiedApexLocationSchema.parse(result);
 }
