3ead2ba398d92cad1625271f122ff98575472600
João de Vasconcelos <10943992+joao-vasconcelos@users.noreply.github.com>
2026-06-20 16:13:29 +0100

    feat(hub): enhance CcflDefault component to display today's CCFL demand
    - Added a new data transformation using useMemo to calculate and format the number of passengers transported today by the CCFL agency.
    - Updated the title and value displayed in the CardDefault component to reflect the new CCFL demand metric.

--- modules/hub/apps/frontend-videowall/src/components/agencies/ccfl/CcflDefault/index.tsx
+++ modules/hub/apps/frontend-videowall/src/components/agencies/ccfl/CcflDefault/index.tsx
@@ -6,7 +6,9 @@ import { CardDefault } from '@/components/CardDefault';
 import { Grid } from '@/components/Grid';
 import { IconCreditCardPay } from '@tabler/icons-react';
 import { API_ROUTES } from '@tmlmobilidade/consts';
+import { Dates } from '@tmlmobilidade/dates';
 import { type DemandByAgencyByOperationalDate } from '@tmlmobilidade/go-types-performance';
+import { useMemo } from 'react';
 import useSWR from 'swr';
 
 /* * */
@@ -22,6 +24,20 @@ export function CcflDefault() {
 	console.log(demandByAgencyByOperationalDateData);
 
 	//
+	// B. Transform data
+
+	const ccflDemandToday = useMemo(() => {
+		const todayOperationalDate = Dates.now('Europe/Lisbon').operational_date_int;
+		const result = demandByAgencyByOperationalDateData?.find((item) => {
+			const isToday = item.operational_date === todayOperationalDate;
+			const isCCFL = item.agency_id === '1';
+			return isToday && isCCFL;
+		});
+		if (!result) return '-';
+		return Intl.NumberFormat('pt-PT', { style: 'decimal' }).format(result.qty);
+	}, [demandByAgencyByOperationalDateData]);
+
+	//
 	// C. Render components
 
 	return (
@@ -34,8 +50,8 @@ export function CcflDefault() {
 					isLoading={demandByAgencyByOperationalDateLoading}
 					isValidating={demandByAgencyByOperationalDateValidating}
 					sentiment="good"
-					title="CM / Passageiros transportados hoje, até agora"
-					valuePrimary={demandByAgencyByOperationalDateData?.length || 0}
+					title="CCFL / Passageiros transportados hoje, até agora"
+					valuePrimary={ccflDemandToday}
 					valueSecondary={1}
 				/>,
 			]}
