b5160b89630d6d5f4b2d942c77647ae4c817cdc3
jusimen <jusi.monteiro@protonmail.com>
2026-06-22 19:11:13 +0100

    feat(types): add ml-v1 RawVehicleEvent type definition

--- packages/types/src/vehicle-events/raw/raw-vehicle-event.ts
+++ packages/types/src/vehicle-events/raw/raw-vehicle-event.ts
@@ -6,6 +6,7 @@ import { RawVehicleEventCmetV1LogSchema } from '@/vehicle-events/raw/cmet/v1-log
 import { RawVehicleEventCpV1Schema } from '@/vehicle-events/raw/cp/v1.js';
 import { RawVehicleEventCrtmAisaV1Schema } from '@/vehicle-events/raw/crtm-aisa/v1.js';
 import { RawVehicleEventFertagusV1Schema } from '@/vehicle-events/raw/fertagus/v1.js';
+import { RawVehicleEventMlV1Schema } from '@/vehicle-events/raw/ml/v1.js';
 import { RawVehicleEventMobiV1Schema } from '@/vehicle-events/raw/mobi/v1.js';
 import { RawVehicleEventTcbV1Schema } from '@/vehicle-events/raw/tcb/v1.js';
 import { RawVehicleEventTtslV1Schema } from '@/vehicle-events/raw/ttsl/v1.js';
@@ -14,6 +15,7 @@ import { z } from 'zod';
 /* * */
 
 export const RawVehicleEventSchema = z.discriminatedUnion('version', [
+	RawVehicleEventMlV1Schema,
 	RawVehicleEventMobiV1Schema,
 	RawVehicleEventCcflV1Schema,
 	RawVehicleEventCmetV1CoreSchema,
