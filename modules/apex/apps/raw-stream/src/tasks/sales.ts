9563c7a995039caf8ac5e3c2e5b4b7130a3de1fe
João de Vasconcelos <10943992+joao-vasconcelos@users.noreply.github.com>
2026-06-24 00:56:07 +0100

    refactor(batch-writer): increase batch size and timeout for improved performance
    - Updated batch size from 500 to 10,000 and added a batch timeout of 30,000 milliseconds across multiple tasks including banking taps, inspection decisions, inspections, locations, refunds, and sales.
    - This change aims to enhance data processing efficiency and reduce the frequency of batch writes.

--- modules/apex/apps/raw-stream/src/tasks/sales.ts
+++ modules/apex/apps/raw-stream/src/tasks/sales.ts
@@ -9,7 +9,8 @@ import { BatchWriter } from '@tmlmobilidade/utils';
 /* * */
 
 const writer = new BatchWriter<SimplifiedApexOnBoardSale>({
-	batch_size: 500,
+	batch_size: 10_000,
+	batch_timeout: 30_000,
 	insertFn: async (data) => {
 		await simplifiedApexOnBoardSalesNew.insert('JSONEachRow', data);
 	},
