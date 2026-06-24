b4bc13a1713169e839dd1924fc62605cf8a79eb5
João de Vasconcelos <10943992+joao-vasconcelos@users.noreply.github.com>
2026-06-24 12:36:39 +0100

    refactor(vehicles): filter vehicle positions by allowed agencies in context provider
    - Updated the VehiclesContextProvider to include a filter that only adds vehicle positions from specified allowed agencies to the GeoJSON feature collection.
    - Removed commented-out agency filtering logic from the publishVehiclesPositions task for cleaner code.

--- modules/hub/apps/publish-realtime/src/tasks/publish-vehicles-positions.ts
+++ modules/hub/apps/publish-realtime/src/tasks/publish-vehicles-positions.ts
@@ -44,8 +44,6 @@ export async function publishVehiclesPositions() {
 	await Promise.all(
 		latestVehicleEventsData.map(async (vehicleEventData) => {
 			try {
-				// Skip if vehicle position is not from an allowed agency
-				if (!['1', '2', '3', '4', '8', '15', '16', '21', '41', '42', '43', '44'].includes(vehicleEventData.agency_id)) return;
 				// Skip if vehicle position does not have a trip_id
 				if (!vehicleEventData.trip_id) return;
 				// Check if there is an active plan for the agency
