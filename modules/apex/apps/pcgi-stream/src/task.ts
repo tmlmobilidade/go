0c7169f06905f550034cbc051998cb0b7fe8d1c7
João de Vasconcelos <10943992+joao-vasconcelos@users.noreply.github.com>
2026-06-24 01:00:30 +0100

    refactor(batch-writer): increase batch size to 5,000 and set batch timeout to 30,000 ms
    - Updated the batch size from 500 to 5,000 and added a batch timeout of 30,000 milliseconds to improve data processing efficiency and reduce the frequency of batch writes.

--- modules/apex/apps/pcgi-stream/src/task.ts
+++ modules/apex/apps/pcgi-stream/src/task.ts
@@ -9,7 +9,8 @@ import { BatchWriter } from '@tmlmobilidade/utils';
 /* * */
 
 const writer = new BatchWriter<RawApexTransaction>({
-	batch_size: 500,
+	batch_size: 5_000,
+	batch_timeout: 30_000,
 	insertFn: async (data) => {
 		const writeOps = data.map(doc => ({
 			updateOne: {
