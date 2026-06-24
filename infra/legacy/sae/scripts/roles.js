92407725570f579df02749d2685b83c8f80a2da3
João de Vasconcelos <10943992+joao-vasconcelos@users.noreply.github.com>
2026-06-24 02:48:23 +0100

    chore(roles): allow tracker ro update rides
    - Modified the 'tracker' role to add 'update' privileges for the 'rides' collection, enhancing the role's capabilities in managing ride data.

--- infra/legacy/sae/scripts/roles.js
+++ infra/legacy/sae/scripts/roles.js
@@ -210,12 +210,12 @@ db.createRole({
 
 db.updateRole('tracker', {
 	privileges: [
-		{ actions: ['find'], resource: { collection: 'rides', db: 'production' } },
 		{ actions: ['find'], resource: { collection: 'plans', db: 'production' } },
 		{ actions: ['find'], resource: { collection: 'stops', db: 'production' } },
 		{ actions: ['find'], resource: { collection: 'hashed_shapes', db: 'production' } },
 		{ actions: ['find'], resource: { collection: 'hashed_trips', db: 'production' } },
 		{ actions: ['find'], resource: { collection: 'hashed_patterns', db: 'production' } },
+		{ actions: ['find', 'update'], resource: { collection: 'rides', db: 'production' } },
 	],
 	roles: [],
 });
