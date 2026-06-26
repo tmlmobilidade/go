e37770b2fcb702e960d3f1fb6c9204f9430d63e7
João de Vasconcelos <10943992+joao-vasconcelos@users.noreply.github.com>
2026-06-26 10:39:21 +0100

    feat(callback): temporarily disable callback functionality
    - Added a console log statement to indicate that the callback functionality is currently disabled, preventing further execution of the function.

--- modules/apex/packages/callback/src/callback.ts
+++ modules/apex/packages/callback/src/callback.ts
@@ -29,6 +29,9 @@ export async function setRidesAsWaiting(data: AnySimplifiedApexDocument[]) {
 	try {
 		//
 
+		console.log('Callback disabled.');
+		return;
+
 		const timer = new Timer();
 
 		//
