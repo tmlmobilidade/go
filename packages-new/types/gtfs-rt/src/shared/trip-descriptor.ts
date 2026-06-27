7c1aa5448af5745c4c2dd2e17813a80f2cf4977a
João de Vasconcelos <10943992+joao-vasconcelos@users.noreply.github.com>
2026-06-27 12:55:10 +0100

    fix(types): update schedule_relationship to be nullable in trip descriptor schema
    - Modified the trip descriptor schema to allow the schedule_relationship field to be nullable, enhancing flexibility in data handling.

--- packages-new/types/gtfs-rt/src/shared/trip-descriptor.ts
+++ packages-new/types/gtfs-rt/src/shared/trip-descriptor.ts
@@ -10,7 +10,7 @@ export const GtfsRtTripDescriptorSchema = z.object({
 	direction_id: z.any(),
 	modified_trip: z.any(),
 	route_id: z.string(),
-	schedule_relationship: GtfsRtScheduleRelationshipSchema,
+	schedule_relationship: GtfsRtScheduleRelationshipSchema.nullish(),
 	start_date: GtfsDateSchema,
 	start_time: GtfsTimeSchema.nullish(),
 	trip_id: z.string(),
