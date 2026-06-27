d1225d52cd9d48fcd6fb4c20005d6ca15ea6d28a
João de Vasconcelos <10943992+joao-vasconcelos@users.noreply.github.com>
2026-06-27 13:47:15 +0100

    feat(types): enhance vehicle event schemas and add new dependencies
    - Added `@tmlmobilidade/go-types-gtfs` as a dependency to improve type definitions.
    - Updated vehicle event schemas to utilize `GtfsRtScheduleRelationshipSchema`, `GtfsDateSchema`, and `GtfsTimeSchema`, allowing for better handling of nullable fields and enhancing data accuracy.
    - Refactored various vehicle event schemas to incorporate new types, ensuring consistency across the project.

--- packages-new/types/gtfs-rt/src/trip-update/index.ts
+++ packages-new/types/gtfs-rt/src/trip-update/index.ts
@@ -1,3 +1,6 @@
+export * from '@/trip-update/stop-time-event.js';
+export * from '@/trip-update/stop-time-properties.js';
+export * from '@/trip-update/stop-time-schedule-relationship.js';
 export * from '@/trip-update/stop-time-update.js';
 export * from '@/trip-update/trip-properties.js';
 export * from '@/trip-update/trip-update.js';
