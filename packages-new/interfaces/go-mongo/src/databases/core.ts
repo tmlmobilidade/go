87193a7c94586e406a493666fe1592fed35b7c73
jusimen <jusi.monteiro@protonmail.com>
2026-07-02 14:40:43 +0100

    feat(go-mongo): implement CoreDatabase class for MongoDB collections
    - Introduced the CoreDatabase class to manage MongoDB collections, including agencies, exports, files, organizations, roles, sessions, users, and verification tokens.
    - Utilized MongoInterfaceTemplate for type-safe interactions with each collection, enhancing the structure and maintainability of database operations.
    - Established a connection to the 'core' database using MongoConnector, laying the groundwork for future database functionalities.

---
+++ packages-new/interfaces/go-mongo/src/databases/core.ts
@@ -0,0 +1,42 @@
+/* * */
+
+import type { Db } from 'mongodb';
+
+import { MongoInterfaceTemplate } from '@/interface.template.js';
+import { MongoConnector } from '@tmlmobilidade/mongo';
+import { Agency, CreateAgencyDto, CreateAgencySchema, CreateFileDto, CreateFileExportDto, CreateFileExportSchema, CreateFileSchema, CreateOrganizationDto, CreateOrganizationSchema, CreateRoleDto, CreateRoleSchema, CreateSessionDto, CreateSessionSchema, CreateUserDto, CreateUserSchema, CreateVerificationTokenDto, CreateVerificationTokenSchema, FileExport, Organization, Role, Session, UpdateAgencyDto, UpdateAgencySchema, UpdateFileDto, UpdateFileExportSchema, UpdateFileSchema, UpdateOrganizationDto, UpdateOrganizationSchema, UpdateRoleDto, UpdateRoleSchema, UpdateSessionDto, UpdateSessionSchema, UpdateUserDto, UpdateUserSchema, UpdateVerificationTokenDto, UpdateVerificationTokenSchema, User, VerificationToken } from '@tmlmobilidade/types';
+/* * */
+
+export class CoreDatabase {
+	//
+
+	//
+	// Collections
+	public readonly agencies: MongoInterfaceTemplate<Agency, CreateAgencyDto, UpdateAgencyDto>;
+	public readonly exports: MongoInterfaceTemplate<FileExport, CreateFileExportDto<any>, Partial<FileExport>>;
+	public readonly files: MongoInterfaceTemplate<File, CreateFileDto, UpdateFileDto>;
+	public readonly organizations: MongoInterfaceTemplate<Organization, CreateOrganizationDto, UpdateOrganizationDto>;
+	public readonly roles: MongoInterfaceTemplate<Role, CreateRoleDto, UpdateRoleDto>;
+	public readonly sessions: MongoInterfaceTemplate<Session, CreateSessionDto, UpdateSessionDto>;
+	public readonly users: MongoInterfaceTemplate<User, CreateUserDto, UpdateUserDto>;
+	public readonly verificationTokens: MongoInterfaceTemplate<VerificationToken, CreateVerificationTokenDto, UpdateVerificationTokenDto>;
+
+	//
+	private readonly database: Db;
+	private readonly databaseName = 'core';
+
+	public constructor(instance: MongoConnector) {
+		// Create the database instance
+		this.database = instance.client.db(this.databaseName);
+
+		// Create collection interfaces
+		this.agencies = new MongoInterfaceTemplate<Agency, CreateAgencyDto, UpdateAgencyDto>('agencies', this.database, CreateAgencySchema, UpdateAgencySchema);
+		this.exports = new MongoInterfaceTemplate<FileExport, CreateFileExportDto<any>, Partial<FileExport>>('exports', this.database, CreateFileExportSchema, UpdateFileExportSchema);
+		this.organizations = new MongoInterfaceTemplate<Organization, CreateOrganizationDto, UpdateOrganizationDto>('organizations', this.database, CreateOrganizationSchema, UpdateOrganizationSchema);
+		this.roles = new MongoInterfaceTemplate<Role, CreateRoleDto, UpdateRoleDto>('roles', this.database, CreateRoleSchema, UpdateRoleSchema);
+		this.sessions = new MongoInterfaceTemplate<Session, CreateSessionDto, UpdateSessionDto>('sessions', this.database, CreateSessionSchema, UpdateSessionSchema);
+		this.users = new MongoInterfaceTemplate<User, CreateUserDto, UpdateUserDto>('users', this.database, CreateUserSchema, UpdateUserSchema);
+		this.verificationTokens = new MongoInterfaceTemplate<VerificationToken, CreateVerificationTokenDto, UpdateVerificationTokenDto>('verificationTokens', this.database, CreateVerificationTokenSchema, UpdateVerificationTokenSchema);
+		this.files = new MongoInterfaceTemplate<File, CreateFileDto, UpdateFileDto>('files', this.database, CreateFileSchema, UpdateFileSchema);
+	}
+}
