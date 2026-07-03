5be5ef3678d53468b2883e7c6e15cdc75769e0d6
João de Vasconcelos <10943992+joao-vasconcelos@users.noreply.github.com>
2026-07-03 19:38:46 +0100

    refactor(vehicle-events): integrate new vehicle events types and update imports
    - Added dependency for `@tmlmobilidade/go-types-vehicle-events` in package.json.
    - Updated import statements in raw-vehicle-events.ts and simplified-vehicle-events.ts to utilize the new vehicle events types.
    - Cleaned up comments and improved documentation for better clarity.

--- packages/databases/src/interfaces/vehicle-events/simplified-vehicle-events.ts
+++ packages/databases/src/interfaces/vehicle-events/simplified-vehicle-events.ts
@@ -1,41 +1,38 @@
-/**
- * Simplified Vehicle Events represent a simplified version of the raw vehicle events.
- * These are stored in ClickHouse for performance and scalability reasons.
-**/
+/* * */
 
 import { GOClickHouseClient } from '@/clients/go-clickhouse.js';
 import { ClickHouseInterfaceTemplate } from '@/templates/clickhouse.js';
-import { type ClickHouseTableSchema, ClickHouseTableEngine } from '@/types/index.js';
-import { type SimplifiedVehicleEvent } from '@tmlmobilidade/types';
+import { ClickHouseTableEngine, type ClickHouseTableSchema } from '@/types/index.js';
+import { type SimplifiedVehicleEvent } from '@tmlmobilidade/go-types-vehicle-events';
 import { asyncSingletonProxy } from '@tmlmobilidade/utils';
 
 /* * */
 
 const tableSchema: ClickHouseTableSchema<SimplifiedVehicleEvent> = {
 	_id: { type: 'String' },
+	agency_code: { type: 'String' },
 	agency_id: { type: 'String' },
+	bearing: { type: 'Nullable(Int64)' },
 	created_at: { type: 'Int64' },
+	current_status: { type: 'Nullable(String)' },
+	driver_id: { type: 'Nullable(String)' },
+	extra_trip_id: { type: 'Nullable(String)' },
 	geohash: { default: 'geohashEncode(longitude, latitude, 7)', type: 'String' },
 	latitude: { type: 'Float64' },
 	longitude: { type: 'Float64' },
+	odometer: { type: 'Nullable(Int64)' },
 	operational_date: { type: 'Date' },
 	received_at: { type: 'Int64' },
-	trip_id: { type: 'String' },
-	vehicle_id: { type: 'String' },
-	// Optional Fields
-	bearing: { type: 'Nullable(Int64)' },
-	current_status: { type: 'Nullable(String)' },
-	door: { type: 'Nullable(String)' },
-	driver_id: { type: 'Nullable(String)' },
-	extra_trip_id: { type: 'Nullable(String)' },
-	odometer: { type: 'Nullable(Int64)' },
-	pattern_id: { type: 'Nullable(String)' },
 	speed: { type: 'Nullable(Int64)' },
 	stop_id: { type: 'Nullable(String)' },
+	trip_id: { type: 'String' },
+	vehicle_id: { type: 'String' },
 };
 
-/* * */
-
+/**
+ * Simplified Vehicle Events represent a simplified version of the raw vehicle events.
+ * These are stored in ClickHouse for performance and scalability reasons.
+ */
 class SimplifiedVehicleEventsNewClass extends ClickHouseInterfaceTemplate<SimplifiedVehicleEvent> {
 	//
 
