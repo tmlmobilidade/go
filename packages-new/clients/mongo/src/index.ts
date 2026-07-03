eb7f38baec14e4e1f0ee9251c740f2fe42d55953
jusimen <jusi.monteiro@protonmail.com>
2026-07-03 11:39:11 +0100

    feat(mongo): export additional MongoDB types for enhanced type safety
    - Updated index.ts to export all types from the 'mongodb' package, improving type safety and usability for MongoDB operations.
    - This change allows for easier access to essential MongoDB types, facilitating better integration and development within the MongoDB client.

--- packages-new/clients/mongo/src/index.ts
+++ packages-new/clients/mongo/src/index.ts
@@ -1,1 +1,32 @@
-export { MongoDatabaseClient, type MongoDatabaseConfig } from './client.js';
+export * from './client.js';
+
+/* * */
+
+export type {
+	AggregationCursor,
+	ChangeStreamDeleteDocument,
+	ChangeStreamDocument,
+	ChangeStreamInsertDocument,
+	ChangeStreamUpdateDocument,
+	Collection,
+	CreateIndexesOptions,
+	Db,
+	DeleteOptions,
+	DeleteResult,
+	Document,
+	Filter,
+	FindOptions,
+	IndexDescription,
+	IndexDescriptionInfo,
+	IndexSpecification,
+	InsertOneOptions,
+	InsertOneResult,
+	MongoClient,
+	MongoClientOptions,
+	OptionalUnlessRequiredId,
+	Sort,
+	UpdateDescription,
+	UpdateOptions,
+	UpdateResult,
+	WithId,
+} from 'mongodb';
