30500d0b079db0756f03f42aad073f00a5772c1a
João de Vasconcelos <10943992+joao-vasconcelos@users.noreply.github.com>
2026-07-08 22:15:52 +0100

    feat(get-rides): enhance ride fetching with session tracking and error handling
    - Introduced a session ID for logging during ride fetching to improve traceability.
    - Implemented error handling to log issues encountered while retrieving rides.
    - Updated the ride fetching logic to use a new method for querying the database, enhancing performance and maintainability.
    - Ensured the busy flag is properly managed to prevent concurrent processing of requests.

--- modules/controller/apps/coordinator/src/get-rides.ts
+++ modules/controller/apps/coordinator/src/get-rides.ts
@@ -14,68 +14,83 @@ let IS_BUSY = false;
 export async function getRides(): Promise<string[]> {
 	//
 
-	const ridesCollection = await rides.getCollection();
-
 	//
 	// The whole point of a coordinator is to prevent multiple instances
 	// from processing the same documents at the same time. For that reason,
 	// we need to make sure that instances request the next batch of documents
 	// sequentially. To do that, we implement a simple lock mechanism.
 
+	const sessionId = Math.random().toString(36).substring(2, 15);
+
 	while (IS_BUSY) {
-		Logger.info({ message: '[rides] Waiting for another request to complete...' });
+		Logger.info({ message: `[${sessionId}] Waiting for another request to complete...` });
 		await new Promise(resolve => setTimeout(resolve, 500));
 	}
 
 	//
-	// Set the busy flag to prevent other requests
-	// from being processed until the current one is done.
+	// Get the next batch of rides
 
-	IS_BUSY = true;
+	try {
+		//
 
-	//
-	// Find all Ride IDs that are waiting analysis and which started before the current time,
-	// sorted in descending order to prioritize the most recent Rides.
+		//
+		// Set the busy flag to prevent other requests
+		// from being processed until the current one is done.
 
-	const fetchTimer = new Timer();
+		IS_BUSY = true;
 
-	const standardWindowInterval = Dates.now('utc').std_window;
+		//
+		// Find all Ride IDs that are waiting analysis and which started before the current time,
+		// sorted in descending order to prioritize the most recent Rides.
 
-	const latestWaitingRides = await ridesCollection
-		.find({ agency_id: { $in: ['41', '42', '43', '44'] }, start_time_scheduled: { $lte: standardWindowInterval.end }, system_status: 'waiting' })
-		.sort({ start_time_scheduled: -1 })
-		.limit(750)
-		.toArray();
+		const fetchTimer = new Timer();
 
-	/* === FOR TESTING === */
-	// const latestWaitingRides = await ridesCollection
-	// 	.find({ _id: 'DC0XN-44-20250303-4412_0_2|300|1955' })
-	// 	.toArray();
-	/* === FOR TESTING === */
+		const standardWindowInterval = Dates.now('utc').std_window;
 
-	const fetchTimerResult = fetchTimer.get();
+		const latestWaitingRides = await rides.findMany(
+			{
+				agency_id: { $in: ['41', '42', '43', '44'] },
+				start_time_scheduled: { $lte: standardWindowInterval.end },
+				system_status: 'waiting',
+			},
+			{
+				limit: 750,
+				projection: { _id: 1 },
+				sort: { start_time_scheduled: -1 },
+			},
+		);
 
-	if (!latestWaitingRides.length) {
-		Logger.info({ message: `[rides] No documents waiting | start_time_scheduled: ${standardWindowInterval.end} (fetch: ${fetchTimerResult})` });
-		IS_BUSY = false;
+		/* === FOR TESTING === */
+		// const latestWaitingRides = await rides.findMany({ _id: 'DC0XN-44-20250303-4412_0_2|300|1955' })
+		/* === FOR TESTING === */
+
+		const fetchTimerResult = fetchTimer.get();
+
+		if (!latestWaitingRides.length) {
+			Logger.info({ message: `[${sessionId}] No documents waiting | start_time_scheduled: ${standardWindowInterval.end} (fetch: ${fetchTimerResult})` });
+			return [];
+		}
+
+		//
+		// Mark those Rides as 'processing' to ensure the next batch of Rdes does not include them,
+		// and return them to the caller instance.
+
+		const markTimer = new Timer();
+
+		const latestWaitingRidesIds = latestWaitingRides.map(item => item._id);
+
+		await rides.updateMany({ _id: { $in: latestWaitingRidesIds } }, { system_status: 'processing' });
+
+		Logger.info({ message: `[${sessionId}] New batch: Qty ${latestWaitingRidesIds.length} | start_time_scheduled: ${latestWaitingRides[latestWaitingRides.length - 1].start_time_scheduled} (fetch: ${fetchTimerResult} | total: ${markTimer.get()})` });
+
+		return latestWaitingRidesIds;
+
+		//
+	} catch (error) {
+		Logger.error({ error, message: `[${sessionId}] Error getting rides: ${error.message}` });
 		return [];
+	} finally {
+		IS_BUSY = false;
+		Logger.info({ message: `[${sessionId}] Session completed` });
 	}
-
-	//
-	// Mark those Rides as 'processing' to ensure the next batch of Rdes does not include them,
-	// and return them to the caller instance.
-
-	const markTimer = new Timer();
-
-	const latestWaitingRidesIds = latestWaitingRides.map(item => item._id);
-
-	await ridesCollection.updateMany({ _id: { $in: latestWaitingRidesIds } }, { $set: { system_status: 'processing' } });
-
-	Logger.info({ message: `[rides] New batch: Qty ${latestWaitingRidesIds.length} | start_time_scheduled: ${latestWaitingRides[latestWaitingRides.length - 1].start_time_scheduled} (fetch: ${fetchTimerResult} | total: ${markTimer.get()})` });
-
-	IS_BUSY = false;
-
-	return latestWaitingRidesIds;
-
-	//
 }
