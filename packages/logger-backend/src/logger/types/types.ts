21799a30e463d16c3648be8352d5d46f2ce8edfe
LoudNeo <meliodasgamer077@gmail.com>
2026-07-09 14:06:13 +0100

    refactor(logger): simplify info logging function
    - Removed unnecessary context handling from the info logging function, streamlining the argument structure.
    - Updated the function to focus solely on the message parameter, enhancing clarity and maintainability.
    - Improved documentation to reflect the simplified usage of the info function.

--- packages/logger-backend/src/logger/types/types.ts
+++ packages/logger-backend/src/logger/types/types.ts
@@ -5,12 +5,3 @@ export interface LoggerColumn {
 }
 
 export type LoggerMessage = (LoggerColumn | string)[] | string;
-
-export type LoggerErrorInputContext = Record<string, unknown> & {
-	message?: string
-	silentConsole?: boolean
-};
-
-export type LoggerInfoInputContext = Record<string, unknown> & {
-	message?: string
-};
