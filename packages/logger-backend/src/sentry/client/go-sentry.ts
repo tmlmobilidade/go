baa21fa488993df754728ea02b7c3b5c7682eb62
LoudNeo <meliodasgamer077@gmail.com>
2026-07-09 15:06:41 +0100

    refactor(sentry): streamline environment checks in Sentry client
    - Updated the Sentry client functions in both backend and frontend to return `undefined` for the 'dev' environment, simplifying the logic.
    - Removed redundant error handling for development checks, enhancing code clarity and maintainability.

--- packages/logger-backend/src/sentry/client/go-sentry.ts
+++ packages/logger-backend/src/sentry/client/go-sentry.ts
@@ -2,17 +2,14 @@ export function getSentryClient(): string {
 	//
 
 	//
+	// If we are on Development, return an empty string
+	if (process.env.ENVIRONMENT === 'dev') {
+		return undefined;
+	}
+
+	//
 	// Validate required environment variables
-
 	if (!process.env.SENTRY_NODE_DSN) {
-		//
-		// Check if are on Development
-		if (process.env.ENVIRONMENT === 'dev') {
-			return '';
-		}
-
-		//
-		// Throw error
 		throw new Error('Missing SENTRY_NODE_DSN, please set the SENTRY_NODE_DSN environment variable for this work my friend!');
 	}
 
--- packages/logger-frontend/src/sentry/client/go-sentry.ts
+++ packages/logger-frontend/src/sentry/client/go-sentry.ts
@@ -2,17 +2,15 @@ export function getSentryClient(): string {
 	//
 
 	//
+	// Check if are on Development
+	if (process.env.ENVIRONMENT === 'dev') {
+		return undefined;
+	}
+
+	//
 	// Validate required environment variables
 
 	if (!process.env.SENTRY_NEXTJS_DSN) {
-		//
-		// Check if are on Development
-		if (process.env.ENVIRONMENT === 'dev') {
-			return '';
-		}
-
-		//
-		// Throw error
 		throw new Error('Missing SENTRY_NEXTJS_DSN, please set the SENTRY_NEXTJS_DSN environment variable for this work my friend!');
 	}
 
