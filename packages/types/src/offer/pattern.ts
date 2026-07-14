4f4b6b3584b67f350eb633cd5759ac559ce5c3f6
jusimen <jusi.monteiro@protonmail.com>
2026-07-14 17:43:59 +0100

    fix(pattern): remove field tts_hash in PatternSchema
    - Eliminated the default value for the tts_hash field in the PatternSchema, ensuring that it must be explicitly set during instantiation.
    - This change improves data integrity by preventing unintended empty string values for tts_hash.

--- packages/types/src/offer/pattern.ts
+++ packages/types/src/offer/pattern.ts
@@ -112,7 +112,6 @@ export const PatternSchema = DocumentSchema.extend({
 	route_id: z.string(),
 	rules: z.array(ScheduleRuleSchema).optional().default([]),
 	shape: ShapeSchema.optional(),
-	tts_hash: z.string().default(''),
 });
 
 export const PatternSimplifiedSchema = z.object({
