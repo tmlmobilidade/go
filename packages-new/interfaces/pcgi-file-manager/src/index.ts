09443128e9bd9150514bcee4af0020127bdb2b71
jusimen <jusi.monteiro@protonmail.com>
2026-07-14 18:13:28 +0100

    feat(pcgi-file-manager): add MongoDB client integration and singleton pattern
    - Introduced new dependencies for MongoDB client, logger, and utility functions in package.json.
    - Implemented PCGIFileManagerClass with a singleton pattern to manage MongoDB connections and location management database initialization.
    - Enhanced code structure for better maintainability and future scalability.

---
+++ packages-new/interfaces/pcgi-file-manager/src/index.ts
@@ -0,0 +1,43 @@
+/* * */
+
+import { type MongoClient, MongoDatabaseClient } from '@tmlmobilidade/go-clients-mongo';
+import { asyncSingletonProxy } from '@tmlmobilidade/utils';
+
+import { LocationManagementDatabase } from './databases/location-management.js';
+
+/* * */
+
+class PCGIFileManagerClass {
+	//
+
+	//
+	//
+	private static _instance: PCGIFileManagerClass;
+
+	private readonly mongoClient: MongoClient;
+
+	//
+	// Databases
+	public readonly locationManagement: LocationManagementDatabase;
+
+	/**
+	 * Establishes a connection to the Mongo database and initializes the collection.
+	 * @throws Error if required RAW_DB_* environment variables are missing or if the connection fails.
+	 */
+	public static async getInstance() {
+		if (!PCGIFileManagerClass._instance) {
+			const mongoClient = await MongoDatabaseClient.getClient({ prefix: 'PCGI_FILE_MANAGER' });
+			PCGIFileManagerClass._instance = new PCGIFileManagerClass(mongoClient);
+		}
+		return PCGIFileManagerClass._instance;
+	}
+
+	//
+	// Constructor
+	private constructor(mongoClient: MongoClient) {
+		this.mongoClient = mongoClient;
+		this.locationManagement = new LocationManagementDatabase(this.mongoClient);
+	}
+}
+
+export const pcgiFileManager = asyncSingletonProxy(PCGIFileManagerClass);
