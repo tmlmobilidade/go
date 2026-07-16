b7d013ff1db1477ed0306c979570ae363da9d1f6
jusimen <jusi.monteiro@protonmail.com>
2026-07-16 16:28:42 +0100

    refactor(notifications): clean up imports and fix typo in user query
    - Removed unused import of roles from the notifications interface.
    - Corrected a typo in the user query method, ensuring proper functionality.
    - This change contributes to the ongoing effort to streamline the codebase and enhance maintainability.

--- packages/interfaces/src/interfaces/notifications/notifications.ts
+++ packages/interfaces/src/interfaces/notifications/notifications.ts
@@ -1,8 +1,6 @@
 /* * */
 
 import { MongoCollectionClass } from '@/common/mongo-collection.js';
-import { roles } from '@/interfaces/auth/roles.js';
-import { goDB } from '@tmlmobilidade/go-interfaces-go-db';
 import { getModuleConfig } from '@tmlmobilidade/consts';
 import { goDB } from '@tmlmobilidade/go-interfaces-go-db';
 import { type CreateNotificationDto, CreateNotificationSchema, type Notification, NotificationPermission, Permission, Role, UpdateNotificationDto, UpdateNotificationSchema, User } from '@tmlmobilidade/types';
@@ -43,7 +41,7 @@ class NotificationsClass extends MongoCollectionClass<Notification, CreateNotifi
 		const rolesWithTopic = await goDB.core.roles.findMany({ 'permissions.action': topic });
 		const roleIdsWithTopic = rolesWithTopic.map(r => r._id);
 
-		const usersWithTopic = await goDB.core.users..findMany({
+		const usersWithTopic = await goDB.core.users.findMany({
 			$or: [
 				{ 'permissions.action': topic },
 				{ role_ids: { $in: roleIdsWithTopic } },
