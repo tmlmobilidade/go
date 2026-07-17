09addc17fdd3c9ee945bd546ac814825b7fc8a37
jusimen <jusi.monteiro@protonmail.com>
2026-07-17 15:32:00 +0100

    refactor: remove deprecated date and offer interfaces
    This commit deletes the annotations, events, holidays, year-periods, fares, lines, patterns, routes, typologies, and zones interfaces from the project, streamlining the codebase by removing unused files and classes.

--- packages/interfaces/src/interfaces/dates/index.ts
+++
@@ -1,4 +0,0 @@
-export * from '@/interfaces/dates/annotations.js';
-export * from '@/interfaces/dates/events.js';
-export * from '@/interfaces/dates/holidays.js';
-export * from '@/interfaces/dates/year-periods.js';
