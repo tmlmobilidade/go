ba2765d1534dedd52669388000859e370b64f7c6
LoudNeo <meliodasgamer077@gmail.com>
2026-07-20 16:40:28 +0100

    fix(layout): update BaseProvider module from 'auth' to 'core'
    - Changed the module prop in BaseProvider within the RootLayout component to 'core' for improved context in the application structure.

--- modules/auth/apps/frontend/src/app/layout.tsx
+++ modules/auth/apps/frontend/src/app/layout.tsx
@@ -17,7 +17,7 @@ export const metadata: Metadata = {
 
 export default async function RootLayout({ children }: PropsWithChildren) {
 	return (
-		<BaseProvider app="frontend" i18n={{ es: i18nResourceKeysEs, pt: i18nResourceKeysPt }} module="auth" version={pjson.version}>
+		<BaseProvider app="frontend" i18n={{ es: i18nResourceKeysEs, pt: i18nResourceKeysPt }} module="core" version={pjson.version}>
 			{children}
 		</BaseProvider>
 	);
