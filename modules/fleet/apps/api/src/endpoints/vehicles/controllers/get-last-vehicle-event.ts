28fdf135504e2103b0ffbd15d1d23ba5119d71c3
João de Vasconcelos <10943992+joao-vasconcelos@users.noreply.github.com>
2026-07-21 13:42:21 +0100

    refactor(dependencies): update vehicle controllers and package dependencies
    - Replaced deprecated imports from '@tmlmobilidade/interfaces' with '@tmlmobilidade/go-interfaces-godb' and '@tmlmobilidade/go-interfaces-labdb' in vehicle-related controllers for improved database interaction.
    - Updated methods for creating, retrieving, updating, and deleting vehicles to align with the new database structure, enhancing code maintainability.
    - Added new dependencies in package.json for better type management and streamlined imports across the codebase.

--- modules/fleet/apps/api/src/endpoints/vehicles/controllers/get-last-vehicle-event.ts
+++ modules/fleet/apps/api/src/endpoints/vehicles/controllers/get-last-vehicle-event.ts
@@ -1,9 +1,9 @@
 /* * */
 
 import { HTTP_STATUS, HttpException } from '@tmlmobilidade/consts';
-import { simplifiedVehicleEventsNew } from '@tmlmobilidade/databases';
 import { type FastifyReply, type FastifyRequest } from '@tmlmobilidade/fastify';
-import { SimplifiedVehicleEvent } from '@tmlmobilidade/types';
+import { labDb } from '@tmlmobilidade/go-interfaces-labdb';
+import { type SimplifiedVehicleEvent } from '@tmlmobilidade/go-types-vehicle-events';
 
 /**
  * Retrieves the last event for a given vehicle.
@@ -18,9 +18,19 @@ export async function getLastVehicleEvent(request: FastifyRequest<{ Params: { id
 
 	//
 	// Fetch the last event for the vehicle
-	const lastEvent = await simplifiedVehicleEventsNew.getLastEvent(vehicleId, agencyId);
+
+	const query = `
+			SELECT *
+			WHERE vehicle_id = '${vehicleId}'
+			AND agency_id = '${agencyId}'
+			ORDER BY created_at DESC
+			LIMIT 1
+		`;
+
+	const lastEvent = await labDb.operation.vehicleEvents.queryFromString<SimplifiedVehicleEvent>(query);
+	if (!lastEvent || lastEvent.length === 0) throw new HttpException(HTTP_STATUS.NOT_FOUND, 'No last event found for vehicle');
 
 	//
 	// Send the last event for the vehicle back to the client
-	reply.send({ data: lastEvent, error: null, statusCode: HTTP_STATUS.OK });
+	reply.send({ data: lastEvent[0], error: null, statusCode: HTTP_STATUS.OK });
 }
