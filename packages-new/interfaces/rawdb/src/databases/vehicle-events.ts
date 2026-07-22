ad65c59eca5f30ae30943ce9aeb739460fb0824b
jusimen <jusi.monteiro@protonmail.com>
2026-07-22 17:27:22 +0100

    refactor(rawdb): rename and reorganize vehicle event collection interfaces
    - Updated the naming convention for vehicle event collection interfaces to improve clarity and consistency.
    - Reorganized the initialization of MongoInterfaceTemplate instances for better readability and maintainability.

--- packages-new/interfaces/rawdb/src/databases/vehicle-events.ts
+++ packages-new/interfaces/rawdb/src/databases/vehicle-events.ts
@@ -11,24 +11,26 @@ export class VehicleEventsDatabase {
 
 	//
 	// Collections
-	public readonly esCrtmAisaFetch: MongoInterfaceTemplate<RawVehicleEvent, RawVehicleEvent>;
-	public readonly esCrtmLaVelozFetch: MongoInterfaceTemplate<RawVehicleEvent, RawVehicleEvent>;
-	public readonly ptTmlCcflFetch: MongoInterfaceTemplate<RawVehicleEvent, RawVehicleEvent>;
-	public readonly ptTmlCmStreamCore: MongoInterfaceTemplate<RawVehicleEvent, RawVehicleEvent>;
-	public readonly ptTmlCmStreamLog: MongoInterfaceTemplate<RawVehicleEvent, RawVehicleEvent>;
-	public readonly ptTmlCmSyncCore: MongoInterfaceTemplate<RawVehicleEvent, RawVehicleEvent>;
-	public readonly ptTmlCmSyncLog: MongoInterfaceTemplate<RawVehicleEvent, RawVehicleEvent>;
-	public readonly ptTmlCpFetch: MongoInterfaceTemplate<RawVehicleEvent, RawVehicleEvent>;
-	public readonly ptTmlFertagusFetch: MongoInterfaceTemplate<RawVehicleEvent, RawVehicleEvent>;
-	public readonly ptTmlMlFetch: MongoInterfaceTemplate<RawVehicleEvent, RawVehicleEvent>;
-	public readonly ptTmlMobiFetch: MongoInterfaceTemplate<RawVehicleEvent, RawVehicleEvent>;
-	public readonly ptTmlTcbFetch: MongoInterfaceTemplate<RawVehicleEvent, RawVehicleEvent>;
-	public readonly ptTmlTtslFetch: MongoInterfaceTemplate<RawVehicleEvent, RawVehicleEvent>;
-	public readonly ptTmpUnirFetch: MongoInterfaceTemplate<RawVehicleEvent, RawVehicleEvent>;
-	public readonly ptTmpUnirLabdbStream: MongoInterfaceTemplate<RawVehicleEvent, RawVehicleEvent>;
-	public readonly ptTmpUnirLabdbSync: MongoInterfaceTemplate<RawVehicleEvent, RawVehicleEvent>;
 
-	//
+	public readonly esCrtmAisa: MongoInterfaceTemplate<RawVehicleEvent, RawVehicleEvent>;
+	public readonly esCrtmLaVeloz: MongoInterfaceTemplate<RawVehicleEvent, RawVehicleEvent>;
+	public readonly ptTmlCcfl: MongoInterfaceTemplate<RawVehicleEvent, RawVehicleEvent>;
+	public readonly ptTmlCmAlsa: MongoInterfaceTemplate<RawVehicleEvent, RawVehicleEvent>;
+	public readonly ptTmlCmRl: MongoInterfaceTemplate<RawVehicleEvent, RawVehicleEvent>;
+	public readonly ptTmlCmTst: MongoInterfaceTemplate<RawVehicleEvent, RawVehicleEvent>;
+	public readonly ptTmlCmVa: MongoInterfaceTemplate<RawVehicleEvent, RawVehicleEvent>;
+	public readonly ptTmlCp: MongoInterfaceTemplate<RawVehicleEvent, RawVehicleEvent>;
+	public readonly ptTmlFertagus: MongoInterfaceTemplate<RawVehicleEvent, RawVehicleEvent>;
+	public readonly ptTmlMl: MongoInterfaceTemplate<RawVehicleEvent, RawVehicleEvent>;
+	public readonly ptTmlMobi: MongoInterfaceTemplate<RawVehicleEvent, RawVehicleEvent>;
+	public readonly ptTmlTcb: MongoInterfaceTemplate<RawVehicleEvent, RawVehicleEvent>;
+	public readonly ptTmlTtsl: MongoInterfaceTemplate<RawVehicleEvent, RawVehicleEvent>;
+	public readonly ptTmpUnirUt1: MongoInterfaceTemplate<RawVehicleEvent, RawVehicleEvent>;
+	public readonly ptTmpUnirUt2: MongoInterfaceTemplate<RawVehicleEvent, RawVehicleEvent>;
+	public readonly ptTmpUnirUt3: MongoInterfaceTemplate<RawVehicleEvent, RawVehicleEvent>;
+	public readonly ptTmpUnirUt4: MongoInterfaceTemplate<RawVehicleEvent, RawVehicleEvent>;
+	public readonly ptTmpUnirUt5: MongoInterfaceTemplate<RawVehicleEvent, RawVehicleEvent>;
+
 	private readonly database: Db;
 	private readonly databaseName = 'vehicle-events';
 
@@ -37,21 +39,23 @@ export class VehicleEventsDatabase {
 		this.database = instance.db(this.databaseName);
 
 		// Create collection interfaces
-		this.esCrtmAisaFetch = new MongoInterfaceTemplate<RawVehicleEvent, RawVehicleEvent>('es-crtm-aisa-fetch', this.database, RawVehicleEventSchema);
-		this.esCrtmLaVelozFetch = new MongoInterfaceTemplate<RawVehicleEvent, RawVehicleEvent>('es-crtm-la-veloz-fetch', this.database, RawVehicleEventSchema);
-		this.ptTmlCcflFetch = new MongoInterfaceTemplate<RawVehicleEvent, RawVehicleEvent>('pt-tml-ccfl-fetch', this.database, RawVehicleEventSchema);
-		this.ptTmlCmStreamCore = new MongoInterfaceTemplate<RawVehicleEvent, RawVehicleEvent>('pt-tml-cm-stream-core', this.database, RawVehicleEventSchema);
-		this.ptTmlCmStreamLog = new MongoInterfaceTemplate<RawVehicleEvent, RawVehicleEvent>('pt-tml-cm-stream-log', this.database, RawVehicleEventSchema);
-		this.ptTmlCmSyncCore = new MongoInterfaceTemplate<RawVehicleEvent, RawVehicleEvent>('pt-tml-cm-sync-core', this.database, RawVehicleEventSchema);
-		this.ptTmlCmSyncLog = new MongoInterfaceTemplate<RawVehicleEvent, RawVehicleEvent>('pt-tml-cm-sync-log', this.database, RawVehicleEventSchema);
-		this.ptTmlCpFetch = new MongoInterfaceTemplate<RawVehicleEvent, RawVehicleEvent>('pt-tml-cp-fetch', this.database, RawVehicleEventSchema);
-		this.ptTmlFertagusFetch = new MongoInterfaceTemplate<RawVehicleEvent, RawVehicleEvent>('pt-tml-fertagus-fetch', this.database, RawVehicleEventSchema);
-		this.ptTmlMlFetch = new MongoInterfaceTemplate<RawVehicleEvent, RawVehicleEvent>('pt-tml-ml-fetch', this.database, RawVehicleEventSchema);
-		this.ptTmlMobiFetch = new MongoInterfaceTemplate<RawVehicleEvent, RawVehicleEvent>('pt-tml-mobi-fetch', this.database, RawVehicleEventSchema);
-		this.ptTmlTcbFetch = new MongoInterfaceTemplate<RawVehicleEvent, RawVehicleEvent>('pt-tml-tcb-fetch', this.database, RawVehicleEventSchema);
-		this.ptTmlTtslFetch = new MongoInterfaceTemplate<RawVehicleEvent, RawVehicleEvent>('pt-tml-ttsl-fetch', this.database, RawVehicleEventSchema);
-		this.ptTmpUnirFetch = new MongoInterfaceTemplate<RawVehicleEvent, RawVehicleEvent>('pt-tmp-unir-fetch', this.database, RawVehicleEventSchema);
-		this.ptTmpUnirLabdbStream = new MongoInterfaceTemplate<RawVehicleEvent, RawVehicleEvent>('pt-tmp-unir-labdb-stream', this.database, RawVehicleEventSchema);
-		this.ptTmpUnirLabdbSync = new MongoInterfaceTemplate<RawVehicleEvent, RawVehicleEvent>('pt-tmp-unir-labdb-sync', this.database, RawVehicleEventSchema);
+		this.esCrtmAisa = new MongoInterfaceTemplate<RawVehicleEvent, RawVehicleEvent>('es-crtm-aisa-fetch', this.database, RawVehicleEventSchema);
+		this.esCrtmLaVeloz = new MongoInterfaceTemplate<RawVehicleEvent, RawVehicleEvent>('es-crtm-la-veloz-fetch', this.database, RawVehicleEventSchema);
+		this.ptTmlCcfl = new MongoInterfaceTemplate<RawVehicleEvent, RawVehicleEvent>('pt-tml-ccfl-fetch', this.database, RawVehicleEventSchema);
+		this.ptTmlCmAlsa = new MongoInterfaceTemplate<RawVehicleEvent, RawVehicleEvent>('pt-tml-cm-alsa', this.database, RawVehicleEventSchema);
+		this.ptTmlCmRl = new MongoInterfaceTemplate<RawVehicleEvent, RawVehicleEvent>('pt-tml-cm-rl', this.database, RawVehicleEventSchema);
+		this.ptTmlCmTst = new MongoInterfaceTemplate<RawVehicleEvent, RawVehicleEvent>('pt-tml-cm-tst', this.database, RawVehicleEventSchema);
+		this.ptTmlCmVa = new MongoInterfaceTemplate<RawVehicleEvent, RawVehicleEvent>('pt-tml-cm-va', this.database, RawVehicleEventSchema);
+		this.ptTmlCp = new MongoInterfaceTemplate<RawVehicleEvent, RawVehicleEvent>('pt-tml-cp-fetch', this.database, RawVehicleEventSchema);
+		this.ptTmlFertagus = new MongoInterfaceTemplate<RawVehicleEvent, RawVehicleEvent>('pt-tml-fertagus-fetch', this.database, RawVehicleEventSchema);
+		this.ptTmlMl = new MongoInterfaceTemplate<RawVehicleEvent, RawVehicleEvent>('pt-tml-ml-fetch', this.database, RawVehicleEventSchema);
+		this.ptTmlMobi = new MongoInterfaceTemplate<RawVehicleEvent, RawVehicleEvent>('pt-tml-mobi-fetch', this.database, RawVehicleEventSchema);
+		this.ptTmlTcb = new MongoInterfaceTemplate<RawVehicleEvent, RawVehicleEvent>('pt-tml-tcb-fetch', this.database, RawVehicleEventSchema);
+		this.ptTmlTtsl = new MongoInterfaceTemplate<RawVehicleEvent, RawVehicleEvent>('pt-tml-ttsl-fetch', this.database, RawVehicleEventSchema);
+		this.ptTmpUnirUt1 = new MongoInterfaceTemplate<RawVehicleEvent, RawVehicleEvent>('pt-tmp-unir-ut1', this.database, RawVehicleEventSchema);
+		this.ptTmpUnirUt2 = new MongoInterfaceTemplate<RawVehicleEvent, RawVehicleEvent>('pt-tmp-unir-ut2', this.database, RawVehicleEventSchema);
+		this.ptTmpUnirUt3 = new MongoInterfaceTemplate<RawVehicleEvent, RawVehicleEvent>('pt-tmp-unir-ut3', this.database, RawVehicleEventSchema);
+		this.ptTmpUnirUt4 = new MongoInterfaceTemplate<RawVehicleEvent, RawVehicleEvent>('pt-tmp-unir-ut4', this.database, RawVehicleEventSchema);
+		this.ptTmpUnirUt5 = new MongoInterfaceTemplate<RawVehicleEvent, RawVehicleEvent>('pt-tmp-unir-ut5', this.database, RawVehicleEventSchema);
 	}
 }
