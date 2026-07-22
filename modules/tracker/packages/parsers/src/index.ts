63883894241b22d2ccbac823620385e8e6d9a76f
João de Vasconcelos <10943992+joao-vasconcelos@users.noreply.github.com>
2026-07-22 20:04:13 +0100

    refactor(labdb-stream): streamline vehicle event processing and remove obsolete task module
    - Removed the `task.ts` module, consolidating vehicle event processing logic into `index.ts` for improved clarity and maintainability.
    - Introduced a new batch writer for handling `SimplifiedVehicleEvent` documents, enhancing data processing efficiency.
    - Updated the event handling logic to utilize the new writer and integrated a callback for setting rides as waiting, improving overall functionality.
    - Cleaned up dependencies in `package.json` to reflect current usage, removing unnecessary packages.

--- modules/tracker/packages/parsers/src/index.ts
+++ modules/tracker/packages/parsers/src/index.ts
@@ -1,32 +1,63 @@
 /* * */
 
-import { parseRawVehicleEventEsCrtmAisaV1 } from '@/es/crtm/aisa/aisa-v1.js';
-import { parseRawVehicleEventEsCrtmLaVelozV1 } from '@/es/crtm/la-veloz/la-veloz-v1.js';
-import { parseRawVehicleEventPtTmlCcflV1 } from '@/pt/tml/ccfl/ccfl-v1.js';
-import { parseRawVehicleEventPtTmlCmetV1Core } from '@/pt/tml/cmet/cmet-v1-core.js';
-import { parseRawVehicleEventPtTmlCmetV1Log } from '@/pt/tml/cmet/cmet-v1-log.js';
-import { parseRawVehicleEventPtTmlCpV1 } from '@/pt/tml/cp/cp-v1.js';
-import { parseRawVehicleEventPtTmlFertagusV1 } from '@/pt/tml/fertagus/fertagus-v1.js';
-import { parseRawVehicleEventPtTmlMlV1 } from '@/pt/tml/ml/ml-v1.js';
-import { parseRawVehicleEventPtTmlMobiV1 } from '@/pt/tml/mobi/mobi-v1.js';
-import { parseRawVehicleEventPtTmlTcbV1 } from '@/pt/tml/tcb/tcb-v1.js';
-import { parseRawVehicleEventPtTmlTtslV1 } from '@/pt/tml/ttsl/ttsl-v1.js';
-import { parseRawVehicleEventPtTmpUnirV1 } from '@/pt/tmp/unir/unir-v1.js';
+import { type ChangeStreamInsertDocument } from '@tmlmobilidade/go-clients-mongo';
 import { type RawVehicleEvent, type SimplifiedVehicleEvent } from '@tmlmobilidade/go-types-vehicle-events';
+import { Logger } from '@tmlmobilidade/logger';
+import { type BatchWriter } from '@tmlmobilidade/utils';
+
+import { PARSER_MAP } from './parsers.js';
 
 /* * */
 
-export const PARSER_MAP: Record<RawVehicleEvent['version'], (vehicleEvent: RawVehicleEvent) => null | SimplifiedVehicleEvent> = {
-	'es-crtm-aisa-v1': parseRawVehicleEventEsCrtmAisaV1,
-	'es-crtm-la-veloz-v1': parseRawVehicleEventEsCrtmLaVelozV1,
-	'pt-tml-ccfl-v1': parseRawVehicleEventPtTmlCcflV1,
-	'pt-tml-cmet-v1-core': parseRawVehicleEventPtTmlCmetV1Core,
-	'pt-tml-cmet-v1-log': parseRawVehicleEventPtTmlCmetV1Log,
-	'pt-tml-cp-v1': parseRawVehicleEventPtTmlCpV1,
-	'pt-tml-fertagus-v1': parseRawVehicleEventPtTmlFertagusV1,
-	'pt-tml-ml-v1': parseRawVehicleEventPtTmlMlV1,
-	'pt-tml-mobi-v1': parseRawVehicleEventPtTmlMobiV1,
-	'pt-tml-tcb-v1': parseRawVehicleEventPtTmlTcbV1,
-	'pt-tml-ttsl-v1': parseRawVehicleEventPtTmlTtslV1,
-	'pt-tmp-unir-v1': parseRawVehicleEventPtTmpUnirV1,
+interface ParseRawVehicleEventIntoSimplifiedVehicleEventParams {
+
+	/**
+	 * The batch writer to be used to write the SimplifiedVehicleEvent
+	 * documents to the SimplifiedVehicleEvents collection.
+	 */
+	batchWriter: BatchWriter<SimplifiedVehicleEvent>
+
+	/**
+	 * The database operation containing the Vehicle Event document
+	 * to be processed into a SimplifiedVehicleEvent document.
+	 */
+	databaseOperation: ChangeStreamInsertDocument<RawVehicleEvent>
+
+	/**
+	 * The callback to be called when the SimplifiedVehicleEvent document is flushed.
+	 */
+	flushCallback: (data: SimplifiedVehicleEvent[]) => Promise<void>
+}
+
+/**
+ * Parse the RawVehicleEvent document into a SimplifiedVehicleEvent document
+ * by validating the operation type, transforming the document, and writing it
+ * to the SimplifiedVehicleEvents collection.
+ * @param params The parameters for the function.
+ * @returns A promise that resolves when the RawVehicleEvent document has been
+ * parsed into a SimplifiedVehicleEvent document.
+ */
+export async function parseRawVehicleEventIntoSimplifiedVehicleEvent({ batchWriter, databaseOperation, flushCallback }: ParseRawVehicleEventIntoSimplifiedVehicleEventParams) {
+	try {
+		//
+
+		//
+		// Extract the full document from the database operation and transform it
+		// into a simplified vehicle event document using the appropriate parser based on the version field.
+
+		const parser = PARSER_MAP[databaseOperation.fullDocument.version];
+		if (!parser) throw new Error(`No parser found for version ${databaseOperation.fullDocument.version}. Skipping document with _id "${databaseOperation.fullDocument._id}"...`);
+
+		const newSimplifiedVehicleEventDocument = parser(databaseOperation.fullDocument);
+		if (!newSimplifiedVehicleEventDocument) throw new Error(`Failed to parse document with _id "${databaseOperation.fullDocument._id}". Skipping...`);
+
+		//
+		// Write the new vehicle event document to the SimplifiedVehicleEvents collection
+
+		await batchWriter.write(newSimplifiedVehicleEventDocument, { flushCallback });
+
+		//
+	} catch (error) {
+		Logger.error({ error, message: `Parsing failed: _id="${databaseOperation.fullDocument._id}" version="${databaseOperation.fullDocument.version}"` });
+	}
 };
---
+++ modules/tracker/packages/parsers/src/parsers.ts
@@ -0,0 +1,32 @@
+/* * */
+
+import { parseRawVehicleEventEsCrtmAisaV1 } from '@/es/crtm/aisa/aisa-v1.js';
+import { parseRawVehicleEventEsCrtmLaVelozV1 } from '@/es/crtm/la-veloz/la-veloz-v1.js';
+import { parseRawVehicleEventPtTmlCcflV1 } from '@/pt/tml/ccfl/ccfl-v1.js';
+import { parseRawVehicleEventPtTmlCmetV1Core } from '@/pt/tml/cmet/cmet-v1-core.js';
+import { parseRawVehicleEventPtTmlCmetV1Log } from '@/pt/tml/cmet/cmet-v1-log.js';
+import { parseRawVehicleEventPtTmlCpV1 } from '@/pt/tml/cp/cp-v1.js';
+import { parseRawVehicleEventPtTmlFertagusV1 } from '@/pt/tml/fertagus/fertagus-v1.js';
+import { parseRawVehicleEventPtTmlMlV1 } from '@/pt/tml/ml/ml-v1.js';
+import { parseRawVehicleEventPtTmlMobiV1 } from '@/pt/tml/mobi/mobi-v1.js';
+import { parseRawVehicleEventPtTmlTcbV1 } from '@/pt/tml/tcb/tcb-v1.js';
+import { parseRawVehicleEventPtTmlTtslV1 } from '@/pt/tml/ttsl/ttsl-v1.js';
+import { parseRawVehicleEventPtTmpUnirV1 } from '@/pt/tmp/unir/unir-v1.js';
+import { type RawVehicleEvent, type SimplifiedVehicleEvent } from '@tmlmobilidade/go-types-vehicle-events';
+
+/* * */
+
+export const PARSER_MAP: Record<RawVehicleEvent['version'], (vehicleEvent: RawVehicleEvent) => null | SimplifiedVehicleEvent> = {
+	'es-crtm-aisa-v1': parseRawVehicleEventEsCrtmAisaV1,
+	'es-crtm-la-veloz-v1': parseRawVehicleEventEsCrtmLaVelozV1,
+	'pt-tml-ccfl-v1': parseRawVehicleEventPtTmlCcflV1,
+	'pt-tml-cmet-v1-core': parseRawVehicleEventPtTmlCmetV1Core,
+	'pt-tml-cmet-v1-log': parseRawVehicleEventPtTmlCmetV1Log,
+	'pt-tml-cp-v1': parseRawVehicleEventPtTmlCpV1,
+	'pt-tml-fertagus-v1': parseRawVehicleEventPtTmlFertagusV1,
+	'pt-tml-ml-v1': parseRawVehicleEventPtTmlMlV1,
+	'pt-tml-mobi-v1': parseRawVehicleEventPtTmlMobiV1,
+	'pt-tml-tcb-v1': parseRawVehicleEventPtTmlTcbV1,
+	'pt-tml-ttsl-v1': parseRawVehicleEventPtTmlTtslV1,
+	'pt-tmp-unir-v1': parseRawVehicleEventPtTmpUnirV1,
+};
