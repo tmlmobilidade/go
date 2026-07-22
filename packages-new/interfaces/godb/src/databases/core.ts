a1a7e7158a14ce3a412eb25b5ba207d9a7707f6d
João de Vasconcelos <10943992+joao-vasconcelos@users.noreply.github.com>
2026-07-22 11:53:24 +0100

    Revert "refactor(godb): update file handling types in CoreDatabase and OfferDatabase"
    This reverts commit 535da830a1547affd9c0ffabde117c53d359953e.

--- packages-new/interfaces/godb/src/databases/core.ts
+++ packages-new/interfaces/godb/src/databases/core.ts
@@ -2,7 +2,7 @@
 
 import { MongoInterfaceTemplate } from '@/interface.template.js';
 import { type Db, type MongoClient } from '@tmlmobilidade/go-clients-mongo';
-import { type Agency, type AppConfig, type CreateAgencyDto, CreateAgencySchema, type CreateAttachmentDto, CreateAttachmentSchema, type CreateFileExportDto, CreateFileExportSchema, type CreateOrganizationDto, CreateOrganizationSchema, type CreateRoleDto, CreateRoleSchema, type CreateSessionDto, CreateSessionSchema, type CreateUserDto, CreateUserSchema, type CreateVerificationTokenDto, CreateVerificationTokenSchema, type FileExport, type Organization, type Role, type Session, type UpdateAgencyDto, UpdateAgencySchema, UpdateAttachmentDto, UpdateAttachmentSchema, UpdateFileExportSchema, type UpdateOrganizationDto, UpdateOrganizationSchema, type UpdateRoleDto, UpdateRoleSchema, type UpdateSessionDto, UpdateSessionSchema, type UpdateUserDto, UpdateUserSchema, type UpdateVerificationTokenDto, UpdateVerificationTokenSchema, type User, type VerificationToken } from '@tmlmobilidade/types';
+import { type Agency, type AppConfig, type CreateAgencyDto, CreateAgencySchema, type CreateFileDto, type CreateFileExportDto, CreateFileExportSchema, CreateFileSchema, type CreateOrganizationDto, CreateOrganizationSchema, type CreateRoleDto, CreateRoleSchema, type CreateSessionDto, CreateSessionSchema, type CreateUserDto, CreateUserSchema, type CreateVerificationTokenDto, CreateVerificationTokenSchema, type File, type FileExport, type Organization, type Role, type Session, type UpdateAgencyDto, UpdateAgencySchema, type UpdateFileDto, UpdateFileExportSchema, UpdateFileSchema, type UpdateOrganizationDto, UpdateOrganizationSchema, type UpdateRoleDto, UpdateRoleSchema, type UpdateSessionDto, UpdateSessionSchema, type UpdateUserDto, UpdateUserSchema, type UpdateVerificationTokenDto, UpdateVerificationTokenSchema, type User, type VerificationToken } from '@tmlmobilidade/types';
 
 /* * */
 
@@ -12,7 +12,7 @@ export class CoreDatabase {
 	public readonly agencies: MongoInterfaceTemplate<Agency, CreateAgencyDto, UpdateAgencyDto>;
 	public readonly appConfigs: MongoInterfaceTemplate<AppConfig, null, null>;
 	public readonly exports: MongoInterfaceTemplate<FileExport, CreateFileExportDto<any>, Partial<FileExport>>;
-	public readonly files: MongoInterfaceTemplate<File, CreateAttachmentDto, UpdateAttachmentDto>;
+	public readonly files: MongoInterfaceTemplate<File, CreateFileDto, UpdateFileDto>;
 	public readonly organizations: MongoInterfaceTemplate<Organization, CreateOrganizationDto, UpdateOrganizationDto>;
 	public readonly roles: MongoInterfaceTemplate<Role, CreateRoleDto, UpdateRoleDto>;
 	public readonly sessions: MongoInterfaceTemplate<Session, CreateSessionDto, UpdateSessionDto>;
@@ -34,6 +34,6 @@ export class CoreDatabase {
 		this.sessions = new MongoInterfaceTemplate<Session, CreateSessionDto, UpdateSessionDto>('sessions', this.database, CreateSessionSchema, UpdateSessionSchema);
 		this.users = new MongoInterfaceTemplate<User, CreateUserDto, UpdateUserDto>('users', this.database, CreateUserSchema, UpdateUserSchema);
 		this.verificationTokens = new MongoInterfaceTemplate<VerificationToken, CreateVerificationTokenDto, UpdateVerificationTokenDto>('verification-tokens', this.database, CreateVerificationTokenSchema, UpdateVerificationTokenSchema);
-		this.files = new MongoInterfaceTemplate<File, CreateAttachmentDto, UpdateAttachmentDto>('files', this.database, CreateAttachmentSchema, UpdateAttachmentSchema);
+		this.files = new MongoInterfaceTemplate<File, CreateFileDto, UpdateFileDto>('files', this.database, CreateFileSchema, UpdateFileSchema);
 	}
 }
