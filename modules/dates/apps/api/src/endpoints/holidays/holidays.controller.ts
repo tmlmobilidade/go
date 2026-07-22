b0fd1af23744f0aa4c51bc904e8645538238cfb7
João de Vasconcelos <10943992+joao-vasconcelos@users.noreply.github.com>
2026-07-22 23:57:35 +0100

    refactor(imports): update Filter import to use go-clients-mongo across multiple controllers
    - Changed the import statement for the Filter type from '@tmlmobilidade/interfaces' to '@tmlmobilidade/go-clients-mongo' in the annotations, events, holidays, year-periods, fares, lines, typologies, and zones controllers for consistency and to utilize the new MongoDB client.

--- modules/dates/apps/api/src/endpoints/holidays/holidays.controller.ts
+++ modules/dates/apps/api/src/endpoints/holidays/holidays.controller.ts
@@ -2,8 +2,8 @@
 
 import { HTTP_STATUS, HttpException } from '@tmlmobilidade/consts';
 import { type FastifyReply, type FastifyRequest } from '@tmlmobilidade/fastify';
+import { type Filter } from '@tmlmobilidade/go-clients-mongo';
 import { goDb } from '@tmlmobilidade/go-interfaces-godb';
-import { type Filter } from '@tmlmobilidade/interfaces';
 import { type CreateHolidayDto, type Holiday, PermissionCatalog, type UpdateHolidayDto } from '@tmlmobilidade/types';
 
 /* * */
