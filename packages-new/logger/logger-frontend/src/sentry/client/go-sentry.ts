94cbe0586eebd149f6f7b50de308c0dbdf65296e
LoudNeo <meliodasgamer077@gmail.com>
2026-07-23 15:12:48 +0100

    feat(sentry): update Sentry client configuration for production environment
    - Modified the `getSentryClient` function to return undefined in non-development environments, enhancing security by preventing exposure of sensitive DSN information in production.

--- packages-new/logger/logger-frontend/src/sentry/client/go-sentry.ts
+++ packages-new/logger/logger-frontend/src/sentry/client/go-sentry.ts
@@ -2,6 +2,12 @@ export function getSentryClient(): string | undefined {
 	//
 
 	//
+	// If the environment is production, return the SENTRY_NEXTJS_DSN
+	if (process.env.NEXT_PUBLIC_ENVIRONMENT !== 'dev') {
+		return undefined;
+	}
+
+	//
 	// Validate required environment variables
 
 	if (!process.env.SENTRY_NEXTJS_DSN) {
