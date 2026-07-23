eceb0462af659620dbacfa761fb82757b778452b
João de Vasconcelos <10943992+joao-vasconcelos@users.noreply.github.com>
2026-07-24 00:01:06 +0100

    refactor(publish-gtfs): standardize agency ID usage across export functions
    - Updated references to agency IDs in multiple export functions to use the new string identifier format, enhancing consistency and clarity.
    - Modified the `exportPlansFile`, `exportStopTimesFile`, `exportTripsFile`, `exportShapesFile`, and others to align with the updated agency data structure.
    - Adjusted logging and data parsing to reflect the changes in agency ID handling, improving maintainability and reducing potential errors.

--- modules/hub/apps/publish-gtfs/src/main.ts
+++ modules/hub/apps/publish-gtfs/src/main.ts
@@ -128,7 +128,7 @@ export async function main() {
 
 			const planTimer = new Timer();
 
-			Logger.info({ message: `[${planIndex + 1}/${allPlansData.length}] - Agency ${planData.gtfs_agency.agency_id} - Plan ${planData._id}` });
+			Logger.info({ message: `[${planIndex + 1}/${allPlansData.length}] - Agency ${planData.agency_id} - Plan ${planData._id}` });
 
 			//
 			// Validate the Plan data before processing.
@@ -216,9 +216,9 @@ export async function main() {
 
 			for await (const routeItem of importedGtfsSql.routes.stream()) {
 				const routeData: GTFS_Route_Extended = routeItem;
-				const publicRouteId = getPublicRouteId(planData.gtfs_agency.agency_id, routeData.route_id);
+				const publicRouteId = getPublicRouteId(planData.agency_id, routeData.route_id);
 				if (thisIsAnActivePlan || !routesMarkedForFinalExport[publicRouteId]) {
-					routesMarkedForFinalExport[publicRouteId] = routeData;
+					routesMarkedForFinalExport[publicRouteId] = { ...routeData, agency_id: planData.agency_id };
 				}
 			}
 
@@ -228,7 +228,7 @@ export async function main() {
 			// Add the plan's referenced agency ID and farthest
 			// feed end date to the global variables for later export.
 
-			referencedAgencyIds.add(planData.gtfs_agency.agency_id);
+			referencedAgencyIds.add(planData.agency_id);
 
 			farthestDateFound = !farthestDateFound || planData.gtfs_feed_info.feed_end_date > farthestDateFound
 				? planData.gtfs_feed_info.feed_end_date
@@ -237,7 +237,7 @@ export async function main() {
 			//
 			// Finally, write the plan entry into the plans.txt file.
 
-			await exportPlansFile(planData.gtfs_agency.agency_id, planData._id, planData.gtfs_feed_info.feed_start_date, planData.gtfs_feed_info.feed_end_date, context);
+			await exportPlansFile(planData, context);
 
 			//
 			// Mark the plan as complete in the database.
