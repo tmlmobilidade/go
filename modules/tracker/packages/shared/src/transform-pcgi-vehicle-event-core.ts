5b2bd142a394ae809fbc217d92925dac3e620372
João de Vasconcelos <10943992+joao-vasconcelos@users.noreply.github.com>
2026-07-24 21:03:39 +0100

    refactor(parsers): update vehicle event parsers and remove outdated schemas
    - Refactored the vehicle event parsers to use agency-specific paths, enhancing modularity and clarity.
    - Removed the outdated `pt-tml-cm-v1` and `pt-tmp-unir-v1` parser files to streamline the codebase.
    - Updated the `PARSER_MAP` to reflect the new parser structure and added new parser imports for additional agency-specific events.
    - Enhanced type safety by updating the `transformPcgiVehicleEventCore` function to accommodate the new schemas.

--- modules/tracker/packages/shared/src/transform-pcgi-vehicle-event-core.ts
+++ modules/tracker/packages/shared/src/transform-pcgi-vehicle-event-core.ts
@@ -1,24 +1,24 @@
 /* * */
 
 import { Dates } from '@tmlmobilidade/dates';
-import { type HashableRawVehicleEvent, type RawVehicleEvent, RawVehicleEventPtTmlCmV1 } from '@tmlmobilidade/go-types-vehicle-events';
+import { type HashableRawVehicleEvent, type RawVehicleEventPtTmlCmAlsaV1, type RawVehicleEventPtTmlCmRlV1, type RawVehicleEventPtTmlCmTstV1, type RawVehicleEventPtTmlCmVaV1 } from '@tmlmobilidade/go-types-vehicle-events';
 import crypto from 'node:crypto';
 
 /* * */
 
 const AGENCY_ID_MAP = [
-	{ agency_code: '41', agency_id: 'LA77N' },
-	{ agency_code: '42', agency_id: 'BNA17' },
-	{ agency_code: '43', agency_id: 'YA15B' },
-	{ agency_code: '44', agency_id: 'A2L1N' },
+	{ agency_code: '41', agency_id: 'LA77N', version: 'pt-tml-cm-va-v1' },
+	{ agency_code: '42', agency_id: 'BNA17', version: 'pt-tml-cm-rl-v1' },
+	{ agency_code: '43', agency_id: 'YA15B', version: 'pt-tml-cm-tst-v1' },
+	{ agency_code: '44', agency_id: 'A2L1N', version: 'pt-tml-cm-alsa-v1' },
 ] as const;
 
 /* * */
 
-export function transformPcgiVehicleEventCore(pcgiVehicleEvent): RawVehicleEvent[] {
+export function transformPcgiVehicleEventCore(pcgiVehicleEvent): (RawVehicleEventPtTmlCmAlsaV1 | RawVehicleEventPtTmlCmRlV1 | RawVehicleEventPtTmlCmTstV1 | RawVehicleEventPtTmlCmVaV1)[] {
 	//
 
-	const result: RawVehicleEvent[] = [];
+	const result: (RawVehicleEventPtTmlCmAlsaV1 | RawVehicleEventPtTmlCmRlV1 | RawVehicleEventPtTmlCmTstV1 | RawVehicleEventPtTmlCmVaV1)[] = [];
 
 	//
 	// Transform each message into a RawVehicleEvent
@@ -45,7 +45,7 @@ export function transformPcgiVehicleEventCore(pcgiVehicleEvent): RawVehicleEvent
 		// This allows us to identify duplicate events
 		// and avoid storing them multiple times in the database.
 
-		const hashableRawEvent: HashableRawVehicleEvent<RawVehicleEventPtTmlCmV1> = {
+		const hashableRawEvent: HashableRawVehicleEvent<RawVehicleEventPtTmlCmAlsaV1 | RawVehicleEventPtTmlCmRlV1 | RawVehicleEventPtTmlCmTstV1 | RawVehicleEventPtTmlCmVaV1> = {
 			agency_id: matchingAgency.agency_id,
 			created_at: Dates.fromSeconds(entity.vehicle.timestamp).unix_timestamp,
 			entity_id: entity._id,
@@ -53,7 +53,7 @@ export function transformPcgiVehicleEventCore(pcgiVehicleEvent): RawVehicleEvent
 				header: pcgiVehicleEvent.content.header,
 				vehicle: entity.vehicle,
 			},
-			version: 'pt-tml-cm-v1',
+			version: matchingAgency.version,
 		};
 
 		const hashableRawEventId = crypto
@@ -69,7 +69,7 @@ export function transformPcgiVehicleEventCore(pcgiVehicleEvent): RawVehicleEvent
 			...hashableRawEvent,
 			_id: hashableRawEventId,
 			received_at: Dates.fromUnixTimestamp(pcgiVehicleEvent.millis).unix_timestamp,
-		});
+		} as typeof result[number]);
 
 		//
 	}
