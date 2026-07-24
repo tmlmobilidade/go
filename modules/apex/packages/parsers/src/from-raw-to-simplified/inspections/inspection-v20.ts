794a94f5a7eee06afbab9d560aae4027edc14254
João de Vasconcelos <johnyvasconcelos@icloud.com>
2026-07-24 18:07:30 +0100

    Add `agency_id` and `agency_code` to `SimplifiedApex` tables (#629)
    * refactor(callback): rename file
    
    * feat(schema): add agency_code field to multiple simplified Apex schemas
    
    - Introduced the agency_code field to the SimplifiedApexBankingTapSchema, SimplifiedApexInspectionDecisionSchema, SimplifiedApexInspectionSchema, SimplifiedApexLocationSchema, SimplifiedApexOnBoardRefundSchema, SimplifiedApexOnBoardSaleSchema, and SimplifiedApexValidationSchema.
    - This addition enhances the schemas by providing a consistent way to reference agency codes across different Apex functionalities.
    
    * feat(schema): add agency_code field to additional simplified Apex schemas
    
    - Added the agency_code field to the SimplifiedApexBankingTap, SimplifiedApexInspectionDecision, SimplifiedApexInspection, SimplifiedApexLocation, SimplifiedApexOnBoardRefund, SimplifiedApexOnBoardSale, and SimplifiedApexValidation schemas.
    - This update ensures consistency in referencing agency codes across various Apex functionalities.
    
    * feat(agency-map): implement agency ID mapping and update schemas
    
    - Added a new agency-map module to map operator long IDs to agency IDs.
    - Updated multiple simplified Apex schemas to utilize the new agency ID mapping, replacing direct operator long ID references with mapped agency IDs for consistency.
    - This enhancement improves the clarity and maintainability of the code by centralizing agency ID management.

--- modules/apex/packages/parsers/src/from-raw-to-simplified/inspections/inspection-v20.ts
+++ modules/apex/packages/parsers/src/from-raw-to-simplified/inspections/inspection-v20.ts
@@ -1,5 +1,6 @@
 /* * */
 
+import { getAgencyIdFromOperatorLongId } from '@/agency-map.js';
 import { Dates } from '@tmlmobilidade/dates';
 import { ApexControlStatusSchema, ApexEnvironmentStatusSchema, type RawApexTransactionInspectionV20, type SimplifiedApexInspection, SimplifiedApexInspectionSchema } from '@tmlmobilidade/go-types-apex';
 import { toUInt64 } from '@tmlmobilidade/utils';
@@ -20,7 +21,8 @@ export function parseRawApexTransactionInspectionV20IntoSimplifiedApexInspection
 
 	const result: SimplifiedApexInspection = {
 		_id: doc.payload.transactionInfo.transactionId,
-		agency_id: doc.payload.operatorInfo.operatorLongID,
+		agency_code: doc.payload.operatorInfo.operatorLongID,
+		agency_id: getAgencyIdFromOperatorLongId(doc.payload.operatorInfo.operatorLongID),
 		apex_version: doc.payload.versionInfo.apexVersion,
 		card_serial_number: toUInt64(doc.payload.cardInfo.cardSerialNumber),
 		control_destination_stop_id: doc.payload.controlServiceInfo.controlDestinationStopLongID,
