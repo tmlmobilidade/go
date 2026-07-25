2f84fc81678e12a0da8bccdb3ec223c7fde20b6e
João de Vasconcelos <10943992+joao-vasconcelos@users.noreply.github.com>
2026-07-25 13:44:05 +0100

    refactor(vehicle-events): integrate vehicle events indexes into MongoInterfaceTemplate instances
    - Updated the VehicleEventsDatabase class to include vehicleEventsIndexes in the MongoInterfaceTemplate instances for various vehicle event types.
    - This change enhances the indexing strategy for vehicle events, improving query performance and data retrieval efficiency.

--- packages-new/interfaces/rawdb/src/databases/vehicle-events.ts
+++ packages-new/interfaces/rawdb/src/databases/vehicle-events.ts
@@ -1,5 +1,6 @@
 /* * */
 
+import { vehicleEventsIndexes } from '@/indexes/vehicle-events/common.js';
 import { MongoInterfaceTemplate } from '@/interface.template.js';
 import { type Db, type MongoClient } from '@tmlmobilidade/go-clients-mongo';
 import { type RawVehicleEventEsCrtmAisa, RawVehicleEventEsCrtmAisaSchema } from '@tmlmobilidade/go-types-vehicle-events';
@@ -54,24 +55,24 @@ export class VehicleEventsDatabase {
 		// Create the database instance
 		this.database = instance.db(this.databaseName);
 		// Create collection interfaces
-		this.esCrtmAisa = new MongoInterfaceTemplate<RawVehicleEventEsCrtmAisa, RawVehicleEventEsCrtmAisa>('es-crtm-aisa', this.database, RawVehicleEventEsCrtmAisaSchema);
-		this.esCrtmLaVeloz = new MongoInterfaceTemplate<RawVehicleEventEsCrtmLaVeloz, RawVehicleEventEsCrtmLaVeloz>('es-crtm-la-veloz', this.database, RawVehicleEventEsCrtmLaVelozSchema);
-		this.ptTmlCcfl = new MongoInterfaceTemplate<RawVehicleEventPtTmlCcfl, RawVehicleEventPtTmlCcfl>('pt-tml-ccfl', this.database, RawVehicleEventPtTmlCcflSchema);
-		this.ptTmlCmAlsa = new MongoInterfaceTemplate<RawVehicleEventPtTmlCmAlsa, RawVehicleEventPtTmlCmAlsa>('pt-tml-cm-alsa', this.database, RawVehicleEventPtTmlCmAlsaSchema);
-		this.ptTmlCmRl = new MongoInterfaceTemplate<RawVehicleEventPtTmlCmRl, RawVehicleEventPtTmlCmRl>('pt-tml-cm-rl', this.database, RawVehicleEventPtTmlCmRlSchema);
-		this.ptTmlCmTst = new MongoInterfaceTemplate<RawVehicleEventPtTmlCmTst, RawVehicleEventPtTmlCmTst>('pt-tml-cm-tst', this.database, RawVehicleEventPtTmlCmTstSchema);
-		this.ptTmlCmVa = new MongoInterfaceTemplate<RawVehicleEventPtTmlCmVa, RawVehicleEventPtTmlCmVa>('pt-tml-cm-va', this.database, RawVehicleEventPtTmlCmVaSchema);
-		this.ptTmlCp = new MongoInterfaceTemplate<RawVehicleEventPtTmlCp, RawVehicleEventPtTmlCp>('pt-tml-cp', this.database, RawVehicleEventPtTmlCpSchema);
-		this.ptTmlFertagus = new MongoInterfaceTemplate<RawVehicleEventPtTmlFertagus, RawVehicleEventPtTmlFertagus>('pt-tml-fertagus', this.database, RawVehicleEventPtTmlFertagusSchema);
-		this.ptTmlMl = new MongoInterfaceTemplate<RawVehicleEventPtTmlMl, RawVehicleEventPtTmlMl>('pt-tml-ml', this.database, RawVehicleEventPtTmlMlSchema);
-		this.ptTmlMobi = new MongoInterfaceTemplate<RawVehicleEventPtTmlMobi, RawVehicleEventPtTmlMobi>('pt-tml-mobi', this.database, RawVehicleEventPtTmlMobiSchema);
-		this.ptTmlTcb = new MongoInterfaceTemplate<RawVehicleEventPtTmlTcb, RawVehicleEventPtTmlTcb>('pt-tml-tcb', this.database, RawVehicleEventPtTmlTcbSchema);
-		this.ptTmlTtsl = new MongoInterfaceTemplate<RawVehicleEventPtTmlTtsl, RawVehicleEventPtTmlTtsl>('pt-tml-ttsl', this.database, RawVehicleEventPtTmlTtslSchema);
-		this.ptTmpUnirUt1 = new MongoInterfaceTemplate<RawVehicleEventPtTmpUnirUt1, RawVehicleEventPtTmpUnirUt1>('pt-tmp-unir-ut1', this.database, RawVehicleEventPtTmpUnirUt1Schema);
-		this.ptTmpUnirUt2 = new MongoInterfaceTemplate<RawVehicleEventPtTmpUnirUt2, RawVehicleEventPtTmpUnirUt2>('pt-tmp-unir-ut2', this.database, RawVehicleEventPtTmpUnirUt2Schema);
-		this.ptTmpUnirUt3 = new MongoInterfaceTemplate<RawVehicleEventPtTmpUnirUt3, RawVehicleEventPtTmpUnirUt3>('pt-tmp-unir-ut3', this.database, RawVehicleEventPtTmpUnirUt3Schema);
-		this.ptTmpUnirUt4 = new MongoInterfaceTemplate<RawVehicleEventPtTmpUnirUt4, RawVehicleEventPtTmpUnirUt4>('pt-tmp-unir-ut4', this.database, RawVehicleEventPtTmpUnirUt4Schema);
-		this.ptTmpUnirUt5 = new MongoInterfaceTemplate<RawVehicleEventPtTmpUnirUt5, RawVehicleEventPtTmpUnirUt5>('pt-tmp-unir-ut5', this.database, RawVehicleEventPtTmpUnirUt5Schema);
-		this.ptTmpUnirUt6 = new MongoInterfaceTemplate<RawVehicleEventPtTmpUnirUt6, RawVehicleEventPtTmpUnirUt6>('pt-tmp-unir-ut6', this.database, RawVehicleEventPtTmpUnirUt6Schema);
+		this.esCrtmAisa = new MongoInterfaceTemplate<RawVehicleEventEsCrtmAisa, RawVehicleEventEsCrtmAisa>('es-crtm-aisa', this.database, RawVehicleEventEsCrtmAisaSchema, vehicleEventsIndexes);
+		this.esCrtmLaVeloz = new MongoInterfaceTemplate<RawVehicleEventEsCrtmLaVeloz, RawVehicleEventEsCrtmLaVeloz>('es-crtm-la-veloz', this.database, RawVehicleEventEsCrtmLaVelozSchema, vehicleEventsIndexes);
+		this.ptTmlCcfl = new MongoInterfaceTemplate<RawVehicleEventPtTmlCcfl, RawVehicleEventPtTmlCcfl>('pt-tml-ccfl', this.database, RawVehicleEventPtTmlCcflSchema, vehicleEventsIndexes);
+		this.ptTmlCmAlsa = new MongoInterfaceTemplate<RawVehicleEventPtTmlCmAlsa, RawVehicleEventPtTmlCmAlsa>('pt-tml-cm-alsa', this.database, RawVehicleEventPtTmlCmAlsaSchema, vehicleEventsIndexes);
+		this.ptTmlCmRl = new MongoInterfaceTemplate<RawVehicleEventPtTmlCmRl, RawVehicleEventPtTmlCmRl>('pt-tml-cm-rl', this.database, RawVehicleEventPtTmlCmRlSchema, vehicleEventsIndexes);
+		this.ptTmlCmTst = new MongoInterfaceTemplate<RawVehicleEventPtTmlCmTst, RawVehicleEventPtTmlCmTst>('pt-tml-cm-tst', this.database, RawVehicleEventPtTmlCmTstSchema, vehicleEventsIndexes);
+		this.ptTmlCmVa = new MongoInterfaceTemplate<RawVehicleEventPtTmlCmVa, RawVehicleEventPtTmlCmVa>('pt-tml-cm-va', this.database, RawVehicleEventPtTmlCmVaSchema, vehicleEventsIndexes);
+		this.ptTmlCp = new MongoInterfaceTemplate<RawVehicleEventPtTmlCp, RawVehicleEventPtTmlCp>('pt-tml-cp', this.database, RawVehicleEventPtTmlCpSchema, vehicleEventsIndexes);
+		this.ptTmlFertagus = new MongoInterfaceTemplate<RawVehicleEventPtTmlFertagus, RawVehicleEventPtTmlFertagus>('pt-tml-fertagus', this.database, RawVehicleEventPtTmlFertagusSchema, vehicleEventsIndexes);
+		this.ptTmlMl = new MongoInterfaceTemplate<RawVehicleEventPtTmlMl, RawVehicleEventPtTmlMl>('pt-tml-ml', this.database, RawVehicleEventPtTmlMlSchema, vehicleEventsIndexes);
+		this.ptTmlMobi = new MongoInterfaceTemplate<RawVehicleEventPtTmlMobi, RawVehicleEventPtTmlMobi>('pt-tml-mobi', this.database, RawVehicleEventPtTmlMobiSchema, vehicleEventsIndexes);
+		this.ptTmlTcb = new MongoInterfaceTemplate<RawVehicleEventPtTmlTcb, RawVehicleEventPtTmlTcb>('pt-tml-tcb', this.database, RawVehicleEventPtTmlTcbSchema, vehicleEventsIndexes);
+		this.ptTmlTtsl = new MongoInterfaceTemplate<RawVehicleEventPtTmlTtsl, RawVehicleEventPtTmlTtsl>('pt-tml-ttsl', this.database, RawVehicleEventPtTmlTtslSchema, vehicleEventsIndexes);
+		this.ptTmpUnirUt1 = new MongoInterfaceTemplate<RawVehicleEventPtTmpUnirUt1, RawVehicleEventPtTmpUnirUt1>('pt-tmp-unir-ut1', this.database, RawVehicleEventPtTmpUnirUt1Schema, vehicleEventsIndexes);
+		this.ptTmpUnirUt2 = new MongoInterfaceTemplate<RawVehicleEventPtTmpUnirUt2, RawVehicleEventPtTmpUnirUt2>('pt-tmp-unir-ut2', this.database, RawVehicleEventPtTmpUnirUt2Schema, vehicleEventsIndexes);
+		this.ptTmpUnirUt3 = new MongoInterfaceTemplate<RawVehicleEventPtTmpUnirUt3, RawVehicleEventPtTmpUnirUt3>('pt-tmp-unir-ut3', this.database, RawVehicleEventPtTmpUnirUt3Schema, vehicleEventsIndexes);
+		this.ptTmpUnirUt4 = new MongoInterfaceTemplate<RawVehicleEventPtTmpUnirUt4, RawVehicleEventPtTmpUnirUt4>('pt-tmp-unir-ut4', this.database, RawVehicleEventPtTmpUnirUt4Schema, vehicleEventsIndexes);
+		this.ptTmpUnirUt5 = new MongoInterfaceTemplate<RawVehicleEventPtTmpUnirUt5, RawVehicleEventPtTmpUnirUt5>('pt-tmp-unir-ut5', this.database, RawVehicleEventPtTmpUnirUt5Schema, vehicleEventsIndexes);
+		this.ptTmpUnirUt6 = new MongoInterfaceTemplate<RawVehicleEventPtTmpUnirUt6, RawVehicleEventPtTmpUnirUt6>('pt-tmp-unir-ut6', this.database, RawVehicleEventPtTmpUnirUt6Schema, vehicleEventsIndexes);
 	}
 }
