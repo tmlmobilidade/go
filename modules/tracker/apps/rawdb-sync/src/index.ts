a2cd0a5d556bd0c137488aaa70c560c1e9cc1cc0
João de Vasconcelos <10943992+joao-vasconcelos@users.noreply.github.com>
2026-07-26 16:40:02 +0100

    feat(rawdb-sync): add new agency configurations for vehicle events
    - Introduced multiple new agency configurations in the syncConfig array to enhance the raw database synchronization process.
    - This update expands the coverage of vehicle events, ensuring better data handling and synchronization across various agencies.

--- modules/tracker/apps/rawdb-sync/src/index.ts
+++ modules/tracker/apps/rawdb-sync/src/index.ts
@@ -12,9 +12,25 @@ import { SyncConfig } from './types.js';
 /* * */
 
 export const syncConfig = [
-
+	{ agency_id: 'G8N1G', collection: rawDb.vehicleEvents.esCrtmAisa },
+	{ agency_id: 'DFS5M', collection: rawDb.vehicleEvents.esCrtmLaVeloz },
+	{ agency_id: 'IA9T6', collection: rawDb.vehicleEvents.ptTmlCcfl },
 	{ agency_id: 'A2L1N', collection: rawDb.vehicleEvents.ptTmlCmAlsa },
-
+	{ agency_id: 'BNA17', collection: rawDb.vehicleEvents.ptTmlCmRl },
+	{ agency_id: 'YA15B', collection: rawDb.vehicleEvents.ptTmlCmTst },
+	{ agency_id: 'LA77N', collection: rawDb.vehicleEvents.ptTmlCmVa },
+	{ agency_id: 'N18KL', collection: rawDb.vehicleEvents.ptTmlCp },
+	{ agency_id: '7NTB1', collection: rawDb.vehicleEvents.ptTmlFertagus },
+	{ agency_id: 'IA2N9', collection: rawDb.vehicleEvents.ptTmlMl },
+	{ agency_id: 'HF16N', collection: rawDb.vehicleEvents.ptTmlMobi },
+	{ agency_id: 'A3H3M', collection: rawDb.vehicleEvents.ptTmlTcb },
+	{ agency_id: 'LTP61', collection: rawDb.vehicleEvents.ptTmlTtsl },
+	{ agency_id: 'KJTOU', collection: rawDb.vehicleEvents.ptTmpUnirUt1 },
+	{ agency_id: '1H6XC', collection: rawDb.vehicleEvents.ptTmpUnirUt2 },
+	{ agency_id: 'OP1VZ', collection: rawDb.vehicleEvents.ptTmpUnirUt3 },
+	{ agency_id: 'VZAS3', collection: rawDb.vehicleEvents.ptTmpUnirUt4 },
+	{ agency_id: '8NDX4', collection: rawDb.vehicleEvents.ptTmpUnirUt5 },
+	{ agency_id: '0AMEO', collection: rawDb.vehicleEvents.ptTmpUnirUt6 },
 ] as const satisfies SyncConfig[];
 
 /* * */
