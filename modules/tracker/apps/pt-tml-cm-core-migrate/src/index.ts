222d124e9aade5f7461148d9bbc76b0070d16426
João de Vasconcelos <10943992+joao-vasconcelos@users.noreply.github.com>
2026-07-26 12:41:04 +0100

    fix(migrate): handle duplicate documents during migration
    - Added logic to delete duplicate documents from the source database when a migration error indicates a duplicate key violation (E11000).
    - This enhancement improves the migration process by ensuring that only unique documents are retained, preventing potential conflicts during data insertion.

--- modules/tracker/apps/pt-tml-cm-core-migrate/src/index.ts
+++ modules/tracker/apps/pt-tml-cm-core-migrate/src/index.ts
@@ -86,6 +86,10 @@ async function main() {
 			Logger.success(`PCGI ID "${document._id}" -> [${parsedDocuments.map(doc => doc.agency_id).join('|')}] (x${currentInsertedDocumentIds.length}) [ ${currentInsertedDocumentIds.join(' | ')} ]`, 1);
 		} catch (error) {
 			Logger.error({ error, message: `Failed to migrate document "${document._id}": ${error.message}` });
+			if (error.message.startsWith('E11000')) {
+				Logger.error({ message: `Duplicate document "${document._id}" found in source database. Deleting it from source database.` });
+				await vehicleEventsCollection.deleteOne({ _id: document._id });
+			}
 		}
 	}
 
