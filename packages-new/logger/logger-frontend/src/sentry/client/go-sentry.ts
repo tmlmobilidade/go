b9c9c04967553df8923370656c7448c960fe2813
LoudNeo <meliodasgamer077@gmail.com>
2026-07-29 12:43:09 +0100

    fix(sentry): improve error handling for missing SENTRY_NEXTJS_DSN
    - Updated getSentryClient to check for the presence of 'document' in globalThis before logging an error for missing SENTRY_NEXTJS_DSN, enhancing compatibility with different environments.
    - Maintained existing error logging to inform users about the missing environment variable.

--- packages-new/logger/logger-frontend/src/sentry/client/go-sentry.ts
+++ packages-new/logger/logger-frontend/src/sentry/client/go-sentry.ts
@@ -1,7 +1,7 @@
 export function getSentryClient(): string | undefined {
 	const dsn = process.env.SENTRY_NEXTJS_DSN;
 
-	if (!dsn) {
+	if (!dsn && 'document' in globalThis) {
 		console.error(new Error('Missing SENTRY_NEXTJS_DSN. Please check your environment variables.'));
 		return undefined;
 	}
