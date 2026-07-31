74db260606664d9e52c391d92c94b440f0518109
João de Vasconcelos <10943992+joao-vasconcelos@users.noreply.github.com>
2026-07-31 18:15:30 +0100

    refactor(api): update import paths for annotations, events, holidays, and year periods
    - Updated import statements in annotations.controller.ts, events.controller.ts, holidays.controller.ts, and year-periods.controller.ts to utilize types from @tmlmobilidade/go-types-offer, enhancing type clarity and maintainability.
    - Added @fastify/multipart dependency in package.json for improved file handling capabilities.
    - Updated SamsExport.context.tsx to import CreateFileExportDto and SamsAnalysisExportProperties from @tmlmobilidade/go-types-downloads, ensuring consistent type usage across the application.

--- modules/dates/apps/api/src/endpoints/annotations/annotations.controller.ts
+++ modules/dates/apps/api/src/endpoints/annotations/annotations.controller.ts
@@ -4,7 +4,8 @@ import { HTTP_STATUS, HttpException } from '@tmlmobilidade/consts';
 import { type FastifyReply, type FastifyRequest } from '@tmlmobilidade/fastify';
 import { type Filter } from '@tmlmobilidade/go-clients-mongo';
 import { goDb } from '@tmlmobilidade/go-interfaces-godb';
-import { type Annotation, type CreateAnnotationDto, PermissionCatalog, type UpdateAnnotationDto } from '@tmlmobilidade/types';
+import { type Annotation, type CreateAnnotationDto, type UpdateAnnotationDto } from '@tmlmobilidade/go-types-offer';
+import { PermissionCatalog } from '@tmlmobilidade/types';
 
 /* * */;
 
--- modules/dates/apps/api/src/endpoints/events/events.controller.ts
+++ modules/dates/apps/api/src/endpoints/events/events.controller.ts
@@ -4,7 +4,8 @@ import { HTTP_STATUS, HttpException } from '@tmlmobilidade/consts';
 import { type FastifyReply, type FastifyRequest } from '@tmlmobilidade/fastify';
 import { type Filter } from '@tmlmobilidade/go-clients-mongo';
 import { goDb } from '@tmlmobilidade/go-interfaces-godb';
-import { type CreateEventDto, type Event, PermissionCatalog, type UpdateEventDto } from '@tmlmobilidade/types';
+import { type CreateEventDto, type Event, type UpdateEventDto } from '@tmlmobilidade/go-types-offer';
+import { PermissionCatalog } from '@tmlmobilidade/types';
 
 /* * */
 
