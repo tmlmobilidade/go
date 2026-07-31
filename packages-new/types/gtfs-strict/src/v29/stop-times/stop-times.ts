469121cd68bb78e26cea22a34bff727ddb8eda08
João de Vasconcelos <10943992+joao-vasconcelos@users.noreply.github.com>
2026-07-31 13:32:44 +0100

    refactor(gtfs): update module exports to use relative paths
    - Changed module exports in index.ts files to use relative paths instead of absolute paths for better compatibility.
    - Refactored GtfsStopTimesSchema in stop-times.ts to GtfsStrictV29StopTimesSchema, adding default values for pickup and drop-off types and omitting the stop_headsign field for clarity.
    - Improved overall organization and maintainability of GTFS type definitions.

--- packages-new/types/gtfs-strict/src/v29/stop-times/stop-times.ts
+++ packages-new/types/gtfs-strict/src/v29/stop-times/stop-times.ts
@@ -1,34 +1,33 @@
 /* * */
 
-import { GtfsBinarySchema } from '@/shared/binary.js';
-import { GtfsTimeSchema } from '@/shared/gtfs-time.js';
-import { GtfsPickupDropoffTypeSchema } from '@/shared/pickup-dropoff-type.js';
+import { GtfsBinarySchema, GtfsPickupDropoffTypeSchema, GtfsStopTimesSchema, GtfsTimeSchema } from '@tmlmobilidade/go-types-gtfs';
 import { NonNegativeNumberSchema } from '@tmlmobilidade/go-types-shared';
 import { z } from 'zod';
 
 /* * */
 
-export const GtfsStopTimesSchema = z.object({
+export const GtfsStrictV29StopTimesSchema = GtfsStopTimesSchema.extend({
 	arrival_time: GtfsTimeSchema,
-	continuous_drop_off: GtfsPickupDropoffTypeSchema.optional(),
-	continuous_pickup: GtfsPickupDropoffTypeSchema.optional(),
+	continuous_drop_off: GtfsPickupDropoffTypeSchema.default('1'),
+	continuous_pickup: GtfsPickupDropoffTypeSchema.default('1'),
 	departure_time: GtfsTimeSchema,
-	drop_off_type: GtfsPickupDropoffTypeSchema.optional(),
-	pickup_type: GtfsPickupDropoffTypeSchema.optional(),
-	shape_dist_traveled: NonNegativeNumberSchema.optional(),
-	stop_headsign: z.string().optional(),
+	drop_off_type: GtfsPickupDropoffTypeSchema.default('1'),
+	pickup_type: GtfsPickupDropoffTypeSchema.default('1'),
+	shape_dist_traveled: NonNegativeNumberSchema,
 	stop_id: z.string(),
 	stop_sequence: NonNegativeNumberSchema,
 	timepoint: GtfsBinarySchema,
 	trip_id: z.string(),
+}).omit({
+	stop_headsign: true,
 });
 
 /**
- * Represents a stop time in the GTFS format.
+ * Represents a stop time in the custom GTFS strict v29 format.
  * A stop time is a record of when a transit vehicle arrives at and departs from a specific stop.
  * It includes information such as the arrival and departure times, the stop ID, the trip ID,
  * and various pickup and drop-off types. This information is crucial for scheduling and
  * coordinating transit services, allowing passengers to know when a vehicle will be at a particular stop
  * and what type of service is available at that stop.
  */
-export type GtfsStopTimes = z.infer<typeof GtfsStopTimesSchema>;
+export type GtfsStrictV29StopTimes = z.infer<typeof GtfsStrictV29StopTimesSchema>;
