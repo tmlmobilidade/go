ea6d16d5e648858979cb86a154270b4a527060e6
João de Vasconcelos <10943992+joao-vasconcelos@users.noreply.github.com>
2026-07-31 19:01:47 +0100

    refactor(imports): validate operational dates in exportPlansFile function
    - Updated the exportPlansFile function to utilize validateOperationalDate for plan start and end dates, enhancing data integrity in GTFS exports.
    - Modified import statement to include validateOperationalDate from @tmlmobilidade/types, improving type clarity and maintainability.

--- modules/hub/apps/publish-gtfs/src/exports/plans.ts
+++ modules/hub/apps/publish-gtfs/src/exports/plans.ts
@@ -2,7 +2,7 @@
 
 import { type ExportGtfsContext } from '@/types/context.js';
 import { Logger } from '@tmlmobilidade/logger';
-import { type OperationalDate, Plan } from '@tmlmobilidade/types';
+import { type OperationalDate, Plan, validateOperationalDate } from '@tmlmobilidade/types';
 
 /* * */
 
@@ -27,8 +27,8 @@ export async function exportPlansFile(planData: Plan, context: ExportGtfsContext
 	const parsedPlansRow: ExportedPlansRow = {
 		agency_id: planData.agency_id,
 		plan_id: planData._id,
-		plan_end_date: planData.gtfs_feed_info.feed_end_date,
-		plan_start_date: planData.gtfs_feed_info.feed_start_date,
+		plan_end_date: validateOperationalDate(planData.gtfs_feed_info.feed_end_date),
+		plan_start_date: validateOperationalDate(planData.gtfs_feed_info.feed_start_date),
 	};
 
 	await context.writers.plans.write(parsedPlansRow);
