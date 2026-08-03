1061d6e86e2b476d168d9874b99bb771900df3b4
LoudNeo <meliodasgamer077@gmail.com>
2026-08-03 11:58:32 +0100

    refactor(stops): streamline form value handling and data association

--- modules/stops/apps/frontend/src/components/stops/create/StopCreateStep1Locations/index.tsx
+++ modules/stops/apps/frontend/src/components/stops/create/StopCreateStep1Locations/index.tsx
@@ -1,9 +1,8 @@
 'use client';
 
 import { useStopCreateContext } from '@/components/stops/create/StopCreate.context';
-import { useLocationsContext } from '@/contexts/Locations.context';
-import { Grid, Section, ValueDisplay } from '@tmlmobilidade/ui';
-import { useMemo } from 'react';
+import { Grid, Section, useLocationsContext, ValueDisplay } from '@tmlmobilidade/ui';
+import { useCallback, useState } from 'react';
 
 /* * */
 
@@ -15,65 +14,26 @@ export function StopCreateStep1Locations() {
 
 	const stopCreateContext = useStopCreateContext();
 	const locationsContext = useLocationsContext();
+	const form = stopCreateContext.data.form;
+	const [, forceFormUpdate] = useState(0);
+
+	const handleFormValuesChange = useCallback(() => {
+		forceFormUpdate(value => value + 1);
+	}, []);
+
+	form.watch('district_id', handleFormValuesChange);
+	form.watch('municipality_id', handleFormValuesChange);
+	form.watch('parish_id', handleFormValuesChange);
+	form.watch('locality_id', handleFormValuesChange);
 
 	//
 	// B. Transform data
 
-	const associatedDistrict = useMemo(() => {
-		// Skip if districts are unavailable
-		if (!locationsContext.data.districts_map) return;
-		// Skip if stop does not yet have a district
-		if (!stopCreateContext.data.form.values.district_id) return;
-		// Get the district from the map
-		const districtId = stopCreateContext.data.form.values.district_id;
-		const matchingDistrictData = locationsContext.data.districts_map.get(districtId);
-		// Return if no matching district data is found
-		if (!matchingDistrictData) return;
-		// Return the matching data
-		return matchingDistrictData;
-	}, [stopCreateContext.data.form.values, locationsContext.data.districts_map]);
-
-	const associatedMunicipality = useMemo(() => {
-		// Skip if municipalities are unavailable
-		if (!locationsContext.data.municipalities_map) return;
-		// Skip if stop does not yet have a municipality
-		if (!stopCreateContext.data.form.values.municipality_id) return;
-		// Get the municipality from the map
-		const municipalityId = stopCreateContext.data.form.values.municipality_id;
-		const matchingMunicipalityData = locationsContext.data.municipalities_map.get(municipalityId);
-		// Return if no matching municipality data is found
-		if (!matchingMunicipalityData) return;
-		// Return the matching data
-		return matchingMunicipalityData;
-	}, [stopCreateContext.data.form.values, locationsContext.data.municipalities_map]);
-
-	const associatedParish = useMemo(() => {
-		// Skip if parishes are unavailable
-		if (!locationsContext.data.parishes_map) return;
-		// Skip if stop does not yet have a parish
-		if (!stopCreateContext.data.form.values.parish_id) return;
-		// Get the parish from the map
-		const parishId = stopCreateContext.data.form.values.parish_id;
-		const matchingParishData = locationsContext.data.parishes_map.get(parishId);
-		// Return if no matching parish data is found
-		if (!matchingParishData) return;
-		// Return the matching data
-		return matchingParishData;
-	}, [stopCreateContext.data.form.values, locationsContext.data.parishes_map]);
-
-	const associatedLocality = useMemo(() => {
-		// Skip if localities are unavailable
-		if (!locationsContext.data.localities_map) return;
-		// Skip if stop does not yet have a locality
-		if (!stopCreateContext.data.form.values.locality_id) return;
-		// Get the locality from the map
-		const localityId = stopCreateContext.data.form.values.locality_id;
-		const matchingLocalityData = locationsContext.data.localities_map.get(localityId);
-		// Return if no matching locality data is found
-		if (!matchingLocalityData) return;
-		// Return the matching data
-		return matchingLocalityData;
-	}, [stopCreateContext.data.form.values, locationsContext.data.localities_map]);
+	const formValues = form.getValues();
+	const associatedDistrict = formValues.district_id ? locationsContext.data.districts_map.get(formValues.district_id) : undefined;
+	const associatedMunicipality = formValues.municipality_id ? locationsContext.data.municipalities_map.get(formValues.municipality_id) : undefined;
+	const associatedParish = formValues.parish_id ? locationsContext.data.parishes_map.get(formValues.parish_id) : undefined;
+	const associatedLocality = formValues.locality_id ? locationsContext.data.localitites_map.get(formValues.locality_id) : undefined;
 
 	//
 	// C. Render components
