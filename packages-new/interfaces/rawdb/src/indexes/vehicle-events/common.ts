6f04f928bba43eb35f64c8b99ce7eb47108e0504
João de Vasconcelos <10943992+joao-vasconcelos@users.noreply.github.com>
2026-08-03 01:14:58 +0100

    refactor(indexes): remove redundant index keys in vehicle events
    - Eliminated unnecessary index keys for 'agency_id' and 'received_at' in the vehicle events indexes, streamlining the indexing structure for improved performance and clarity.

--- packages-new/interfaces/rawdb/src/indexes/vehicle-events/common.ts
+++ packages-new/interfaces/rawdb/src/indexes/vehicle-events/common.ts
@@ -7,9 +7,6 @@ import { type RawVehicleEvent } from '@tmlmobilidade/go-types-vehicle-events';
 
 export const vehicleEventsIndexes: SimplifiedMongoIndex<RawVehicleEvent>[] = [
 	{ key: { created_at: 1 } },
-	{ key: { agency_id: 1, created_at: 1 } },
 	// eslint-disable-next-line perfectionist/sort-objects
 	{ key: { version: 1, created_at: 1 } },
-	// eslint-disable-next-line perfectionist/sort-objects
-	{ key: { version: 1, received_at: 1 } },
 ];
