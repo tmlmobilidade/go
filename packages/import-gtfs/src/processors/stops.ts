8c1f890bb459ee3c4d2913874ef30b035c85c09c
LoudNeo <meliodasgamer077@gmail.com>
2026-08-03 12:07:48 +0100

    fix(stops): ensure stop_id is processed as a number and enhance stop data handling
    - Updated the stop_id retrieval to convert it to a number for consistency in data validation.
    - Enhanced the stop data writing process to include additional fields and default values, improving data integrity and completeness.

--- packages/import-gtfs/src/processors/stops.ts
+++ packages/import-gtfs/src/processors/stops.ts
@@ -2,6 +2,7 @@
 
 import { type ImportGtfsContext } from '@/types/context.js';
 import { parseCsvFile } from '@/utils/parse-csv.js';
+import { type HubGtfsExportStops } from '@tmlmobilidade/go-types-public-info';
 import { Logger } from '@tmlmobilidade/logger';
 import { Timer } from '@tmlmobilidade/timer';
 import { type GTFS_Stop_Extended_Raw, validateGtfsStopExtended } from '@tmlmobilidade/types';
@@ -24,9 +25,32 @@ export async function processStopsFile(context: ImportGtfsContext): Promise<void
 			// Validate the current row against the proper type
 			const validatedData = validateGtfsStopExtended(data);
 			// Skip if stop already exists
-			if (context.gtfs.stops.get('stop_id', validatedData.stop_id)) return;
+			if (context.gtfs.stops.get('stop_id', Number(validatedData.stop_id))) return;
 			// Save the exported row
-			context.gtfs.stops.write(validatedData);
+			context.gtfs.stops.write({
+				district_id: validatedData.district_id ?? '',
+				district_name: validatedData.district_name ?? '',
+				flags: validatedData.flags ?? '',
+				legacy_ids: validatedData.legacy_ids ?? '',
+				lifecycle_status: 'draft',
+				locality_id: validatedData.locality_id ?? '',
+				locality_name: validatedData.locality_name ?? '',
+				location_type: String(validatedData.location_type ?? 0) as HubGtfsExportStops['location_type'],
+				municipality_id: validatedData.municipality_id ?? '',
+				municipality_name: validatedData.municipality_name ?? '',
+				parent_station: validatedData.parent_station ?? '',
+				parish_id: validatedData.parish_id ?? '',
+				parish_name: validatedData.parish_name ?? '',
+				platform_code: validatedData.platform_code ?? '',
+				stop_code: Number(validatedData.stop_code),
+				stop_id: Number(validatedData.stop_id),
+				stop_lat: validatedData.stop_lat,
+				stop_lon: validatedData.stop_lon,
+				stop_name: validatedData.stop_name,
+				stop_short_name: validatedData.stop_short_name ?? '',
+				tts_stop_name: validatedData.tts_stop_name ?? '',
+				wheelchair_boarding: String(validatedData.wheelchair_boarding ?? 0) as HubGtfsExportStops['wheelchair_boarding'],
+			});
 		};
 
 		//
