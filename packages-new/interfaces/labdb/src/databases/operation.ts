c185ceddfe0dea522d9a42d3152288ff18fe6458
João de Vasconcelos <10943992+joao-vasconcelos@users.noreply.github.com>
2026-08-03 23:10:18 +0100

    feat(operation): expand ride analysis schemas and types for enhanced data handling
    - Added multiple new schemas for ride analysis, including those for vehicle events at the last stop and various expected metrics.
    - Updated existing schemas to include additional fields and improved data types, enhancing the overall structure and integrity of the data.
    - Refactored the OperationDatabase to initialize new templates for the added schemas, ensuring comprehensive data tracking and analysis capabilities.

--- packages-new/interfaces/labdb/src/databases/operation.ts
+++ packages-new/interfaces/labdb/src/databases/operation.ts
@@ -1,9 +1,9 @@
 /* * */
 
 import { ClickHouseInterfaceTemplate } from '@/interface.template.js';
-import { rideAnalysisAtLeastOneVehicleEventOnFirstStopTableSchema, ridesTableSchema, simplifiedVehicleEventTableSchema } from '@/schemas/operation.js';
+import { rideAnalysisAtLeastOneVehicleEventOnFirstStopTableSchema, rideAnalysisAtLeastOneVehicleEventOnLastStopTableSchema, rideAnalysisExpectedApexValidationIntervalTableSchema, rideAnalysisExpectedDriverIdQtyTableSchema, rideAnalysisExpectedStartTimeTableSchema, rideAnalysisExpectedVehicleEventDelayTableSchema, rideAnalysisExpectedVehicleEventIntervalTableSchema, rideAnalysisExpectedVehicleEventQtyTableSchema, rideAnalysisExpectedVehicleIdQtyTableSchema, rideAnalysisMatchingApexLocationsTableSchema, rideAnalysisMatchingVehicleIdsTableSchema, rideAnalysisSimpleOneApexValidationTableSchema, rideAnalysisSimpleOneVehicleEventOrApexValidationTableSchema, rideAnalysisSimpleThreeVehicleEventsTableSchema, rideAnalysisTransactionSequentialityTableSchema, ridesTableSchema, simplifiedVehicleEventTableSchema } from '@/schemas/operation.js';
 import { ClickHouseClient } from '@tmlmobilidade/go-clients-clickhouse';
-import { type RideAnalysisAtLeastOneVehicleEventOnFirstStop, type RideAnalysisEndedAtLastStop, type RideAnalysisExpectedApexValidationInterval, type RideAnalysisExpectedDriverIdQty, type RideAnalysisExpectedStartTime, type RideAnalysisExpectedVehicleEventDelay, type RideAnalysisExpectedVehicleEventInterval, type RideAnalysisExpectedVehicleEventQty, type RideAnalysisExpectedVehicleIdQty, type RideAnalysisMatchingApexLocations, type RideAnalysisMatchingVehicleIds, type RideAnalysisSimpleOneApexValidation, type RideAnalysisSimpleOneVehicleEventOrApexValidation, type RideAnalysisSimpleThreeVehicleEvents, type RideAnalysisTransactionSequentiality } from '@tmlmobilidade/go-types-operation';
+import { type RideAnalysisAtLeastOneVehicleEventOnFirstStop, type RideAnalysisAtLeastOneVehicleEventOnLastStop, type RideAnalysisExpectedApexValidationInterval, type RideAnalysisExpectedDriverIdQty, type RideAnalysisExpectedStartTime, type RideAnalysisExpectedVehicleEventDelay, type RideAnalysisExpectedVehicleEventInterval, type RideAnalysisExpectedVehicleEventQty, type RideAnalysisExpectedVehicleIdQty, type RideAnalysisMatchingApexLocations, type RideAnalysisMatchingVehicleIds, type RideAnalysisSimpleOneApexValidation, type RideAnalysisSimpleOneVehicleEventOrApexValidation, type RideAnalysisSimpleThreeVehicleEvents, type RideAnalysisTransactionSequentiality } from '@tmlmobilidade/go-types-operation';
 import { type Ride } from '@tmlmobilidade/go-types-operation';
 import { type SimplifiedVehicleEvent } from '@tmlmobilidade/go-types-vehicle-events';
 
@@ -13,7 +13,7 @@ export class OperationDatabase {
 	//
 
 	public readonly rideAnalysisAtLeastOneVehicleEventOnFirstStop: ClickHouseInterfaceTemplate<RideAnalysisAtLeastOneVehicleEventOnFirstStop>;
-	public readonly rideAnalysisEndedAtLastStop: ClickHouseInterfaceTemplate<RideAnalysisEndedAtLastStop>;
+	public readonly rideAnalysisAtLeastOneVehicleEventOnLastStop: ClickHouseInterfaceTemplate<RideAnalysisAtLeastOneVehicleEventOnLastStop>;
 	public readonly rideAnalysisExpectedApexValidationInterval: ClickHouseInterfaceTemplate<RideAnalysisExpectedApexValidationInterval>;
 	public readonly rideAnalysisExpectedDriverIdQty: ClickHouseInterfaceTemplate<RideAnalysisExpectedDriverIdQty>;
 	public readonly rideAnalysisExpectedStartTime: ClickHouseInterfaceTemplate<RideAnalysisExpectedStartTime>;
@@ -35,7 +35,77 @@ export class OperationDatabase {
 	public constructor(instance: ClickHouseClient) {
 		this.rideAnalysisAtLeastOneVehicleEventOnFirstStop = new ClickHouseInterfaceTemplate<RideAnalysisAtLeastOneVehicleEventOnFirstStop>(instance, this.databaseName, 'ride_analysis_at_least_one_vehicle_event_on_first_stop', rideAnalysisAtLeastOneVehicleEventOnFirstStopTableSchema, {
 			engine: 'ReplacingMergeTree(updated_at)',
-			orderBy: ['ride_id', 'updated_at'],
+			orderBy: ['ride_id'],
+			partitionBy: 'intDiv(operational_date, 100)',
+		});
+		this.rideAnalysisAtLeastOneVehicleEventOnLastStop = new ClickHouseInterfaceTemplate<RideAnalysisAtLeastOneVehicleEventOnLastStop>(instance, this.databaseName, 'ride_analysis_at_least_one_vehicle_event_on_last_stop', rideAnalysisAtLeastOneVehicleEventOnLastStopTableSchema, {
+			engine: 'ReplacingMergeTree(updated_at)',
+			orderBy: ['ride_id'],
+			partitionBy: 'intDiv(operational_date, 100)',
+		});
+		this.rideAnalysisExpectedApexValidationInterval = new ClickHouseInterfaceTemplate<RideAnalysisExpectedApexValidationInterval>(instance, this.databaseName, 'ride_analysis_expected_apex_validation_interval', rideAnalysisExpectedApexValidationIntervalTableSchema, {
+			engine: 'ReplacingMergeTree(updated_at)',
+			orderBy: ['ride_id'],
+			partitionBy: 'intDiv(operational_date, 100)',
+		});
+		this.rideAnalysisExpectedDriverIdQty = new ClickHouseInterfaceTemplate<RideAnalysisExpectedDriverIdQty>(instance, this.databaseName, 'ride_analysis_expected_driver_id_qty', rideAnalysisExpectedDriverIdQtyTableSchema, {
+			engine: 'ReplacingMergeTree(updated_at)',
+			orderBy: ['ride_id'],
+			partitionBy: 'intDiv(operational_date, 100)',
+		});
+		this.rideAnalysisExpectedStartTime = new ClickHouseInterfaceTemplate<RideAnalysisExpectedStartTime>(instance, this.databaseName, 'ride_analysis_expected_start_time', rideAnalysisExpectedStartTimeTableSchema, {
+			engine: 'ReplacingMergeTree(updated_at)',
+			orderBy: ['ride_id'],
+			partitionBy: 'intDiv(operational_date, 100)',
+		});
+		this.rideAnalysisExpectedVehicleEventDelay = new ClickHouseInterfaceTemplate<RideAnalysisExpectedVehicleEventDelay>(instance, this.databaseName, 'ride_analysis_expected_vehicle_event_delay', rideAnalysisExpectedVehicleEventDelayTableSchema, {
+			engine: 'ReplacingMergeTree(updated_at)',
+			orderBy: ['ride_id'],
+			partitionBy: 'intDiv(operational_date, 100)',
+		});
+		this.rideAnalysisExpectedVehicleEventInterval = new ClickHouseInterfaceTemplate<RideAnalysisExpectedVehicleEventInterval>(instance, this.databaseName, 'ride_analysis_expected_vehicle_event_interval', rideAnalysisExpectedVehicleEventIntervalTableSchema, {
+			engine: 'ReplacingMergeTree(updated_at)',
+			orderBy: ['ride_id'],
+			partitionBy: 'intDiv(operational_date, 100)',
+		});
+		this.rideAnalysisExpectedVehicleEventQty = new ClickHouseInterfaceTemplate<RideAnalysisExpectedVehicleEventQty>(instance, this.databaseName, 'ride_analysis_expected_vehicle_event_qty', rideAnalysisExpectedVehicleEventQtyTableSchema, {
+			engine: 'ReplacingMergeTree(updated_at)',
+			orderBy: ['ride_id'],
+			partitionBy: 'intDiv(operational_date, 100)',
+		});
+		this.rideAnalysisExpectedVehicleIdQty = new ClickHouseInterfaceTemplate<RideAnalysisExpectedVehicleIdQty>(instance, this.databaseName, 'ride_analysis_expected_vehicle_id_qty', rideAnalysisExpectedVehicleIdQtyTableSchema, {
+			engine: 'ReplacingMergeTree(updated_at)',
+			orderBy: ['ride_id'],
+			partitionBy: 'intDiv(operational_date, 100)',
+		});
+		this.rideAnalysisMatchingApexLocations = new ClickHouseInterfaceTemplate<RideAnalysisMatchingApexLocations>(instance, this.databaseName, 'ride_analysis_matching_apex_locations', rideAnalysisMatchingApexLocationsTableSchema, {
+			engine: 'ReplacingMergeTree(updated_at)',
+			orderBy: ['ride_id'],
+			partitionBy: 'intDiv(operational_date, 100)',
+		});
+		this.rideAnalysisMatchingVehicleIds = new ClickHouseInterfaceTemplate<RideAnalysisMatchingVehicleIds>(instance, this.databaseName, 'ride_analysis_matching_vehicle_ids', rideAnalysisMatchingVehicleIdsTableSchema, {
+			engine: 'ReplacingMergeTree(updated_at)',
+			orderBy: ['ride_id'],
+			partitionBy: 'intDiv(operational_date, 100)',
+		});
+		this.rideAnalysisSimpleOneApexValidation = new ClickHouseInterfaceTemplate<RideAnalysisSimpleOneApexValidation>(instance, this.databaseName, 'ride_analysis_simple_one_apex_validation', rideAnalysisSimpleOneApexValidationTableSchema, {
+			engine: 'ReplacingMergeTree(updated_at)',
+			orderBy: ['ride_id'],
+			partitionBy: 'intDiv(operational_date, 100)',
+		});
+		this.rideAnalysisSimpleOneVehicleEventOrApexValidation = new ClickHouseInterfaceTemplate<RideAnalysisSimpleOneVehicleEventOrApexValidation>(instance, this.databaseName, 'ride_analysis_simple_one_vehicle_event_or_apex_validation', rideAnalysisSimpleOneVehicleEventOrApexValidationTableSchema, {
+			engine: 'ReplacingMergeTree(updated_at)',
+			orderBy: ['ride_id'],
+			partitionBy: 'intDiv(operational_date, 100)',
+		});
+		this.rideAnalysisSimpleThreeVehicleEvents = new ClickHouseInterfaceTemplate<RideAnalysisSimpleThreeVehicleEvents>(instance, this.databaseName, 'ride_analysis_simple_three_vehicle_events', rideAnalysisSimpleThreeVehicleEventsTableSchema, {
+			engine: 'ReplacingMergeTree(updated_at)',
+			orderBy: ['ride_id'],
+			partitionBy: 'intDiv(operational_date, 100)',
+		});
+		this.rideAnalysisTransactionSequentiality = new ClickHouseInterfaceTemplate<RideAnalysisTransactionSequentiality>(instance, this.databaseName, 'ride_analysis_transaction_sequentiality', rideAnalysisTransactionSequentialityTableSchema, {
+			engine: 'ReplacingMergeTree(updated_at)',
+			orderBy: ['ride_id'],
 			partitionBy: 'intDiv(operational_date, 100)',
 		});
 		this.rides = new ClickHouseInterfaceTemplate<Ride>(instance, this.databaseName, 'rides', ridesTableSchema, {
@@ -52,7 +122,7 @@ export class OperationDatabase {
 
 	public async init() {
 		await this.rideAnalysisAtLeastOneVehicleEventOnFirstStop.init();
-		await this.rideAnalysisEndedAtLastStop.init();
+		await this.rideAnalysisAtLeastOneVehicleEventOnLastStop.init();
 		await this.rideAnalysisExpectedApexValidationInterval.init();
 		await this.rideAnalysisExpectedDriverIdQty.init();
 		await this.rideAnalysisExpectedStartTime.init();
--- packages-new/interfaces/labdb/src/schemas/operation.ts
+++ packages-new/interfaces/labdb/src/schemas/operation.ts
@@ -1,7 +1,7 @@
 /* * */
 
 import { type ClickHouseTableSchema } from '@tmlmobilidade/go-clients-clickhouse';
-import { type Ride, type RideAnalysisAtLeastOneVehicleEventOnFirstStop, type RideAnalysisBase } from '@tmlmobilidade/go-types-operation';
+import { type Ride, type RideAnalysisAtLeastOneVehicleEventOnFirstStop, RideAnalysisAtLeastOneVehicleEventOnLastStop, type RideAnalysisBase, type RideAnalysisExpectedApexValidationInterval, RideAnalysisExpectedDriverIdQty, RideAnalysisExpectedStartTime, RideAnalysisExpectedVehicleEventDelay, RideAnalysisExpectedVehicleEventInterval, RideAnalysisExpectedVehicleEventQty, RideAnalysisExpectedVehicleIdQty, RideAnalysisMatchingApexLocations, RideAnalysisMatchingVehicleIds, RideAnalysisSimpleOneApexValidation, RideAnalysisSimpleOneVehicleEventOrApexValidation, RideAnalysisSimpleThreeVehicleEvents, RideAnalysisTransactionSequentiality } from '@tmlmobilidade/go-types-operation';
 import { type SimplifiedVehicleEvent } from '@tmlmobilidade/go-types-vehicle-events';
 
 /* * */
@@ -29,7 +29,9 @@ export const simplifiedVehicleEventTableSchema: ClickHouseTableSchema<Simplified
 /* * */
 
 const rideAnalysisBaseTableSchema: ClickHouseTableSchema<RideAnalysisBase> = {
+	agency_id: { type: 'LowCardinality(String)' },
 	is_accepted: { type: 'Bool' },
+	operational_date: { type: 'UInt32' },
 	processing_status: { type: 'LowCardinality(String)' },
 	reason: { type: 'LowCardinality(Nullable(String))' },
 	remarks: { type: 'Nullable(String) CODEC(ZSTD)' },
@@ -41,7 +43,120 @@ const rideAnalysisBaseTableSchema: ClickHouseTableSchema<RideAnalysisBase> = {
 
 export const rideAnalysisAtLeastOneVehicleEventOnFirstStopTableSchema: ClickHouseTableSchema<RideAnalysisAtLeastOneVehicleEventOnFirstStop> = {
 	...rideAnalysisBaseTableSchema,
-	value: { type: 'Nullable(UInt8) CODEC(T64, ZSTD)' },
+	vehicle_events_on_first_stop_qty: { type: 'Nullable(UInt16) CODEC(T64, ZSTD)' },
+};
+
+/* * */
+
+export const rideAnalysisAtLeastOneVehicleEventOnLastStopTableSchema: ClickHouseTableSchema<RideAnalysisAtLeastOneVehicleEventOnLastStop> = {
+	...rideAnalysisBaseTableSchema,
+	vehicle_events_on_last_stop_qty: { type: 'Nullable(UInt16) CODEC(T64, ZSTD)' },
+};
+
+/* * */
+
+export const rideAnalysisExpectedApexValidationIntervalTableSchema: ClickHouseTableSchema<RideAnalysisExpectedApexValidationInterval> = {
+	...rideAnalysisBaseTableSchema,
+	observed_average_interval: { type: 'Nullable(UInt16) CODEC(T64, ZSTD)' },
+	observed_max_interval: { type: 'Nullable(UInt16) CODEC(T64, ZSTD)' },
+	observed_min_interval: { type: 'Nullable(UInt16) CODEC(T64, ZSTD)' },
+};
+
+/* * */
+
+export const rideAnalysisExpectedDriverIdQtyTableSchema: ClickHouseTableSchema<RideAnalysisExpectedDriverIdQty> = {
+	...rideAnalysisBaseTableSchema,
+	observed_driver_ids_qty: { type: 'Nullable(UInt8) CODEC(T64, ZSTD)' },
+};
+
+/* * */
+
+export const rideAnalysisExpectedStartTimeTableSchema: ClickHouseTableSchema<RideAnalysisExpectedStartTime> = {
+	...rideAnalysisBaseTableSchema,
+	observed_start_time: { type: 'Nullable(Int64)' },
+	observed_start_time_delta: { type: 'Nullable(UInt16) CODEC(T64, ZSTD)' },
+};
+
+/* * */
+
+export const rideAnalysisExpectedVehicleEventDelayTableSchema: ClickHouseTableSchema<RideAnalysisExpectedVehicleEventDelay> = {
+	...rideAnalysisBaseTableSchema,
+	observed_average_delay: { type: 'Nullable(UInt16) CODEC(T64, ZSTD)' },
+	observed_max_delay: { type: 'Nullable(UInt16) CODEC(T64, ZSTD)' },
+	observed_min_delay: { type: 'Nullable(UInt16) CODEC(T64, ZSTD)' },
+};
+
+/* * */
+
+export const rideAnalysisExpectedVehicleEventIntervalTableSchema: ClickHouseTableSchema<RideAnalysisExpectedVehicleEventInterval> = {
+	...rideAnalysisBaseTableSchema,
+	observed_average_interval: { type: 'Nullable(UInt16) CODEC(T64, ZSTD)' },
+	observed_max_interval: { type: 'Nullable(UInt16) CODEC(T64, ZSTD)' },
+	observed_min_interval: { type: 'Nullable(UInt16) CODEC(T64, ZSTD)' },
+};
+
+/* * */
+
+export const rideAnalysisExpectedVehicleEventQtyTableSchema: ClickHouseTableSchema<RideAnalysisExpectedVehicleEventQty> = {
+	...rideAnalysisBaseTableSchema,
+	expected_vehicle_events_qty: { type: 'Nullable(UInt16) CODEC(T64, ZSTD)' },
+	observed_vehicle_events_qty: { type: 'Nullable(UInt16) CODEC(T64, ZSTD)' },
+};
+
+/* * */
+
+export const rideAnalysisExpectedVehicleIdQtyTableSchema: ClickHouseTableSchema<RideAnalysisExpectedVehicleIdQty> = {
+	...rideAnalysisBaseTableSchema,
+	observed_vehicle_ids_qty: { type: 'Nullable(UInt8) CODEC(T64, ZSTD)' },
+};
+
+/* * */
+
+export const rideAnalysisMatchingApexLocationsTableSchema: ClickHouseTableSchema<RideAnalysisMatchingApexLocations> = {
+	...rideAnalysisBaseTableSchema,
+	expected_apex_locations_qty: { type: 'Nullable(UInt16) CODEC(T64, ZSTD)' },
+	matching_apex_locations_qty: { type: 'Nullable(UInt16) CODEC(T64, ZSTD)' },
+	missing_apex_locations_qty: { type: 'Nullable(UInt16) CODEC(T64, ZSTD)' },
+};
+
+/* * */
+
+export const rideAnalysisMatchingVehicleIdsTableSchema: ClickHouseTableSchema<RideAnalysisMatchingVehicleIds> = {
+	...rideAnalysisBaseTableSchema,
+	extra_apex_vehicle_ids_qty: { type: 'Nullable(UInt8) CODEC(T64, ZSTD)' },
+	extra_vehicle_events_vehicle_ids_qty: { type: 'Nullable(UInt8) CODEC(T64, ZSTD)' },
+	matching_vehicle_ids_qty: { type: 'Nullable(UInt8) CODEC(T64, ZSTD)' },
+	total_vehicle_ids_qty: { type: 'Nullable(UInt8) CODEC(T64, ZSTD)' },
+};
+
+/* * */
+
+export const rideAnalysisSimpleOneApexValidationTableSchema: ClickHouseTableSchema<RideAnalysisSimpleOneApexValidation> = {
+	...rideAnalysisBaseTableSchema,
+};
+
+/* * */
+
+export const rideAnalysisSimpleOneVehicleEventOrApexValidationTableSchema: ClickHouseTableSchema<RideAnalysisSimpleOneVehicleEventOrApexValidation> = {
+	...rideAnalysisBaseTableSchema,
+};
+
+/* * */
+
+export const rideAnalysisSimpleThreeVehicleEventsTableSchema: ClickHouseTableSchema<RideAnalysisSimpleThreeVehicleEvents> = {
+	...rideAnalysisBaseTableSchema,
+	stop_ids_first: { type: 'Array(LowCardinality(String))' },
+	stop_ids_last: { type: 'Array(LowCardinality(String))' },
+	stop_ids_middle: { type: 'Array(LowCardinality(String))' },
+};
+
+/* * */
+
+export const rideAnalysisTransactionSequentialityTableSchema: ClickHouseTableSchema<RideAnalysisTransactionSequentiality> = {
+	...rideAnalysisBaseTableSchema,
+	expected_transactions_qty: { type: 'Nullable(UInt16) CODEC(T64, ZSTD)' },
+	found_transactions_qty: { type: 'Nullable(UInt16) CODEC(T64, ZSTD)' },
+	missing_transactions_qty: { type: 'Nullable(UInt16) CODEC(T64, ZSTD)' },
 };
 
 /* * */
@@ -50,14 +165,14 @@ export const ridesTableSchema: ClickHouseTableSchema<Ride> = {
 	_id: { type: 'String' },
 	agency_code: { type: 'LowCardinality(String)' },
 	agency_id: { type: 'LowCardinality(String)' },
-	apex_banking_taps_amount: { type: 'Nullable(UInt32) CODEC(T64, ZSTD)' },
-	apex_banking_taps_qty: { type: 'Nullable(UInt32) CODEC(T64, ZSTD)' },
-	apex_locations_qty: { type: 'Nullable(UInt32) CODEC(T64, ZSTD)' },
-	apex_refunds_amount: { type: 'Nullable(UInt32) CODEC(T64, ZSTD)' },
-	apex_refunds_qty: { type: 'Nullable(UInt32) CODEC(T64, ZSTD)' },
-	apex_sales_amount: { type: 'Nullable(UInt32) CODEC(T64, ZSTD)' },
-	apex_sales_qty: { type: 'Nullable(UInt32) CODEC(T64, ZSTD)' },
-	apex_validations_qty: { type: 'Nullable(UInt32) CODEC(T64, ZSTD)' },
+	apex_banking_taps_amount: { type: 'Nullable(Int32) CODEC(T64, ZSTD)' },
+	apex_banking_taps_qty: { type: 'Nullable(UInt16) CODEC(T64, ZSTD)' },
+	apex_locations_qty: { type: 'Nullable(UInt16) CODEC(T64, ZSTD)' },
+	apex_refunds_amount: { type: 'Nullable(Int32) CODEC(T64, ZSTD)' },
+	apex_refunds_qty: { type: 'Nullable(UInt16) CODEC(T64, ZSTD)' },
+	apex_sales_amount: { type: 'Nullable(Int32) CODEC(T64, ZSTD)' },
+	apex_sales_qty: { type: 'Nullable(UInt16) CODEC(T64, ZSTD)' },
+	apex_validations_qty: { type: 'Nullable(UInt16) CODEC(T64, ZSTD)' },
 	direction_id: { type: 'UInt8' },
 	driver_ids: { type: 'Array(LowCardinality(String))' },
 	end_time_observed: { type: 'Nullable(Int64) CODEC(DoubleDelta, ZSTD)' },
@@ -69,13 +184,13 @@ export const ridesTableSchema: ClickHouseTableSchema<Ride> = {
 	operational_date: { type: 'UInt32' },
 	passengers_estimated: { type: 'Nullable(UInt32) CODEC(T64, ZSTD)' },
 	passengers_observed: { type: 'Nullable(UInt32) CODEC(T64, ZSTD)' },
-	passengers_observed_banking_taps_amount: { type: 'Nullable(UInt32) CODEC(T64, ZSTD)' },
-	passengers_observed_banking_taps_qty: { type: 'Nullable(UInt32) CODEC(T64, ZSTD)' },
-	passengers_observed_prepaid_amount: { type: 'Nullable(UInt32) CODEC(T64, ZSTD)' },
-	passengers_observed_prepaid_qty: { type: 'Nullable(UInt32) CODEC(T64, ZSTD)' },
-	passengers_observed_sales_amount: { type: 'Nullable(UInt32) CODEC(T64, ZSTD)' },
-	passengers_observed_sales_qty: { type: 'Nullable(UInt32) CODEC(T64, ZSTD)' },
-	passengers_observed_subscription_qty: { type: 'Nullable(UInt32) CODEC(T64, ZSTD)' },
+	passengers_observed_banking_taps_amount: { type: 'Nullable(Int32) CODEC(T64, ZSTD)' },
+	passengers_observed_banking_taps_qty: { type: 'Nullable(UInt16) CODEC(T64, ZSTD)' },
+	passengers_observed_prepaid_amount: { type: 'Nullable(Int32) CODEC(T64, ZSTD)' },
+	passengers_observed_prepaid_qty: { type: 'Nullable(UInt16) CODEC(T64, ZSTD)' },
+	passengers_observed_sales_amount: { type: 'Nullable(Int32) CODEC(T64, ZSTD)' },
+	passengers_observed_sales_qty: { type: 'Nullable(UInt16) CODEC(T64, ZSTD)' },
+	passengers_observed_subscription_qty: { type: 'Nullable(UInt16) CODEC(T64, ZSTD)' },
 	plan_id: { type: 'LowCardinality(String)' },
 	processing_status: { type: 'LowCardinality(String)' },
 	route_color: { type: 'FixedString(6)' },
