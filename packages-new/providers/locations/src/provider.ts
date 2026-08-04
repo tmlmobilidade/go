ef9bc45c566607ba31c223a1158d73132d211f19
jusimen <jusi.monteiro@protonmail.com>
2026-08-04 12:54:04 +0100

    refactor(locations): enhance location retrieval methods and streamline data handling
    - Updated location retrieval methods to include findDistrictById, findLocalityById, findMunicipalityById, and findParishById for improved data access.
    - Simplified the findDistrictByGeo and findLocalityByGeo methods by removing unnecessary type assertions.
    - Reorganized the findLocationByGeo method to ensure a consistent order of location type retrieval.
    - Removed redundant methods for municipality and parish geo retrieval, enhancing code clarity.

--- packages-new/providers/locations/src/provider.ts
+++ packages-new/providers/locations/src/provider.ts
@@ -3,7 +3,7 @@
 import { HTTP_STATUS, HttpException } from '@tmlmobilidade/consts';
 import { goDb } from '@tmlmobilidade/go-interfaces-godb';
 import { type District, type Locality, type Municipality, type Parish } from '@tmlmobilidade/types';
-import { asyncSingletonProxy, flattenObject } from '@tmlmobilidade/utils';
+import { asyncSingletonProxy } from '@tmlmobilidade/utils';
 
 /* * */
 
@@ -19,25 +19,63 @@ class LocationsProviderClass {
 		return LocationsProviderClass._instance;
 	}
 
-	/*  Find By Geo */
-
 	async findDistrictByGeo(lat: number, lon: number): Promise<District | null> {
-		const district = await goDb.locations.districts.findOne(this.geoFilter(lat, lon) as any);
+		const district = await goDb.locations.districts.findOne(this.geoFilter(lat, lon));
 		if (!district) return null;
+		return { _id: district._id, ...district.properties };
+	}
+
+	async findDistrictById(id: string): Promise<District | null> {
+		const district = await goDb.locations.districts.findOne({ _id: id });
+		if (!district) return null;
+		return { _id: district._id, ...district.properties };
 	}
 
 	async findLocalityByGeo(lat: number, lon: number): Promise<Locality | null> {
-		return goDb.locations.localities.findOne(this.geoFilter(lat, lon) as any);
+		const locality = await goDb.locations.localities.findOne(this.geoFilter(lat, lon));
+		if (!locality) return null;
+		return { _id: locality._id, ...locality.properties };
 	}
 
+	async findLocalityById(id: string): Promise<Locality | null> {
+		const locality = await goDb.locations.localities.findOne({ _id: id });
+		if (!locality) return null;
+		return { _id: locality._id, ...locality.properties };
+	}
+
+	async findMunicipalityByGeo(lat: number, lon: number): Promise<Municipality | null> {
+		const municipality = await goDb.locations.municipalities.findOne(this.geoFilter(lat, lon));
+		if (!municipality) return null;
+		return { _id: municipality._id, ...municipality.properties };
+	}
+
+	async findMunicipalityById(id: string): Promise<Municipality | null> {
+		const municipality = await goDb.locations.municipalities.findOne({ _id: id });
+		if (!municipality) return null;
+		return { _id: municipality._id, ...municipality.properties };
+	}
+
+	async findParishByGeo(lat: number, lon: number): Promise<null | Parish> {
+		const parish = await goDb.locations.parishes.findOne(this.geoFilter(lat, lon));
+		if (!parish) return null;
+		return { _id: parish._id, ...parish.properties };
+	}
+
+	async findParishById(id: string): Promise<null | Parish> {
+		const parish = await goDb.locations.parishes.findOne({ _id: id });
+		if (!parish) return null;
+		return { _id: parish._id, ...parish.properties };
+	}
+
+	// Find Location by Geo
 	async findLocationByGeo(lat: number, lon: number) {
 		if (!lat || !lon) throw new HttpException(HTTP_STATUS.BAD_REQUEST, 'Missing latitude or longitude');
 
-		const [municipality, parish, district, locality] = await Promise.all([
-			this.findMunicipalityByGeo(lat, lon),
-			this.findParishByGeo(lat, lon),
+		const [district, locality, municipality, parish] = await Promise.all([
 			this.findDistrictByGeo(lat, lon),
 			this.findLocalityByGeo(lat, lon),
+			this.findMunicipalityByGeo(lat, lon),
+			this.findParishByGeo(lat, lon),
 		]);
 
 		return {
@@ -48,14 +86,6 @@ class LocationsProviderClass {
 			municipality,
 			parish,
 		};
-	}
-
-	async findMunicipalityByGeo(lat: number, lon: number): Promise<Municipality | null> {
-		return goDb.locations.municipalities.findOne(this.geoFilter(lat, lon) as any);
-	}
-
-	async findParishByGeo(lat: number, lon: number): Promise<null | Parish> {
-		return goDb.locations.parishes.findOne(this.geoFilter(lat, lon) as any);
 	}
 
 	/*  Private */
