c4a4d3da91973053df1bc2155f8faae5b67a16e9
jusimen <jusi.monteiro@protonmail.com>
2026-08-05 02:08:11 +0100

    feat(stops): enhance StopCreateStepNames component with input fields
    - Added input fields for full name, short name, and TTS name in the StopCreateStepNames component to improve user input for stop creation.
    - Implemented ContextFormController for better form handling and validation.
    - Updated TextInput components with descriptions and error handling for a more informative user experience.

--- modules/stops/apps/frontend/src/components/stops/create/StopCreateStepNames/index.tsx
+++ modules/stops/apps/frontend/src/components/stops/create/StopCreateStepNames/index.tsx
@@ -1,10 +1,64 @@
+/* * */
+
+import { ContextFormController, Divider, Section, TextInput } from '@tmlmobilidade/ui';
+
+import { useStopCreateContext } from '../StopCreate.context';
+
+/* * */
+
 export function StopCreateStepNames() {
 	//
+	// A. Setup variables
+
+	const stopCreateContext = useStopCreateContext();
 
 	//
-	// A. Render components
+	// B. Render components
 
 	return (
-		<div>StopCreateStepNames</div>
+		<Section gap="sm" width="100%">
+			<ContextFormController
+				control={stopCreateContext.form.instance.control}
+				name="name"
+				render={({ field, fieldState }) => (
+					<TextInput
+						description="Este é o nome principal e será apresentado nos canais digitais."
+						error={fieldState.error?.message}
+						label="Designação Completa da Paragem"
+						onChange={field.onChange}
+						value={field.value ?? ''}
+						w="100%"
+						data-autofocus
+						required
+					/>
+				)}
+			/>
+			<ContextFormController
+				control={stopCreateContext.form.instance.control}
+				name="short_name"
+				render={({ field }) => (
+					<TextInput
+						description="Esta versão abreviada automaticamente será utilizada em suportes com limitações de espaço, como postaletes e horários impressos."
+						label="Nome Curto (automático)"
+						value={field.value ?? ''}
+						w="100%"
+						readOnly
+					/>
+				)}
+			/>
+			<ContextFormController
+				control={stopCreateContext.form.instance.control}
+				name="tts_name"
+				render={({ field }) => (
+					<TextInput
+						description="O nome a ser utilizado pelo sistema de TTS (Text-to-Speech)."
+						label="Nome TTS (automático)"
+						value={field.value ?? ''}
+						w="100%"
+						readOnly
+					/>
+				)}
+			/>
+		</Section>
 	);
 }
