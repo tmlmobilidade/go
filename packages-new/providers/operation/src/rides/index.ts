abeef0ae76a2bc9fac265a8839b667b2dc868521
João de Vasconcelos <10943992+joao-vasconcelos@users.noreply.github.com>
2026-08-10 18:16:28 +0100

    refactor(rides): remove find-hashed-trip functions to streamline codebase
    * Deleted the `find-hashed-trip-by-id.ts` and `find-hashed-trip-by-ride-id.ts` files to eliminate unused code and improve maintainability.
    * Updated the `index.ts` file to reflect these removals, ensuring a cleaner export structure.
    * This cleanup aims to enhance code clarity by removing obsolete components from the rides module.

--- packages-new/providers/operation/src/rides/index.ts
+++ packages-new/providers/operation/src/rides/index.ts
@@ -1,7 +1,5 @@
 /* * */
 
-import { findHashedTripById } from './find-hashed-trip-by-id.js';
-import { findHashedTripByRideId } from './find-hashed-trip-by-ride-id.js';
 import { findRideById } from './find-ride-by-id.js';
 import { findRides } from './find-rides.js';
 import { updateRideById } from './update-ride-by-id.js';
@@ -10,8 +8,6 @@ import { updateRides } from './update-rides.js';
 /* * */
 
 export const ridesProvider = {
-	findHashedTripById,
-	findHashedTripByRideId,
 	findRideById,
 	findRides,
 	updateRideById,
