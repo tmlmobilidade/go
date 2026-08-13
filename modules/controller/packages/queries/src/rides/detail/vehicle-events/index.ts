b394b4bab54044b8c90ce103af0c9e2481f662ad
João de Vasconcelos <10943992+joao-vasconcelos@users.noreply.github.com>
2026-08-13 17:43:30 +0100

    refactor(rides): restructure ride detail components and update exports
    * Updated the export path for ride details to reflect the new directory structure, enhancing modularity.
    * Removed several unused files related to ride detail functionality, streamlining the codebase and improving maintainability.

---
+++ modules/controller/packages/queries/src/rides/detail/vehicle-events/index.ts
@@ -0,0 +1,4 @@
+export * from './controller-rides-detail-endpoint.js';
+export * from './controller-rides-detail-filters.js';
+export * from './controller-rides-detail-item.js';
+export * from './controller-rides-detail-query.js';
