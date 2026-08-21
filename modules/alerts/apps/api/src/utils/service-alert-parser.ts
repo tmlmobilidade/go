09c55c8cab5213a7b7d21716a14bc03ffff37af1
LoudNeo <meliodasgamer077@gmail.com>
2026-08-21 11:30:27 +0100

    refactor(imports): add new type import and update existing ones in service-alert-parser
    * Added `Attachment` type import from `@tmlmobilidade/go-types-core` to `service-alert-parser.ts` for improved type clarity.
    * Removed the previous import of `Attachment` from `@tmlmobilidade/types` to align with updated type definitions.
    * Updated `package.json` to include `@tmlmobilidade/go-types-core` as a dependency, ensuring consistent usage of the latest type definitions across the module.

--- modules/alerts/apps/api/src/utils/service-alert-parser.ts
+++ modules/alerts/apps/api/src/utils/service-alert-parser.ts
@@ -1,8 +1,8 @@
 import { type Line } from '@carrismetropolitana/api-types/network';
 import { HTTP_STATUS, HttpException } from '@tmlmobilidade/consts';
 import { storageProvider } from '@tmlmobilidade/go-providers-storage';
+import { type Attachment } from '@tmlmobilidade/go-types-core';
 import { type Alert } from '@tmlmobilidade/go-types-operation';
-import { type Attachment } from '@tmlmobilidade/types';
 import { type ServiceAlertResponseItem } from '@tmlmobilidade/types';
 import { type EntitySelector } from 'gtfs-types';
 
