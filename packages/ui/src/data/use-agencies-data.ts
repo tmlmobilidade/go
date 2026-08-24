b84fc81e00efdad9bd01a3215e99abd95f97a35e
João de Vasconcelos <10943992+joao-vasconcelos@users.noreply.github.com>
2026-08-24 22:59:32 +0100

    refactor(tags): update import paths for AgenciesPlatform types
    * Changed import paths for `AgenciesPlatformRequest` and `AgenciesPlatformResponse` from `@tmlmobilidade/go-types-core` to `@tmlmobilidade/go-types-platform` to align with the new type definitions.
    * Ensured consistency in type usage across the `AgencyTag` and `use-agencies-data` files for improved maintainability.

--- packages/ui/src/data/use-agencies-data.ts
+++ packages/ui/src/data/use-agencies-data.ts
@@ -1,7 +1,7 @@
 'use client';
 
 import { API_ROUTES } from '@tmlmobilidade/consts';
-import { type AgenciesPlatformRequest, type AgenciesPlatformResponse } from '@tmlmobilidade/go-types-core';
+import { type AgenciesPlatformRequest, type AgenciesPlatformResponse } from '@tmlmobilidade/go-types-platform';
 import { type ApiResponse, type UnixTimestamp } from '@tmlmobilidade/go-types-shared';
 import { useMemo } from 'react';
 import useSWR from 'swr';
