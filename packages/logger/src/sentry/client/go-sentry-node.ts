92ab1639b47905f994db377da09b05006adb7036
LoudNeo <meliodasgamer077@gmail.com>
2026-08-24 10:20:36 +0100

    refactor(sentry): update Sentry client environment validation
    * Modified the `getSentryClient` function to return null if the environment is not production, enhancing the handling of environment-specific configurations.
    * Added comments to clarify the purpose of the environment variable validation, improving code readability.

--- packages/logger/src/sentry/client/go-sentry-node.ts
+++ packages/logger/src/sentry/client/go-sentry-node.ts
@@ -2,7 +2,14 @@ export function getSentryClient(): string {
 	//
 
 	//
-	// Validate required environment variables
+	// If the environment is not production, return null.
+
+	if (process.env.environment !== 'production') {
+		return null;
+	}
+
+	//
+	// Validate required environment variables.
 
 	if (!process.env.SENTRY_NODE_DSN) {
 		throw new Error('Missing SENTRY_NODE_DSN, please set the SENTRY_NODE_DSN environment variable for this work my friend!');
