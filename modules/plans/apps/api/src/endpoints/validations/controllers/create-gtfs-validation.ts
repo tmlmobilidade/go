c9c5d3cf9308e0cc7488a3ee206bfd84fdf22596
João de Vasconcelos <10943992+joao-vasconcelos@users.noreply.github.com>
2026-08-28 11:19:06 +0100

    refactor(validation): enhance GTFS validation logic and update imports
    * Updated the GTFS validation controller to include the `GtfsValidationSchema` for improved validation handling.
    * Reorganized imports to include `Dates` from `@tmlmobilidade/go-utils-dates`, enhancing clarity and consistency in the codebase.

--- modules/plans/apps/api/src/endpoints/validations/controllers/create-gtfs-validation.ts
+++ modules/plans/apps/api/src/endpoints/validations/controllers/create-gtfs-validation.ts
@@ -1,13 +1,13 @@
 /* * */
 
 import { HTTP_STATUS, HttpException } from '@tmlmobilidade/consts';
-import { Dates } from '@tmlmobilidade/dates';
 import { type FastifyReply, type FastifyRequest, sendErrorApiResponse, sendSuccessApiResponse } from '@tmlmobilidade/go-clients-fastify';
 import { goDb } from '@tmlmobilidade/go-interfaces-godb';
 import { storageProvider } from '@tmlmobilidade/go-providers-storage';
 import { type GtfsAgency, type GtfsFeedInfo } from '@tmlmobilidade/go-types-gtfs';
-import { type CreateGtfsValidationDto, type GtfsValidation } from '@tmlmobilidade/go-types-operation';
+import { type CreateGtfsValidationDto, type GtfsValidation, GtfsValidationSchema } from '@tmlmobilidade/go-types-operation';
 import { PermissionCatalog } from '@tmlmobilidade/go-types-permissions';
+import { Dates } from '@tmlmobilidade/go-utils-dates';
 import { generateRandomString } from '@tmlmobilidade/strings';
 import { createWriteStream } from 'fs';
 import { readFileSync, unlinkSync } from 'node:fs';
@@ -115,13 +115,13 @@ export async function createGtfsValidation(request: FastifyRequest, reply: Fasti
 			},
 			{
 				onSuccess: async (_ctx, attachment, session) => {
-					finalValidationData = await goDb.operation.gtfsValidations.insertOne({
+					const validatedGtfsValidation = GtfsValidationSchema.parse({
 						...validationData,
 						_id: validationId,
 						file_id: attachment._id,
-					}, {
-						options: { session },
 					});
+
+					finalValidationData = await goDb.operation.gtfsValidations.insertOne(validatedGtfsValidation, { session });
 				},
 			},
 		);
