f56bb1ffbc647a8eaaa504ac935009f59adb14a5
João de Vasconcelos <10943992+joao-vasconcelos@users.noreply.github.com>
2026-08-29 11:44:59 +0100

    Add geo module and update exports in index.ts

--- packages/geo/src/index.ts
+++ packages/geo/src/index.ts
@@ -9,4 +9,5 @@ export * from '@/hashed-shapes-to-geojson.js';
 export * from '@/is-point-in-polygon.js';
 export * from '@/measurements.js';
 export * from '@/polyline.js';
+export * from '@/vehicle-event-to-geojson.js';
 export * from '@turf/turf';
