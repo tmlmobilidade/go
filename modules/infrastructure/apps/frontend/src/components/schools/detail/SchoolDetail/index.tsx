7fcc042a4cbf6941d9ce75e597a56ca83f80665b
Bruno Castelo <brunoalexcastelo@gmail.com>
2026-08-31 10:36:20 +0100

    feat(schools): add location types and clean up imports in SchoolDetail component
    * Added `@tmlmobilidade/go-types-location` to `package.json` for improved type management.
    * Cleaned up redundant imports in the `SchoolDetail` component, enhancing code clarity and maintainability.

--- modules/infrastructure/apps/frontend/src/components/schools/detail/SchoolDetail/index.tsx
+++ modules/infrastructure/apps/frontend/src/components/schools/detail/SchoolDetail/index.tsx
@@ -1,14 +1,14 @@
 'use client';
 
+import { SchoolDetailFootnote } from '@/components/schools/detail/SchoolDetailFootnote';
+import { SchoolDetailHeader } from '@/components/schools/detail/SchoolDetailHeader';
+import { useSchoolsDetailFormContext } from '@/components/schools/detail/SchoolsDetailForm.context';
 import { SchoolDetailSectionAddress } from '@/components/schools/detail/sections/SchoolDetailSectionAddress';
 import { SchoolDetailSectionAdministrative } from '@/components/schools/detail/sections/SchoolDetailSectionAdministrative';
 import { SchoolDetailSectionEducation } from '@/components/schools/detail/sections/SchoolDetailSectionEducation';
 import { SchoolDetailSectionGeneral } from '@/components/schools/detail/sections/SchoolDetailSectionGeneral';
 import { SchoolDetailSectionOperations } from '@/components/schools/detail/sections/SchoolDetailSectionOperations';
 import { SchoolDetailSectionOrganization } from '@/components/schools/detail/sections/SchoolDetailSectionOrganization';
-import { SchoolDetailFootnote } from '@/components/schools/detail/SchoolDetailFootnote';
-import { SchoolDetailHeader } from '@/components/schools/detail/SchoolDetailHeader';
-import { useSchoolsDetailFormContext } from '@/components/schools/detail/SchoolsDetailForm.context';
 import { useSchoolsDetailSchoolData } from '@/components/schools/detail/use-schools-detail-school-data';
 import { ErrorDisplay, LoadingOverlay, Pane } from '@tmlmobilidade/ui';
 
