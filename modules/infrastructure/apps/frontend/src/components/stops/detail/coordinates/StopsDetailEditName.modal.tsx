46790c5bfab4b39f8eb33078010538186e459bd7
João de Vasconcelos <10943992+joao-vasconcelos@users.noreply.github.com>
2026-08-31 23:59:56 +0100

    Remove obsolete StopDetail coordinate components and related logic
    * Deleted StopDetailCoordinatesMap, StopDetailCoordinatesModal, StopDetailCoordinatesSelect, and associated styles to streamline the codebase.
    * Removed unused imports and references to these components in the StopsDetail context, enhancing maintainability.
    * This cleanup improves clarity and reduces complexity in the stops detail section of the application.

---
+++ modules/infrastructure/apps/frontend/src/components/stops/detail/coordinates/StopsDetailEditName.modal.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import { closeModal, openModal } from '@tmlmobilidade/ui';
+
+import { StopsDetailEditNameFormContextProvider } from './StopsDetailEditNameForm.context';
+import { StopsDetailEditNameModal } from './StopsDetailEditNameModal';
+
+/* * */
+
+const MODAL_ID = 'stops-detail-section-general-edit-name-modal';
+
+/* * */
+
+export const openStopsDetailEditNameModal = () => {
+	openModal({
+		children: (
+			<StopsDetailEditNameFormContextProvider>
+				<StopsDetailEditNameModal />
+			</StopsDetailEditNameFormContextProvider>
+		),
+		closeOnClickOutside: false,
+		closeOnEscape: false,
+		modalId: MODAL_ID,
+		padding: 0,
+		size: 'xl',
+		withCloseButton: false,
+	});
+};
+
+/* * */
+
+export const closeStopsDetailEditNameModal = () => {
+	closeModal(MODAL_ID);
+};
---
+++ modules/infrastructure/apps/frontend/src/components/stops/detail/coordinates/StopsDetailEditName/index.tsx
@@ -0,0 +1,48 @@
+'use client';
+
+import { hasPermissionResource } from '@tmlmobilidade/go-types-permissions';
+import { Inline, useMeData, ValueDisplay } from '@tmlmobilidade/ui';
+import { useMemo } from 'react';
+
+import { useStopsDetailFormContext } from '../../StopsDetailForm.context';
+import { useStopsDetailData } from '../../use-stops-detail-data';
+import { openStopsDetailEditNameModal } from '../StopsDetailEditName.modal';
+
+/* * */
+
+export function StopsDetailEditName() {
+	//
+
+	//
+	// A. Setup variables
+
+	const { data } = useStopsDetailData();
+
+	const { data: meData } = useMeData();
+
+	const { capabilities } = useStopsDetailFormContext();
+
+	//
+	// B. Transform data
+
+	const canEditName = useMemo(() => {
+		const hasPermission = hasPermissionResource(meData?.permissions, {
+			requiredPermission: { action: 'edit_name', scope: 'stops' },
+			requiredValue: data?.municipality_id,
+			resourceKey: 'municipality_ids',
+		});
+		return hasPermission && !capabilities.updateEnabled;
+	}, [data?.municipality_id, meData?.permissions, capabilities.updateEnabled]);
+
+	//
+	// C. Render components
+
+	return (
+		<ValueDisplay
+			footer={canEditName && <Inline onClick={openStopsDetailEditNameModal} dotted>Editar</Inline>}
+			label="Nome Único da Paragem"
+			value={data?.name ?? 'N/A'}
+			variant="bordered"
+		/>
+	);
+}
