a1e8797e28d92c1944b76e11dc33a639f609a4a1
João de Vasconcelos <10943992+joao-vasconcelos@users.noreply.github.com>
2026-09-01 20:06:17 +0100

    Enhance setPlanStatus function to include timestamp and reset last_hash
    * Updated the `setPlanStatus` function to set the `last_hash` to null and add a timestamp using the `Dates` utility, improving data tracking for plan status updates.

--- modules/operation/apps/rides-feeder/src/set-plan-status.ts
+++ modules/operation/apps/rides-feeder/src/set-plan-status.ts
@@ -2,6 +2,7 @@
 
 import { goDb } from '@tmlmobilidade/go-interfaces-godb';
 import { type ProcessingStatus } from '@tmlmobilidade/go-types-shared';
+import { Dates } from '@tmlmobilidade/go-utils-dates';
 
 /* * */
 
@@ -12,6 +13,12 @@ export async function setPlanStatus(planId: string, status: ProcessingStatus) {
 
 	await plansCollection.updateOne(
 		{ _id: { $eq: planId } },
-		{ $set: { 'apps.controller.status': status } },
+		{
+			$set: {
+				'apps.controller.last_hash': null,
+				'apps.controller.status': status,
+				'apps.controller.timestamp': Dates.now('Europe/Lisbon').unix_milliseconds,
+			},
+		},
 	);
 };
