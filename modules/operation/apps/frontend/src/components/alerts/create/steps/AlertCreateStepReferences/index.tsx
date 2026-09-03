5fff90758fc1f05ef62680d20afdc5f69e602f0e
João de Vasconcelos <10943992+joao-vasconcelos@users.noreply.github.com>
2026-09-03 19:11:41 +0100

    Refactor alert handling to utilize catalog structure
    * Updated alert filtering logic in multiple components to reference `matchingAgencyData.alerts.catalog` instead of `matchingAgencyData.alerts_map`, ensuring consistency in data access for alert creation and detail sections.
    * Adjusted the mapping of causes, effects, and references to align with the new catalog structure, improving the overall clarity and maintainability of the alert creation process.

--- modules/operation/apps/frontend/src/components/alerts/create/steps/AlertCreateStepReferences/index.tsx
+++ modules/operation/apps/frontend/src/components/alerts/create/steps/AlertCreateStepReferences/index.tsx
@@ -51,7 +51,7 @@ export function AlertCreateStepReferences() {
 		// Map to the format needed for rendering the buttons
 		// and sort alphabetically by label.
 		return AlertReferenceTypeValues
-			.filter(referenceTypeValue => !!matchingAgencyData.alerts_map?.[causeValue]?.[effectValue]?.[referenceTypeValue])
+			.filter(referenceTypeValue => !!matchingAgencyData.alerts.catalog?.[causeValue]?.[effectValue]?.[referenceTypeValue])
 			.sort((a, b) => a.localeCompare(b));
 	}, [agenciesData, agencyIdValue, causeValue, effectValue]);
 
