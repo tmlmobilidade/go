ef05c8c98dec0a96f44162277bbcc1151207f16f
João de Vasconcelos <10943992+joao-vasconcelos@users.noreply.github.com>
2026-09-03 13:19:25 +0100

    Update agency data retrieval in buildAgencyTxt function
    * Changed the source of agency email, phone, fare URL, and website URL from `contacts` to `details` in the `buildAgencyTxt` function, ensuring accurate data mapping from the agencyData structure.

--- modules/operation/apps/organizer/src/tasks/plans/normalize-plans/agency.ts
+++ modules/operation/apps/organizer/src/tasks/plans/normalize-plans/agency.ts
@@ -14,14 +14,14 @@ export function buildAgencyTxt(agencyData: Agency): string {
 	// Build and validate the agency row.
 
 	const agencyRow: GtfsStrictV30Agency = {
-		agency_email: agencyData.open_data?.contacts?.email,
-		agency_fare_url: agencyData.open_data?.contacts?.fare_url,
+		agency_email: agencyData.open_data?.details?.email,
+		agency_fare_url: agencyData.open_data?.details?.fare_url,
 		agency_id: agencyData._id,
 		agency_lang: agencyData.primary_language,
 		agency_name: agencyData.name,
-		agency_phone: agencyData.open_data?.contacts?.phone,
+		agency_phone: agencyData.open_data?.details?.phone,
 		agency_timezone: agencyData.timezone,
-		agency_url: agencyData.open_data?.contacts?.website_url,
+		agency_url: agencyData.open_data?.details?.website_url,
 	};
 
 	const validatedAgencyRow = GtfsStrictV30AgencySchema.parse(agencyRow);
