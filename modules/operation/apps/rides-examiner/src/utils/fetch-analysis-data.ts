97541b8f6d161cc6c237ffe39d458e42a721289b
jusimen <jusi.monteiro@protonmail.com>
2026-09-04 16:58:52 +0100

    Refactor fetchAnalysisData to use dynamic SQL parameters
    * Updated SQL queries in the fetchAnalysisData function to utilize dynamic parameters for operational date filtering, enhancing code maintainability and readability.
    * Removed the need for a separate operational date range string, streamlining the query construction process for improved performance and clarity.

--- modules/operation/apps/rides-examiner/src/utils/fetch-analysis-data.ts
+++ modules/operation/apps/rides-examiner/src/utils/fetch-analysis-data.ts
@@ -21,19 +21,20 @@ export async function fetchAnalysisData(rideData: Ride): Promise<AnalysisData> {
 	const minOperationalDate = Dates.fromUnixMilliseconds(standardWindowInterval.start).operational_date_int;
 	const maxOperationalDate = Dates.fromUnixMilliseconds(standardWindowInterval.end).operational_date_int;
 	const operationalDateRange = OperationalDateIntSchema.array().parse(Array.from({ length: maxOperationalDate - minOperationalDate + 1 }, (_, i) => minOperationalDate + i));
-	const operationalDateRangeString = operationalDateRange.join(',');
 
 	//
 	// Fetch data from LabDB in parallel.
+	const dynamicWhere = `operational_date IN (${operationalDateRange.join(',')}) AND created_at >= $1 AND created_at <= $2 AND agency_id = $3 AND trip_id = $4`;
+	const dynamicParams = { 1: standardWindowInterval.start, 2: standardWindowInterval.end, 3: rideData.agency_id, 4: rideData.trip_id };
 
 	const hashedTripPromise = labDb.operation.hashedTrips.select('*', '_id = $1', { 1: rideData.hashed_trip_id });
 	const hashedShapePromise = labDb.operation.hashedShapes.select('*', '_id = $1', { 1: rideData.hashed_shape_id });
-	const simplifiedApexBankingTapsPromise = labDb.simplifiedApex.bankingTaps.select('*', `operational_date IN ($5) AND created_at >= $1 AND created_at <= $2 AND agency_id = $3 AND trip_id = $4`, { 1: standardWindowInterval.start, 2: standardWindowInterval.end, 3: rideData.agency_id, 4: rideData.trip_id, 5: operationalDateRangeString });
-	const simplifiedApexLocationsPromise = labDb.simplifiedApex.locations.select('*', `operational_date IN ($5) AND created_at >= $1 AND created_at <= $2 AND agency_id = $3 AND trip_id = $4`, { 1: standardWindowInterval.start, 2: standardWindowInterval.end, 3: rideData.agency_id, 4: rideData.trip_id, 5: operationalDateRangeString });
-	const simplifiedApexOnBoardRefundsPromise = labDb.simplifiedApex.refunds.select('*', `operational_date IN ($5) AND created_at >= $1 AND created_at <= $2 AND agency_id = $3 AND trip_id = $4`, { 1: standardWindowInterval.start, 2: standardWindowInterval.end, 3: rideData.agency_id, 4: rideData.trip_id, 5: operationalDateRangeString });
-	const simplifiedApexOnBoardSalesPromise = labDb.simplifiedApex.sales.select('*', `operational_date IN ($5) AND created_at >= $1 AND created_at <= $2 AND agency_id = $3 AND trip_id = $4`, { 1: standardWindowInterval.start, 2: standardWindowInterval.end, 3: rideData.agency_id, 4: rideData.trip_id, 5: operationalDateRangeString });
-	const simplifiedApexValidationsPromise = labDb.simplifiedApex.validations.select('*', `operational_date IN ($5) AND created_at >= $1 AND created_at <= $2 AND agency_id = $3 AND trip_id = $4`, { 1: standardWindowInterval.start, 2: standardWindowInterval.end, 3: rideData.agency_id, 4: rideData.trip_id, 5: operationalDateRangeString });
-	const vehicleEventsPromise = labDb.operation.simplifiedVehicleEvents.select('*', `operational_date IN ($5) AND created_at >= $1 AND created_at <= $2 AND agency_id = $3 AND trip_id = $4 AND extra_trip_id IS NULL`, { 1: standardWindowInterval.start, 2: standardWindowInterval.end, 3: rideData.agency_id, 4: rideData.trip_id, 5: operationalDateRangeString });
+	const simplifiedApexBankingTapsPromise = labDb.simplifiedApex.bankingTaps.select('*', dynamicWhere, dynamicParams);
+	const simplifiedApexLocationsPromise = labDb.simplifiedApex.locations.select('*', dynamicWhere, dynamicParams);
+	const simplifiedApexOnBoardRefundsPromise = labDb.simplifiedApex.refunds.select('*', dynamicWhere, dynamicParams);
+	const simplifiedApexOnBoardSalesPromise = labDb.simplifiedApex.sales.select('*', dynamicWhere, dynamicParams);
+	const simplifiedApexValidationsPromise = labDb.simplifiedApex.validations.select('*', dynamicWhere, dynamicParams);
+	const vehicleEventsPromise = labDb.operation.simplifiedVehicleEvents.select('*', `${dynamicWhere} AND extra_trip_id IS NULL`, dynamicParams);
 
 	const [
 		hashedTripData,
