1c51bc9e5db2ad0776507683c4d8b52520db773c
João de Vasconcelos <10943992+joao-vasconcelos@users.noreply.github.com>
2026-09-05 19:03:37 +0100

    Refactor GTFS file retrieval logic for improved error handling and response headers
    * Removed unnecessary logging and streamlined error handling when fetching the GTFS file from the storage service, returning a simplified error response.
    * Updated response headers to ensure proper content disposition and caching behavior, enhancing the file download experience for clients.

--- modules/hub/apps/api/src/endpoints/v1/plans/controllers/get-gtfs.ts
+++ modules/hub/apps/api/src/endpoints/v1/plans/controllers/get-gtfs.ts
@@ -3,7 +3,6 @@
 import { HTTP_STATUS, HttpException } from '@tmlmobilidade/consts';
 import { type FastifyReply, type FastifyRequest } from '@tmlmobilidade/go-clients-fastify';
 import { storageProvider } from '@tmlmobilidade/go-providers-storage';
-import { Logger } from '@tmlmobilidade/logger';
 
 /**
  * Download the latest GTFS merged file.
@@ -16,26 +15,16 @@ export async function getGtfs(request: FastifyRequest, reply: FastifyReply<strin
 	if (!foundFileData) throw new HttpException(HTTP_STATUS.NOT_FOUND, 'File not found');
 	// Stream the file in the given URL to the client
 	const storageServiceResponse = await fetch(foundFileData.url);
-	if (!storageServiceResponse.ok || !storageServiceResponse.body) {
-		Logger.error({ message: `[hub/v1/plans:getGtfs()] Failed to fetch file from storage service. URL: ${foundFileData.url}, Status: ${storageServiceResponse.status}` });
-		return reply
-			.header('access-control-allow-origin', '*')
-			.header('cache-control', 'public, max-age=300')
-			.code(HTTP_STATUS.INTERNAL_SERVER_ERROR)
-			.send({
-				data: null,
-				error: 'Could not fetch file from storage service.',
-				status_code: HTTP_STATUS.INTERNAL_SERVER_ERROR,
-			});
-	}
+	if (!storageServiceResponse.ok || !storageServiceResponse.body) return reply.code(500).send('Could not fetch file.');
 	// Set headers and pipe the response body to the client
 	reply.header('access-control-allow-origin', '*');
-	reply.header('cache-control', 'public, max-age=300');
-	reply.header('Content-Disposition', `attachment; filename="gtfs-latest.zip"`);
-	reply.header('Content-Type', 'application/zip');
+	reply.header('content-disposition', `attachment; filename="gtfs-latest.zip"`);
+	reply.header('content-type', 'application/zip');
+	reply.header('cache-control', 'no-store'); // Disable nginx and client caching
+	reply.header('X-Accel-Buffering', 'no'); // Disable nginx buffering to memory
 	// Set content length if available
-	const contentLength = storageServiceResponse.headers.get('Content-Length');
-	if (contentLength) reply.header('Content-Length', contentLength);
+	const contentLength = storageServiceResponse.headers.get('content-length');
+	if (contentLength) reply.header('content-length', contentLength);
 	// Pipe the response body to the client
 	return reply.send(storageServiceResponse.body);
 }
