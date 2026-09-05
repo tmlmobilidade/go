cbf86ef2019e24ce7e47d76846ec19ab01c58a09
João de Vasconcelos <10943992+joao-vasconcelos@users.noreply.github.com>
2026-09-05 20:57:52 +0100

    Refactor network endpoint imports and remove obsolete controller files
    * Updated import paths in network routes to use the new handlers directory, improving code organization.
    * Deleted outdated controller files for lines, patterns, routes, shapes, and stops, streamlining the codebase and removing unused code.

--- modules/hub/apps/api/src/endpoints/v1/network/routes.ts
+++ modules/hub/apps/api/src/endpoints/v1/network/routes.ts
@@ -1,10 +1,10 @@
 /* * */
 
-import { getLines } from '@/endpoints/v1/network/controllers/get-lines.js';
-import { getPattern } from '@/endpoints/v1/network/controllers/get-pattern.js';
-import { getRoutes } from '@/endpoints/v1/network/controllers/get-routes.js';
-import { getShape } from '@/endpoints/v1/network/controllers/get-shape.js';
-import { getStops } from '@/endpoints/v1/network/controllers/get-stops.js';
+import { getLines } from '@/endpoints/v1/network/handlers/get-lines.js';
+import { getPattern } from '@/endpoints/v1/network/handlers/get-pattern.js';
+import { getRoutes } from '@/endpoints/v1/network/handlers/get-routes.js';
+import { getShape } from '@/endpoints/v1/network/handlers/get-shape.js';
+import { getStops } from '@/endpoints/v1/network/handlers/get-stops.js';
 import { type FastifyInstance, FastifyService } from '@tmlmobilidade/go-clients-fastify';
 
 /* * */
