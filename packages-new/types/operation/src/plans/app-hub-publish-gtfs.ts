393b6242bd76594614358ac6741c5b70214440fa
João de Vasconcelos <10943992+joao-vasconcelos@users.noreply.github.com>
2026-09-05 23:36:43 +0100

    Refactor GTFS publish plan schema to remove unused fields
    * Removed the last_hash and metadata_hash fields from the PlanAppHubPublishGtfsSchema, simplifying the schema and improving data clarity.
    * This change aligns the schema with current requirements and enhances validation efficiency.

--- packages-new/types/operation/src/plans/app-hub-publish-gtfs.ts
+++ packages-new/types/operation/src/plans/app-hub-publish-gtfs.ts
@@ -6,9 +6,7 @@ import { z } from 'zod';
 /* * */
 
 export const PlanAppHubPublishGtfsSchema = z.object({
-	last_hash: z.string().nullable().default(null),
 	message: z.string().nullable().default(null),
-	metadata_hash: z.string().nullable().default(null),
 	status: ProcessingStatusSchema.default('waiting'),
 	timestamp: UnixMillisecondsSchema.nullable().default(null),
 }).default({});
