c0f6db980fac0491eb5078eafb6befa26c5c2e22
João de Vasconcelos <10943992+joao-vasconcelos@users.noreply.github.com>
2026-09-05 20:37:20 +0100

    Enhance Hub GTFS trips schema with detailed validation
    * Updated the HubV1GtfsTripsSchema to include additional fields such as bikes_allowed, cars_allowed, direction_id, and trip_headsign, improving the schema's validation and comprehensiveness.
    * Replaced the previous GtfsTripsSchema import with a new z.object definition, ensuring better data integrity for GTFS trip exports.

--- packages-new/types/hub/src/v1/gtfs/trips.ts
+++ packages-new/types/hub/src/v1/gtfs/trips.ts
@@ -1,11 +1,24 @@
 /* * */
 
-import { GtfsTripsSchema } from '@tmlmobilidade/go-types-gtfs';
+import { GtfsTernarySchema, GtfsTripDirectionSchema } from '@tmlmobilidade/go-types-gtfs';
 import { z } from 'zod';
 
 /* * */
 
-export const HubV1GtfsTripsSchema = GtfsTripsSchema;
+export const HubV1GtfsTripsSchema = z.object({
+	bikes_allowed: GtfsTernarySchema.default('0'),
+	block_id: z.string().optional(),
+	cars_allowed: GtfsTernarySchema.default('0'),
+	direction_id: GtfsTripDirectionSchema,
+	pattern_id: z.string(),
+	route_id: z.string(),
+	service_id: z.string(),
+	shape_id: z.string(),
+	trip_headsign: z.string().default(''),
+	trip_id: z.string(),
+	trip_short_name: z.string().default(''),
+	wheelchair_accessible: GtfsTernarySchema.default('0'),
+});
 
 /**
  * Representation of a GTFS trip for the Hub V1 GTFS that is being created.
