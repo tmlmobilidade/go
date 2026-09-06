03681230b65f0864ecaac84bcd3337dbba77150b
João de Vasconcelos <10943992+joao-vasconcelos@users.noreply.github.com>
2026-09-06 21:18:29 +0100

    Remove unused ride retrieval logic and related imports
    * Commented out the entire implementation of the releaseStuckRidesTask function, including the logger and timer initializations, as well as the ride fetching and updating logic.
    * Deleted the findRides utility function to streamline the ridesProvider interface, as it is no longer utilized in the codebase.

--- modules/operation/packages/utils/src/rides/index.ts
+++ modules/operation/packages/utils/src/rides/index.ts
@@ -1,7 +1,6 @@
 /* * */
 
 import { findRideById } from './find-ride-by-id.js';
-import { findRides } from './find-rides.js';
 import { updateRideById } from './update-ride-by-id.js';
 import { updateRides } from './update-rides.js';
 
@@ -9,7 +8,6 @@ import { updateRides } from './update-rides.js';
 
 export const ridesProvider = {
 	findRideById,
-	findRides,
 	updateRideById,
 	updateRides,
 };
