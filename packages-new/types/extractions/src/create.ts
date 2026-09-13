d9f8356539f74cbfebe6783663f26136d91d0ed2
João de Vasconcelos <10943992+joao-vasconcelos@users.noreply.github.com>
2026-09-13 16:10:20 +0100

    Add OperationRides extraction schemas to create and extraction modules
    - Introduced OperationRidesV1ExtractionCreateSchema and OperationRidesV1ExtractionSchema to the ExtractionCreateSchema and ExtractionSchema respectively, expanding the extraction capabilities.
    - Updated the index file for the operation rides module to export the new schemas for better accessibility.

--- packages-new/types/extractions/src/create.ts
+++ packages-new/types/extractions/src/create.ts
@@ -3,11 +3,13 @@
 import { z } from 'zod';
 
 import { InfrastructureStopsV1ExtractionCreateSchema } from './modules/infrastructure/stops/v1/create.js';
+import { OperationRidesV1ExtractionCreateSchema } from './modules/operation/rides/v1/create.js';
 
 /* * */
 
 export const ExtractionCreateSchema = z.discriminatedUnion('version', [
 	InfrastructureStopsV1ExtractionCreateSchema,
+	OperationRidesV1ExtractionCreateSchema,
 ]);
 
 export type ExtractionCreate = z.infer<typeof ExtractionCreateSchema>;
--- packages-new/types/extractions/src/extraction.ts
+++ packages-new/types/extractions/src/extraction.ts
@@ -3,11 +3,13 @@
 import { z } from 'zod';
 
 import { InfrastructureStopsV1ExtractionSchema } from './modules/infrastructure/stops/v1/extraction.js';
+import { OperationRidesV1ExtractionSchema } from './modules/operation/rides/v1/extraction.js';
 
 /* * */
 
 export const ExtractionSchema = z.discriminatedUnion('version', [
 	InfrastructureStopsV1ExtractionSchema,
+	OperationRidesV1ExtractionSchema,
 ]);
 
 export type Extraction = z.infer<typeof ExtractionSchema>;
