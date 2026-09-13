fb179a19429686799cea01cc88b311fe1bc1993f
João de Vasconcelos <10943992+joao-vasconcelos@users.noreply.github.com>
2026-09-13 03:27:50 +0100

    Add new API routes for extraction management
    - Introduced `PUT /lock/:id` and `DELETE /delete/:id` endpoints to handle locking and deleting extractions, enhancing the API's functionality.
    - Integrated corresponding handlers for the new routes to ensure proper request processing and authorization.
    - Updated route definitions in `routes.ts` to reflect the latest changes in extraction management.

--- modules/core/apps/api/src/endpoints/platform/extractions/routes.ts
+++ modules/core/apps/api/src/endpoints/platform/extractions/routes.ts
@@ -3,7 +3,9 @@
 import { authorizationMiddleware, FastifyService } from '@tmlmobilidade/go-clients-fastify';
 
 import { createExtractionHandler } from './handlers/create-extraction.js';
+import { deleteExtractionHandler } from './handlers/delete-extraction.js';
 import { listExtractionsHandler } from './handlers/list-extractions.js';
+import { lockExtractionHandler } from './handlers/lock-extraction.js';
 
 /* * */
 
@@ -20,6 +22,10 @@ server.register(
 		instance.get('/list', { preHandler: authorizationMiddleware() }, listExtractionsHandler);
 
 		instance.post('/create', { preHandler: authorizationMiddleware() }, createExtractionHandler);
+
+		instance.put('/lock/:id', { preHandler: authorizationMiddleware() }, lockExtractionHandler);
+
+		instance.delete('/delete/:id', { preHandler: authorizationMiddleware() }, deleteExtractionHandler);
 
 		next();
 	},
