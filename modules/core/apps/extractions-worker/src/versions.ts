827e1a3048c5c120c9561e1a424ece904d935f44
João de Vasconcelos <10943992+joao-vasconcelos@users.noreply.github.com>
2026-09-13 17:28:31 +0100

    Update extraction-worker to include operation rides extraction
    - Added dependency for '@tmlmobilidade/go-extractions-operation-rides' in package.json.
    - Updated VERSIONS_MAP to include 'operation-rides-v1' extraction function, integrating it into the existing extraction workflow.

--- modules/core/apps/extractions-worker/src/versions.ts
+++ modules/core/apps/extractions-worker/src/versions.ts
@@ -1,10 +1,12 @@
 /* * */
 
 import { extractInfrastructureStopsV1 } from '@tmlmobilidade/go-extractions-infrastructure-stops';
+import { operationRidesV1Extraction } from '@tmlmobilidade/go-extractions-operation-rides';
 import { type Extraction, type ExtractionTaskContext, type ExtractionTaskResult } from '@tmlmobilidade/go-types-extractions';
 
 /* * */
 
 export const VERSIONS_MAP: Record<Extraction['version'], (context: ExtractionTaskContext, extraction: Extraction) => Promise<ExtractionTaskResult>> = {
 	'infrastructure-stops-v1': extractInfrastructureStopsV1,
+	'operation-rides-v1': operationRidesV1Extraction,
 };
