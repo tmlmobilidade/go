c139cb6857270afc8dc33eebee179951d1710d9c
João de Vasconcelos <10943992+joao-vasconcelos@users.noreply.github.com>
2026-09-14 17:33:59 +0100

    Reduce chunk size in apex and tracker synchronization tasks

--- modules/apex/apps/raw-sync-banking-taps/src/task.ts
+++ modules/apex/apps/raw-sync-banking-taps/src/task.ts
@@ -81,7 +81,7 @@ export async function syncApexBankingTaps(timeChunk: PerformInTimeChunksItem) {
 					'_id IN $1',
 					{ 1: chunk },
 				);
-			}, 1_000);
+			}, 250);
 		},
 
 		distinctDestinationDbFn: async () => {
