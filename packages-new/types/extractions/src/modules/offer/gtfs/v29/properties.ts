891ade7f816958d9ced028edc5894a358670de36
jusimen <jusi.monteiro@protonmail.com>
2026-09-14 14:24:37 +0100

    Enhance OfferGtfsV29ExtractionPropertiesSchema with additional fields
    - Added new properties to the OfferGtfsV29ExtractionPropertiesSchema, including agency_ids, calendars_clip_start_date, calendars_clip_end_date, feed_start_date, feed_end_date, lines_exclude, lines_include, lines_mode, numeric_calendar_codes, stop_sequence_start, and stops_export_all.
    - Improved schema definition for better data validation and extraction capabilities.

--- packages-new/types/extractions/src/modules/offer/gtfs/v29/properties.ts
+++ packages-new/types/extractions/src/modules/offer/gtfs/v29/properties.ts
@@ -1,12 +1,34 @@
 /* * */
 
+import { LinesModeSchema } from '@tmlmobilidade/go-types-offer';
+import { OperationalDateSchema } from '@tmlmobilidade/go-types-shared';
 import { z } from 'zod';
 
 /* * */
 
 export const OfferGtfsV29ExtractionPropertiesSchema = z.object({
 
-	// Adicionar filtros aqui
+	agency_ids: z.array(z.string()),
+
+	calendars_clip_end_date: OperationalDateSchema,
+
+	calendars_clip_start_date: OperationalDateSchema,
+
+	feed_end_date: OperationalDateSchema,
+
+	feed_start_date: OperationalDateSchema,
+
+	lines_exclude: z.array(z.string()).default([]),
+
+	lines_include: z.array(z.string()).default([]),
+
+	lines_mode: LinesModeSchema.default('all'),
+
+	numeric_calendar_codes: z.boolean().default(false),
+
+	stop_sequence_start: z.number().default(1),
+
+	stops_export_all: z.boolean().default(true),
 
 });
 
