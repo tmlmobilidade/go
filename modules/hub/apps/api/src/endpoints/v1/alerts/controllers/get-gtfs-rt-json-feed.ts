85144d3cb6fa4de24a23776771dd438756a9f689
João de Vasconcelos <10943992+joao-vasconcelos@users.noreply.github.com>
2026-09-15 12:43:26 +0100

    Refactor GTFS-RT and JSON feed controllers to utilize ApiResponse structure
    - Updated the getGtfsRtJsonFeed and getJsonFeed controllers to replace manual response handling with sendErrorApiResponse and sendSuccessApiResponse for improved consistency and error management.
    - Enhanced response structure by standardizing status codes and response formats, ensuring better integration with the overall API response handling.

--- modules/hub/apps/api/src/endpoints/v1/alerts/controllers/get-gtfs-rt-json-feed.ts
+++ modules/hub/apps/api/src/endpoints/v1/alerts/controllers/get-gtfs-rt-json-feed.ts
@@ -1,10 +1,8 @@
 /* * */
 
-import { HTTP_STATUS } from '@tmlmobilidade/consts';
-import { type FastifyReply, type FastifyRequest } from '@tmlmobilidade/go-clients-fastify';
+import { type FastifyReply, type FastifyRequest, sendErrorApiResponse, sendSuccessApiResponse } from '@tmlmobilidade/go-clients-fastify';
 import { cacheDb } from '@tmlmobilidade/go-interfaces-cachedb';
 import { type GtfsRtFeedMessage } from '@tmlmobilidade/go-types-gtfs-rt';
-import { getEmptyGtfsRtFeedMessage } from '@tmlmobilidade/gtfs-rt';
 import { Logger } from '@tmlmobilidade/logger';
 
 /**
@@ -19,24 +17,13 @@ export async function getGtfsRtJsonFeed(request: FastifyRequest, reply: FastifyR
 
 	if (!cachedData) {
 		Logger.error({ message: '[hub/v1/alerts:getGtfsRtJsonFeed()] No GTFS-RT feed found in cache. Returning empty message.' });
-		return reply
-			.code(HTTP_STATUS.NO_CONTENT)
-			.header('access-control-allow-origin', '*')
-			.header('cache-control', 'public, max-age=20')
-			.send({
-				data: getEmptyGtfsRtFeedMessage(),
-				error: null,
-				status_code: HTTP_STATUS.NO_CONTENT,
-			});
+		return sendErrorApiResponse(reply, {
+			error: 'No GTFS-RT feed found in cache.',
+			status_code: '404',
+		});
 	};
 
-	return reply
-		.code(HTTP_STATUS.OK)
-		.header('access-control-allow-origin', '*')
-		.header('cache-control', 'public, max-age=20')
-		.send({
-			data: JSON.parse(cachedData),
-			error: null,
-			status_code: HTTP_STATUS.OK,
-		});
+	return sendSuccessApiResponse(reply, JSON.parse(cachedData), {
+		max_age: '30s',
+	});
 }
