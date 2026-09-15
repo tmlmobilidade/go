7bd16bcc6cc2350a9e3d57943f9b0fd5f6bc2fad
jusimen <jusi.monteiro@protonmail.com>
2026-09-15 14:16:14 +0100

    feat(api): Add new endpoints for fetching individual network entities
    - Introduced new routes for fetching individual stops, lines, and routes by their IDs, enhancing the API's capability to retrieve specific network data.
    - Updated the routes file to include handlers for getLine, getRoute, and getStop, improving the granularity of data access for clients.

---
+++ modules/hub/apps/api/src/endpoints/v1/network/handlers/get-stop.ts
@@ -0,0 +1,29 @@
+/* * */
+
+import { type FastifyReply, type FastifyRequest, sendErrorApiResponse, sendSuccessApiResponse } from '@tmlmobilidade/go-clients-fastify';
+import { cacheDb } from '@tmlmobilidade/go-interfaces-cachedb';
+import { type HubV1ApiStop } from '@tmlmobilidade/go-types-hub';
+import { Logger } from '@tmlmobilidade/logger';
+
+/**
+ * Retrieves a stop by its ID from cache.
+ * @param request The request object.
+ * @param reply The reply object.
+ */
+export async function getStopHandler(request: FastifyRequest<{ Params: { id: string } }>, reply: FastifyReply<HubV1ApiStop>) {
+	//
+
+	const cachedData = await cacheDb.getNew<HubV1ApiStop>(`hub:v1:network:stops:${request.params.id}`);
+
+	if (!cachedData) {
+		Logger.error({ message: `[hub/v1/network:getStop(${request.params.id})] No cached data found for stop ${request.params.id}` });
+		return sendErrorApiResponse(reply, {
+			error: `[hub/v1/network:getStop(${request.params.id})] No cached data found for stop ${request.params.id}`,
+			status_code: '404',
+		});
+	};
+
+	return sendSuccessApiResponse(reply, cachedData.data, {
+		max_age: '1h',
+	});
+}
