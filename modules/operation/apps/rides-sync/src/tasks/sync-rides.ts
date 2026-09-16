5290d1328cbdad4b2eea0ebeec5a6c51a6100bdb
João de Vasconcelos <10943992+joao-vasconcelos@users.noreply.github.com>
2026-09-16 03:11:29 +0100

    Remove unnecessary wait time in syncRides task to streamline execution
    - Eliminated the 60-second wait time after the deletion operation in the syncRides function, enhancing the overall performance and efficiency of the ride synchronization process.

--- modules/operation/apps/rides-sync/src/tasks/sync-rides.ts
+++ modules/operation/apps/rides-sync/src/tasks/sync-rides.ts
@@ -85,7 +85,6 @@ export async function syncRides(timeChunk: PerformInTimeChunksItem) {
 				});
 				Logger.info({ message: `Deleted ${chunk.length} documents from the destination database in ${deleteTimer.get()}.` });
 			}, 500);
-			await new Promise(resolve => setTimeout(resolve, 60_000));
 		},
 
 		distinctDestinationDbFn: async () => {
